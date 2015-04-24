505acde4e216e6467858e698179fced5cf7ddedd
Bert Ramakers <bertramakers@me.com>
2015-04-24 14:32:06 +0200

    III-7: Controller for all requests concerning saved searches.

---
+++ src/SavedSearchesControllerProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex;
+
+use CultuurNet\UDB3\SavedSearch\Command\SubscribeToSavedSearch;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SavedSearchesControllerProvider implements ControllerProviderInterface
+{
+    /**
+     * Returns routes to connect to the given application.
+     *
+     * @param Application $app An Application instance
+     *
+     * @return ControllerCollection A ControllerCollection instance
+     */
+    public function connect(Application $app)
+    {
+        // Creates a new controller based on the default route
+        $controllers = $app['controllers_factory'];
+
+        $controllers->post(
+            '/subscribe',
+            function (Request $request, Application $app) {
+                /* @var \CultureFeed_User $user */
+                $user = $app['current_user'];
+
+                $name = $request->request->get('name');
+                $query = $request->request->get('query');
+
+                $command = new SubscribeToSavedSearch($user->id, $name, $query);
+
+                /** @var \Broadway\CommandHandling\CommandBusInterface $commandBus */
+                $commandBus = $app['event_command_bus'];
+                $commandId = $commandBus->dispatch($command);
+
+                return JsonResponse::create(
+                    ['commandId' => $commandId]
+                );
+            }
+        );
+    }
+}
