e0182d293730f7e46afc74152d44cbe8ce0785b2
Kristof Coomans <kristof@2dotstwice.be>
2015-11-19 22:03:44 +0100

    III-508: Add controller to add a new organizer

--- src/OrganizerControllerProvider.php
+++ src/OrganizerControllerProvider.php
@@ -6,12 +6,17 @@
 namespace CultuurNet\UDB3\Silex;
 
 use CultuurNet\Hydra\PagedCollection;
+use CultuurNet\UDB3\Address;
 use CultuurNet\UDB3\EntityServiceInterface;
+use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use CultuurNet\UDB3\Organizer\OrganizerEditingServiceInterface;
 use CultuurNet\UDB3\Organizer\ReadModel\Lookup\OrganizerLookupServiceInterface;
 use CultuurNet\UDB3\Symfony\JsonLdResponse;
+use CultuurNet\UDB3\Title;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class OrganizerControllerProvider implements ControllerProviderInterface
@@ -54,6 +59,78 @@ class OrganizerControllerProvider implements ControllerProviderInterface
                 return (new JsonLdResponse($pagedCollection));
             }
         );
+
+        $controllers->post(
+            '/api/1.0/organizer',
+            function (Request $request, Application $app) {
+                $response = new JsonResponse();
+                $body_content = json_decode($request->getContent());
+
+                try {
+
+                    if (empty($body_content->name)) {
+                        throw new \InvalidArgumentException('Required fields are missing');
+                    }
+
+                    $addresses = array();
+                    if (!empty($body_content->address->streetAddress) &&
+                        !empty($body_content->address->locality) &&
+                        !empty($body_content->address->postalCode) &&
+                        !empty($body_content->address->country)) {
+                        $addresses[] = new Address(
+                            $body_content->address->streetAddress,
+                            $body_content->address->postalCode,
+                            $body_content->address->locality,
+                            $body_content->address->country
+                        );
+                    }
+
+                    $phones = array();
+                    $emails = array();
+                    $urls = array();
+                    if (!empty($body_content->contact)) {
+                        foreach ($body_content->contact as $contactInfo) {
+                            if ($contactInfo->type == 'phone') {
+                                $phones[] = $contactInfo->value;
+                            }
+                            elseif ($contactInfo->type == 'email') {
+                                $emails[] = $contactInfo->value;
+                            }
+                            elseif ($contactInfo->type == 'url') {
+                                $urls[] = $contactInfo->value;
+                            }
+                        }
+                    }
+
+                    /** @var OrganizerEditingServiceInterface $organizerEditor */
+                    $organizerEditor = $app['organizer_editing_service'];
+
+                    $organizer_id = $organizerEditor->createOrganizer(
+                        new Title($body_content->name),
+                        $addresses,
+                        $phones,
+                        $emails,
+                        $urls
+                    );
+
+                    /** @var IriGeneratorInterface $organizerIriGenerator */
+                    $organizerIriGenerator = $app['organizer_iri_generator'];
+
+                    $response->setData(
+                        [
+                            'organizerId' => $organizer_id,
+                            'url' => $organizerIriGenerator->iri($organizer_id),
+                        ]
+                    );
+                } catch (\Exception $e) {
+                    $response->setStatusCode(400);
+                    $response->setData(['error' => $e->getMessage()]);
+                }
+
+                return $response;
+            }
+        );
+
 
         return $controllers;
     }
