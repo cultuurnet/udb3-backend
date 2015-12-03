6c48550c5de3e9345b645edc4090903f2ca31b7a
Kristof Coomans <kristof@2dotstwice.be>
2015-12-03 16:41:17 +0100

    Add option to amqp-listen CLI command to set the delay of messages

--- src/Console/AMQPListenCommand.php
+++ src/Console/AMQPListenCommand.php
@@ -8,6 +8,7 @@ namespace CultuurNet\UDB3\Silex\Console;
 use Knp\Command\Command;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -17,7 +18,13 @@ class AMQPListenCommand extends Command
     {
         $this
             ->setName('amqp-listen')
-            ->setDescription('Listens for incoming messages from a message broker with AMQP');
+            ->setDescription('Listens for incoming messages from a message broker with AMQP')
+            ->addOption(
+                'delay',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Seconds to delay the handling of each incoming message'
+            );
     }
 
     private function handleSignal(OutputInterface $output, $signal)
@@ -40,6 +47,12 @@ class AMQPListenCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->registerSignalHandlers($output);
+
+        $delay = $input->getOption('delay');
+        if (null !== $delay) {
+            $app = $this->getSilexApplication();
+            $app['amqp-execution-delay'] = $input->getOption('delay');
+        }
 
         $output->writeln('Connecting...');
         $connection = $this->getAMQPConnection();
