57609d4656827135043b88cebb396955e781ac62
Bram Cordie <bramcordie@gmail.com>
2015-12-04 16:20:43 +0100

    III-588: Report fatal job errors back to the client

--- worker_bootstrap.php
+++ worker_bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use CultuurNet\UDB3\Log\ContextEnrichingLogger;
+
 require_once 'vendor/autoload.php';
 
 Resque_Event::listen(
@@ -15,10 +17,25 @@ Resque_Event::listen(
         $context = unserialize(base64_decode($args['context']));
         $app['impersonator']->impersonate($context);
 
+        $app['logger.fatal_job_error'] = new ContextEnrichingLogger(
+            $app['logger.command_bus'],
+            array('job_id' => $job->payload['id'])
+        );
+
+        $errorLoggingShutdownHandler = function () use ($app) {
+            $error = error_get_last();
+
+            if ($error["type"] == E_ERROR) {
+                $app['logger.fatal_job_error']->error('job_failed');
+            }
+        };
+
         // Allows to access the command bus in perform() of jobs that
         // come out of the queue.
         \CultuurNet\UDB3\CommandHandling\QueueJob::setCommandBus(
             $app['event_command_bus_out']
         );
+
+        register_shutdown_function($errorLoggingShutdownHandler);
     }
 );
