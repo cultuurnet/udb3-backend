ec2ad1e5d22220b61cce9e1fb57116c64ef85b9c
Bert Ramakers <bertramakers@me.com>
2016-02-03 14:43:10 +0100

    III-702: Use SavedSearchesRestController.

--- src/SavedSearches/SavedSearchesControllerProvider.php
+++ src/SavedSearches/SavedSearchesControllerProvider.php
@@ -6,6 +6,8 @@ use CultuurNet\UDB3\SavedSearches\Command\SavedSearchCommand;
 use CultuurNet\UDB3\SavedSearches\Command\SubscribeToSavedSearchJSONDeserializer;
 use CultuurNet\UDB3\SavedSearches\Command\UnsubscribeFromSavedSearch;
 use CultuurNet\UDB3\SavedSearches\ReadModel\SavedSearchRepositoryInterface;
+use CultuurNet\UDB3\Symfony\CommandDeserializerController;
+use CultuurNet\UDB3\Symfony\SavedSearches\SavedSearchesRestController;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
@@ -24,82 +26,24 @@ class SavedSearchesControllerProvider implements ControllerProviderInterface
      */
     public function connect(Application $app)
     {
-        // Creates a new controller based on the default route.
+        $app['saved_searches_controller'] = $app->share(
+            function (Application $app) {
+                return new SavedSearchesRestController(
+                    $app['saved_searches_repository'],
+                    $app['current_user'],
+                    $app['event_command_bus']
+                );
+            }
+        );
+
         /* @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
-        $controllerProvider = $this;
 
-        $controllers->post(
-            '/',
-            function (Request $request, Application $app) use ($controllerProvider) {
-                $userId = $controllerProvider->getUserIDFromApp($app);
-                $deserializer = new SubscribeToSavedSearchJSONDeserializer($userId);
-                $data = new String($request->getContent());
+        $controllers->get('/', 'saved_searches_controller:ownedByCurrentUser');
+        $controllers->post('/', 'saved_searches_controller:save');
 
-                $command = $deserializer->deserialize($data);
-                $commandId = $controllerProvider->dispatchEventCommand($command, $app);
-
-                return $controllerProvider->getResponseForCommandId($commandId);
-            }
-        );
-
-        $controllers->delete(
-            '/{id}',
-            function (Application $app, $id) use ($controllerProvider) {
-                $userId = $controllerProvider->getUserIDFromApp($app);
-                $searchId = new String($id);
-
-                $command = new UnsubscribeFromSavedSearch($userId, $searchId);
-                $commandId = $controllerProvider->dispatchEventCommand($command, $app);
-
-                return $controllerProvider->getResponseForCommandId($commandId);
-            }
-        );
-
-        $controllers->get(
-            '/',
-            function (Application $app) {
-                /** @var SavedSearchRepositoryInterface $savedSearches */
-                $savedSearches = $app['saved_searches_repository'];
-
-                return JsonResponse::create($savedSearches->ownedByCurrentUser());
-            }
-        );
+        $controllers->delete('/{id}', 'saved_searches_controller:delete');
 
         return $controllers;
-    }
-
-    /**
-     * @param Application $app
-     * @return String
-     */
-    private function getUserIDFromApp(Application $app)
-    {
-        /* @var \CultureFeed_User $user */
-        $user = $app['current_user'];
-        return new String($user->id);
-    }
-
-    /**
-     * @param SavedSearchCommand $command
-     * @param Application $app
-     * @return String
-     */
-    private function dispatchEventCommand(SavedSearchCommand $command, Application $app)
-    {
-        /** @var \Broadway\CommandHandling\CommandBusInterface $commandBus */
-        $commandBus = $app['event_command_bus'];
-        $commandId = $commandBus->dispatch($command);
-        return new String($commandId);
-    }
-
-    /**
-     * @param String $commandId
-     * @return JsonResponse
-     */
-    private function getResponseForCommandId(String $commandId) {
-        return JsonResponse::create(
-            ['commandId' => (string) $commandId]
-        );
     }
 }
