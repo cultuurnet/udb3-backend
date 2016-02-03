e952fb996a34225282913a0cdb352bb5f0f88a60
Kristof Coomans <kristof@2dotstwice.be>
2016-02-03 20:43:27 +0100

    III-702: Remove obsolete imports

--- src/SavedSearches/SavedSearchesControllerProvider.php
+++ src/SavedSearches/SavedSearchesControllerProvider.php
@@ -2,27 +2,15 @@
 
 namespace CultuurNet\UDB3\Silex\SavedSearches;
 
-use CultuurNet\UDB3\SavedSearches\Command\SavedSearchCommand;
-use CultuurNet\UDB3\SavedSearches\Command\SubscribeToSavedSearchJSONDeserializer;
-use CultuurNet\UDB3\SavedSearches\Command\UnsubscribeFromSavedSearch;
-use CultuurNet\UDB3\SavedSearches\ReadModel\SavedSearchRepositoryInterface;
-use CultuurNet\UDB3\Symfony\CommandDeserializerController;
 use CultuurNet\UDB3\Symfony\SavedSearches\SavedSearchesRestController;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use ValueObjects\String\String;
 
 class SavedSearchesControllerProvider implements ControllerProviderInterface
 {
     /**
-     * Returns routes to connect to the given application.
-     *
-     * @param Application $app An Application instance
-     *
-     * @return ControllerCollection A ControllerCollection instance
+     * @inheritdoc
      */
     public function connect(Application $app)
     {
