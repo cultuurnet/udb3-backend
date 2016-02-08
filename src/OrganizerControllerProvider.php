7d98dd3deeae67afa7a4e840d0e86b15441647ae
Nick <nick@2dotstwice.be>
2016-02-08 15:11:58 +0100

    III-700: refactor OrganizerController:findByPartOfTitle & OrganizerController:createOrganizer

--- src/OrganizerControllerProvider.php
+++ src/OrganizerControllerProvider.php
@@ -27,7 +27,10 @@ class OrganizerControllerProvider implements ControllerProviderInterface
         $app['organizer_controller'] = $app->share(
             function (Application $app) {
                 return new OrganizerController(
-                    $app['organizer_service']
+                    $app['organizer_service'],
+                    $app['organizer_lookup'],
+                    $app['organizer_editing_service'],
+                    $app['organizer_iri_generator']
                 );
             }
         );
@@ -41,109 +44,13 @@ class OrganizerControllerProvider implements ControllerProviderInterface
 
         $controllers->get(
             '/api/1.0/organizer/suggest/{term}',
-            function (Request $request, $term, Application $app) {
-                /** @var OrganizerLookupServiceInterface $organizerLookupService */
-                $organizerLookupService = $app['organizer_lookup'];
-
-                // @todo Add & process pagination parameters
-
-                $ids = $organizerLookupService->findOrganizersByPartOfTitle($term);
-
-                $members = [];
-                if (!empty($ids)) {
-                    /** @var EntityServiceInterface $organizerService */
-                    $organizerService = $app['organizer_service'];
-
-                    $members = array_map(
-                        function ($id) use ($organizerService) {
-                            return json_decode($organizerService->getEntity($id));
-                        },
-                        $ids
-                    );
-                }
-
-                $pagedCollection = new PagedCollection(
-                    1,
-                    1000,
-                    $members,
-                    count($members)
-                );
-
-                return (new JsonLdResponse($pagedCollection));
-            }
+            'organizer_controller:findByPartOfTitle'
         );
 
         $controllers->post(
             '/api/1.0/organizer',
-            function (Request $request, Application $app) {
-                $response = new JsonResponse();
-                $body_content = json_decode($request->getContent());
-
-                try {
-
-                    if (empty($body_content->name)) {
-                        throw new \InvalidArgumentException('Required fields are missing');
-                    }
-
-                    $addresses = array();
-                    if (!empty($body_content->address->streetAddress) &&
-                        !empty($body_content->address->locality) &&
-                        !empty($body_content->address->postalCode) &&
-                        !empty($body_content->address->country)) {
-                        $addresses[] = new Address(
-                            $body_content->address->streetAddress,
-                            $body_content->address->postalCode,
-                            $body_content->address->locality,
-                            $body_content->address->country
-                        );
-                    }
-
-                    $phones = array();
-                    $emails = array();
-                    $urls = array();
-                    if (!empty($body_content->contact)) {
-                        foreach ($body_content->contact as $contactInfo) {
-                            if ($contactInfo->type == 'phone') {
-                                $phones[] = $contactInfo->value;
-                            }
-                            elseif ($contactInfo->type == 'email') {
-                                $emails[] = $contactInfo->value;
-                            }
-                            elseif ($contactInfo->type == 'url') {
-                                $urls[] = $contactInfo->value;
-                            }
-                        }
-                    }
-
-                    /** @var OrganizerEditingServiceInterface $organizerEditor */
-                    $organizerEditor = $app['organizer_editing_service'];
-
-                    $organizer_id = $organizerEditor->createOrganizer(
-                        new Title($body_content->name),
-                        $addresses,
-                        $phones,
-                        $emails,
-                        $urls
-                    );
-
-                    /** @var IriGeneratorInterface $organizerIriGenerator */
-                    $organizerIriGenerator = $app['organizer_iri_generator'];
-
-                    $response->setData(
-                        [
-                            'organizerId' => $organizer_id,
-                            'url' => $organizerIriGenerator->iri($organizer_id),
-                        ]
-                    );
-                } catch (\Exception $e) {
-                    $response->setStatusCode(400);
-                    $response->setData(['error' => $e->getMessage()]);
-                }
-
-                return $response;
-            }
+            'organizer_controller:createOrganizer'
         );
-
 
         return $controllers;
     }
