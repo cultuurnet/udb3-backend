753ca08021a1c8fc1c1588041b0ffeace7a8bc57
Bram Cordie <bramcordie@gmail.com>
2016-02-08 22:50:31 +0100

    III-658: Update dependencies and configure permission provider

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -5,7 +5,7 @@
 
 namespace CultuurNet\UDB3\Silex;
 
-use CultuurNet\UDB3\Event\ReadModel\Permission\Doctrine\SchemaConfigurator;
+use CultuurNet\UDB3\Offer\ReadModel\Permission\Doctrine\DBALRepository;
 use CultuurNet\UDB3\UiTID\CdbXmlCreatedByToUserIdResolver;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
@@ -19,12 +19,14 @@ class PermissionServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $app['event_permission.table_name'] = new StringLiteral('event_permission_readmodel');
+        $app['event_permission.id_field'] = new StringLiteral('event_id');
 
         $app['event_permission.repository'] = $app->share(
             function (Application $app) {
-                return new \CultuurNet\UDB3\Event\ReadModel\Permission\Doctrine\DBALRepository(
+                return new DBALRepository(
                     $app['event_permission.table_name'],
-                    $app['dbal_connection']
+                    $app['dbal_connection'],
+                    $app['event_permission.id_field']
                 );
             }
         );
