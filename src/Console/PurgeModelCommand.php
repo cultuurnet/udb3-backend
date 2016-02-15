d1c5e16dc9fb008d92f627d457c5d0c853a4ea68
Luc Wollants <luc.wollants@apptite.be>
2016-02-15 18:36:23 +0100

    III-633 The purge command now uses a service provider to manage the purge services.

--- src/Console/PurgeModelCommand.php
+++ src/Console/PurgeModelCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CleanModelCommand extends Command
+class PurgeModelCommand extends Command
 {
     const MODEL_OPTION = 'model';
 
