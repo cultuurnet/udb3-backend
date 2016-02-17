101c979e5285d020265a20ad16aacbd66a2d8d8b
Luc Wollants <luc.wollants@apptite.be>
2016-02-17 14:20:35 +0100

    III-742: removed autoload from bootstrap.

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 use CultuurNet\SilexServiceProviderOAuth\OAuthServiceProvider;
 use CultuurNet\SymfonySecurityOAuth\Model\Provider\TokenProviderInterface;
 use CultuurNet\SymfonySecurityOAuthRedis\NonceProvider;
