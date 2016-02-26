8d06a28ad2bc989ea66a5174a18b9a52cf24e099
Bert Ramakers <bertramakers@me.com>
2016-02-26 11:28:53 +0100

    III-680: Moved all search services from bootstrap.php to SearchServiceProvider.

--- bootstrap.php
+++ bootstrap.php
@@ -9,12 +9,7 @@ use CultuurNet\SymfonySecurityOAuthRedis\TokenProviderCache;
 use Guzzle\Log\ClosureLogAdapter;
 use Guzzle\Plugin\Log\LogPlugin;
 use Silex\Application;
-use CultuurNet\UDB3\SearchAPI2\DefaultSearchService as SearchAPI2;
-use CultuurNet\UDB3\SearchAPI2\Filters\UDB3Place as UDB3PlaceFilter;
-use CultuurNet\UDB3\SearchAPI2\FilteredSearchService;
 use DerAlex\Silex\YamlConfigServiceProvider;
-use CultuurNet\UDB3\Search\PullParsingSearchService;
-use CultuurNet\UDB3\Search\CachedDefaultSearchService;
 use CultuurNet\UDB3\Iri\CallableIriGenerator;
 use JDesrosiers\Silex\Provider\CorsServiceProvider;
 use ValueObjects\String\String;
@@ -135,82 +130,6 @@ $app['uitid_consumer_credentials'] = $app->share(
             $consumerConfig['key'],
             $consumerConfig['secret']
         );
-    }
-);
-
-$app['search_api_2'] = $app->share(
-    function ($app) {
-        $searchApiUrl =
-            $app['config']['uitid']['base_url'] .
-            $app['config']['uitid']['apis']['search'];
-
-        return new SearchAPI2(
-            $searchApiUrl,
-            $app['uitid_consumer_credentials']
-        );
-    }
-);
-
-$app['filtered_search_api_2'] = $app->share(
-    function ($app) {
-        $filteredSearchService = new FilteredSearchService(
-            $app['search_api_2']
-        );
-        $filteredSearchService->filter(new \CultuurNet\UDB3\SearchAPI2\Filters\NotUDB3Place());
-        return $filteredSearchService;
-    }
-);
-
-$app['search_service'] = $app->share(
-    function ($app) {
-        /** @var \Qandidate\Toggle\ToggleManager $toggles */
-        $toggles = $app['toggles'];
-
-        $includePlaces = $toggles->active(
-            'search-include-places',
-            $app['toggles.context']
-        );
-
-        $searchAPI = $includePlaces ? 'search_api_2' : 'filtered_search_api_2';
-
-        return new PullParsingSearchService(
-            $app[$searchAPI],
-            $app['iri_generator'],
-            $app['place_iri_generator']
-        );
-    }
-);
-
-$app['cached_search_service'] = $app->share(
-    function ($app) {
-        return new CachedDefaultSearchService(
-            $app['search_service'],
-            $app['cache']('default_search')
-        );
-    }
-);
-
-$app['search_cache_manager'] = $app->share(
-    function (Application $app) {
-        $parameters = $app['config']['cache']['redis'];
-
-        return new \CultuurNet\UDB3\Search\Cache\CacheManager(
-            $app['cached_search_service'],
-            new Predis\Client($parameters)
-        );
-    }
-);
-
-$app['search_cache_manager'] = $app->extend(
-    'search_cache_manager',
-    function(\CultuurNet\UDB3\Search\Cache\CacheManager $manager, Application $app) {
-        $logger = new \Monolog\Logger('search_cache_manager');
-        $logger->pushHandler(
-            new \Monolog\Handler\StreamHandler(__DIR__ . '/log/search_cache_manager.log')
-        );
-        $manager->setLogger($logger);
-
-        return $manager;
     }
 );
 
--- src/Search/SearchServiceProvider.php
+++ src/Search/SearchServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace CultuurNet\UDB3\Silex\Search;
 
+use CultuurNet\UDB3\Search\CachedDefaultSearchService;
+use CultuurNet\UDB3\Search\PullParsingSearchService;
 use CultuurNet\UDB3\Search\ResultsGenerator;
+use CultuurNet\UDB3\SearchAPI2\DefaultSearchService as SearchAPI2;
+use CultuurNet\UDB3\SearchAPI2\Filters\UDB3Place as UDB3PlaceFilter;
+use CultuurNet\UDB3\SearchAPI2\FilteredSearchService;
+use Predis\Client;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -13,6 +19,82 @@ class SearchServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
+        $app['search_api_2'] = $app->share(
+            function ($app) {
+                $searchApiUrl =
+                    $app['config']['uitid']['base_url'] .
+                    $app['config']['uitid']['apis']['search'];
+
+                return new SearchAPI2(
+                    $searchApiUrl,
+                    $app['uitid_consumer_credentials']
+                );
+            }
+        );
+
+        $app['filtered_search_api_2'] = $app->share(
+            function ($app) {
+                $filteredSearchService = new FilteredSearchService(
+                    $app['search_api_2']
+                );
+                $filteredSearchService->filter(new \CultuurNet\UDB3\SearchAPI2\Filters\NotUDB3Place());
+                return $filteredSearchService;
+            }
+        );
+
+        $app['search_service'] = $app->share(
+            function ($app) {
+                /** @var \Qandidate\Toggle\ToggleManager $toggles */
+                $toggles = $app['toggles'];
+
+                $includePlaces = $toggles->active(
+                    'search-include-places',
+                    $app['toggles.context']
+                );
+
+                $searchAPI = $includePlaces ? 'search_api_2' : 'filtered_search_api_2';
+
+                return new PullParsingSearchService(
+                    $app[$searchAPI],
+                    $app['iri_generator'],
+                    $app['place_iri_generator']
+                );
+            }
+        );
+
+        $app['cached_search_service'] = $app->share(
+            function ($app) {
+                return new CachedDefaultSearchService(
+                    $app['search_service'],
+                    $app['cache']('default_search')
+                );
+            }
+        );
+
+        $app['search_cache_manager'] = $app->share(
+            function (Application $app) {
+                $parameters = $app['config']['cache']['redis'];
+
+                return new \CultuurNet\UDB3\Search\Cache\CacheManager(
+                    $app['cached_search_service'],
+                    new Client($parameters)
+                );
+            }
+        );
+
+        $app['search_cache_manager'] = $app->extend(
+            'search_cache_manager',
+            function(\CultuurNet\UDB3\Search\Cache\CacheManager $manager, Application $app) {
+                $logger = new \Monolog\Logger('search_cache_manager');
+                $logger->pushHandler(
+                    new \Monolog\Handler\StreamHandler(__DIR__ . '/log/search_cache_manager.log')
+                );
+                $manager->setLogger($logger);
+
+                return $manager;
+            }
+        );
+
         $app['search_results_generator'] = $app->share(
             function (Application $app) {
                 return new ResultsGenerator(
