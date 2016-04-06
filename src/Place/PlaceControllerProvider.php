a3d0dcba77ed7b051c36c2a9e380bfe36f493ebb
Bram Cordie <bramcordie@gmail.com>
2016-04-06 09:59:10 +0200

    III-595: Codestylez

--- src/Place/PlaceControllerProvider.php
+++ src/Place/PlaceControllerProvider.php
@@ -46,7 +46,7 @@ class PlaceControllerProvider implements ControllerProviderInterface
         $controllers
             ->get('place/{cdbid}', 'place_controller:get')
             ->bind('place');
-        $controllers->delete('place/{cdbid}','place_editing_controller:deletePlace');
+        $controllers->delete('place/{cdbid}', 'place_editing_controller:deletePlace');
         $controllers->get('place/{cdbid}/events', 'place_editing_controller:getEvents');
 
         $controllers->get('places', 'place_controller:getByPostalCode');
