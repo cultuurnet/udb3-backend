5d2e9034a96eca27bb84ce4e8f47c6bc3c04f3a9
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-22 17:01:21 +0200

    III-940: Update migration script, as we need an event store
    Plans changed!

--- src/Migrations/Version20160621151445.php
+++ src/Migrations/Version20160621151445.php
@@ -21,9 +21,14 @@ class Version20160621151445 extends AbstractMigration
 
         $table->addColumn('id', 'integer', array('autoincrement' => true));
         $table->addColumn('uuid', 'guid', array('length' => 36));
-        $table->addColumn('name', 'string', array('length' => 255));
+        $table->addColumn('playhead', 'integer', array('unsigned' => true));
+        $table->addColumn('payload', 'text');
+        $table->addColumn('metadata', 'text');
+        $table->addColumn('recorded_on', 'string', array('length' => 32));
+        $table->addColumn('type', 'text');
 
         $table->setPrimaryKey(array('id'));
+        $table->addUniqueIndex(array('uuid', 'playhead'));
     }
 
     /**
@@ -32,6 +37,5 @@ class Version20160621151445 extends AbstractMigration
     public function down(Schema $schema)
     {
         $schema->dropTable(self::ROLES);
-
     }
 }
