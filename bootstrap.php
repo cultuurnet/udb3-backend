bdd772f718beae6548f0e805853a344519664a78
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-07-15 09:56:13 +0200

    III-9£40: Initialize necessary objects for roles

--- bootstrap.php
+++ bootstrap.php
@@ -1239,6 +1239,94 @@ $app['organizer_service'] = $app->share(
     }
 );
 
+/** Roles */
+
+$app['role_iri_generator'] = $app->share(
+    function ($app) {
+        return new CallableIriGenerator(
+            function ($roleId) use ($app) {
+                return $app['config']['url'] . '/roles/' . $roleId;
+            }
+        );
+    }
+);
+
+$app['role_store'] = $app->share(
+    function ($app) {
+        return new \Broadway\EventStore\DBALEventStore(
+            $app['dbal_connection'],
+            $app['eventstore_payload_serializer'],
+            new \Broadway\Serializer\SimpleInterfaceSerializer(),
+            'roles'
+        );
+    }
+);
+
+$app['real_organizer_repository'] = $app->share(
+    function ($app) {
+        $repository = new \CultuurNet\UDB3\Role\RoleRepository(
+            $app['role_store'],
+            $app['event_bus'],
+
+        );
+
+        return $repository;
+    }
+);
+
+$app['organizer_jsonld_repository'] = $app->share(
+    function ($app) {
+        return new \CultuurNet\UDB3\Doctrine\Event\ReadModel\CacheDocumentRepository(
+            $app['organizer_jsonld_cache']
+        );
+    }
+);
+
+$app['role_detail_cache'] = $app->share(
+    function ($app) {
+        return $app['cache']('role_detail');
+    }
+);
+
+$app['role_read_repository'] = $app->share(
+    function ($app) {
+        return new \CultuurNet\UDB3\Doctrine\Event\ReadModel\CacheDocumentRepository(
+            $app['role_detail_cache']
+        );
+    }
+);
+
+$app['role_detail_projector'] = $app->share(
+    function ($app) {
+        return new \CultuurNet\UDB3\Role\ReadModel\Detail\Projector(
+            $app['role_read_repository']
+        );
+    }
+);
+
+$app['role_permissions_cache'] = $app->share(
+    function ($app) {
+        return $app['cache']('role_permissions');
+    }
+);
+
+$app['role_permissions_read_repository'] = $app->share(
+    function ($app) {
+        return new \CultuurNet\UDB3\Doctrine\Event\ReadModel\CacheDocumentRepository(
+            $app['role_permissions_cache']
+        );
+    }
+);
+
+$app['role_detail_projector'] = $app->share(
+    function ($app) {
+        return new \CultuurNet\UDB3\Role\ReadModel\Permissions\Projector(
+            $app['role_permissions_read_repository']
+        );
+    }
+);
+
+
 $app['event_export_notification_mail_factory'] = $app->share(
     function ($app) {
         return new \CultuurNet\UDB3\EventExport\Notification\Swift\DefaultMessageFactory(
