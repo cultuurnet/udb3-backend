580ad43696c26a4f4eab6f15c09d818231b07a7c
Bert Ramakers <bertramakers@me.com>
2016-07-27 11:06:46 +0200

    III-1192: Implemenation of SearchUserController.

--- src/User/UserControllerProvider.php
+++ src/User/UserControllerProvider.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\Silex\User;
 
+use CultuurNet\UDB3\Symfony\User\SearchUserController;
 use CultuurNet\UDB3\Symfony\User\UserLabelMemoryRestController;
 use Silex\Application;
 use Silex\ControllerCollection;
@@ -20,10 +21,20 @@ class UserControllerProvider implements ControllerProviderInterface
             }
         );
 
+        $app['user_search_controller'] = $app->share(
+            function (Application $app) {
+                return new SearchUserController(
+                    $app['culturefeed']
+                );
+            }
+        );
+
         /** @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
 
         $controllers->get('api/1.0/user/labels', 'user_label_memory_controller:all');
+
+        $controllers->get('users/', 'user_search_controller:search');
 
         return $controllers;
     }
