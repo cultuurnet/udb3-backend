b53517118531fedd52d197895693b05caf89e9e1
Luc Wollants <luc.wollants@apptite.be>
2016-07-28 10:39:28 +0200

    III-1107 Added migration script to rename relation table columns from relationType to offerType and relationId to offerId.

---
+++ src/Migrations/Version20160728102259.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160728102259 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->changeColumnName('relationType', 'offerType');
+        $this->changeColumnName('relationId', 'offerId');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->changeColumnName('offerType', 'relationType');
+        $this->changeColumnName('offerId', 'relationId');
+    }
+
+    /**
+     * @param string $oldName
+     * @param string $newName
+     */
+    private function changeColumnName($oldName, $newName)
+    {
+        $this->connection->exec(
+            "ALTER TABLE labels_relations CHANGE $oldName $newName VARCHAR(255)"
+        );
+    }
+}
