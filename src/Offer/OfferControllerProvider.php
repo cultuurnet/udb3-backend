6102953b160159a0bdc2c5ac0bb8529ccab131ec
Bram Cordie <bramcordie@gmail.com>
2016-09-12 12:38:08 +0200

    III-1230: Configure Offer patch controller

--- src/Offer/OfferControllerProvider.php
+++ src/Offer/OfferControllerProvider.php
@@ -4,7 +4,9 @@ namespace CultuurNet\UDB3\Silex\Offer;
 
 use CultuurNet\UDB3\DescriptionJSONDeserializer;
 use CultuurNet\UDB3\LabelJSONDeserializer;
+use CultuurNet\UDB3\Offer\OfferType;
 use CultuurNet\UDB3\Symfony\Offer\EditOfferRestController;
+use CultuurNet\UDB3\Symfony\Offer\PatchOfferRestController;
 use CultuurNet\UDB3\TitleJSONDeserializer;
 use Silex\Application;
 use Silex\ControllerCollection;
@@ -24,6 +26,7 @@ class OfferControllerProvider implements ControllerProviderInterface
 
         foreach ($offerServices as $offerType => $serviceName) {
             $controllerName = "{$offerType}_offer_controller";
+            $patchControllerName = "patch_{$offerType}_controller";
 
             $app[$controllerName] = $app->share(
                 function (Application $app) use ($serviceName) {
@@ -34,6 +37,11 @@ class OfferControllerProvider implements ControllerProviderInterface
                         new DescriptionJSONDeserializer()
                     );
                 }
+            );
+
+            $app[$patchControllerName] = new PatchOfferRestController(
+                OfferType::fromCaseInsensitiveValue($offerType),
+                $app['event_command_bus']
             );
 
             $controllers
@@ -58,6 +66,12 @@ class OfferControllerProvider implements ControllerProviderInterface
                 ->post(
                     "{$offerType}/{cdbid}/{lang}/description",
                     "{$controllerName}:translateDescription"
+                );
+
+            $controllers
+                ->patch(
+                    "{$offerType}/{offerId}",
+                    "{$patchControllerName}:handle"
                 );
         }
 
