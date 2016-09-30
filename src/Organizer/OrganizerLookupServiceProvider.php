47ce57f4ff2fc8e3890a72b8bab4650542ea3a92
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-09-30 21:29:25 +0200

    III-1344: Add the website search to the organizer lookup service provider

--- src/Organizer/OrganizerLookupServiceProvider.php
+++ src/Organizer/OrganizerLookupServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace CultuurNet\UDB3\Silex\Organizer;
 
+use CultuurNet\UDB3\Organizer\ReadModel\Search\Doctrine\SchemaConfigurator;
+use CultuurNet\UDB3\Silex\DatabaseSchemaInstaller;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use ValueObjects\String\String as StringLiteral;
 
 class OrganizerLookupServiceProvider implements ServiceProviderInterface
 {
@@ -14,6 +17,45 @@ class OrganizerLookupServiceProvider implements ServiceProviderInterface
                 // At the moment, the index.repository service maintains
                 // an index of data for various purposes.
                 return $app['index.repository'];
+            }
+        );
+
+        $app['organizer_website.table_name'] = new StringLiteral('organizer_website');
+
+        $app['organizer_website.repository'] = $app->share(
+            function (Application $app) {
+                return new \CultuurNet\UDB3\ORganizer\ReadModel\Search\Doctrine\DBALRepository(
+                    $app['dbal_connection'],
+                    $app['organizer_website.table_name']
+                );
+            }
+        );
+
+        $app['organizer_website.projector'] = $app->share(
+            function (Application $app) {
+                $projector = new \CultuurNet\UDB3\Organizer\ReadModel\Search\Projector(
+                    $app['organizer_website.repository']
+                );
+
+                return $projector;
+            }
+        );
+
+        $app['organizer_website.schema_configurator'] = $app->share(
+            function (Application $app) {
+                return new SchemaConfigurator($app['organizer_website.table_name']);
+            }
+        );
+
+        // Add our schema configurator to the database installer.
+        $app['database.installer'] = $app->extend(
+            'database.installer',
+            function (DatabaseSchemaInstaller $installer, Application $app) {
+                $installer->addSchemaConfigurator(
+                    $app['organizer_website.schema_configurator']
+                );
+
+                return $installer;
             }
         );
     }
