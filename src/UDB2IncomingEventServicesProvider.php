1214461d61dfb0703034e69ef3f95edfdeb8f84e
Bert Ramakers <bertramakers@me.com>
2016-12-07 17:49:48 +0100

    III-1663: Fixed bootstrapping of cdbxml enricher by injecting an xml validation service.

--- src/UDB2IncomingEventServicesProvider.php
+++ src/UDB2IncomingEventServicesProvider.php
@@ -12,9 +12,9 @@ use CultuurNet\UDB3\Cdb\CdbId\EventCdbIdExtractor;
 use CultuurNet\UDB3\Cdb\Event\Not;
 use CultuurNet\UDB3\Cdb\ExternalId\ArrayMappingService;
 use CultuurNet\UDB3\UDB2\Actor\ActorEventApplier;
+use CultuurNet\UDB3\UDB2\Actor\ActorEventCdbXmlEnricher;
 use CultuurNet\UDB3\UDB2\Actor\ActorToUDB3OrganizerFactory;
 use CultuurNet\UDB3\UDB2\Actor\ActorToUDB3PlaceFactory;
-use CultuurNet\UDB3\UDB2\Actor\EventCdbXmlEnricher as ActorEventCdbXmlEnricher;
 use CultuurNet\UDB3\UDB2\Actor\Specification\QualifiesAsOrganizerSpecification;
 use CultuurNet\UDB3\UDB2\Actor\Specification\QualifiesAsPlaceSpecification;
 use CultuurNet\UDB3\UDB2\Event\EventApplier;
@@ -24,6 +24,9 @@ use CultuurNet\UDB3\UDB2\Event\EventToUDB3PlaceFactory;
 use CultuurNet\UDB3\UDB2\Label\LabelImporter;
 use CultuurNet\UDB3\UDB2\LabeledAsUDB3Place;
 use CultuurNet\UDB3\UDB2\OfferToSapiUrlTransformer;
+use CultuurNet\UDB3\UDB2\XSD\CachedInMemoryXSDReader;
+use CultuurNet\UDB3\UDB2\XSD\FileGetContentsXSDReader;
+use CultuurNet\UDB3\UDB2\XSD\XSDAwareXMLValidationService;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
@@ -135,6 +138,16 @@ class UDB2IncomingEventServicesProvider implements ServiceProviderInterface
             }
         );
 
+        $app['cdbxml_enricher_xml_validation_service'] = $app->share(
+            function (Application $app) {
+                $reader = new CachedInMemoryXSDReader(
+                    new FileGetContentsXSDReader($app['udb2_cdbxml_enricher.xsd'])
+                );
+
+                return new XSDAwareXMLValidationService($reader, LIBXML_ERR_ERROR);
+            }
+        );
+
         $app['cdbxml_enricher_logger'] = $app->share(
             function (Application $app) {
                 $logger = new \Monolog\Logger('udb2-events-cdbxml-enricher');
@@ -147,14 +160,15 @@ class UDB2IncomingEventServicesProvider implements ServiceProviderInterface
             function (Application $app) use ($importFromSapi) {
                 $enricher = new EventCdbXmlEnricher(
                     $app['event_bus'],
-                    $app['cdbxml_enricher_http_client_adapter']
+                    $app['cdbxml_enricher_http_client_adapter'],
+                    $app['cdbxml_enricher_xml_validation_service']
                 );
 
                 $enricher->setLogger($app['cdbxml_enricher_logger']);
 
                 if ($importFromSapi) {
-                    $eventUrlFormat = $app['config']['udb2_import']['event_url_format'];
-                    if (!isset($eventUrlFormat)) {
+                    $eventUrlFormat = $app['udb2_cdbxml_enricher.event_url_format'];
+                    if (is_null($eventUrlFormat)) {
                         throw new \Exception('can not import events from sapi without configuring an url format');
                     }
                     $transformer = new OfferToSapiUrlTransformer($eventUrlFormat);
@@ -169,14 +183,15 @@ class UDB2IncomingEventServicesProvider implements ServiceProviderInterface
             function (Application $app) use ($importFromSapi) {
                 $enricher = new ActorEventCdbXmlEnricher(
                     $app['event_bus'],
-                    $app['cdbxml_enricher_http_client_adapter']
+                    $app['cdbxml_enricher_http_client_adapter'],
+                    $app['cdbxml_enricher_xml_validation_service']
                 );
 
                 $enricher->setLogger($app['cdbxml_enricher_logger']);
 
                 if ($importFromSapi) {
-                    $actorUrlFormat = $app['config']['udb2_import']['actor_url_format'];
-                    if (!isset($actorUrlFormat)) {
+                    $actorUrlFormat = $app['udb2_cdbxml_enricher.actor_url_format'];
+                    if (is_null($actorUrlFormat)) {
                         throw new \Exception('can not import actors from sapi without configuring an url format');
                     }
                     $transformer = new OfferToSapiUrlTransformer($actorUrlFormat);
