4d6a4977cc4dcf6c3f8ed33025f5ed1211fc018a
Luc Wollants <luc.wollants@apptite.be>
2016-12-13 11:18:44 +0100

    III-1490 Make the type column smaller to avoid warning when setting the index.

--- src/Migrations/merge_event_stores.sql
+++ src/Migrations/merge_event_stores.sql
@@ -5,9 +5,11 @@ CREATE TABLE event_store LIKE events;
 
 --
 -- Put an index on the type column of the new event_store.
+-- The column that needs an index can not be to long, so limit to 128 characters.
+-- If the column would be too long a MySQL warning 1071 occurs.
 --
-ALTER TABLE event_store MODIFY type VARCHAR(512) NOT NULL;
-ALTER TABLE event_store ADD INDEX type_index (type);
+ALTER TABLE event_store MODIFY type VARCHAR(128) NOT NULL;
+ALTER TABLE event_store ADD INDEX (type);
 
 --
 -- Add an aggregate column
