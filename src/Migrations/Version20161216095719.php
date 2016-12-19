05759dfb85653f0bf82decaf71fee41f21405a8f
Luc Wollants <luc.wollants@apptite.be>
2016-12-19 13:18:36 +0100

    III-1490 Added Doctrine migration script to create the event store with aggregate type column.

---
+++ src/Migrations/Version20161216095719.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161216095719 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('event_store');
+
+        $table->addColumn('id', 'integer', array('autoincrement' => true));
+        $table->addColumn('uuid', 'guid', array('length' => 36,));
+        $table->addColumn('playhead', 'integer', array('unsigned' => true));
+        $table->addColumn('payload', 'text');
+        $table->addColumn('metadata', 'text');
+        $table->addColumn('recorded_on', 'string', array('length' => 32));
+        $table->addColumn('type', 'string', array('length' => 128));
+        $table->addColumn('aggregate_type', 'string', array('length' => 128));
+
+        $table->setPrimaryKey(array('id'));
+
+        $table->addUniqueIndex(array('uuid', 'playhead'));
+
+        $table->addIndex(['type']);
+        $table->addIndex(['aggregate_type']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('event_store');
+    }
+}
