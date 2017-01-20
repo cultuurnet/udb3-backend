6c44307c60e19fed376766f3212aa67a402091d3
Kristof Coomans <kristof@2dotstwice.be>
2017-01-20 16:06:59 +0100

    III-1807: Search for events based on date range

--- src/Console/ConcludeCommand.php
+++ src/Console/ConcludeCommand.php
@@ -3,10 +3,9 @@
 namespace CultuurNet\UDB3\Silex\Console;
 
 use Broadway\CommandHandling\CommandBusInterface;
-use Broadway\Domain\Metadata;
-use CultuurNet\UDB3\CommandHandling\ResqueCommandBus;
-use CultuurNet\UDB3\CommandHandling\SimpleContextAwareCommandBus;
+use Carbon\Carbon;
 use CultuurNet\UDB3\Event\Commands\Conclude;
+use CultuurNet\UDB3\Search\ResultsGenerator;
 use CultuurNet\UDB3\Silex\Impersonator;
 use Knp\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,24 +14,97 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ConcludeCommand extends Command
 {
+    const TIMEZONE = 'Europe/Brussels';
+
     public function configure()
     {
         $this
             ->setName('event:conclude')
-            ->setDescription('Conclude a specific event.')
+            ->setDescription('Conclude events of which the end date falls in a specific date range.')
             ->addArgument(
-                'cdbid',
-                InputArgument::REQUIRED
+                'lower-boundary',
+                InputArgument::OPTIONAL,
+                'The lower boundary of the end date range',
+                Carbon::yesterday(self::TIMEZONE)->toDateTimeString()
+            )
+            ->addArgument(
+                'upper-boundary',
+                InputArgument::OPTIONAL,
+                'The upper boundary of the end date range',
+                Carbon::yesterday(self::TIMEZONE)->setTime(23, 59, 59)->toDateTimeString()
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $lowerBoundaryInput = $input->getArgument('lower-boundary');
+
+        $lowerDateBoundary = null;
+        if ($lowerBoundaryInput !== '*') {
+            $lowerDateBoundary = new Carbon(
+                $lowerBoundaryInput,
+                self::TIMEZONE
+            );
+        }
+
+        $upperDateBoundary = new Carbon(
+            $input->getArgument('upper-boundary'),
+            self::TIMEZONE
+        );
+
+        if ($lowerDateBoundary && $lowerDateBoundary->greaterThanOrEqualTo($upperDateBoundary)) {
+            throw new \InvalidArgumentException(
+                'lower-boundary needs to be before upper-boundary'
+            );
+        }
+
+        $latestAllowedUpperBoundary = Carbon::yesterday(self::TIMEZONE)->setTime(23, 59, 59);
+        if ($upperDateBoundary->greaterThan($latestAllowedUpperBoundary)) {
+            throw new \InvalidArgumentException(
+                'your upper boundary is too high, latest allowed upper boundary is ' . $latestAllowedUpperBoundary->toDateTimeString()
+            );
+        }
+
+        $finder = $this->getFinder();
+
+        $sapiDateRange = $this->buildDateRangeString($lowerDateBoundary, $upperDateBoundary);
+
+        $query = "availableto:{$sapiDateRange}";
+        $output->writeln('Executing search query: ' . $query);
+
+        $results = $finder->search($query);
+
         $commandBus = $this->getCommandBus();
 
-        $commandBus->dispatch(
-            new Conclude($input->getArgument('cdbid'))
-        );
+        foreach ($results as $result) {
+            print_r($result);
+
+            $commandBus->dispatch(
+                new Conclude($input->getArgument('cdbid'))
+            );
+        }
+    }
+
+    private function buildDateRangeString(Carbon $lowerDateBoundary = null, Carbon $upperDateBoundary = null)
+    {
+        $format = 'Y-m-d\TH:i:s\Z';
+
+        $from = $lowerDateBoundary ? $lowerDateBoundary->tz('UTC')->format($format) : '*';
+        $to = $upperDateBoundary->tz('UTC')->format($format);
+
+        return "[{$from} TO {$to}]";
+    }
+
+    /**
+     * @return ResultsGenerator
+     */
+    private function getFinder()
+    {
+        $app = $this->getSilexApplication();
+
+        $finder = new ResultsGenerator($app['search_service']);
+
+        return $finder;
     }
 
     /**
