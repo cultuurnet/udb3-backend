a788b97429c36c879e1e1c19ea05d6db68021d04
Luc Wollants <luc.wollants@apptite.be>
2017-08-01 09:39:46 +0200

    III-2231 Add migration for column types and primary key of the index table.

---
+++ src/Migrations/Version20170731151814.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170731151814 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->getIndexTable($schema)
+            ->setPrimaryKey(['entity_id']);
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'zip',
+                [
+                    'type' => Type::getType('string'),
+                    'length' => 32,
+                ]
+        );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'created',
+                [
+                    'type' => Type::getType('string'),
+                    'length' => 32,
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'updated',
+                [
+                    'type' => Type::getType('string'),
+                    'length' => 32,
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'owning_domain',
+                [
+                    'type' => Type::getType('string'),
+                    'length' => 256,
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'entity_iri',
+                [
+                    'type' => Type::getType('string'),
+                    'length' => 256,
+                ]
+            );
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->getIndexTable($schema)
+            ->dropPrimaryKey();
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'zip',
+                [
+                    'type' => Type::getType('text'),
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'created',
+                [
+                    'type' => Type::getType('text'),
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'updated',
+                [
+                    'type' => Type::getType('text'),
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'owning_domain',
+                [
+                    'type' => Type::getType('text'),
+                ]
+            );
+
+        $this->getIndexTable($schema)
+            ->changeColumn(
+                'entity_iri',
+                [
+                    'type' => Type::getType('text'),
+                ]
+            );
+    }
+
+    /**
+     * @param Schema $schema
+     * @return Table
+     */
+    private function getIndexTable(Schema $schema)
+    {
+        return $schema->getTable('index_readmodel');
+    }
+}
