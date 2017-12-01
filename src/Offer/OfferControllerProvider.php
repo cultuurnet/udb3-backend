b31da5dba01e008dd80fc6891be22703f59479e7
Luc Wollants <luc.wollants@apptite.be>
2017-12-01 14:49:49 +0100

    III-2413 Correct bootstrapping for updateFacilities on offer controller provider.

--- src/Offer/OfferControllerProvider.php
+++ src/Offer/OfferControllerProvider.php
@@ -11,12 +11,14 @@ use CultuurNet\UDB3\Symfony\Deserializer\Calendar\CalendarForPlaceDataValidator;
 use CultuurNet\UDB3\Symfony\Deserializer\Calendar\CalendarJSONDeserializer;
 use CultuurNet\UDB3\Symfony\Deserializer\Calendar\CalendarJSONParser;
 use CultuurNet\UDB3\Symfony\Deserializer\DataValidator\DataValidatorInterface;
+use CultuurNet\UDB3\Symfony\Deserializer\Place\FacilitiesJSONDeserializer;
 use CultuurNet\UDB3\Symfony\Deserializer\PriceInfo\PriceInfoJSONDeserializer;
 use CultuurNet\UDB3\Symfony\Deserializer\TitleJSONDeserializer;
 use CultuurNet\UDB3\Symfony\Offer\EditOfferRestController;
 use CultuurNet\UDB3\Symfony\Offer\OfferPermissionController;
 use CultuurNet\UDB3\Symfony\Offer\OfferPermissionsController;
 use CultuurNet\UDB3\Symfony\Offer\PatchOfferRestController;
+use CultuurNet\UDB3\Symfony\Place\PlaceFacilityResolver;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
@@ -54,7 +56,9 @@ class OfferControllerProvider implements ControllerProviderInterface
                         new CalendarJSONDeserializer(
                             new CalendarJSONParser(),
                             $this->getDataCalendarValidator($offerType)
-                        )
+                        ),
+                        //todo: One for places and one for events.
+                        new FacilitiesJSONDeserializer(new PlaceFacilityResolver())
                     );
                 }
             );
@@ -106,6 +110,7 @@ class OfferControllerProvider implements ControllerProviderInterface
             $controllers->put("{$offerType}/{cdbid}/calendar", "{$controllerName}:updateCalendar");
             $controllers->put("{$offerType}/{cdbid}/type/{typeId}", "{$controllerName}:updateType");
             $controllers->put("{$offerType}/{cdbid}/theme/{themeId}", "{$controllerName}:updateTheme");
+            $controllers->put("{$offerType}/{cdbid}/facilities/", "{$controllerName}:updateFacilities");
 
             $controllers->delete("{$offerType}/{cdbid}/labels/{label}", "{$controllerName}:removeLabel")
                 ->assert('label', '.*');
@@ -137,6 +142,12 @@ class OfferControllerProvider implements ControllerProviderInterface
                 ->post(
                     "{$offerType}/{cdbid}/{lang}/description",
                     "{$controllerName}:updateDescription"
+                );
+
+            $controllers
+                ->post(
+                    "{$offerType}/{cdbid}/facilities",
+                    "{$controllerName}:updateFacilitiesWithLabel"
                 );
 
             $controllers
