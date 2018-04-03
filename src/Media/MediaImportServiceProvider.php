6e63020210eb886b20582b70de25b2847cd341e3
Luc Wollants <luc.wollants@apptite.be>
2018-04-03 19:31:55 +0200

    III-2565 Update dependency to fix MediaManagerImageCollectionFactory namespace issue.

--- src/Media/MediaImportServiceProvider.php
+++ src/Media/MediaImportServiceProvider.php
@@ -2,18 +2,7 @@
 
 namespace CultuurNet\UDB3\Silex\Media;
 
-use Broadway\EventStore\DBALEventStore;
-use Broadway\Serializer\SimpleInterfaceSerializer;
-use Broadway\UuidGenerator\Rfc4122\Version4Generator;
-use CultuurNet\UDB3\Import\MediaObject\MediaManagerImageCollectionFactory;
-use CultuurNet\UDB3\Iri\CallableIriGenerator;
-use CultuurNet\UDB3\Media\ImageUploaderService;
-use CultuurNet\UDB3\Media\MediaManager;
-use CultuurNet\UDB3\Media\MediaObjectRepository;
-use CultuurNet\UDB3\Media\Serialization\MediaObjectSerializer;
-use CultuurNet\UDB3\Media\SimplePathGenerator;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
+use CultuurNet\UDB3\Model\Import\MediaObject\MediaManagerImageCollectionFactory;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
