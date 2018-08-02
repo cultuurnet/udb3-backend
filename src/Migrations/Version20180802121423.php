68a7152f8f198b3822febafbf3a9aeb5863c266d
Stijn Swaanen <swaanenstijn@gmail.com>
2018-08-02 14:47:19 +0200

    PWK-670 add country column in index_readmodel table

---
+++ src/Migrations/Version20180802121423.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180802121423 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->getTable('index_readmodel');
+
+        $table->addColumn('country', Type::STRING)
+            ->setLength(2)
+            ->setNotnull(true);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema
+            ->getTable('index_readmodel')
+            ->dropColumn('country');
+    }
+}
