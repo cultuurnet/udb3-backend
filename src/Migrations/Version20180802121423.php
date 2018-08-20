c28cea48e87a673b2eb4cbfd3fefc5351fe31801
Stijn Swaanen <swaanenstijn@gmail.com>
2018-08-20 10:30:12 +0200

    PWK-670 move default country BE to postUp

--- src/Migrations/Version20180802121423.php
+++ src/Migrations/Version20180802121423.php
@@ -19,9 +19,15 @@ class Version20180802121423 extends AbstractMigration
         $table = $schema->getTable('index_readmodel');
 
         $table->addColumn('country', Type::STRING)
-            ->setLength(2)
-            ->setDefault('BE')
-            ->setNotnull(true);
+            ->setLength(2);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postUp(Schema $schema)
+    {
+        $this->connection->executeQuery("UPDATE index_readmodel SET country = 'BE'");
     }
 
     /**
