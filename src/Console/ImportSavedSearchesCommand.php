5333a6f14a623845bf71fc7337c461ef7e9a7553
Kristof Coomans <kristof@2dotstwice.be>
2018-11-22 07:58:49 +0100

    III-2290: Add option to filter on user (intended for testing purposes)

--- src/Console/ImportSavedSearchesCommand.php
+++ src/Console/ImportSavedSearchesCommand.php
@@ -15,6 +15,7 @@ class ImportSavedSearchesCommand extends AbstractCommand
 {
     private const CSV_FILE_ARG = 'csv_file';
     private const CSV_DELIMETER_OPT = 'csv_delimiter';
+    private const USER_OPT = 'user';
 
     /**
      * @inheritdoc
@@ -35,6 +36,12 @@ class ImportSavedSearchesCommand extends AbstractCommand
                 InputOption::VALUE_OPTIONAL,
                 'Delimeter for the csv file (default is comma).',
                 ','
+            )
+            ->addOption(
+                self::USER_OPT,
+                '',
+                InputOption::VALUE_REQUIRED,
+                'Only import records of the given user'
             );
     }
 
@@ -53,10 +60,19 @@ class ImportSavedSearchesCommand extends AbstractCommand
                 $input->getOption(self::CSV_DELIMETER_OPT)
             );
 
+        $user = $input->getOption(self::USER_OPT);
+        if ($user) {
+            $output->writeln('Only importing saved searches for user with UUID ' . $user);
+        }
+
         $output->writeln('Starting import...');
 
         $records = $csvReader->getRecords();
         foreach ($records as $record) {
+            if ($user && $record['USER_UUID'] !== $user) {
+                continue;
+            }
+
             $output->writeln('Importing query with name: ' . $record['NAME']);
 
             $this->getUDB3SavedSearchesRepository()->write(
