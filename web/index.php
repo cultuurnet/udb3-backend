c93fef466e157f775d51c1afd2cabcd13a5a4063
Kristof Coomans <kristof@2dotstwice.be>
2019-01-09 15:53:31 +0100

    III-2750: Fix exception handling, wrong arguments (those for Silex 2.x) were passed to closure

--- web/index.php
+++ web/index.php
@@ -224,7 +224,10 @@ $app->after(function (Request $request, Response $response, Application $app) {
     $app['response_logger']->logResponse($request, $response);
 });
 
-$app->error(function (\Exception $e, Request $request, $code, Application $app) {
+$app->error(function (\Exception $e, $code) use ($app) {
+    /** @var \Symfony\Component\HttpFoundation\RequestStack $requestStack */
+    $requestStack = $app['request_stack'];
+    $request = $requestStack->getCurrentRequest();
     $app['error_logger']->logError($e, $request, $code);
 });
 
