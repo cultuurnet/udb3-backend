8a89489af10bf2d0b7e424fecb6f1d877bb16ed8
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-14 08:06:42 +0200

    Make sure deserializer tests with new event structure

--- tests/Curators/Events/NewsArticleAboutEventAddedJSONDeserializerTest.php
+++ tests/Curators/Events/NewsArticleAboutEventAddedJSONDeserializerTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Curators\Events;
 
+use CultuurNet\Deserializer\DataValidationException;
+use CultuurNet\Deserializer\MissingValueException;
+use CultuurNet\UDB3\Curators\Publisher;
 use PHPUnit\Framework\TestCase;
 use ValueObjects\StringLiteral\StringLiteral;
 
@@ -26,7 +29,12 @@ final class NewsArticleAboutEventAddedJSONDeserializerTest extends TestCase
      */
     public function it_should_throw_an_exception_if_news_article_id_is_missing()
     {
-        $event = json_encode(['eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51']);
+        $event = json_encode(
+            [
+                'eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+                'publisher' => 'bruzz',
+            ]
+        );
         $this->deserializer->deserialize(new StringLiteral($event));
     }
 
@@ -36,7 +44,45 @@ final class NewsArticleAboutEventAddedJSONDeserializerTest extends TestCase
      */
     public function it_should_throw_an_exception_if_event_id_is_missing()
     {
-        $event = json_encode(['newsArticleId' => 74567]);
+        $event = json_encode(
+            [
+                'newsArticleId' => 74567,
+                'publisher' => 'bruzz',
+            ]
+        );
+        $this->deserializer->deserialize(new StringLiteral($event));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_publisher_is_missing()
+    {
+        $event = json_encode(
+            [
+                'eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+                'newsArticleId' => 74567,
+            ]
+        );
+
+        $this->expectException(MissingValueException::class);
+        $this->deserializer->deserialize(new StringLiteral($event));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_publisher_is_unknown()
+    {
+        $event = json_encode(
+            [
+                'eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+                'newsArticleId' => 74567,
+                'publisher' => 'DEFINITELY_NOT_A_PUBLISHER',
+            ]
+        );
+
+        $this->expectException(DataValidationException::class);
         $this->deserializer->deserialize(new StringLiteral($event));
     }
 
@@ -47,13 +93,15 @@ final class NewsArticleAboutEventAddedJSONDeserializerTest extends TestCase
     {
         $expected = new NewsArticleAboutEventAdded(
             'c4c19563-06e3-43fa-a15c-73a91c54b27e',
-            'C0D870F6-2883-4565-A020-7CF12BDE5F51'
+            'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+            Publisher::bruzz()
         );
 
         $event = json_encode(
             [
-            'newsArticleId' => 'c4c19563-06e3-43fa-a15c-73a91c54b27e',
-            'eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+                'newsArticleId' => 'c4c19563-06e3-43fa-a15c-73a91c54b27e',
+                'eventId' => 'C0D870F6-2883-4565-A020-7CF12BDE5F51',
+                'publisher' => 'bruzz',
             ]
         );
         $actual = $this->deserializer->deserialize(new StringLiteral($event));
