ff097706376464c40f0a41614795f6c30f9cfbfa
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-14 08:30:19 +0200

    Expect to not add label when publisher has no label configured

--- tests/Curators/NewsArticleProcessManagerTest.php
+++ tests/Curators/NewsArticleProcessManagerTest.php
@@ -10,6 +10,7 @@ use CultuurNet\UDB3\Label;
 use CultuurNet\UDB3\Offer\OfferEditingServiceInterface;
 use CultuurNet\UDB3\Curators\Events\NewsArticleAboutEventAddedJSONDeserializer;
 use CultuurNet\UDB3\SimpleEventBus;
+use InvalidArgumentException;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
@@ -89,7 +90,7 @@ final class NewsArticleProcessManagerTest extends TestCase
     /**
      * @test
      */
-    public function it_should_add_a_curatoren_label_to_an_event_if_a_news_article_is_created_about_it()
+    public function it_should_add_a_matching_label_to_an_event_if_a_news_article_is_created_about_it()
     {
         $message = new AMQPMessage(
             json_encode(
@@ -118,6 +119,35 @@ final class NewsArticleProcessManagerTest extends TestCase
                 'F8E5055F-66C4-4929-ABB9-822B9F5328F1',
                 $expectedLabel
             );
+
+        $this->eventBusForwardingConsumer->consume($message);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_not_add_a_label_if_no_matching_label_was_configured()
+    {
+        $message = new AMQPMessage(
+            json_encode(
+                [
+                    'newsArticleId' => 'c4c19563-06e3-43fa-a15c-73a91c54b27e',
+                    'eventId' => 'F8E5055F-66C4-4929-ABB9-822B9F5328F1',
+                    'publisher' => 'bruzz',
+                ]
+            ),
+            [
+                'content_type' => 'application/vnd.cultuurnet.curators-api.events.news-article-about-event-added+json',
+            ]
+        );
+        $message->delivery_info = $this->messageDeliveryInfo;
+
+        $this->labelFactory->expects($this->once())
+            ->method('forPublisher')
+            ->with(Publisher::bruzz())
+            ->willThrowException(new InvalidArgumentException());
+
+        $this->offerEditingService->expects($this->never())->method('addLabel');
 
         $this->eventBusForwardingConsumer->consume($message);
     }
