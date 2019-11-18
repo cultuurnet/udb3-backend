29a7fba78972fca329680e32fd4e4853fcef0ebe
zvonimir <zspajich@gmail.com>
2019-11-18 11:40:35 +0100

    Refactor HistoryPlaceController - improve readability

--- src/Http/Place/HistoryPlaceRestController.php
+++ src/Http/Place/HistoryPlaceRestController.php
@@ -5,17 +5,19 @@ namespace CultuurNet\UDB3\Http\Place;
 use CultuurNet\UDB3\Event\ReadModel\DocumentGoneException;
 use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
 use CultuurNet\UDB3\Http\ApiProblemJsonResponseTrait;
+use CultuurNet\UDB3\Http\HttpFoundation\ApiProblemJsonResponse;
 use CultuurNet\UDB3\Http\Management\User\UserIdentificationInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class HistoryPlaceRestController
 {
+    use ApiProblemJsonResponseTrait;
+
     private const HISTORY_ERROR_NOT_FOUND = 'An error occurred while getting the history of the event with id %s!';
     private const HISTORY_ERROR_GONE = 'An error occurred while getting the history of the event with id %s which was removed!';
     private const HISTORY_ERROR_FORBIDDEN = 'Forbidden to access event history.';
 
-    use ApiProblemJsonResponseTrait;
     /**
      * @var DocumentRepositoryInterface
      */
@@ -37,31 +39,42 @@ class HistoryPlaceRestController
 
     public function get(string $eventId): JsonResponse
     {
-        $response = null;
-
         if (!$this->userIdentification->isGodUser()) {
-            return $this->createApiProblemJsonResponse(
-                self::HISTORY_ERROR_FORBIDDEN,
-                $eventId,
-                Response::HTTP_FORBIDDEN
-            );
+            return $this->forbiddenResponse($eventId);
         }
 
         try {
             $document = $this->historyRepository->get($eventId);
 
-            if ($document) {
-                $response = JsonResponse::create()
-                    ->setContent($document->getRawBody());
-
-                $response->headers->set('Vary', 'Origin');
-            } else {
-                $response = $this->createApiProblemJsonResponseNotFound(self::HISTORY_ERROR_NOT_FOUND, $eventId);
+            if ($document === null) {
+                return $this->notFoundResponse($eventId);
             }
-        } catch (DocumentGoneException $documentGoneException) {
-            $response = $this->createApiProblemJsonResponseGone(self::HISTORY_ERROR_GONE, $eventId);
-        }
 
-        return $response;
+            $response = JsonResponse::create()
+                ->setContent($document->getRawBody());
+            $response->headers->set('Vary', 'Origin');
+            return $response;
+        } catch (DocumentGoneException $documentGoneException) {
+            return $this->documentGoneResponse($eventId);
+        }
+    }
+
+    private function forbiddenResponse(string $eventId): ApiProblemJsonResponse
+    {
+        return $this->createApiProblemJsonResponse(
+            self::HISTORY_ERROR_FORBIDDEN,
+            $eventId,
+            Response::HTTP_FORBIDDEN
+        );
+    }
+
+    private function notFoundResponse(string $eventId): ApiProblemJsonResponse
+    {
+        return $this->createApiProblemJsonResponseNotFound(self::HISTORY_ERROR_NOT_FOUND, $eventId);
+    }
+
+    private function documentGoneResponse(string $eventId): ApiProblemJsonResponse
+    {
+        return $this->createApiProblemJsonResponseGone(self::HISTORY_ERROR_GONE, $eventId);
     }
 }
