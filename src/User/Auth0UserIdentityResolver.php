1c594e65a68454a7b7beb4a3e7a9eafcd572b413
Bert Ramakers <bert@madewithlove.be>
2020-01-21 18:04:22 +0100

    III-3203 Add @todo's

--- src/User/Auth0UserIdentityResolver.php
+++ src/User/Auth0UserIdentityResolver.php
@@ -22,6 +22,11 @@ final class Auth0UserIdentityResolver implements UserIdentityResolverInterface
 
     public function getUserById(StringLiteral $userId): ?UserIdentityDetails
     {
+        // @TODO use https://auth0.com/docs/users/search/v3/get-users-endpoint
+        // @see https://madewithlove.slack.com/archives/CSLQN091S/p1579522101065500
+        // example query: `user_id:"<id>" OR app_metadata.uitidv1id:"<id>"`
+
+        // NOTE! The code below is just for testing the access token for now, it's NOT the correct API call!
         $user = $this->auth0->users()
             ->get($userId->toNative());
 
@@ -33,11 +38,20 @@ final class Auth0UserIdentityResolver implements UserIdentityResolverInterface
 
     public function getUserByEmail(EmailAddress $email): ?UserIdentityDetails
     {
+        // @TODO use https://auth0.com/docs/users/search/v3/get-users-by-email-endpoint
+        // This could return multiple users. Check with Erwin that we link accounts with the same email address so they
+        // are "unique"!
+        // If so, just return the first one.
+        // If not, we have a bigger problem because we assume e-mail addresses to be unique in multiple places.
         return null;
     }
 
     public function getUserByNick(StringLiteral $nick): ?UserIdentityDetails
     {
+        // @TODO use https://auth0.com/docs/users/search/v3/get-users-endpoint
+        // NOTE! Since we use email as the fallback for username if it's not found in the token claims, we should also
+        // support email here. So do something similar to `email:"<username>" OR <username field>:<username>`
+        // Not sure what the username field is currently, check with Erwin.
         return null;
     }
 }
