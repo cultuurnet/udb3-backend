a59b1ec8d806b0587168d34f73da54c5b3c75383
zvonimir <zspajich@gmail.com>
2020-01-22 14:31:47 +0100

    Add migration to increase user_id field size in tables

---
+++ app/Migrations/Version20200122131812.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20200122131812 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function up(Schema $schema)
+    {           
+
+        $this->addSql('ALTER TABLE event_permission_readmodel MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE organizer_permission_readmodel MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE place_permission_readmodel MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE saved_searches_sapi2 MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE saved_searches_sapi3 MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user_roles MODIFY COLUMN user_id VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+
+    }
+}
