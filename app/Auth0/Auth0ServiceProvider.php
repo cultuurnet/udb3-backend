0e60c037d0c0ef26addfe59d2d10e7ac24f4c7d1
zvonimir <zspajich@gmail.com>
2020-01-24 11:25:05 +0100

    Wire up Auth0ManagementToken Generator

--- app/Auth0/Auth0ServiceProvider.php
+++ app/Auth0/Auth0ServiceProvider.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Silex\Auth0;
 
 use Auth0\SDK\API\Management;
+use CultuurNet\UDB3\User\Auth0ManagementTokenGenerator;
+use CultuurNet\UDB3\User\Auth0ManagementTokenProvider;
 use CultuurNet\UDB3\User\Auth0UserIdentityResolver;
+use CultuurNet\UDB3\User\InMemoryRepository;
+use GuzzleHttp\Client;
+use Lcobucci\JWT\Parser;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -13,22 +18,29 @@ final class Auth0ServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app): void
     {
-        $app[Management::class] = $app::share(
+        $app['auth0.management-token'] = $app::share(
             function (Application $app) {
-                // @TODO make domain configurable
-                // @TODO the regular JWT is not valid for the management API
-                // @see https://auth0.com/docs/api/management/v2/tokens
-                return new Management(
-                    (string) $app['jwt']->jwtToken(),
-                    $app['config']['jwt']['auth0']['domain']
+                $provider = new Auth0ManagementTokenProvider(
+                    new Auth0ManagementTokenGenerator(
+                        new Client(),
+                        $app['config']['jwt']['auth0']['client_id'],
+                        $app['config']['jwt']['auth0']['domain'],
+                        $app['config']['jwt']['auth0']['client_secret']
+                    ),
+                    new InMemoryRepository(),
+                    new Parser()
                 );
+                return $provider->token();
             }
         );
 
         $app[Auth0UserIdentityResolver::class] = $app::share(
             function (Application $app) {
                 return new Auth0UserIdentityResolver(
-                    $app[Management::class]
+                    new Management(
+                        $app['auth0.management-token'],
+                        $app['config']['jwt']['auth0']['domain']
+                    )
                 );
             }
         );
