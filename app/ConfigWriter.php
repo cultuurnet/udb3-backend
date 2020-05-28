aca0177c42ec7187cd176c6e4e780daa17091d69
Jonas Drieghe <jonasd@madewithlove.be>
2020-05-28 10:20:09 +0200

    Add ConfigWriter

---
+++ app/ConfigWriter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex;
+
+use Silex\Application;
+
+class ConfigWriter
+{
+    /**
+     * @var Application
+     */
+    private $application;
+
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    public function merge(array $configSection): void
+    {
+        $config = $this->application['config'];
+        $this->application['config'] = array_merge($config, $configSection);
+    }
+}
