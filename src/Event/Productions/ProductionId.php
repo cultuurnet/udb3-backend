fa2bd8c4e9c1be0a1a4e6515f30ac7a164126851
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-02 09:52:03 +0200

    Add ProductionId VO

---
+++ src/Event/Productions/ProductionId.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CultuurNet\UDB3\Event\Productions;
+
+use Rhumsaa\Uuid\Uuid;
+
+final class ProductionId
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    private function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function generate(): self
+    {
+        return new self(Uuid::uuid4());
+    }
+
+    public static function fromNative(string $id): self
+    {
+        return new self($id);
+    }
+
+    public function toNative(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(ProductionId $other): bool
+    {
+        return $this->id === $other->id;
+    }
+}
