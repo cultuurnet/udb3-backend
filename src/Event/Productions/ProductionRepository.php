77bbc99303b75b6082e10274d3639a06a7ea9eff
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-04 12:09:37 +0200

    Add repository for Productions

---
+++ src/Event/Productions/ProductionRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CultuurNet\UDB3\Event\Productions;
+
+use Cake\Chronos\Chronos;
+use CultuurNet\UDB3\EntityNotFoundException;
+use CultuurNet\UDB3\Label\ReadModels\Doctrine\AbstractDBALRepository;
+use Doctrine\DBAL\Connection;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class ProductionRepository extends AbstractDBALRepository
+{
+    public function __construct(Connection $connection)
+    {
+        parent::__construct($connection, new StringLiteral('productions'));
+    }
+
+    public function add(Production $production)
+    {
+        foreach ($production->getEventIds() as $eventId) {
+            $this->getConnection()->insert(
+                $this->getTableName()->toNative(),
+                [
+                    'event_id' => $eventId,
+                    'production_id' => $production->getProductionId()->toNative(),
+                    'name' => $production->getName(),
+                    'added_at' => Chronos::now()->format(DATE_ATOM),
+                ]
+            );
+        }
+    }
+
+    public function find(ProductionId $productionId)
+    {
+        $results = $this->getConnection()->fetchAll(
+            'SELECT * FROM productions WHERE production_id = :productionId',
+            [
+                'productionId' => $productionId->toNative(),
+            ]
+        );
+
+        if (!$results) {
+            throw new EntityNotFoundException('No production found for id ' . $productionId->toNative());
+        }
+
+        $production = new Production(
+            $productionId,
+            $results[0]['name'],
+            []
+        );
+
+        foreach ($results as $result) {
+            $production = $production->addEvent($result['event_id']);
+        }
+
+        return $production;
+    }
+}
---
+++ tests/Event/Productions/Doctrine/SchemaConfigurator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CultuurNet\UDB3\Event\Productions\Doctrine;
+
+use CultuurNet\UDB3\Doctrine\DBAL\SchemaConfiguratorInterface;
+use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\Type;
+
+class SchemaConfigurator implements SchemaConfiguratorInterface
+{
+    private const TABLE = 'productions';
+
+    public function configure(AbstractSchemaManager $schemaManager)
+    {
+        $schema = $schemaManager->createSchema();
+
+        if (!$schema->hasTable(self::TABLE)) {
+            $table = $this->createTable($schema);
+            $schemaManager->createTable($table);
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     * @return Table
+     */
+    private function createTable(Schema $schema)
+    {
+        $table = $schema->createTable(self::TABLE);
+
+        $table->addColumn('event_id', Type::GUID)->setLength(36)->setNotnull(true);
+        $table->addColumn('production_id', Type::GUID)->setLength(36)->setNotnull(true);
+        $table->addColumn('name', Type::STRING)->setLength(255)->setNotnull(true);
+        $table->addColumn('added_at', Type::DATE_IMMUTABLE)->setNotnull(true);
+
+        $table->setPrimaryKey(['event_id']);
+        $table->addIndex(['production_id']);
+
+        return $table;
+    }
+}
