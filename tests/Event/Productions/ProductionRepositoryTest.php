70a95d0c74a738661afe57801978ee930c60c78b
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-04 18:59:58 +0200

    Add test to prove we can add events to existing productions

--- tests/Event/Productions/ProductionRepositoryTest.php
+++ tests/Event/Productions/ProductionRepositoryTest.php
@@ -4,6 +4,7 @@ namespace CultuurNet\UDB3\Event\Productions;
 
 use CultuurNet\UDB3\DBALTestConnectionTrait;
 use CultuurNet\UDB3\Event\Productions\Doctrine\SchemaConfigurator;
+use Doctrine\DBAL\DBALException;
 use PHPUnit\Framework\TestCase;
 use Rhumsaa\Uuid\Uuid;
 use ValueObjects\StringLiteral\StringLiteral;
@@ -34,7 +35,33 @@ class ProductionRepositoryTest extends TestCase
         $this->assertEquals($production, $result);
     }
 
-    private function givenThereIsAProduction(): Production
+    /**
+     * @test
+     */
+    public function it_can_add_an_event_to_an_existing_production(): void
+    {
+        $production = $this->givenThereIsAProduction();
+        $eventToAdd = Uuid::uuid4()->toString();
+        $this->repository->addEvent($eventToAdd, $production);
+
+        $persistedProduction = $this->repository->find($production->getProductionId());
+        $this->assertTrue($persistedProduction->containsEvent($eventToAdd));
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_add_an_event_to_a_production_when_it_belongs_to_another_production(): void
+    {
+        $production = $this->givenThereIsAProduction('foo');
+        $otherProduction = $this->givenThereIsAProduction('bar');
+        $eventToAdd = $otherProduction->getEventIds()[0];
+
+        $this->expectException(DBALException::class);
+        $this->repository->addEvent($eventToAdd, $production);
+    }
+
+    private function givenThereIsAProduction(string $name = 'foo'): Production
     {
         $production = Production::createEmpty($name);
         $production = $production->addEvent(Uuid::uuid4()->toString());
