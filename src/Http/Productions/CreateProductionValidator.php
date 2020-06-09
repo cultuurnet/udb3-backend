bb8c0116ed9c9ba4367e87f36ab78a61a31e76f8
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-09 08:50:43 +0200

    Implement validator

--- src/Http/Productions/CreateProductionValidator.php
+++ src/Http/Productions/CreateProductionValidator.php
@@ -7,8 +7,42 @@ use CultuurNet\UDB3\Http\Deserializer\DataValidator\DataValidatorInterface;
 
 class CreateProductionValidator implements DataValidatorInterface
 {
-    public function validate(array $data)
+    public function validate(array $data): void
     {
-        throw new DataValidationException();
+        $messages = [];
+        $messages = array_merge($messages, $this->validateName($data));
+        $messages = array_merge($messages, $this->validateEvents($data));
+
+        if (!empty($messages)) {
+            $exception = new DataValidationException();
+            $exception->setValidationMessages($messages);
+            throw $exception;
+        }
+    }
+
+    private function validateName(array $data): array
+    {
+        if (!isset($data['name'])) {
+            return ["Property 'name' is required"];
+        }
+
+        if (empty(trim($data['name']))) {
+            return ["Property 'name' cannot be empty"];
+        }
+
+        return [];
+    }
+
+    private function validateEvents(array $data): array
+    {
+        if (!isset($data['eventIds'])) {
+            return ["Property 'eventIds' is required"];
+        }
+
+        if (count($data['eventIds']) < 2) {
+            return ["At least two event ids should be provided"];
+        }
+
+        return [];
     }
 }
