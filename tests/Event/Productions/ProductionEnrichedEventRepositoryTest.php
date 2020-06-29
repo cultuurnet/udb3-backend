498fd257b89e2c4a1bc13c22e368c9eed1fafdea
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-29 14:15:03 +0200

    Add test to ensure productions are properly returned

--- tests/Event/Productions/ProductionEnrichedEventRepositoryTest.php
+++ tests/Event/Productions/ProductionEnrichedEventRepositoryTest.php
@@ -59,4 +59,38 @@ class ProductionEnrichedEventRepositoryTest extends TestCase
         $this->assertEquals($eventId, $actual->getBody()->{'@id'});
         $this->assertNull($actual->getBody()->production);
     }
+
+    /**
+     * @test
+     */
+    public function it_returns_production_data_when_event_belongs_to_a_production(): void
+    {
+        $eventId = Uuid::uuid4()->toString();
+        $originalJsonDocument = new JsonDocument(
+            $eventId,
+            json_encode((object) ['@id' => $eventId])
+        );
+
+        $otherEventId = Uuid::uuid4()->toString();
+        $productionId = ProductionId::generate();
+        $productionName = 'The Teenage Mutant Ninja String Quartet in Concert - Heroes in a half Cello';
+        $production = new Production(
+            $productionId,
+            $productionName,
+            [
+                $eventId,
+                $otherEventId,
+            ]
+        );
+
+        $this->eventRepository->method('get')->willReturn($originalJsonDocument);
+        $this->productionRepository->method('findProductionForEventId')->willReturn($production);
+
+        $actual = $this->productionEnrichedEventRepository->get($eventId);
+
+        $this->assertEquals($eventId, $actual->getBody()->{'@id'});
+        $this->assertEquals($productionId->toNative(), $actual->getBody()->production->id);
+        $this->assertEquals($productionName, $actual->getBody()->production->title);
+        $this->assertEquals([$otherEventId], $actual->getBody()->production->otherEvents);
+    }
 }
