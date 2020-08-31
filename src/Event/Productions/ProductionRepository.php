13081f80278e5e4cdf099ebaf0d6b0e1cfa9937d
Bert Ramakers <bertramakers@me.com>
2020-08-31 17:24:31 +0200

    Concat the wildcard in PHP, not MySQL

--- src/Event/Productions/ProductionRepository.php
+++ src/Event/Productions/ProductionRepository.php
@@ -133,7 +133,8 @@ class ProductionRepository extends AbstractDBALRepository
             ->from($this->getTableName()->toNative());
 
         if (!empty($keyword)) {
-            $query = $query->where('MATCH(name) AGAINST(CONCAT(:keyword, \'*\') IN BOOLEAN MODE)')
+            $keyword .= '*';
+            $query = $query->where('MATCH(name) AGAINST(:keyword IN BOOLEAN MODE)')
                 ->setParameter(':keyword', $keyword);
         }
 
