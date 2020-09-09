3681e87807a1051f17a201d28873d0e77e666c5e
Bert Ramakers <bertramakers@me.com>
2020-09-09 14:39:54 +0200

    Remove docblocks and unused import

--- app/Migrations/Version20200909121744.php
+++ app/Migrations/Version20200909121744.php
@@ -4,16 +4,9 @@ namespace CultuurNet\UDB3\Silex\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20200909121744 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
     public function up(Schema $schema): void
     {
         $table = $schema->createTable('similar_events');
@@ -42,10 +35,6 @@ class Version20200909121744 extends AbstractMigration
         );
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
 
