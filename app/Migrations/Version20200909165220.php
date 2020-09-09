bec23cc04ff5da816c2ce54f5ed74cb4ae024060
Bert Ramakers <bertramakers@me.com>
2020-09-09 18:53:43 +0200

    Make it possible to have 2 numbers after the decimal point in the similarity column

---
+++ app/Migrations/Version20200909165220.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20200909165220 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $schema->getTable('similar_events')
+            ->changeColumn('similarity', ['scale' => 2]);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->getTable('similar_events')
+            ->changeColumn('similarity', ['scale' => 0]);
+    }
+}
