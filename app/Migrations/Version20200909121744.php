f0334449b93a59d4779960a8bf1b21b3ec7849ea
Bert Ramakers <bertramakers@me.com>
2020-09-09 14:40:15 +0200

    Implement down()

--- app/Migrations/Version20200909121744.php
+++ app/Migrations/Version20200909121744.php
@@ -37,7 +37,6 @@ class Version20200909121744 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $schema->dropTable('similar_events');
     }
 }
