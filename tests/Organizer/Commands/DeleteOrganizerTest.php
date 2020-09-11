ba36e1ca8e2ea519cad0396e83404ccd3a7ae6ae
Jonas Drieghe <jonasd@madewithlove.be>
2020-09-11 13:18:40 +0200

    Pass correct number of constructor arguments

--- tests/Organizer/Commands/DeleteOrganizerTest.php
+++ tests/Organizer/Commands/DeleteOrganizerTest.php
@@ -12,7 +12,7 @@ class DeleteOrganizerTest extends TestCase
      */
     public function it_requires_the_permission_to_manage_organizers()
     {
-        $command = new DeleteOrganizer('C95FB255-B1F3-4F3F-A48A-E9B845310732', 'D2723C33-677C-4358-BA3F-FF364BD12088');
+        $command = new DeleteOrganizer('C95FB255-B1F3-4F3F-A48A-E9B845310732');
         $expectedPermission = Permission::ORGANISATIES_BEHEREN();
 
         $this->assertEquals($expectedPermission, $command->getPermission());
