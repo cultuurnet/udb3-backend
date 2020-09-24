e98d609f1d080b7d1c931f3fe900e6a942be3d2e
Jonas Drieghe <jonasd@madewithlove.be>
2020-09-24 07:34:10 +0200

    Import type hints

--- src/Security/CultureFeedUserIdentification.php
+++ src/Security/CultureFeedUserIdentification.php
@@ -2,27 +2,27 @@
 
 namespace CultuurNet\UDB3\Security;
 
+use CultureFeed_User;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class CultureFeedUserIdentification implements UserIdentificationInterface
 {
     /**
-     * @var \CultureFeed_User
+     * @var CultureFeed_User
      */
     private $cultureFeedUser;
 
     /**
-     * @var \string[]
+     * @var string[]
      */
     private $permissionList;
 
     /**
-     * CultureFeedUserIdentification constructor.
-     * @param \CultureFeed_User $cultureFeedUser
-     * @param \string[] $permissionList
+     * @param CultureFeed_User $cultureFeedUser
+     * @param string[] $permissionList
      */
     public function __construct(
-        \CultureFeed_User $cultureFeedUser,
+        CultureFeed_User $cultureFeedUser,
         array $permissionList
     ) {
         $this->cultureFeedUser = $cultureFeedUser;
