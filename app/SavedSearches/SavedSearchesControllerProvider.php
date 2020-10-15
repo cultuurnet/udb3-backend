0190b23ca554bd55750ba45b77cb8478682c3b17
Jonas Drieghe <jonasd@madewithlove.be>
2020-10-15 11:55:49 +0200

    Always use Sapi v3 when dealing with saved searches
    We can safely return a 404 on /v2

--- app/SavedSearches/SavedSearchesControllerProvider.php
+++ app/SavedSearches/SavedSearchesControllerProvider.php
@@ -35,10 +35,10 @@ class SavedSearchesControllerProvider implements ControllerProviderInterface
         /* @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('/{sapiVersion}', 'saved_searches_read_controller:ownedByCurrentUser');
+        $controllers->get('/v3', 'saved_searches_read_controller:ownedByCurrentUser');
 
-        $controllers->post('/{sapiVersion}', 'saved_searches_edit_controller:save');
-        $controllers->delete('/{sapiVersion}/{id}', 'saved_searches_edit_controller:delete');
+        $controllers->post('/v3', 'saved_searches_edit_controller:save');
+        $controllers->delete('/v3/{id}', 'saved_searches_edit_controller:delete');
 
         return $controllers;
     }
