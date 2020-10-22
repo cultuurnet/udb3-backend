6fb6e6b8b1d4f132048c43e9b00c6f8eb402cb36
Luc Wollants <lucwollants@gmail.com>
2020-10-22 16:32:39 +0200

    III-3501 Create the offer popularity table

---
+++ app/Migrations/Version20201022154706.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20201022154706 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('offer_popularity');
+
+        $table->addColumn(
+            'offer_id',
+            'guid',
+            [
+                'length' => 36,
+                'notnull' => true,
+            ]
+        );
+        $table->addColumn(
+            'offer_type',
+            'string',
+            [
+                'length' => 32,
+                'notnull' => true,
+            ]
+        );
+        $table->addColumn(
+            'popularity',
+            'bigint',
+            [
+                'notnull' => true,
+            ]
+        );
+        $table->addColumn(
+            'creation_date',
+            'datetime',
+            [
+                'notnull' => true,
+            ]
+        );
+
+        $table->addIndex(
+            [
+                'offer_id',
+            ],
+            'offer_id_index'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('offer_popularity');
+    }
+}
