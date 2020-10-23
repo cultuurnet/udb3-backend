5d32aa3ad7219c22c82b571ed029b90246ae52a8
Luc Wollants <lucwollants@gmail.com>
2020-10-23 12:04:27 +0200

    III-3583 Add concrete DBAL implementation for Popularity repo

---
+++ src/Offer/Popularity/DBALPopularityRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\Popularity;
+
+use Doctrine\DBAL\Connection;
+
+class DBALPopularityRepository implements PopularityRepository
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function get(string $offerId): Popularity
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $popularity = $queryBuilder
+            ->select('popularity')
+            ->from('offer_popularity')
+            ->where('offer_id = :offer_id')
+            ->setParameter(':offer_id', $offerId)
+            ->execute()
+            ->fetchColumn();
+
+        return new Popularity((int) $popularity ?: 0);
+    }
+}
