ea13434e07b73245efe26a37d9f94760eeb20b9a
Luc Wollants <lucwollants@gmail.com>
2020-10-23 12:05:05 +0200

    III-3583 Add test for DBALPopularityRepository

---
+++ tests/Offer/Popularity/DBALPopularityRepositoryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\Popularity;
+
+use CultuurNet\UDB3\DBALTestConnectionTrait;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class DBALPopularityRepositoryTest extends TestCase
+{
+    use DBALTestConnectionTrait;
+
+    /**
+     * @var DBALPopularityRepository
+     */
+    private $dbalPopularityRepository;
+
+    protected function setUp(): void
+    {
+        $this->createTable(
+            PopularitySchemaConfigurator::getTableDefinition(
+                $this->createSchema()
+            )
+        );
+
+        $this->getConnection()->insert(
+            'offer_popularity',
+            [
+                'offer_id' => '388ffe26-84c3-40ec-8ab3-79466a2ab691',
+                'offer_type' => 'event',
+                'popularity' => 85762498,
+                'creation_date' => (new DateTime())->format(\DATE_ATOM),
+            ]
+        );
+
+        $this->dbalPopularityRepository = new DBALPopularityRepository($this->getConnection());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_get_a_popularity_by_offer_id(): void
+    {
+        $actualPopularity = $this->dbalPopularityRepository->get('388ffe26-84c3-40ec-8ab3-79466a2ab691');
+
+        $this->assertEquals(new Popularity(85762498), $actualPopularity);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_zero_popularity_for_a_missing_offer(): void
+    {
+        $actualPopularity = $this->dbalPopularityRepository->get('dbeff38d-9edd-47bf-87e2-4d2622c7a0d8');
+
+        $this->assertEquals(new Popularity(0), $actualPopularity);
+    }
+}
