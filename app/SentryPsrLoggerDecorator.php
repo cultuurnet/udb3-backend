c34221e939fe5f55cf01f31eef72cbf6d72d715f
Bert Ramakers <bertramakers@me.com>
2020-10-26 11:29:06 +0100

    Use square bracket syntax for arrays

--- app/SentryPsrLoggerDecorator.php
+++ app/SentryPsrLoggerDecorator.php
@@ -31,54 +31,54 @@ final class SentryPsrLoggerDecorator implements LoggerInterface
         $this->logger = $logger;
     }
 
-    public function error($message, array $context = array()): void
+    public function error($message, array $context = []): void
     {
         $this->sendThrowableToSentry($context);
         $this->logger->error($message, $context);
     }
 
-    public function emergency($message, array $context = array()): void
+    public function emergency($message, array $context = []): void
     {
         $this->sendThrowableToSentry($context);
         $this->logger->emergency($message, $context);
     }
 
-    public function alert($message, array $context = array()): void
+    public function alert($message, array $context = []): void
     {
         $this->logger->alert($message, $context);
     }
 
-    public function critical($message, array $context = array()): void
+    public function critical($message, array $context = []): void
     {
         $this->logger->critical($message, $context);
     }
 
-    public function warning($message, array $context = array()): void
+    public function warning($message, array $context = []): void
     {
         $this->logger->warning($message, $context);
     }
 
-    public function notice($message, array $context = array()): void
+    public function notice($message, array $context = []): void
     {
         $this->logger->notice($message, $context);
     }
 
-    public function info($message, array $context = array()): void
+    public function info($message, array $context = []): void
     {
         $this->logger->info($message, $context);
     }
 
-    public function debug($message, array $context = array()): void
+    public function debug($message, array $context = []): void
     {
         $this->logger->debug($message, $context);
     }
 
-    public function log($level, $message, array $context = array()): void
+    public function log($level, $message, array $context = []): void
     {
         $this->logger->log($level, $message, $context);
     }
 
-    private function sendThrowableToSentry(array $context = array()): void
+    private function sendThrowableToSentry(array $context = []): void
     {
         $throwable = $context['exception'] ?? null;
         if ($throwable instanceof Throwable) {
