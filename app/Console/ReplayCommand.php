60b6a318489ea47cc9592fe2d4eb43b5e6cd5d05
Luc Wollants <lucwollants@gmail.com>
2020-12-10 13:30:50 +0100

    Put replay command again in the default namespace.

--- app/Console/ReplayCommand.php
+++ app/Console/ReplayCommand.php
@@ -76,7 +76,7 @@ class ReplayCommand extends AbstractCommand
         );
 
         $this
-            ->setName('offer:replay')
+            ->setName('replay')
             ->setDescription('Replay the event stream to the event bus with only read models attached.')
             ->addArgument(
                 'aggregate',
