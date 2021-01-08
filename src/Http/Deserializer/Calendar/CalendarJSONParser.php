5aabf06c55e491a7640ebf8767cbf7f8b137407f
Luc Wollants <lucwollants@gmail.com>
2021-01-08 11:42:35 +0100

    Implement getStatus on CalendarJSONParser

--- src/Http/Deserializer/Calendar/CalendarJSONParser.php
+++ src/Http/Deserializer/Calendar/CalendarJSONParser.php
@@ -5,6 +5,7 @@ namespace CultuurNet\UDB3\Http\Deserializer\Calendar;
 use CultuurNet\UDB3\Calendar\DayOfWeekCollection;
 use CultuurNet\UDB3\Calendar\OpeningHour;
 use CultuurNet\UDB3\Calendar\OpeningTime;
+use CultuurNet\UDB3\Event\ValueObjects\Status;
 use CultuurNet\UDB3\Timestamp;
 
 class CalendarJSONParser implements CalendarJSONParserInterface
@@ -25,6 +26,15 @@ class CalendarJSONParser implements CalendarJSONParserInterface
         }
 
         return new \DateTime($data['endDate']);
+    }
+
+    public function getStatus(array $data): ?Status
+    {
+        if (!isset($data['status'])) {
+            return null;
+        }
+
+        return Status::deserialize($data['status']);
     }
 
     /**
