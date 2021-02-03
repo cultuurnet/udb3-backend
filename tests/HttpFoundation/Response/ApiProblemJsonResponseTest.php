f3c7bdcc0df6c6b40bff2160fccdcf7add47db54
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-03 07:32:44 +0100

    Add tests

---
+++ tests/HttpFoundation/Response/ApiProblemJsonResponseTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CultuurNet\UDB3\HttpFoundation\Response;
+
+use Crell\ApiProblem\ApiProblem;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiProblemJsonResponseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_has_400_bad_request_as_default_status()
+    {
+        $apiProblemJsonResponse = new ApiProblemJsonResponse(new ApiProblem());
+
+        $this->assertEquals(
+            Response::HTTP_BAD_REQUEST,
+            $apiProblemJsonResponse->getStatusCode()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_uses_status_from_the_given_api_problem_if_it_has_one()
+    {
+        $apiProblem = new ApiProblem();
+        $apiProblem->setStatus(Response::HTTP_NOT_FOUND);
+
+        $apiProblemJsonResponse = new ApiProblemJsonResponse($apiProblem);
+
+        $this->assertEquals(
+            Response::HTTP_NOT_FOUND,
+            $apiProblemJsonResponse->getStatusCode()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_sets_a_problem_json_content_type_header()
+    {
+        $apiProblemJsonResponse = new ApiProblemJsonResponse(new ApiProblem());
+        $contentType = $apiProblemJsonResponse->headers->get('Content-Type', '');
+
+        $this->assertEquals('application/problem+json', $contentType);
+    }
+}
---
+++ tests/HttpFoundation/Response/JsonLdResponseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CultuurNet\UDB3\HttpFoundation\Response;
+
+use PHPUnit\Framework\TestCase;
+
+class JsonLdResponseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_adds_a_jsonld_content_type_header()
+    {
+        $data = ['@id' => 'http://acme.com/foo'];
+        $response = new JsonLdResponse($data);
+        $contentType = $response->headers->get('Content-Type', '');
+
+        $this->assertEquals('application/ld+json', $contentType);
+    }
+}
