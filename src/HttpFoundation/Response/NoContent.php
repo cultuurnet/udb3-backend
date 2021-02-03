03300e787da3b92458621975cd78d6fadee6083a
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-03 07:58:43 +0100

    Duplicate NoContent helper to HttpFoundation namespace

---
+++ src/HttpFoundation/Response/NoContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CultuurNet\UDB3\HttpFoundation\Response;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class NoContent extends Response
+{
+    public function __construct(array $headers = [])
+    {
+        parent::__construct('', 204, $headers);
+    }
+}
