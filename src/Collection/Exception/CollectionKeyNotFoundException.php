7fd92b9258a0653f0b409e2d4fa379d0341d9e5c
Luc Wollants <lucwollants@gmail.com>
2021-02-04 14:58:18 +0100

    Update src/Collection/Exception/CollectionKeyNotFoundException.php
    Co-authored-by: Jonas Drieghe <jonasd@madewithlove.be>

--- src/Collection/Exception/CollectionKeyNotFoundException.php
+++ src/Collection/Exception/CollectionKeyNotFoundException.php
@@ -4,10 +4,7 @@ namespace CultuurNet\UDB3\Collection\Exception;
 
 class CollectionKeyNotFoundException extends \Exception
 {
-    /**
-     * @param string $key
-     */
-    public function __construct($key)
+    public function __construct(string $key)
     {
         parent::__construct(sprintf('The specified key "%s" was not found in the collection.', $key), 404);
     }
