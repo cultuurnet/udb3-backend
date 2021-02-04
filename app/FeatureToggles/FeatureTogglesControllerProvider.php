3533391dace13b32492a763be964329bf6d63100
Luc Wollants <lucwollants@gmail.com>
2021-02-04 09:40:06 +0100

    Move the SilexFeatureToggles folder

---
+++ app/FeatureToggles/FeatureTogglesControllerProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ */
+
+namespace TwoDotsTwice\SilexFeatureToggles;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class FeatureTogglesControllerProvider implements ControllerProviderInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function connect(Application $app)
+    {
+        /** @var ControllerCollection $controllers */
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/toggles', function (Application $app) {
+            /** @var \Qandidate\Toggle\ToggleManager $toggles */
+            $toggles = $app['toggles'];
+
+            $toggleStates = [];
+
+            /** @var \Qandidate\Toggle\Toggle $toggle */
+            foreach ($toggles->all() as $toggle) {
+                $toggleStates[$toggle->getName()] = $toggle->activeFor(
+                    $app['toggles.context']
+                );
+            }
+
+            return new JsonResponse(
+                $toggleStates
+            );
+        });
+
+        return $controllers;
+    }
+}
---
+++ app/FeatureToggles/FeatureTogglesProvider.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @file
+ */
+
+namespace TwoDotsTwice\SilexFeatureToggles;
+
+use Qandidate\Toggle\Context;
+use Qandidate\Toggle\Serializer\InMemoryCollectionSerializer;
+use Qandidate\Toggle\ToggleManager;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class FeatureTogglesProvider implements ServiceProviderInterface
+{
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * FeatureTogglesProvider constructor.
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function register(Application $app)
+    {
+        $config = $this->config;
+
+        // Global context for feature toggles. You can add to this context from
+        // your own middlewares, by extending the service, etc.
+        $app['toggles.context'] = $app->share(
+            function () {
+                return new Context();
+            }
+        );
+
+        // The feature toggles manager which can be asked for the current state
+        // of feature toggles.
+        $app['toggles'] = $app->share(
+            function () use ($config) {
+                $serializer = new InMemoryCollectionSerializer();
+                $collection = $serializer->deserialize($config);
+
+                $toggles = new ToggleManager($collection);
+                return $toggles;
+            }
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function boot(Application $app)
+    {
+
+    }
+}
