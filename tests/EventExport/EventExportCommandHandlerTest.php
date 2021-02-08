c4c213e83a0da81f68614418b9cd6ca9f5b8f5ac
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-08 12:57:55 +0100

    We're mocking the logger here

--- tests/EventExport/EventExportCommandHandlerTest.php
+++ tests/EventExport/EventExportCommandHandlerTest.php
@@ -35,7 +35,7 @@ class EventExportCommandHandlerTest extends TestCase
     private $eventExportCommandHandler;
 
     /**
-     * @var LoggerInterface
+     * @var LoggerInterface|MockObject
      */
     private $logger;
 
