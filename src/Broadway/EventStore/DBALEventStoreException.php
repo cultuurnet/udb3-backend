02a85ce30b57a36b2c2e49429048dbaac5cd791e
Luc Wollants <lucwollants@gmail.com>
2021-02-18 17:25:37 +0100

    Add custom DBALEventStore implementation

---
+++ src/Broadway/EventStore/DBALEventStoreException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CultuurNet\UDB3\Broadway\EventStore;
+
+use Broadway\EventStore\EventStoreException;
+use Doctrine\DBAL\DBALException;
+
+/**
+ * Wraps exceptions thrown by the DBAL event store.
+ */
+class DBALEventStoreException extends EventStoreException
+{
+    public static function create(DBALException $exception)
+    {
+        return new DBALEventStoreException(null, 0, $exception);
+    }
+}
