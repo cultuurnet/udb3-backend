fb835ffdcaf7c25a16bd7a47486f9a9ed79c6d48
Luc Wollants <lucwollants@gmail.com>
2021-02-23 11:15:02 +0100

    Use import statements instead of FQN

--- config/cli-config.php
+++ config/cli-config.php
@@ -5,16 +5,22 @@
 
 $application = require __DIR__ . '/../bootstrap.php';
 
+use Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand;
+use Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand;
+use Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand;
+use Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand;
+use Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand;
 use Doctrine\DBAL\Tools\Console\ConsoleRunner;
+use Symfony\Component\Console\Helper\QuestionHelper;
 
 $connection = $application['dbal_connection'];
 
 $commands = [
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
-    new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand(),
+    new ExecuteCommand(),
+    new GenerateCommand(),
+    new MigrateCommand(),
+    new StatusCommand(),
+    new VersionCommand(),
 ];
 
 $helperSet = ConsoleRunner::createHelperSet($connection);
