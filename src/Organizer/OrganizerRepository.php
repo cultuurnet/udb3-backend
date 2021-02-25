a08f8833cb421e82109b7bc7b20c01d2be0a41dc
Bert Ramakers <bertramakers@me.com>
2021-02-25 16:15:04 +0100

    Overwrite OrganizerRepository::load() to add better return type
    The type of $id could not be set to string, it would make the method incompatible with the parent method

--- src/Organizer/OrganizerRepository.php
+++ src/Organizer/OrganizerRepository.php
@@ -22,4 +22,13 @@ class OrganizerRepository extends EventSourcingRepository
             $eventStreamDecorators
         );
     }
+
+    public function load($id): Organizer
+    {
+        // Overwritten to add better return type based on the Aggregate class passed in the constructor.
+        // Docblock added for PHPStan
+        /** @var Organizer $organizer */
+        $organizer = parent::load($id);
+        return $organizer;
+    }
 }
