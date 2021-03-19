05fbbdc4754426f026e5b6d374f94c8e20c7486e
Bert Ramakers <bertramakers@me.com>
2021-03-19 12:36:49 +0100

    Add decorator for Sentry Monolog handler
    This decorator will set the tags and user info before passing the record to the actual Sentry handler.
    This is the recommended approach as preparation for upgrading to 3.x later, and allows us to already set user info which we couldn't do with the SentryTagsProcessor.

---
+++ app/Error/SentryHandlerScopeDecorator.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Silex\Error;
+
+use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
+use CultuurNet\UDB3\Jwt\Udb3Token;
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Handler\HandlerInterface;
+use Sentry\State\Scope;
+use function Sentry\withScope;
+
+/**
+ * @see https://github.com/getsentry/sentry-php/blob/master/UPGRADE-3.0.md
+ */
+final class SentryHandlerScopeDecorator implements HandlerInterface
+{
+    /**
+     * @var HandlerInterface
+     */
+    private $decoratedHandler;
+
+    /**
+     * @var Udb3Token|null
+     */
+    private $udb3Token;
+
+    /**
+     * @var ApiKey|null
+     */
+    private $apiKey;
+
+    /**
+     * @var string|null
+     */
+    private $apiName;
+
+    public function __construct(
+        HandlerInterface $decoratedHandler,
+        ?Udb3Token $udb3Token,
+        ?ApiKey $apiKey,
+        ?string $apiName
+    ) {
+        $this->decoratedHandler = $decoratedHandler;
+        $this->udb3Token = $udb3Token;
+        $this->apiKey = $apiKey;
+        $this->apiName = $apiName;
+    }
+
+    public function handle(array $record): bool
+    {
+        $result = false;
+
+        withScope(function (Scope $scope) use ($record, &$result): void {
+            $scope->setTags($this->createApiTags());
+            $scope->setUser($this->createUserInfo());
+
+            $result = $this->decoratedHandler->handle($record);
+        });
+
+        return $result;
+    }
+
+    private function createApiTags(): array
+    {
+        return [
+            'api_key' => $this->apiKey ? $this->apiKey->toString() : 'null',
+            'api_name' => $this->apiName ?? 'null',
+        ];
+    }
+
+    private function createUserInfo(): array
+    {
+        if (!$this->udb3Token) {
+            return ['id' => 'anonymous'];
+        }
+
+        return [
+            'id' => $this->udb3Token->id(),
+            'username' => $this->udb3Token->userName(),
+            'email' => $this->udb3Token->email(),
+            'uid' => $this->udb3Token->jwtToken()->getClaim('uid', 'null'),
+            'uitidv1id' => $this->udb3Token->jwtToken()->getClaim('https://publiq.be/uitidv1id', 'null'),
+            'sub' => $this->udb3Token->jwtToken()->getClaim('sub', 'null'),
+        ];
+    }
+
+    public function handleBatch(array $records): void
+    {
+        $this->decoratedHandler->handleBatch($records);
+    }
+
+    public function isHandling(array $record): bool
+    {
+        return $this->decoratedHandler->isHandling($record);
+    }
+
+    public function close(): void
+    {
+        $this->decoratedHandler->close();
+    }
+
+    public function pushProcessor($callback): void
+    {
+        $this->decoratedHandler->pushProcessor($callback);
+    }
+
+    public function popProcessor(): callable
+    {
+        return $this->decoratedHandler->popProcessor();
+    }
+
+    public function setFormatter(FormatterInterface $formatter): void
+    {
+        $this->decoratedHandler->setFormatter($formatter);
+    }
+
+    public function getFormatter(): FormatterInterface
+    {
+        return $this->decoratedHandler->getFormatter();
+    }
+}
