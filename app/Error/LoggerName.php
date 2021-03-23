683da0af2082b5f0d471bc8e1502baa37670530b
Bert Ramakers <bertramakers@me.com>
2021-03-23 16:59:30 +0100

    Close off LoggerName constructor

--- app/Error/LoggerName.php
+++ app/Error/LoggerName.php
@@ -16,7 +16,7 @@ final class LoggerName
      */
     private $loggerName;
 
-    public function __construct(string $fileNameWithoutSuffix, ?string $customLoggerName = null)
+    private public function __construct(string $fileNameWithoutSuffix, ?string $customLoggerName = null)
     {
         $fileNameWithoutSuffix = str_replace('_', '-', $fileNameWithoutSuffix);
         $customLoggerName = $customLoggerName ? str_replace('_', '-', $customLoggerName) : null;
