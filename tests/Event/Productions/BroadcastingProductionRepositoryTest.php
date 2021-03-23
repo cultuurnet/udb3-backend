4cb5b92a3a1137c5e5629451dc78cb1d39386faa
Bert Ramakers <bertramakers@me.com>
2021-03-23 12:20:12 +0100

    Add test for one new event added to a production

--- tests/Event/Productions/BroadcastingProductionRepositoryTest.php
+++ tests/Event/Productions/BroadcastingProductionRepositoryTest.php
@@ -88,4 +88,51 @@ class BroadcastingProductionRepositoryTest extends TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_broadcast_event_projected_to_jsonld_for_events_in_a_production_when_an_event_is_added(): void
+    {
+        $newEventId = '0054706e-a197-4e15-bab0-0473dc378884';
+
+        $production = new Production(
+            ProductionId::fromNative('599fc3af-0023-4c59-a0ab-05c9ad7f54cc'),
+            'mock production',
+            [
+                'd6a65aa8-d871-4a3e-a7ef-81926ad62371',
+                'bf1668d0-ce82-4e38-b284-2947f12850d6',
+                'a40ca8ff-cdae-406c-9124-f5874ef8056a',
+            ]
+        );
+
+        $this->decoratee->expects($this->once())
+            ->method('addEvent')
+            ->with($newEventId, $production);
+
+        $this->repository->addEvent($newEventId, $production);
+
+        $expected = [
+            new EventProjectedToJSONLD(
+                '0054706e-a197-4e15-bab0-0473dc378884',
+                'https://io.uitdatabank.be/events/0054706e-a197-4e15-bab0-0473dc378884'
+            ),
+            new EventProjectedToJSONLD(
+                'd6a65aa8-d871-4a3e-a7ef-81926ad62371',
+                'https://io.uitdatabank.be/events/d6a65aa8-d871-4a3e-a7ef-81926ad62371'
+            ),
+            new EventProjectedToJSONLD(
+                'bf1668d0-ce82-4e38-b284-2947f12850d6',
+                'https://io.uitdatabank.be/events/bf1668d0-ce82-4e38-b284-2947f12850d6'
+            ),
+            new EventProjectedToJSONLD(
+                'a40ca8ff-cdae-406c-9124-f5874ef8056a',
+                'https://io.uitdatabank.be/events/a40ca8ff-cdae-406c-9124-f5874ef8056a'
+            ),
+        ];
+
+        $actual = $this->eventBus->getEvents();
+
+        $this->assertEquals($expected, $actual);
+    }
 }
