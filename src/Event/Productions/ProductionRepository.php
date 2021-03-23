b71005c91b9577a4d260716eeec3c766baea300a
Bert Ramakers <bertramakers@me.com>
2021-03-23 11:20:52 +0100

    Remove unused import

--- src/Event/Productions/ProductionRepository.php
+++ src/Event/Productions/ProductionRepository.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Event\Productions;
 
-use CultuurNet\UDB3\EntityNotFoundException;
-
 interface ProductionRepository
 {
     public function add(Production $production): void;
