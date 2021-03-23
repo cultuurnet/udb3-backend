1479b79a4e3ff838a34ceb527eaeb1cbf566fc76
Bert Ramakers <bertramakers@me.com>
2021-03-23 12:43:12 +0100

    Add missing @throws annotations

--- src/Event/Productions/ProductionRepository.php
+++ src/Event/Productions/ProductionRepository.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Event\Productions;
 
+use CultuurNet\UDB3\EntityNotFoundException;
+
 interface ProductionRepository
 {
     public function add(Production $production): void;
 
+    /**
+     * @throws EntityNotFoundException
+     */
     public function find(ProductionId $productionId): Production;
 
     public function addEvent(string $eventId, Production $production): void;
@@ -23,5 +28,8 @@ interface ProductionRepository
 
     public function count(string $keyword): int;
 
+    /**
+     * @throws EntityNotFoundException
+     */
     public function findProductionForEventId(string $eventId): Production;
 }
