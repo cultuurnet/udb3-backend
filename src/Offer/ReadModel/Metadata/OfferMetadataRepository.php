58d3b497ef288a6475aff9e6127fb4f9cec4ff61
Jonas Drieghe <jonasd@madewithlove.be>
2021-03-26 12:22:39 +0100

    Add basic repository to store and retrieve offer metadata

---
+++ src/Offer/ReadModel/Metadata/OfferMetadataRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\ReadModel\Metadata;
+
+use CultuurNet\UDB3\EntityNotFoundException;
+use CultuurNet\UDB3\Label\ReadModels\Doctrine\AbstractDBALRepository;
+use Doctrine\DBAL\Connection;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class OfferMetadataRepository extends AbstractDBALRepository
+{
+    public const TABLE = 'offer_metadata';
+
+    public function __construct(Connection $connection)
+    {
+        parent::__construct($connection, new StringLiteral(self::TABLE));
+    }
+
+    public function get(string $offerId)
+    {
+        $result = $this->getConnection()
+            ->createQueryBuilder()
+            ->select('*')
+            ->from(self::TABLE)
+            ->where('id = :offerId')
+            ->setParameter('offerId', $offerId)
+            ->execute()
+            ->fetch();
+
+        if (!$result) {
+            throw new EntityNotFoundException('Could not find offer metadata for offer with id ' . $offerId);
+        }
+
+        return new OfferMetadata(
+            $offerId,
+            $result['created_by_api_consumer']
+        );
+    }
+
+    private function exists(string $offerId): bool
+    {
+        try {
+            $this->get($offerId);
+            return true;
+        } catch (EntityNotFoundException $e) {
+            return false;
+        }
+    }
+
+
+    public function save(OfferMetadata $offerMetadata): void
+    {
+        if (!$this->exists($offerMetadata->getOfferId())) {
+            $this->insert($offerMetadata);
+        } else {
+            $this->update($offerMetadata);
+        }
+    }
+
+    private function insert(OfferMetadata $offerMetadata): void
+    {
+        $this->getConnection()->insert(
+            self::TABLE,
+            [
+                'id' => $offerMetadata->getOfferId(),
+                'created_by_api_consumer' => $offerMetadata->getCreatedByApiConsumer(),
+            ]
+        );
+    }
+
+    private function update(OfferMetadata $offerMetadata): void
+    {
+        $this->getConnection()->update(
+            self::TABLE,
+            [
+                'created_by_api_consumer' => $offerMetadata->getCreatedByApiConsumer(),
+            ],
+            [
+                'id' => $offerMetadata->getOfferId(),
+            ]
+        );
+    }
+}
---
+++ tests/Offer/ReadModel/Metadata/OfferMetadataRepositoryTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\ReadModel\Metadata;
+
+use CultuurNet\UDB3\DBALTestConnectionTrait;
+use CultuurNet\UDB3\EntityNotFoundException;
+use CultuurNet\UDB3\Offer\ReadModel\Metadata\Doctrine\OfferMetadataSchemaConfigurator;
+use PHPUnit\Framework\TestCase;
+
+class OfferMetadataRepositoryTest extends TestCase
+{
+    use DBALTestConnectionTrait;
+
+    /**
+     * @var OfferMetadataRepository
+     */
+    private $repository;
+
+    protected function setUp(): void
+    {
+        $schema = $this->createSchema();
+        $this->createTable(
+            OfferMetadataSchemaConfigurator::getTableDefinition($schema)
+        );
+
+        $this->repository = new OfferMetadataRepository($this->getConnection());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_persist_offer_metadata(): void
+    {
+        $offerId = 'offer_id';
+        $createdByApiConsumer = 'uitdatabank-ui';
+
+        $offerMetadata = new OfferMetadata($offerId, $createdByApiConsumer);
+        $this->repository->save($offerMetadata);
+
+        $persistedOfferMetadata = $this->repository->get($offerId);
+        $this->assertEquals($createdByApiConsumer, $persistedOfferMetadata->getCreatedByApiConsumer());
+        $this->assertEquals($offerId, $persistedOfferMetadata->getOfferId());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throw_when_nothing_is_found(): void
+    {
+        $this->expectException(EntityNotFoundException::class);
+        $this->repository->get('offer_id');
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_update_existing_offer_metadata(): void
+    {
+        $offerId = 'offer_id';
+        $createdByApiConsumer = 'uitdatabank-ui';
+        $updatedCreatedByApiConsumer = 'other-api-consumer';
+
+        $offerMetadata = new OfferMetadata($offerId, $createdByApiConsumer);
+        $this->repository->save($offerMetadata);
+
+        $updatedOfferMetadata = $offerMetadata->withCreatedByApiConsumer($updatedCreatedByApiConsumer);
+        $this->repository->save($updatedOfferMetadata);
+
+        $persistedOfferMetadata = $this->repository->get($offerId);
+        $this->assertEquals($updatedCreatedByApiConsumer, $persistedOfferMetadata->getCreatedByApiConsumer());
+        $this->assertEquals($offerId, $persistedOfferMetadata->getOfferId());
+    }
+}
