2d3a4129998bcf47e243c6d9d4746fa592ce1603
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-12 19:21:57 +0200

    Add PSR-7 compatible ApiProblemJsonResponse

---
+++ src/Http/Response/ApiProblemJsonResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Response;
+
+use Crell\ApiProblem\ApiProblem;
+use Zend\Diactoros\Response\JsonResponse;
+
+/**
+ * Response type application/problem+json.
+ *
+ * @see https://tools.ietf.org/html/draft-nottingham-http-problem-07
+ **/
+class ApiProblemJsonResponse extends JsonResponse
+{
+    public function __construct(ApiProblem $problem, $headers = [])
+    {
+        $headers += ['Content-Type' => 'application/problem+json'];
+
+        $status = 400;
+        if (null !== $problem->getStatus()) {
+            $headers += [
+                'X-Status-Code' => $problem->getStatus(),
+            ];
+            $status = $problem->getStatus();
+        }
+
+        $data = $problem->asArray();
+
+        parent::__construct(
+            $data,
+            $status,
+            $headers
+        );
+    }
+}
