b8e50c8a2ce8f9bd21e3f3a422b9edda2f358317
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-12 19:06:59 +0200

    Add PSR-7 compatible JsonLdResponse

---
+++ src/Http/Response/JsonLdResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Response;
+
+use Zend\Diactoros\Response\JsonResponse;
+
+class JsonLdResponse extends JsonResponse
+{
+    public function __construct($data = null, $status = 200, $headers = [])
+    {
+        $headers += ['Content-Type' => 'application/ld+json'];
+
+        parent::__construct(
+            $data,
+            $status,
+            $headers
+        );
+    }
+}
