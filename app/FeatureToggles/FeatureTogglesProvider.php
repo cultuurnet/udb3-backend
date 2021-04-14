bd169fbbda023c889c55a33bf9615b3ce79c4048
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-14 18:29:10 +0200

    Remove provisioning for unused services

--- app/FeatureToggles/FeatureTogglesProvider.php
+++ app/FeatureToggles/FeatureTogglesProvider.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\FeatureToggles;
 
-use Qandidate\Toggle\Context;
-use Qandidate\Toggle\Serializer\InMemoryCollectionSerializer;
-use Qandidate\Toggle\ToggleManager;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -25,26 +22,6 @@ class FeatureTogglesProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $config = $this->config;
-
-        // Global context for feature toggles. You can add to this context from
-        // your own middlewares, by extending the service, etc.
-        $app['toggles.context'] = $app->share(
-            function () {
-                return new Context();
-            }
-        );
-
-        // The feature toggles manager which can be asked for the current state
-        // of feature toggles.
-        $app['toggles'] = $app->share(
-            function () use ($config) {
-                $serializer = new InMemoryCollectionSerializer();
-                $collection = $serializer->deserialize($config);
-
-                $toggles = new ToggleManager($collection);
-                return $toggles;
-            }
-        );
     }
 
     public function boot(Application $app)
