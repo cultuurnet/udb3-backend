794360ea5e9c9a825aefebe0a4756fa762ed9d59
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-14 13:16:00 +0200

    Remove unused notFound factory method

--- src/ReadModel/DocumentDoesNotExist.php
+++ src/ReadModel/DocumentDoesNotExist.php
@@ -8,15 +8,8 @@ use Exception;
 
 final class DocumentDoesNotExist extends Exception
 {
-    private const NOT_FOUND = 404;
-
     public static function withId(string $id): self
     {
         return new self("Document with id ${id} not found.", 404);
-    }
-
-    public static function notFound(string $id): DocumentDoesNotExist
-    {
-        return new self("Document with id ${id} not found.", self::NOT_FOUND);
     }
 }
