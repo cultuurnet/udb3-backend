e0fdbff87cd5a99d16b95e1aff6322ffed6e59ba
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-19 13:26:29 +0200

    Simplify test provisioning

--- tests/Storage/PurgeServiceManagerTest.php
+++ tests/Storage/PurgeServiceManagerTest.php
@@ -24,16 +24,10 @@ class PurgeServiceManagerTest extends TestCase
      */
     public function it_can_store_an_array_of_PurgeServiceInterfaces()
     {
-        $this->addReadModel_PurgeServiceInterfaces($this->purgeServiceManager);
+        $this->purgeServiceManager->addPurgeService($this->createMockedPurgeService());
+        $this->purgeServiceManager->addPurgeService($this->createMockedPurgeService());
 
         $this->assertCount(2, $this->purgeServiceManager->getPurgeServices());
-    }
-
-
-    private function addReadModel_PurgeServiceInterfaces(PurgeServiceManager $purgeServiceManager)
-    {
-        $purgeServiceManager->addPurgeService($this->createMockedPurgeService());
-        $purgeServiceManager->addPurgeService($this->createMockedPurgeService());
     }
 
     /**
