1bee4088dc78d663b10eab0e6e9c2831f7ed7efa
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-19 13:22:09 +0200

    Clean up types and docblocks in CLI command

--- app/Console/PurgeModelCommand.php
+++ app/Console/PurgeModelCommand.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Console;
 
-use CultuurNet\UDB3\Storage\PurgeServiceInterface;
 use CultuurNet\UDB3\Storage\PurgeServiceManager;
 use Knp\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class PurgeModelCommand
- *
- * @package CultuurNet\UDB3\Silex\Console
- */
 class PurgeModelCommand extends Command
 {
     /**
@@ -28,18 +22,12 @@ class PurgeModelCommand extends Command
         $this->purgeServiceManager = $purgeServiceManager;
     }
 
-
     protected function configure()
     {
-        $this
-            ->setName('purge')
-            ->setDescription('Purge all read models');
+        $this->setName('purge')->setDescription('Purge all read models');
     }
 
-    /**
-     * @return int
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         foreach ($this->purgeServiceManager->getReadModelPurgeServices() as $purgeService) {
             $purgeService->purgeAll();
