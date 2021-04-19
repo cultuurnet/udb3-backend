87cf37b1d7e9b212223f85869a30318d5a2eee92
Jonas Drieghe <jonasd@madewithlove.be>
2021-04-19 13:38:57 +0200

    Build PurgeServiceManager in consistent state in tests

--- tests/Storage/PurgeServiceManagerTest.php
+++ tests/Storage/PurgeServiceManagerTest.php
@@ -4,37 +4,20 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Storage;
 
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class PurgeServiceManagerTest extends TestCase
 {
     /**
-     * @var PurgeServiceManager
-     */
-    private $purgeServiceManager;
-
-    protected function setUp()
-    {
-        $this->purgeServiceManager = new PurgeServiceManager();
-    }
-
-    /**
      * @test
      */
     public function it_can_store_an_array_of_PurgeServiceInterfaces()
     {
-        $this->purgeServiceManager->addPurgeService($this->createMockedPurgeService());
-        $this->purgeServiceManager->addPurgeService($this->createMockedPurgeService());
+        $purgeServiceManager = new PurgeServiceManager([
+            $this->createMock(PurgeServiceInterface::class),
+            $this->createMock(PurgeServiceInterface::class),
+        ]);
 
-        $this->assertCount(2, $this->purgeServiceManager->getPurgeServices());
-    }
-
-    /**
-     * @return PurgeServiceInterface|MockObject
-     */
-    private function createMockedPurgeService()
-    {
-        return $this->createMock(PurgeServiceInterface::class);
+        $this->assertCount(2, $purgeServiceManager->getPurgeServices());
     }
 }
