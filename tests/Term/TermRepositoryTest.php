024bd1fe18d2e38440fa0155a7b81a42bc7e3c7f
Bert Ramakers <bertramakers@me.com>
2021-05-31 11:01:29 +0200

    Add TermRepository to fetch a term (with label if known) by id

---
+++ tests/Term/TermRepositoryTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace CultuurNet\UDB3\Term;
+
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Category;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryID;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryLabel;
+use PHPUnit\Framework\TestCase;
+
+final class TermRepositoryTest extends TestCase
+{
+    private const MAPPING = [
+        '0.41.0.0.0' => [
+            'name' => [
+                'nl' => 'Thema of pretpark',
+                'fr' => 'Parc à thème ou parc d\'attractions',
+                'de' => 'Unterhaltungspark',
+                'en' => 'Theme park',
+            ],
+        ],
+        '0.59.0.0.0' => [
+            'name' => [
+                'nl' => 'Sportactiviteit',
+                'fr' => 'Activité sportive',
+                'de' => 'Geben Wettbewerb',
+                'en' => 'Sports activity',
+            ],
+        ],
+        '3CuHvenJ+EGkcvhXLg9Ykg' => [
+            'name' => [
+                'nl' => 'Archeologische Site',
+            ],
+        ],
+        'rJRFUqmd6EiqTD4c7HS90w' => [
+            'name' => [
+                'nl' => 'School of onderwijscentrum',
+            ],
+        ],
+        'no_name' => [],
+        'no_name_nl' => [
+            'name' => [
+                'fr' => '...',
+            ],
+        ],
+    ];
+
+    /**
+     * @var TermRepository
+     */
+    private $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new TermRepository(self::MAPPING);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_an_existing_term_with_label_by_id(): void
+    {
+        $givenId = '0.41.0.0.0';
+        $expectedTerm = new Category(
+            new CategoryID('0.41.0.0.0'),
+            new CategoryLabel('Thema of pretpark')
+        );
+        $actualTerm = $this->repository->getById($givenId);
+        $this->assertEquals($expectedTerm, $actualTerm);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_an_existing_term_without_label(): void
+    {
+        $givenId = 'no_name';
+        $expectedTerm = new Category(
+            new CategoryID('no_name')
+        );
+        $actualTerm = $this->repository->getById($givenId);
+        $this->assertEquals($expectedTerm, $actualTerm);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_an_existing_term_without_dutch_label(): void
+    {
+        $givenId = 'no_name_nl';
+        $expectedTerm = new Category(
+            new CategoryID('no_name_nl')
+        );
+        $actualTerm = $this->repository->getById($givenId);
+        $this->assertEquals($expectedTerm, $actualTerm);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_for_an_id_that_does_not_exist(): void
+    {
+        $givenId = 'does_not_exist';
+        $this->expectException(TermNotFoundException::class);
+        $this->repository->getById($givenId);
+    }
+}
