99188b97dc6f5ecc356627be191424b47a7a043e
Bert Ramakers <bertramakers@me.com>
2021-06-29 17:47:17 +0200

    Copy id() and getClientId() to JsonWebToken

--- src/Jwt/Symfony/Authentication/JsonWebToken.php
+++ src/Jwt/Symfony/Authentication/JsonWebToken.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Jwt\Symfony\Authentication;
 
 use CultuurNet\UDB3\Jwt\Udb3Token;
+use Lcobucci\JWT\Token;
 use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
 class JsonWebToken extends AbstractToken
 {
     /**
-     * @var Udb3Token
+     * @var Token
      */
     private $jwt;
 
@@ -18,11 +19,34 @@ class JsonWebToken extends AbstractToken
     {
         parent::__construct();
         $this->setAuthenticated($authenticated);
-        $this->jwt = $jwt;
+        $this->jwt = $jwt->jwtToken();
+    }
+
+    public function id(): string
+    {
+        if ($this->jwt->hasClaim('uid')) {
+            return $this->jwt->getClaim('uid');
+        }
+
+        if ($this->jwt->hasClaim('https://publiq.be/uitidv1id')) {
+            return $this->jwt->getClaim('https://publiq.be/uitidv1id');
+        }
+
+        return $this->jwt->getClaim('sub');
+    }
+
+    public function getClientId(): ?string
+    {
+        // Check first if the token has the claim, to prevent an OutOfBoundsException (thrown if the default is set to
+        // null and the claim is missing).
+        if ($this->jwt->hasClaim('azp')) {
+            return (string) $this->jwt->getClaim('azp');
+        }
+        return null;
     }
 
     public function getCredentials(): Udb3Token
     {
-        return $this->jwt;
+        return new Udb3Token($this->jwt);
     }
 }
---
+++ tests/Jwt/Symfony/Authentication/JwtUserTokenTest.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Jwt\Symfony\Authentication;
+
+use CultuurNet\UDB3\Jwt\Udb3Token;
+use Lcobucci\JWT\Claim\Basic;
+use Lcobucci\JWT\Token;
+use Lcobucci\JWT\Token as Jwt;
+use PHPUnit\Framework\TestCase;
+
+class JwtUserTokenTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_the_udb3_token_as_credentials(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Jwt(
+                ['alg' => 'none'],
+                [],
+                null,
+                $payload = ['header', 'payload']
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertEquals($udb3Token, $jwt->getCredentials());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_set_as_authenticated(): void
+    {
+        $jwt = new JsonWebToken(new Udb3Token(new Jwt()), true);
+        $this->assertTrue($jwt->isAuthenticated());
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_uid_claim_as_id_if_present(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Token(
+                ['alg' => 'none'],
+                [
+                    'uid' => new Basic('uid', '6e3ef9b3-e37b-428e-af30-05f3a96dbbe4'),
+                    'https://publiq.be/uitidv1id' => new Basic(
+                        'https://publiq.be/uitidv1id',
+                        'b55f041e-5c5e-4850-9fb8-8cf73d538c56'
+                    ),
+                    'sub' => new Basic('sub', 'auth0|ce6abd8f-b1e2-4bce-9dde-08af64438e87'),
+                ]
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertEquals('6e3ef9b3-e37b-428e-af30-05f3a96dbbe4', $jwt->id());
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_uitid_v1_claim_as_id_if_present(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Token(
+                ['alg' => 'none'],
+                [
+                    'https://publiq.be/uitidv1id' => new Basic(
+                        'https://publiq.be/uitidv1id',
+                        'b55f041e-5c5e-4850-9fb8-8cf73d538c56'
+                    ),
+                    'sub' => new Basic('sub', 'auth0|ce6abd8f-b1e2-4bce-9dde-08af64438e87'),
+                ]
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertEquals('b55f041e-5c5e-4850-9fb8-8cf73d538c56', $jwt->id());
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_sub_claim_as_id(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Token(
+                ['alg' => 'none'],
+                [
+                    'sub' => new Basic('sub', 'auth0|ce6abd8f-b1e2-4bce-9dde-08af64438e87'),
+                ]
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertEquals('auth0|ce6abd8f-b1e2-4bce-9dde-08af64438e87', $jwt->id());
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_client_id_from_azp_claim_if_present(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Token(
+                ['alg' => 'none'],
+                [
+                    'azp' => new Basic('azp', 'jndYaQY9BSa9W7FQqDEGI0WEi4KlU6vJ'),
+                ]
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertEquals('jndYaQY9BSa9W7FQqDEGI0WEi4KlU6vJ', $jwt->getClientId());
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_as_client_id_if_azp_claim_is_missing(): void
+    {
+        $udb3Token = new Udb3Token(
+            new Token(
+                ['alg' => 'none'],
+                [
+                    'sub' => new Basic('sub', 'auth0|ce6abd8f-b1e2-4bce-9dde-08af64438e87'),
+                ]
+            )
+        );
+
+        $jwt = new JsonWebToken($udb3Token);
+
+        $this->assertNull($jwt->getClientId());
+    }
+}
