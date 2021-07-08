f275b20438ea49b925a9e1ec466d388af5e92936
Luc Wollants <lucwollants@gmail.com>
2021-07-08 09:28:28 +0200

    Bootstrap the new rename endpoint

--- app/Event/ProductionControllerProvider.php
+++ app/Event/ProductionControllerProvider.php
@@ -53,6 +53,7 @@ class ProductionControllerProvider implements ControllerProviderInterface
         $controllers->put('/{productionId}/events/{eventId}', ProductionsWriteController::class . ':addEventToProduction');
         $controllers->delete('/{productionId}/events/{eventId}', ProductionsWriteController::class . ':removeEventFromProduction');
         $controllers->post('/{productionId}/merge/{fromProductionId}', ProductionsWriteController::class . ':mergeProductions');
+        $controllers->put('/{productionId}/name/{productionName}', ProductionsWriteController::class . ':renameProduction');
 
         $controllers->post('/skip', ProductionsWriteController::class . ':skipEvents');
 
