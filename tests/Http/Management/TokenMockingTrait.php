2c7dd76cb83269f56f87eb9afa56d10067913e24
Bert Ramakers <bertramakers@me.com>
2021-07-15 17:35:03 +0200

    Use AbstractToken instead of JsonWebToken for mocking in other tests

--- tests/Http/Management/TokenMockingTrait.php
+++ tests/Http/Management/TokenMockingTrait.php
@@ -6,11 +6,24 @@ namespace CultuurNet\UDB3\Http\Management;
 
 use CultuurNet\UDB3\Jwt\Symfony\Authentication\JsonWebToken;
 use CultuurNet\UDB3\Jwt\Symfony\Authentication\JsonWebTokenFactory;
+use CultuurNet\UDB3\Jwt\Symfony\Authentication\Token\AbstractToken;
+use CultuurNet\UDB3\Jwt\Symfony\Authentication\Token\JwtProviderV1Token;
+use CultuurNet\UDB3\Jwt\Symfony\Authentication\Token\MockTokenStringFactory;
 
 trait TokenMockingTrait
 {
-    private function createMockToken(string $userId): JsonWebToken
+    private function createMockToken(string $userId): AbstractToken
     {
-        return JsonWebTokenFactory::createWithClaims(['uid' => $userId])->authenticate();
+        return (
+            new JwtProviderV1Token(
+                MockTokenStringFactory::createWithClaims(
+                    [
+                        'uid' => $userId,
+                        'nick' => 'nick',
+                        'email' => 'mock@example.com',
+                    ]
+                )
+            )
+        )->authenticate();
     }
 }
