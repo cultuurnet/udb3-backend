848aabdf2fb6f8cb84475f61638eae6bec9d77f6
Bert Ramakers <bertramakers@me.com>
2021-07-15 15:57:53 +0200

    Add validation methods to Token interface

--- src/Jwt/Symfony/Authentication/Token/Token.php
+++ src/Jwt/Symfony/Authentication/Token/Token.php
@@ -8,4 +8,9 @@ interface Token
 {
     public function getUserId(): string;
     public function getUserIdentityDetails(): ?UserIdentityDetails;
+
+    public function isUsableAtCurrentTime(): bool;
+    public function hasValidIssuer(array $validIssuers): bool;
+    public function hasAudience(string $audience): bool;
+    public function verifyRsaSha256Signature(string $publicKey, ?string $keyPassphrase = null): bool;
 }
