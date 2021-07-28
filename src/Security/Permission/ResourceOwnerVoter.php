92e82f4203ab6fe33037a1eb1f8d5fb4ecaf6690
Bert Ramakers <bertramakers@me.com>
2021-07-28 16:08:28 +0200

    Move ResourceOwnerVoter to Security namespace
    Since it's also used for organizers

--- src/Security/Permission/ResourceOwnerVoter.php
+++ src/Security/Permission/ResourceOwnerVoter.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-namespace CultuurNet\UDB3\Offer\Security\Permission;
+namespace CultuurNet\UDB3\Security\Permission;
 
 use CultuurNet\UDB3\Offer\ReadModel\Permission\PermissionQueryInterface;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
-use CultuurNet\UDB3\Security\Permission\PermissionVoter;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class ResourceOwnerVoter implements PermissionVoter
