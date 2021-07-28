92c097eb2e2c585c59102f836e411e45bf83072c
Bert Ramakers <bertramakers@me.com>
2021-07-28 15:04:23 +0200

    Refactor RoleConstraintVoter to inline the logic from Sapi3UserPermissionMatcher

--- src/Offer/Security/Permission/RoleConstraintVoter.php
+++ src/Offer/Security/Permission/RoleConstraintVoter.php
@@ -4,36 +4,60 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer\Security\Permission;
 
-use CultuurNet\UDB3\Offer\Security\UserPermissionMatcherInterface;
+use CultuurNet\UDB3\Offer\Security\Sapi3SearchQueryFactory;
+use CultuurNet\UDB3\Role\ReadModel\Constraints\UserConstraintsReadRepositoryInterface;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Search\CountingSearchServiceInterface;
 use CultuurNet\UDB3\Security\Permission\PermissionVoterInterface;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class RoleConstraintVoter implements PermissionVoterInterface
 {
     /**
-     * @var UserPermissionMatcherInterface
+     * @var UserConstraintsReadRepositoryInterface
      */
-    private $userPermissionMatcher;
-
-
-    public function __construct(UserPermissionMatcherInterface $userPermissionMatcher)
-    {
-        $this->userPermissionMatcher = $userPermissionMatcher;
-    }
+    private $userConstraintsReadRepository;
 
     /**
-     * @return bool
+     * @var Sapi3SearchQueryFactory
      */
+    private $searchQueryFactory;
+
+    /**
+     * @var CountingSearchServiceInterface
+     */
+    private $searchService;
+
+    public function __construct(
+        UserConstraintsReadRepositoryInterface $userConstraintsReadRepository,
+        Sapi3SearchQueryFactory $searchQueryFactory,
+        CountingSearchServiceInterface $searchService
+    ) {
+        $this->userConstraintsReadRepository = $userConstraintsReadRepository;
+        $this->searchQueryFactory = $searchQueryFactory;
+        $this->searchService = $searchService;
+    }
+
     public function isAllowed(
         Permission $permission,
         StringLiteral $itemId,
         StringLiteral $userId
-    ) {
-        return $this->userPermissionMatcher->itMatchesOffer(
+    ): bool {
+        $constraints = $this->userConstraintsReadRepository->getByUserAndPermission(
             $userId,
-            $permission,
+            $permission
+        );
+        if (count($constraints) < 1) {
+            return false;
+        }
+
+        $query = $this->searchQueryFactory->createFromConstraints(
+            $constraints,
             $itemId
         );
+
+        $totalItems = $this->searchService->search($query);
+
+        return $totalItems === 1;
     }
 }
