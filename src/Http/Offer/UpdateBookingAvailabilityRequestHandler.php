617b2d35ef9bf581f1e2dab84d357d171ee49810
Luc Wollants <lucwollants@gmail.com>
2021-08-04 13:54:04 +0200

    Create Request handler for UpdateBookingAvailability

---
+++ src/Http/Offer/UpdateBookingAvailabilityRequestHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\HttpFoundation\Response\NoContent;
+use CultuurNet\UDB3\Offer\Commands\UpdateBookingAvailability;
+use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailability;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class UpdateBookingAvailabilityRequestHandler
+{
+    /**
+     * @var CommandBus
+     */
+    private $commandBus;
+
+    /**
+     * @var UpdateBookingAvailabilityValidator
+     */
+    private $updateBookingAvailabilityValidator;
+
+    public function __construct(
+        CommandBus $commandBus,
+        UpdateBookingAvailabilityValidator $updateBookingAvailabilityValidator
+    ) {
+        $this->commandBus = $commandBus;
+        $this->updateBookingAvailabilityValidator = $updateBookingAvailabilityValidator;
+    }
+
+    public function handle(Request $request, string $offerId): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $this->updateBookingAvailabilityValidator->validate($data);
+
+        $this->commandBus->dispatch(
+            new UpdateBookingAvailability($offerId, BookingAvailability::fromNative($data['type']))
+        );
+
+        return new NoContent();
+    }
+}
