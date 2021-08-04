5cd1814582443b90f2c591be44f9373a25a5770b
Luc Wollants <lucwollants@gmail.com>
2021-08-04 11:03:19 +0200

    Add handler to update the booking availability

---
+++ src/Offer/CommandHandlers/UpdateBookingAvailabilityHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Offer\Commands\UpdateBookingAvailability;
+use CultuurNet\UDB3\Offer\OfferRepository;
+
+class UpdateBookingAvailabilityHandler implements CommandHandler
+{
+    /**
+     * @var OfferRepository
+     */
+    private $offerRepository;
+
+    public function __construct(OfferRepository $offerRepository)
+    {
+        $this->offerRepository = $offerRepository;
+    }
+
+    public function handle($command): void
+    {
+        if (!$command instanceof UpdateBookingAvailability) {
+            return;
+        }
+
+        $offer = $this->offerRepository->load($command->getItemId());
+        $offer->updateBookingAvailability($command->getBookingAvailability());
+        $this->offerRepository->save($offer);
+    }
+}
---
+++ tests/CultuurNet/UDB3/Offer/CommandHandlers/UpdateBookingAvailabilityHandlerTest.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventStore\EventStore;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType;
+use CultuurNet\UDB3\Event\EventRepository;
+use CultuurNet\UDB3\Event\Events\CalendarUpdated;
+use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\Offer\Commands\UpdateBookingAvailability;
+use CultuurNet\UDB3\Offer\OfferRepository;
+use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailability;
+use CultuurNet\UDB3\Place\PlaceRepository;
+use CultuurNet\UDB3\Timestamp;
+use CultuurNet\UDB3\Title;
+use DateTime;
+
+final class UpdateBookingAvailabilityHandlerTest extends CommandHandlerScenarioTestCase
+{
+    protected function createCommandHandler(EventStore $eventStore, EventBus $eventBus): CommandHandler
+    {
+        return new UpdateBookingAvailabilityHandler(
+            new OfferRepository(
+                new EventRepository($eventStore, $eventBus),
+                new PlaceRepository($eventStore, $eventBus)
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_update_offers_with_permanent_calendar(): void
+    {
+        $permanentEventCreated = new EventCreated(
+            '1',
+            new Language('nl'),
+            new Title('Permanent Event'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(CalendarType::PERMANENT())
+        );
+
+        $updateBookingAvailability = new UpdateBookingAvailability('1', BookingAvailability::unavailable());
+
+        $this->scenario
+            ->withAggregateId('1')
+            ->given([$permanentEventCreated])
+            ->when($updateBookingAvailability)
+            ->then([]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_update_offers_with_periodic_calendar(): void
+    {
+        $periodicEventCreated = new EventCreated(
+            '1',
+            new Language('nl'),
+            new Title('Periodic Event'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(
+                CalendarType::PERIODIC(),
+                new DateTime('2020-01-01 10:00:00'),
+                new DateTime('2020-01-01 12:00:00')
+            )
+        );
+
+        $updateBookingAvailability = new UpdateBookingAvailability('1', BookingAvailability::unavailable());
+
+        $this->scenario
+            ->withAggregateId('1')
+            ->given([$periodicEventCreated])
+            ->when($updateBookingAvailability)
+            ->then([]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_update_offers_with_single_calendar(): void
+    {
+        $singleEventCreated = new EventCreated(
+            '1',
+            new Language('nl'),
+            new Title('Single Event'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(
+                CalendarType::SINGLE(),
+                null,
+                null,
+                [
+                    new Timestamp(
+                        new DateTime('2020-01-01 10:00:00'),
+                        new DateTime('2020-01-01 12:00:00')
+                    ),
+                ]
+            )
+        );
+
+        $updateBookingAvailability = new UpdateBookingAvailability('1', BookingAvailability::unavailable());
+
+        $this->scenario
+            ->withAggregateId('1')
+            ->given([$singleEventCreated])
+            ->when($updateBookingAvailability)
+            ->then([
+                new CalendarUpdated(
+                    '1',
+                    (new Calendar(
+                        CalendarType::SINGLE(),
+                        null,
+                        null,
+                        [
+                            (new Timestamp(
+                                new DateTime('2020-01-01 10:00:00'),
+                                new DateTime('2020-01-01 12:00:00')
+                            ))->withBookingAvailability(BookingAvailability::unavailable()),
+                        ]
+                    ))->withBookingAvailability(BookingAvailability::unavailable())
+                ),
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_update_offers_with_multiple_calendar(): void
+    {
+        $multipleEventCreated = new EventCreated(
+            '1',
+            new Language('nl'),
+            new Title('Single Event'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(
+                CalendarType::MULTIPLE(),
+                null,
+                null,
+                [
+                    new Timestamp(
+                        new DateTime('2020-01-01 10:00:00'),
+                        new DateTime('2020-01-01 12:00:00')
+                    ),
+                    new Timestamp(
+                        new DateTime('2020-01-03 10:00:00'),
+                        new DateTime('2020-01-03 12:00:00')
+                    ),
+                ]
+            )
+        );
+
+        $updateBookingAvailability = new UpdateBookingAvailability('1', BookingAvailability::unavailable());
+
+        $this->scenario
+            ->withAggregateId('1')
+            ->given([$multipleEventCreated])
+            ->when($updateBookingAvailability)
+            ->then([
+                new CalendarUpdated(
+                    '1',
+                    (new Calendar(
+                        CalendarType::MULTIPLE(),
+                        null,
+                        null,
+                        [
+                            (new Timestamp(
+                                new DateTime('2020-01-01 10:00:00'),
+                                new DateTime('2020-01-01 12:00:00')
+                            ))->withBookingAvailability(BookingAvailability::unavailable()),
+                            (new Timestamp(
+                                new DateTime('2020-01-03 10:00:00'),
+                                new DateTime('2020-01-03 12:00:00')
+                            ))->withBookingAvailability(BookingAvailability::unavailable()),
+                        ]
+                    ))->withBookingAvailability(BookingAvailability::unavailable())
+                ),
+            ]);
+    }
+}
