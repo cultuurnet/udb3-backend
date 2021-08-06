58f5d639c6bb59f76de2dba2c2279db6366a6bed
Bert Ramakers <bertramakers@me.com>
2021-08-06 16:01:41 +0200

    Replace mock requests with request builder

--- tests/Http/Request/Body/JsonRequestBodyParserTest.php
+++ tests/Http/Request/Body/JsonRequestBodyParserTest.php
@@ -2,9 +2,8 @@
 
 namespace CultuurNet\UDB3\Http\Request\Body;
 
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamInterface;
 
 class JsonRequestBodyParserTest extends TestCase
 {
@@ -13,9 +12,15 @@ class JsonRequestBodyParserTest extends TestCase
      */
     private $parser;
 
+    /**
+     * @var Psr7RequestBuilder
+     */
+    private $requestBuilder;
+
     protected function setUp()
     {
         $this->parser = new JsonRequestBodyParser();
+        $this->requestBuilder = new Psr7RequestBuilder();
     }
 
     /**
@@ -23,7 +28,7 @@ class JsonRequestBodyParserTest extends TestCase
      */
     public function it_returns_the_decoded_json_body_as_an_associative_array(): void
     {
-        $given = $this->createMockRequestWithBody('{"foo":"bar"}');
+        $given = $this->requestBuilder->withBodyFromString('{"foo":"bar"}')->build('PUT');
         $expected = ['foo' => 'bar'];
         $actual = $this->parser->parse($given);
         $this->assertEquals($expected, $actual);
@@ -34,7 +39,7 @@ class JsonRequestBodyParserTest extends TestCase
      */
     public function it_throws_when_body_is_missing(): void
     {
-        $given = $this->createMockRequestWithBody('');
+        $given = $this->requestBuilder->withBodyFromString('')->build('PUT');
         $this->expectException(RequestBodyMissing::class);
         $this->parser->parse($given);
     }
@@ -44,22 +49,8 @@ class JsonRequestBodyParserTest extends TestCase
      */
     public function it_throws_when_body_is_invalid_json(): void
     {
-        $given = $this->createMockRequestWithBody('{{}');
+        $given = $this->requestBuilder->withBodyFromString('{{}')->build('PUT');
         $this->expectException(RequestBodyInvalidSyntax::class);
         $this->parser->parse($given);
-    }
-
-    private function createMockRequestWithBody(string $body): ServerRequestInterface
-    {
-        $stream = $this->createMock(StreamInterface::class);
-        $stream->method('__toString')
-            ->willReturn($body);
-
-        $mock = $this->createMock(ServerRequestInterface::class);
-        $mock->method('getBody')
-            ->willReturn($stream);
-
-        /** @var ServerRequestInterface $mock */
-        return $mock;
     }
 }
