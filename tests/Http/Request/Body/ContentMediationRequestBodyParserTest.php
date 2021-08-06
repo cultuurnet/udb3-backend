f5d27572764392a9bda1df66503297317467bad8
Bert Ramakers <bertramakers@me.com>
2021-08-06 16:06:26 +0200

    Add parser that parses a body differently based on the content-type
    For now it only parses JSON but we can also make it parse xml, yml, ... if we ever need/want to

---
+++ tests/Http/Request/Body/ContentMediationRequestBodyParserTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace CultuurNet\UDB3\Http\Request\Body;
+
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use PHPUnit\Framework\TestCase;
+
+class ContentMediationRequestBodyParserTest extends TestCase
+{
+    /**
+     * @var ContentMediationRequestBodyParser
+     */
+    private $parser;
+
+    /**
+     * @var Psr7RequestBuilder
+     */
+    private $requestBuilder;
+
+    protected function setUp(): void
+    {
+        $this->parser = new ContentMediationRequestBodyParser();
+        $this->requestBuilder = new Psr7RequestBuilder();
+    }
+
+    /**
+     * @test
+     */
+    public function it_parses_content_type_application_json_as_json(): void
+    {
+        $given = $this->requestBuilder
+            ->withHeader('content-type', 'application/json')
+            ->withBodyFromString('{"foo":"bar"}')
+            ->build('PUT');
+
+        $expected = ['foo' => 'bar'];
+        $actual = $this->parser->parse($given);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function it_parses_unknown_content_types_as_json(): void
+    {
+        $given = $this->requestBuilder
+            ->withHeader('content-type', 'blabla')
+            ->withBodyFromString('{"foo":"bar"}')
+            ->build('PUT');
+
+        $expected = ['foo' => 'bar'];
+        $actual = $this->parser->parse($given);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function it_parses_missing_content_types_as_json(): void
+    {
+        $given = $this->requestBuilder
+            ->withBodyFromString('{"foo":"bar"}')
+            ->build('PUT');
+
+        $expected = ['foo' => 'bar'];
+        $actual = $this->parser->parse($given);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
