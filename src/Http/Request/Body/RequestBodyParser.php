ea2bd2a05543df70d7e5a961af9cf9ec7e3508a4
Bert Ramakers <bertramakers@me.com>
2021-08-06 16:16:07 +0200

    Add strict types where missing

--- src/Http/Request/Body/RequestBodyParser.php
+++ src/Http/Request/Body/RequestBodyParser.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CultuurNet\UDB3\Http\Request\Body;
 
 use Psr\Http\Message\ServerRequestInterface;
--- tests/Http/Request/Body/ContentMediationRequestBodyParserTest.php
+++ tests/Http/Request/Body/ContentMediationRequestBodyParserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CultuurNet\UDB3\Http\Request\Body;
 
 use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
