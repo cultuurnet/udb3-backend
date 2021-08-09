5ad19effe54925d0ece7ba3cc620f5a18691389c
Bert Ramakers <bertramakers@me.com>
2021-08-09 18:06:46 +0200

    Fix asJson()

--- web/index.php
+++ web/index.php
@@ -238,6 +238,6 @@ try {
     // We're outside of the Silex app, so we cannot use the standard way to return a Response object.
     http_response_code($apiProblem->getStatus());
     header('Content-Type: application/json');
-    echo $apiProblem->asJson();
+    echo $apiProblem->toArray();
     exit;
 }
