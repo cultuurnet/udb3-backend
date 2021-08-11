2395a5262a12291f829a3cda49ca1904b43c2278
Luc Wollants <lucwollants@gmail.com>
2021-08-11 14:33:18 +0200

    Add test to update booking on 1 sub event

--- tests/Event/CommandHandlers/UpdateSubEventsHandlerTest.php
+++ tests/Event/CommandHandlers/UpdateSubEventsHandlerTest.php
@@ -22,6 +22,7 @@ use CultuurNet\UDB3\Event\ValueObjects\StatusType;
 use CultuurNet\UDB3\Event\ValueObjects\SubEventUpdate;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Offer\CalendarTypeNotSupported;
+use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailability;
 use CultuurNet\UDB3\Timestamp;
 use CultuurNet\UDB3\Title;
 use DateTime;
@@ -252,6 +253,52 @@ final class UpdateSubEventsHandlerTest extends CommandHandlerScenarioTestCase
                                     [new StatusReason(new Language('nl'), 'Tijdelijk niet beschikbaar')]
                                 )
                             ),
+                        ]
+                    )
+                ),
+            ],
+            'Update booking availability on 1 sub event' => [
+                new EventCreated(
+                    '1',
+                    new Language('nl'),
+                    new Title('Multiple Event'),
+                    new EventType('0.50.4.0.0', 'concert'),
+                    new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+                    new Calendar(
+                        CalendarType::MULTIPLE(),
+                        null,
+                        null,
+                        [
+                            new Timestamp(
+                                new DateTime('2020-01-01 10:00:00'),
+                                new DateTime('2020-01-01 12:00:00')
+                            ),
+                            new Timestamp(
+                                new DateTime('2020-01-03 10:00:00'),
+                                new DateTime('2020-01-03 12:00:00')
+                            ),
+                        ]
+                    )
+                ),
+                new UpdateSubEvents(
+                    '1',
+                    (new SubEventUpdate(1))->withBookingAvailability(BookingAvailability::unavailable())
+                ),
+                new CalendarUpdated(
+                    '1',
+                    new Calendar(
+                        CalendarType::MULTIPLE(),
+                        new DateTime('2020-01-01 10:00:00'),
+                        new DateTime('2020-01-03 12:00:00'),
+                        [
+                            new Timestamp(
+                                new DateTime('2020-01-01 10:00:00'),
+                                new DateTime('2020-01-01 12:00:00')
+                            ),
+                            (new Timestamp(
+                                new DateTime('2020-01-03 10:00:00'),
+                                new DateTime('2020-01-03 12:00:00')
+                            ))->withBookingAvailability(BookingAvailability::unavailable()),
                         ]
                     )
                 ),
