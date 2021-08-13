db48c2f55f5c995bf60ddfab46e25207880e4f48
Bert Ramakers <bertramakers@me.com>
2021-08-13 14:57:13 +0200

    Don't validate the schema directly in UpdateSubEventsRequestBodyParser

--- src/Http/Event/UpdateSubEventsRequestBodyParser.php
+++ src/Http/Event/UpdateSubEventsRequestBodyParser.php
@@ -4,40 +4,20 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Event;
 
-use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
-use CultuurNet\UDB3\Http\ApiProblem\SchemaError;
 use CultuurNet\UDB3\Http\Request\Body\ContentNegotiationRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\JsonRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
-use Opis\JsonSchema\Errors\ErrorFormatter;
-use Opis\JsonSchema\Validator;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class UpdateSubEventsRequestBodyParser implements RequestBodyParser
 {
     public function parse(ServerRequestInterface $request)
     {
-        $data = (new ContentNegotiationRequestBodyParser())->parse($request);
-        $this->validateSchema($data);
+        $data = (new ContentNegotiationRequestBodyParser())
+            ->withJsonRequestBodyParser(
+                new JsonRequestBodyParser(file_get_contents(__DIR__ . '/UpdateSubEventsSchema.json'))
+            )
+            ->parse($request);
         return $data;
-    }
-
-    /**
-     * @throws ApiProblem
-     */
-    private function validateSchema($data): void
-    {
-        $validator = new Validator(null, 100);
-        $result = $validator->validate($data, file_get_contents(__DIR__ . '/UpdateSubEventsSchema.json'));
-
-        if (!$result->isValid()) {
-            $errors = (new ErrorFormatter())->format($result->error());
-            $schemaErrors = [];
-            foreach ($errors as $jsonPointer => $errorsPerPointer) {
-                foreach ($errorsPerPointer as $error) {
-                    $schemaErrors[] = new SchemaError($jsonPointer, $error);
-                }
-            }
-            throw ApiProblem::bodyInvalidData(...$schemaErrors);
-        }
     }
 }
