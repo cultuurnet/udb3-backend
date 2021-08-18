7af3db313e00e140afa221d7fe258036ca8d87e5
Luc Wollants <lucwollants@gmail.com>
2021-08-18 16:14:31 +0200

    Adapt tests to work with the new Auth0Token en expires datetime

--- tests/User/Auth0ManagementTokenProviderTest.php
+++ tests/User/Auth0ManagementTokenProviderTest.php
@@ -7,7 +7,8 @@ namespace CultuurNet\UDB3\Silex\User;
 use CultuurNet\UDB3\User\Auth0ManagementTokenGenerator;
 use CultuurNet\UDB3\User\Auth0ManagementTokenProvider;
 use CultuurNet\UDB3\User\Auth0ManagementTokenRepository;
-use Lcobucci\JWT\Parser;
+use CultuurNet\UDB3\User\Auth0Token;
+use DateTimeImmutable;
 use Lcobucci\JWT\Token;
 use PHPUnit\Framework\TestCase;
 
@@ -16,104 +17,103 @@ class Auth0ManagementTokenProviderTest extends TestCase
     /**
      * @test
      */
-    public function it_generates_new_token_if_no_token_in_repository()
+    public function it_generates_new_token_if_no_token_in_repository(): void
     {
         $tokenRepository = $this->createMock(Auth0ManagementTokenRepository::class);
         $tokenRepository->expects($this->atLeast(1))
             ->method('token')
             ->willReturn(null);
 
+        $token = new Auth0Token(
+            'my_token',
+            new DateTimeImmutable(),
+            3600
+        );
+
         $tokenRepository->expects($this->once())
-            ->method('store');
+            ->method('store')
+            ->with($token);
 
         $tokenGenerator = $this->createMock(Auth0ManagementTokenGenerator::class);
         $tokenGenerator->expects($this->atLeast(1))
             ->method('newToken')
-            ->willReturn('Token');
-
-        $parser = $this->createMock(Parser::class);
+            ->willReturn($token);
 
         $service = new Auth0ManagementTokenProvider(
             $tokenGenerator,
-            $tokenRepository,
-            $parser
+            $tokenRepository
         );
 
         $result = $service->token();
 
-        $this->assertEquals('Token', $result);
+        $this->assertEquals('my_token', $result);
     }
 
     /**
      * @test
      */
-    public function it_returns_token_if_valid_token_is_in_repository()
+    public function it_returns_token_if_valid_token_is_in_repository(): void
     {
+        $token = new Auth0Token(
+            'my_token',
+            new DateTimeImmutable(),
+            3600
+        );
+
         $tokenRepository = $this->createMock(Auth0ManagementTokenRepository::class);
         $tokenRepository->expects($this->atLeast(1))
             ->method('token')
-            ->willReturn('Token');
+            ->willReturn($token);
 
         $tokenGenerator = $this->createMock(Auth0ManagementTokenGenerator::class);
         $tokenGenerator->expects($this->never())
             ->method('newToken');
 
-        $token = $this->createMock(Token::class);
-        $token->expects($this->atLeast(1))
-            ->method('isExpired')
-            ->willReturn(false);
-
-        $parser = $this->createMock(Parser::class);
-        $parser->expects($this->atLeast(1))
-            ->method('parse')
-            ->with('Token')
-            ->willReturn($token);
-
         $service = new Auth0ManagementTokenProvider(
             $tokenGenerator,
-            $tokenRepository,
-            $parser
+            $tokenRepository
         );
 
         $result = $service->token();
 
-        $this->assertEquals('Token', $result);
+        $this->assertEquals('my_token', $result);
     }
 
     /**
      * @test
      */
-    public function it_generates_new_token_if_current_on_is_expired()
+    public function it_generates_new_token_if_current_is_about_to_expire(): void
     {
+        // A token is also considered as expired when it will expire within 5 minutes.
+        $expiredToken = new Auth0Token(
+            'expired_token',
+            new DateTimeImmutable(),
+            300
+        );
+
+        $newToken = new Auth0Token(
+            'new_token',
+            new DateTimeImmutable(),
+            3600
+        );
+
         $tokenRepository = $this->createMock(Auth0ManagementTokenRepository::class);
         $tokenRepository->expects($this->atLeast(1))
             ->method('token')
-            ->willReturn('Token');
+            ->willReturn($expiredToken);
 
         $tokenGenerator = $this->createMock(Auth0ManagementTokenGenerator::class);
         $tokenGenerator->expects($this->atLeast(1))
             ->method('newToken')
-            ->willReturn('New-Token');
-
-        $token = $this->createMock(Token::class);
-        $token->expects($this->atLeast(1))
-            ->method('isExpired')
-            ->willReturn(true);
-
-        $parser = $this->createMock(Parser::class);
-        $parser->expects($this->atLeast(1))
-            ->method('parse')
-            ->with('Token')
-            ->willReturn($token);
+            ->willReturn($newToken);
 
         $service = new Auth0ManagementTokenProvider(
             $tokenGenerator,
-            $tokenRepository,
-            $parser
+            $tokenRepository
         );
 
         $result = $service->token();
 
-        $this->assertEquals('New-Token', $result);
+        $this->assertEquals('new_token', $result);
     }
 }
