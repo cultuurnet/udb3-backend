f40e3d742a0924d0ad71cbb840bf58270af4a888
Luc Wollants <lucwollants@gmail.com>
2021-08-18 16:11:16 +0200

    Add new Auth0Token VO as simple wrapper around token
    It makes it also possible to easily maintain issued at and expires at datetimes

---
+++ src/User/Auth0Token.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\User;
+
+use DateTimeImmutable;
+
+final class Auth0Token
+{
+    private string $token;
+
+    private DateTimeImmutable $issuedAt;
+
+    private int $expiresIn;
+
+    public function __construct(string $token, DateTimeImmutable $issuedAt, int $expiresIn)
+    {
+        $this->token = $token;
+        $this->issuedAt = $issuedAt;
+        $this->expiresIn = $expiresIn;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    public function getIssuedAt(): DateTimeImmutable
+    {
+        return $this->issuedAt;
+    }
+
+    public function getExpiresIn(): int
+    {
+        return $this->expiresIn;
+    }
+
+    public function getExpiresAt(): DateTimeImmutable
+    {
+        return $this->issuedAt->modify('+' . $this->expiresIn . 'seconds');
+    }
+}
