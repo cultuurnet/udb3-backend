a57bf145ac4aae856e49ae0fb8af4438787ef941
Bert Ramakers <bertramakers@me.com>
2021-08-20 18:09:06 +0200

    Implement RequestHandler on UpdateBookingAvailabilityRequestHandler

--- src/Http/Offer/UpdateBookingAvailabilityRequestHandler.php
+++ src/Http/Offer/UpdateBookingAvailabilityRequestHandler.php
@@ -10,6 +10,8 @@ use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
+use CultuurNet\UDB3\Http\Request\RequestHandler;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
 use CultuurNet\UDB3\Offer\Commands\UpdateBookingAvailability;
 use CultuurNet\UDB3\Offer\CalendarTypeNotSupported;
@@ -18,7 +20,7 @@ use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailabilityType;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-final class UpdateBookingAvailabilityRequestHandler
+final class UpdateBookingAvailabilityRequestHandler implements RequestHandler
 {
     private CommandBus $commandBus;
 
@@ -34,8 +36,11 @@ final class UpdateBookingAvailabilityRequestHandler
         );
     }
 
-    public function handle(ServerRequestInterface $request, string $offerId): ResponseInterface
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $routeParameters = new RouteParameters($request);
+        $offerId = $routeParameters->get('offerId');
+
         $data = (object) $this->updateBookingAvailabilityParser->parse($request)->getParsedBody();
 
         try {
