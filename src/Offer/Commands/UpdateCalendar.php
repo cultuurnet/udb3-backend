0540eb4cb1d528911a104656ad9b7bb09eeaa5f5
Bert Ramakers <bertramakers@me.com>
2021-08-20 15:52:23 +0200

    Make UpdateCalendar final

--- src/Offer/Commands/UpdateCalendar.php
+++ src/Offer/Commands/UpdateCalendar.php
@@ -6,7 +6,7 @@ namespace CultuurNet\UDB3\Offer\Commands;
 
 use CultuurNet\UDB3\Calendar;
 
-class UpdateCalendar extends AbstractCommand
+final class UpdateCalendar extends AbstractCommand
 {
     /**
      * @var Calendar
