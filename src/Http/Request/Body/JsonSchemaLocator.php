01514650b6666a3238606344d017d6ebbdd6919a
Bert Ramakers <bertramakers@me.com>
2021-08-23 18:45:17 +0200

    Remove unused JsonSchemaLocator::loadSchema()
    We don't want to load the schema ourselves because that does not resolve the $refs

--- src/Http/Request/Body/JsonSchemaLocator.php
+++ src/Http/Request/Body/JsonSchemaLocator.php
@@ -32,36 +32,6 @@ final class JsonSchemaLocator
         self::$schemaDirectory = realpath($schemaDirectory);
     }
 
-    public static function loadSchema(string $schemaFileName): string
-    {
-        if (self::$schemaDirectory === null) {
-            throw new RuntimeException(
-                'JsonSchemaLocator::setSchemaDirectory() should be called at least once before calling getSchema().'
-            );
-        }
-
-        // Prevent usages of hardcoded strings so we can easily refactor the file locations later if they ever change.
-        if (!in_array($schemaFileName, self::getConstants(), true)) {
-            throw new InvalidArgumentException(
-                $schemaFileName . ' is not in the list of known schema files, please use a predefined constant on the JsonSchemaLocator class (or add one).'
-            );
-        }
-
-        $schemaFileLocation = self::$schemaDirectory . '/' . ltrim($schemaFileName, '/');
-        if (!is_file($schemaFileLocation)) {
-            throw new RuntimeException($schemaFileLocation . ' is not a file.');
-        }
-
-        $schema = file_get_contents($schemaFileLocation);
-        if ($schema === false) {
-            throw new RuntimeException(
-                'Could not read ' . $schemaFileLocation
-            );
-        }
-
-        return $schema;
-    }
-
     public static function createValidator(int $maxErrors = 1): Validator
     {
         $resolver = new SchemaResolver();
