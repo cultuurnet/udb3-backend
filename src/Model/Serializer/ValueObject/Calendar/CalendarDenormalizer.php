11811d288320c28c1de7a1ba249a9cef656ecdf6
Bert Ramakers <bertramakers@me.com>
2021-08-24 15:32:44 +0200

    Linting™

--- src/Model/Serializer/ValueObject/Calendar/CalendarDenormalizer.php
+++ src/Model/Serializer/ValueObject/Calendar/CalendarDenormalizer.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Model\Serializer\ValueObject\Calendar;
 
 use CultuurNet\UDB3\Model\ValueObject\Calendar\BookingAvailability;
-use CultuurNet\UDB3\Model\ValueObject\Calendar\BookingAvailabilityType;
 use CultuurNet\UDB3\Model\ValueObject\Calendar\Calendar;
 use CultuurNet\UDB3\Model\ValueObject\Calendar\DateRange;
 use CultuurNet\UDB3\Model\ValueObject\Calendar\SubEvents;
