20f41392c74034d9b852d9cd1826a8d95d58be1d
Bert Ramakers <bertramakers@me.com>
2021-08-24 16:11:10 +0200

    Add SubEventUpdates collection and denormalizer

---
+++ src/Model/Serializer/ValueObject/Calendar/SubEventUpdatesDenormalizer.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Model\Serializer\ValueObject\Calendar;
+
+use CultuurNet\UDB3\Model\ValueObject\Calendar\SubEventUpdate;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\SubEventUpdates;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+final class SubEventUpdatesDenormalizer implements DenormalizerInterface
+{
+    private SubEventUpdateDenormalizer $subEventUpdateDenormalizer;
+
+    public function __construct()
+    {
+        $this->subEventUpdateDenormalizer = new SubEventUpdateDenormalizer();
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = array()): SubEventUpdates
+    {
+        $updates = [];
+        foreach ($data as $subEventUpdateData) {
+            $updates[] = $this->subEventUpdateDenormalizer->denormalize($subEventUpdateData, SubEventUpdate::class);
+        }
+        return new SubEventUpdates(...$updates);
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $type === SubEventUpdates::class;
+    }
+}
