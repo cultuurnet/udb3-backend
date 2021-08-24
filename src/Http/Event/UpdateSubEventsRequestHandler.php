ca61b7eae7996f32e7b7421bb83f03d798f26d05
Bert Ramakers <bertramakers@me.com>
2021-08-24 15:56:28 +0200

    Use denormalizer in UpdateSubEventsRequestHandler

--- src/Http/Event/UpdateSubEventsRequestHandler.php
+++ src/Http/Event/UpdateSubEventsRequestHandler.php
@@ -6,10 +6,6 @@ namespace CultuurNet\UDB3\Http\Event;
 
 use Broadway\CommandHandling\CommandBus;
 use CultuurNet\UDB3\Event\Commands\UpdateSubEvents;
-use CultuurNet\UDB3\Event\ValueObjects\Status;
-use CultuurNet\UDB3\Event\ValueObjects\StatusReason;
-use CultuurNet\UDB3\Event\ValueObjects\StatusType;
-use CultuurNet\UDB3\Event\ValueObjects\SubEventUpdate;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
@@ -17,9 +13,8 @@ use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
 use CultuurNet\UDB3\Http\Request\RequestHandler;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
-use CultuurNet\UDB3\Language;
-use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailability;
-use CultuurNet\UDB3\Offer\ValueObjects\BookingAvailabilityType;
+use CultuurNet\UDB3\Model\Serializer\ValueObject\Calendar\SubEventUpdateDenormalizer;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\SubEventUpdate;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -29,6 +24,8 @@ class UpdateSubEventsRequestHandler implements RequestHandler
 
     private RequestBodyParser $updateSubEventsParser;
 
+    private SubEventUpdateDenormalizer $subEventUpdateDenormalizer;
+
     public function __construct(CommandBus $commandBus)
     {
         $this->commandBus = $commandBus;
@@ -36,6 +33,8 @@ class UpdateSubEventsRequestHandler implements RequestHandler
         $this->updateSubEventsParser = RequestBodyParserFactory::createBaseParser(
             JsonSchemaValidatingRequestBodyParser::fromFile(JsonSchemaLocator::EVENT_SUB_EVENT_PATCH)
         );
+
+        $this->subEventUpdateDenormalizer = new SubEventUpdateDenormalizer();
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -47,45 +46,13 @@ class UpdateSubEventsRequestHandler implements RequestHandler
 
         $updateSubEvents = [];
         foreach ($updates as $update) {
-            $subEventUpdate = new SubEventUpdate($update['id']);
-
-            if (isset($update['status'])) {
-                $subEventUpdate = $subEventUpdate->withStatus(
-                    new Status(
-                        StatusType::fromNative($update['status']['type']),
-                        $this->parseReason($update)
-                    )
-                );
-            }
-
-            if (isset($update['bookingAvailability'])) {
-                $subEventUpdate = $subEventUpdate->withBookingAvailability(
-                    new BookingAvailability(BookingAvailabilityType::fromNative($update['bookingAvailability']['type']))
-                );
-            }
-
-            $updateSubEvents[] = $subEventUpdate;
+            $updateSubEvents[] = \CultuurNet\UDB3\Event\ValueObjects\SubEventUpdate::fromUdb3ModelsSubEventUpdate(
+                $this->subEventUpdateDenormalizer->denormalize($update, SubEventUpdate::class)
+            );
         }
 
         $this->commandBus->dispatch(new UpdateSubEvents($eventId, ...$updateSubEvents));
 
         return new NoContentResponse();
-    }
-
-    /**
-     * @return StatusReason[]
-     */
-    private function parseReason(array $data): array
-    {
-        if (!isset($data['status']['reason'])) {
-            return [];
-        }
-
-        $reason = [];
-        foreach ($data['status']['reason'] as $language => $translatedReason) {
-            $reason[] = new StatusReason(new Language($language), $translatedReason);
-        }
-
-        return $reason;
     }
 }
