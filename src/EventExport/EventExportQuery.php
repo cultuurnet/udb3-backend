8e0de3535e839b1831a97844da555498cbcc538b
Bert Ramakers <bertramakers@me.com>
2021-08-25 11:59:00 +0200

    Remove abstract TrimmedString VO

--- src/EventExport/EventExportQuery.php
+++ src/EventExport/EventExportQuery.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\EventExport;
 
-use CultuurNet\UDB3\TrimmedString;
+use ValueObjects\StringLiteral\StringLiteral;
 
-class EventExportQuery extends TrimmedString
+class EventExportQuery extends StringLiteral
 {
-    /**
-     * @inheritdoc
-     */
-    public function __construct($value)
+    public function __construct(string $value)
     {
-        parent::__construct($value);
+        parent::__construct(trim($value));
 
         if ($this->isEmpty()) {
             throw new \InvalidArgumentException('Query can not be empty');
--- src/Title.php
+++ src/Title.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3;
 
 use CultuurNet\UDB3\Model\ValueObject\Text\Title as Udb3ModelTitle;
+use ValueObjects\StringLiteral\StringLiteral;
 
 /**
  * @deprecated
  *   Use CultuurNet\UDB3\Model\ValueObject\Text\Title instead where possible.
  */
-class Title extends TrimmedString implements \JsonSerializable
+class Title extends StringLiteral implements \JsonSerializable
 {
-    public function __construct($value)
+    public function __construct(string $value)
     {
-        parent::__construct($value);
+        parent::__construct(trim($value));
 
         if ($this->isEmpty()) {
             throw new \InvalidArgumentException('Title can not be empty.');
