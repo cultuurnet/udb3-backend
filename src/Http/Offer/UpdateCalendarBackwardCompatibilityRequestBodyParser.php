363d47d6f63ce3f976301c90d57ad48b78837446
Bert Ramakers <bertramakers@me.com>
2021-08-27 10:20:54 +0200

    Remove polyfill of missing subEvent.status and subEvent.bookingAvailability

--- src/Http/Offer/UpdateCalendarBackwardCompatibilityRequestBodyParser.php
+++ src/Http/Offer/UpdateCalendarBackwardCompatibilityRequestBodyParser.php
@@ -43,49 +43,6 @@ final class UpdateCalendarBackwardCompatibilityRequestBodyParser implements Requ
             unset($data->timeSpans);
         }
 
-        // Add default status and bookingAvailability to subEvent(s) if missing.
-        // Technically they are required on the schema of subEvent as a side effect of using $ref to the subEvent model
-        // which has status and bookingAvailability set to required because they are always present on the read model.
-        // For backward compatibility we need to set defaults when they are missing so the validator does not complain.
-        // This is also the same behaviour as when we used the old CalendarJSONParser.
-        $defaultStatusType = isset($data->status, $data->status->type) ? $data->status->type : 'Available';
-        $defaultStatusReason = isset($data->status, $data->status->reason) ? $data->status->reason : null;
-        $defaultBookingAvailabilityType = isset($data->bookingAvailability, $data->bookingAvailability->type) ? $data->bookingAvailability->type : 'Available';
-        if (isset($data->subEvent) && is_array($data->subEvent)) {
-            $data->subEvent = array_map(
-                function ($subEvent) use ($defaultStatusType, $defaultStatusReason, $defaultBookingAvailabilityType) {
-                    if ($subEvent instanceof stdClass &&
-                        !isset($subEvent->status)) {
-                        $subEvent->status = (object) [];
-                    }
-                    if (isset($subEvent->status) &&
-                        is_object($subEvent->status) &&
-                        !isset($subEvent->status->type)) {
-                        $subEvent->status->type = $defaultStatusType;
-                    }
-                    if (isset($subEvent->status) &&
-                        is_object($subEvent->status) &&
-                        !isset($subEvent->status->reason) &&
-                        !is_null($defaultStatusReason)) {
-                        $subEvent->status->reason = $defaultStatusReason;
-                    }
-
-                    if ($subEvent instanceof stdClass &&
-                        !isset($subEvent->bookingAvailability)) {
-                        $subEvent->bookingAvailability = (object) [];
-                    }
-                    if (isset($subEvent->bookingAvailability) &&
-                        is_object($subEvent->bookingAvailability) &&
-                        !isset($subEvent->bookingAvailability->type)) {
-                        $subEvent->bookingAvailability->type = $defaultBookingAvailabilityType;
-                    }
-
-                    return $subEvent;
-                },
-                $data->subEvent
-            );
-        }
-
         return $request->withParsedBody($data);
     }
 }
