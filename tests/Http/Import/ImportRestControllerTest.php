610d400fb93b690dbd907237b515dba12f6a53f2
Luc Wollants <lucwollants@gmail.com>
2021-09-27 15:10:01 +0200

    Add test to verify throwing the resourceIdAlreadyInUse problem

--- tests/Http/Import/ImportRestControllerTest.php
+++ tests/Http/Import/ImportRestControllerTest.php
@@ -9,9 +9,13 @@ use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
 use CultuurNet\UDB3\ApiGuard\ApiKey\Reader\QueryParameterApiKeyReader;
 use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerInterface;
 use CultuurNet\UDB3\ApiGuard\Consumer\InMemoryConsumerRepository;
+use CultuurNet\UDB3\EventSourcing\DBAL\DBALEventStoreException;
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
 use CultuurNet\UDB3\Iri\CallableIriGenerator;
 use CultuurNet\UDB3\Model\Import\DecodedDocument;
 use CultuurNet\UDB3\Model\Import\DocumentImporterInterface;
+use Doctrine\DBAL\Driver\Mysqli\MysqliException;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Respect\Validation\Exceptions\ValidationException;
@@ -274,5 +278,45 @@ class ImportRestControllerTest extends TestCase
         $this->expectExceptionMessage('JSON-LD missing.');
 
         $this->controller->importWithId($request, '2f66ac1d-fd5a-431e-ba72-821da4176391');
+    }
+
+    /**
+     * @test
+     */
+    public function it_reports_an_id_already_in_use(): void
+    {
+        $id = 'c25ea5b8-acd2-4987-a207-6ee11444fde8';
+        $json = json_encode([
+            'name' => [
+                'nl' => 'Voorbeeld naam',
+            ],
+        ], JSON_THROW_ON_ERROR);
+        $request = Request::create('www.uitdatabank.dev', 'GET', [], [], [], [], $json);
+
+        $expectedDocument = new DecodedDocument(
+            $id,
+            [
+                '@id' => 'https://io.uitdatabank.be/mock/c25ea5b8-acd2-4987-a207-6ee11444fde8',
+                'name' => [
+                    'nl' => 'Voorbeeld naam',
+                ],
+            ]
+        );
+
+        $this->expectException(ApiProblem::class);
+        $this->expectExceptionMessage('Resource id already in use');
+
+        $this->importer->expects($this->once())
+            ->method('import')
+            ->with($expectedDocument)
+            ->willThrowException(
+                new DBALEventStoreException(
+                    'message',
+                    0,
+                    new UniqueConstraintViolationException('message', new MysqliException('message'))
+                )
+            );
+
+        $this->controller->importWithId($request, $id);
     }
 }
