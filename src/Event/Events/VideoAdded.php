45cfb12d2963038436e582e1bcd15b12660cdfbe
Jonas <jonas@cultuurnet.be>
2021-09-30 13:39:23 +0200

    III-4232 Add Implementations of VideoAdded Class

---
+++ src/Event/Events/VideoAdded.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Events;
+
+use CultuurNet\UDB3\Offer\Events\AbstractVideoAdded;
+
+class VideoAdded extends AbstractVideoAdded
+{
+
+}
---
+++ src/Place/Events/VideoAdded.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place\Events;
+
+use CultuurNet\UDB3\Offer\Events\AbstractVideoAdded;
+
+class VideoAdded extends AbstractVideoAdded
+{
+
+}
