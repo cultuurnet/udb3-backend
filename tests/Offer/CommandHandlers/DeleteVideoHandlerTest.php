4886b8330990e8f242bc5198500029edffcc7991
Luc Wollants <lucwollants@gmail.com>
2021-10-11 10:35:55 +0200

    Test the new DeleteVideoHandler

---
+++ tests/Offer/CommandHandlers/DeleteVideoHandlerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Silex\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventStore\EventStore;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType;
+use CultuurNet\UDB3\Event\EventRepository;
+use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\Events\VideoAdded;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\Video;
+use CultuurNet\UDB3\Model\ValueObject\Web\Url;
+use CultuurNet\UDB3\Offer\CommandHandlers\DeleteVideoHandler;
+use CultuurNet\UDB3\Offer\Commands\Video\DeleteVideo;
+use CultuurNet\UDB3\Offer\OfferRepository;
+use CultuurNet\UDB3\Place\PlaceRepository;
+use CultuurNet\UDB3\Title;
+
+final class DeleteVideoHandlerTest extends CommandHandlerScenarioTestCase
+{
+    protected function createCommandHandler(
+        EventStore $eventStore,
+        EventBus   $eventBus
+    ): CommandHandler {
+        $repository = new OfferRepository(
+            new EventRepository($eventStore, $eventBus),
+            new PlaceRepository($eventStore, $eventBus)
+        );
+
+        return new DeleteVideoHandler($repository);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_deleting_a_video_from_an_event(): void
+    {
+        $eventId = new UUID('208dbe98-ffaa-41cb-9ada-7ec8e0651f48');
+
+        $videoId = new UUID('91c75325-3830-4000-b580-5778b2de4548');
+        $video = new Video(
+            $videoId,
+            new Url('https://www.youtube.com/watch?v=123')
+        );
+
+        $this->scenario
+            ->withAggregateId($eventId->toString())
+            ->given([
+                $this->getEventCreated($eventId),
+                new VideoAdded($eventId, $video),
+            ])
+            ->when(new DeleteVideo($eventId, $videoId))
+            ->then([
+                new VideoAdded($eventId, $video),
+            ]);
+    }
+
+    private function getEventCreated(UUID $eventId): EventCreated
+    {
+        return new EventCreated(
+            $eventId->toString(),
+            new Language('nl'),
+            new Title('some representative title'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(CalendarType::PERMANENT())
+        );
+    }
+}
