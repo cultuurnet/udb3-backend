07e4d0cb41f664cb4c6684cdbff45ea390953593
Luc Wollants <lucwollants@gmail.com>
2021-10-11 10:35:26 +0200

    Create the DeleteVideo command for offer

---
+++ src/Offer/Commands/Video/DeleteVideo.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\Commands\Video;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\AuthorizableCommand;
+
+final class DeleteVideo implements AuthorizableCommand
+{
+    private UUID $offerId;
+
+    private UUID $videoId;
+
+    public function __construct(UUID $offerId, UUID $videoId)
+    {
+        $this->offerId = $offerId;
+        $this->videoId = $videoId;
+    }
+
+    public function getVideoId(): UUID
+    {
+        return $this->videoId;
+    }
+
+    public function getItemId(): string
+    {
+        return $this->offerId->toString();
+    }
+
+    public function getPermission(): Permission
+    {
+        return Permission::AANBOD_BEWERKEN();
+    }
+}
