7fb8ebdafb6bbba7925b7e5dd50d6b5c1216bdcb
Luc Wollants <lucwollants@gmail.com>
2021-10-11 12:09:53 +0200

    Add the DeleteVideoRequestHandler

---
+++ src/Http/Offer/DeleteVideoRequestHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Offer\Commands\Video\DeleteVideo;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class DeleteVideoRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+
+        $offerId = $routeParameters->get('offerId');
+        $videoId = $routeParameters->get('videoId');
+
+        $this->commandBus->dispatch(new DeleteVideo(new UUID($offerId), new UUID($videoId)));
+
+        return new NoContentResponse();
+    }
+}
