12a8277323437624ce23f4835712760721773656
Bert Ramakers <bertramakers@me.com>
2021-10-11 15:09:44 +0200

    Remove unused import

--- src/Http/Offer/GetCalendarSummaryRequestHandler.php
+++ src/Http/Offer/GetCalendarSummaryRequestHandler.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Offer;
 
-use CultuurNet\CalendarSummaryV3\CalendarFormatterInterface;
 use CultuurNet\CalendarSummaryV3\CalendarHTMLFormatter;
 use CultuurNet\CalendarSummaryV3\CalendarPlainTextFormatter;
 use CultuurNet\CalendarSummaryV3\Offer\Offer;
