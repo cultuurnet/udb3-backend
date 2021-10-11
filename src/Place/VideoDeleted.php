be1431418546cef5effc7ade6eda4cd6f11310cc
Luc Wollants <lucwollants@gmail.com>
2021-10-11 09:35:01 +0200

    Create the concrete classes for the VideoDeleted event

---
+++ src/Place/VideoDeleted.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place;
+
+use CultuurNet\UDB3\Offer\Events\AbstractVideoDeleted;
+
+final class VideoDeleted extends AbstractVideoDeleted
+{
+}
