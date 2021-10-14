2fab98895ca6f12ae249d7a826e05b50e05bbaf1
Luc Wollants <lucwollants@gmail.com>
2021-10-14 13:16:55 +0200

    Create new VideoUpdated event as simple extensions on AbstractVideoEvent

---
+++ src/Event/Events/VideoUpdated.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Events;
+
+use CultuurNet\UDB3\Offer\Events\AbstractVideoEvent;
+
+final class VideoUpdated extends AbstractVideoEvent
+{
+}
