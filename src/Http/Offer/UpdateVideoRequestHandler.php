75db3b73b0b7390a2ccee1736fbfaaad56a548d3
Jonas <jonas@cultuurnet.be>
2021-10-18 17:13:00 +0200

    III-4242 create to UpdateVideoVideoRequestHandler

---
+++ src/Http/Offer/UpdateVideoRequestHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Offer\Commands\Video\UpdateVideos;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class UpdateVideoRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+    private RequestBodyParser $parser;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+
+        $this->parser = RequestBodyParserFactory::createBaseParser(
+            new JsonSchemaValidatingRequestBodyParser(JsonSchemaLocator::OFFER_VIDEOS_PATCH),
+        );
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $offerId = $routeParameters->getOfferId();
+
+
+        // $this->commandBus->dispatch(new UpdateVideos($offerId, $video));
+
+        return new NoContentResponse();
+    }
+}
