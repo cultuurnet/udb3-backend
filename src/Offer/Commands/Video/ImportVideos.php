46769f80f07f7f4f9df88b7be3e4390aed6d72f7
Luc Wollants <lucwollants@gmail.com>
2021-10-19 09:33:22 +0200

    Removed newline

--- src/Offer/Commands/Video/ImportVideos.php
+++ src/Offer/Commands/Video/ImportVideos.php
@@ -24,8 +24,7 @@ final class ImportVideos implements AuthorizableCommand
     {
         return $this->videos;
     }
-
-
+    
     public function getItemId(): string
     {
         return $this->offerId;
