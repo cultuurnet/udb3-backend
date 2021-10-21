058ee98c6c914dd0efae340668046166f45eb257
Luc Wollants <lucwollants@gmail.com>
2021-10-21 14:10:17 +0200

    Use the UpdateVideosDenormalizer inside the UpdateVideosRequestHandler

--- src/Http/Offer/UpdateVideosRequestHandler.php
+++ src/Http/Offer/UpdateVideosRequestHandler.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Http\Offer;
 
 use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
-use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Offer\Commands\Video\UpdateVideoDenormalizer;
 use CultuurNet\UDB3\Offer\Commands\Video\UpdateVideos;
+use CultuurNet\UDB3\Offer\Commands\Video\UpdateVideosDenormalizer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -19,15 +21,10 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class UpdateVideosRequestHandler implements RequestHandlerInterface
 {
     private CommandBus $commandBus;
-    private RequestBodyParser $parser;
 
     public function __construct(CommandBus $commandBus)
     {
         $this->commandBus = $commandBus;
-
-        $this->parser = RequestBodyParserFactory::createBaseParser(
-            new JsonSchemaValidatingRequestBodyParser(JsonSchemaLocator::OFFER_VIDEOS_PATCH),
-        );
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -35,8 +32,22 @@ final class UpdateVideosRequestHandler implements RequestHandlerInterface
         $routeParameters = new RouteParameters($request);
         $offerId = $routeParameters->getOfferId();
 
+        $parser = RequestBodyParserFactory::createBaseParser(
+            new JsonSchemaValidatingRequestBodyParser(JsonSchemaLocator::OFFER_VIDEOS_PATCH),
+            new DenormalizingRequestBodyParser(
+                new UpdateVideosDenormalizer(
+                    new UpdateVideoDenormalizer($offerId)
+                ),
+                UpdateVideos::class
+            )
+        );
 
-        // $this->commandBus->dispatch(new UpdateVideos($offerId, $video));
+        /** @var UpdateVideos $updateVideos */
+        $updateVideos = $parser->parse($request)->getParsedBody();
+
+        foreach ($updateVideos as $updateVideo) {
+            $this->commandBus->dispatch($updateVideo);
+        }
 
         return new NoContentResponse();
     }
