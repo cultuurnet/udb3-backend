a8858e2cba5a03dba629471a5a50376c21f047e3
Bert Ramakers <bertramakers@me.com>
2021-10-25 18:09:43 +0200

    Mark ThemeUpdated for Place as deprecated and make it extend PlaceEvent

--- src/Place/Events/ThemeUpdated.php
+++ src/Place/Events/ThemeUpdated.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Place\Events;
 
-use CultuurNet\UDB3\Offer\Events\AbstractThemeUpdated;
+use CultuurNet\UDB3\Place\PlaceEvent;
 
-final class ThemeUpdated extends AbstractThemeUpdated
+/**
+ * @deprecated Only here to make sure an event stream can be loaded.
+ * Places should never have a theme but this was incorrectly provided as a possibility in the past.
+ */
+final class ThemeUpdated extends PlaceEvent
 {
+    public static function deserialize(array $data): ThemeUpdated
+    {
+        return new self($data['item_id']);
+    }
 }
