43549e6c286819c0a3552480566bb1d3e07af99c
Bert Ramakers <bertramakers@me.com>
2021-10-25 18:10:53 +0200

    Move contents of AbstractThemeUpdated to ThemeUpdated (Event)

--- src/Event/Events/ThemeUpdated.php
+++ src/Event/Events/ThemeUpdated.php
@@ -5,7 +5,41 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Event\Events;
 
 use CultuurNet\UDB3\Offer\Events\AbstractThemeUpdated;
+use CultuurNet\UDB3\Theme;
 
 final class ThemeUpdated extends AbstractThemeUpdated
 {
+    /**
+     * @var Theme
+     */
+    protected $theme;
+
+    /**
+     * @param string $itemId
+     */
+    final public function __construct($itemId, Theme $theme)
+    {
+        parent::__construct($itemId);
+        $this->theme = $theme;
+    }
+
+    public function getTheme(): Theme
+    {
+        return $this->theme;
+    }
+
+    public function serialize(): array
+    {
+        return parent::serialize() + [
+                'theme' => $this->theme->serialize(),
+            ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deserialize(array $data)
+    {
+        return new static($data['item_id'], Theme::deserialize($data['theme']));
+    }
 }
