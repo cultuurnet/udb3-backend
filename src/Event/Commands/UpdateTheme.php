5e74c9c90bf66fb2488538822c368fb3a492f18a
Bert Ramakers <bertramakers@me.com>
2021-10-26 09:56:50 +0200

    Remove AbstractUpdateTheme

--- src/Event/Commands/UpdateTheme.php
+++ src/Event/Commands/UpdateTheme.php
@@ -4,8 +4,30 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Event\Commands;
 
-use CultuurNet\UDB3\Offer\Commands\AbstractUpdateTheme;
+use CultuurNet\UDB3\Offer\Commands\AbstractCommand;
+use CultuurNet\UDB3\Theme;
 
-class UpdateTheme extends AbstractUpdateTheme
+class UpdateTheme extends AbstractCommand
 {
+    /**
+     * @var Theme
+     */
+    protected $theme;
+
+    /**
+     * @param string $itemId
+     */
+    public function __construct($itemId, Theme $theme)
+    {
+        parent::__construct($itemId);
+        $this->theme = $theme;
+    }
+
+    /**
+     * @return Theme
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
 }
