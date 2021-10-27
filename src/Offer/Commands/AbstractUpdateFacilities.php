859351104b319308e170d631cc660b59cb57ed76
Bert Ramakers <bertramakers@me.com>
2021-10-27 16:45:51 +0200

    Make AbstractUpdateFacilities concrete

--- src/Offer/Commands/AbstractUpdateFacilities.php
+++ src/Offer/Commands/AbstractUpdateFacilities.php
@@ -6,7 +6,7 @@ namespace CultuurNet\UDB3\Offer\Commands;
 
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
 
-abstract class AbstractUpdateFacilities extends AbstractCommand
+class AbstractUpdateFacilities extends AbstractCommand
 {
     /**
      * Facilities to be added.
