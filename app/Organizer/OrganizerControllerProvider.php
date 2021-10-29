cd450c6296145ac5dd04cea7930a7869e49d2d38
Luc Wollants <lucwollants@gmail.com>
2021-10-29 11:44:51 +0200

    Bootstrap the command handlers correctly

--- app/Organizer/OrganizerControllerProvider.php
+++ app/Organizer/OrganizerControllerProvider.php
@@ -6,16 +6,10 @@ namespace CultuurNet\UDB3\Silex\Organizer;
 
 use CultuurNet\UDB3\Http\Organizer\UpdateAddressRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\UpdateTitleRequestHandler;
-use CultuurNet\UDB3\Organizer\CommandHandler\AddLabelHandler;
-use CultuurNet\UDB3\Organizer\CommandHandler\ImportLabelsHandler;
-use CultuurNet\UDB3\Organizer\CommandHandler\RemoveLabelHandler;
-use CultuurNet\UDB3\Organizer\CommandHandler\UpdateAddressHandler;
-use CultuurNet\UDB3\Organizer\CommandHandler\UpdateTitleHandler;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
 use CultuurNet\UDB3\Http\Offer\OfferPermissionsController;
 use CultuurNet\UDB3\Http\Organizer\EditOrganizerRestController;
 use CultuurNet\UDB3\Http\Organizer\ReadOrganizerRestController;
-use CultuurNet\UDB3\Silex\Labels\LabelServiceProvider;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
@@ -104,48 +98,12 @@ class OrganizerControllerProvider implements ControllerProviderInterface, Servic
             }
         );
 
-        $app[AddLabelHandler::class] = $app->share(
-            function (Application $app) {
-                return new AddLabelHandler(
-                    $app['organizer_repository'],
-                    $app[LabelServiceProvider::JSON_READ_REPOSITORY],
-                    $app['labels.constraint_aware_service']
-                );
-            }
-        );
-
-        $app[RemoveLabelHandler::class] = $app->share(
-            function (Application $app) {
-                return new RemoveLabelHandler(
-                    $app['organizer_repository'],
-                    $app[LabelServiceProvider::JSON_READ_REPOSITORY]
-                );
-            }
-        );
-
-        $app[ImportLabelsHandler::class] = $app->share(
-            function (Application $app) {
-                return new ImportLabelsHandler(
-                    $app['organizer_repository'],
-                    $app['labels.constraint_aware_service']
-                );
-            }
-        );
-
         $app[UpdateTitleRequestHandler::class] = $app->share(
             fn (Application $application) => new UpdateTitleRequestHandler($app['event_command_bus'])
         );
 
         $app[UpdateAddressRequestHandler::class] = $app->share(
             fn (Application $application) => new UpdateAddressRequestHandler($app['event_command_bus'])
-        );
-
-        $app[UpdateTitleHandler::class] = $app->share(
-            fn (Application $application) => new UpdateTitleHandler($app['organizer_repository'])
-        );
-
-        $app[UpdateAddressHandler::class] = $app->share(
-            fn (Application $application) => new UpdateAddressHandler($app['organizer_repository'])
         );
     }
 
