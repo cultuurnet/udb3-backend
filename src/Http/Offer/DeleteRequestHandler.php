867ac37a9dc286d86fa7cec6e28f01ea65fd6ca2
Bert Ramakers <bertramakers@me.com>
2021-11-04 16:14:46 +0100

    Add request handler to delete events/places

---
+++ src/Http/Offer/DeleteRequestHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\CommandBus;
+use Broadway\Repository\AggregateNotFoundException;
+use CultuurNet\UDB3\Event\Commands\DeleteEvent;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Offer\Commands\AbstractDeleteOffer;
+use CultuurNet\UDB3\Offer\OfferType;
+use CultuurNet\UDB3\Place\Commands\DeletePlace;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
+
+final class DeleteRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $offerType = $routeParameters->getOfferType();
+        $offerId = $routeParameters->getOfferId();
+
+        $command = $this->createDeleteCommand($offerType, $offerId);
+
+        $this->commandBus->dispatch($command);
+
+        return new NoContentResponse();
+    }
+
+    private function createDeleteCommand(OfferType $offerType, string $offerId): AbstractDeleteOffer
+    {
+        if ($offerType->sameValueAs(OfferType::EVENT())) {
+            return new DeleteEvent($offerId);
+        }
+        if ($offerType->sameValueAs(OfferType::PLACE())) {
+            return new DeletePlace($offerId);
+        }
+        throw new RuntimeException('No AbstractDeleteOffer implementation found for unknown offer type ' . $offerType->toNative());
+    }
+}
---
+++ tests/Http/Offer/DeleteRequestHandlerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use CultuurNet\UDB3\Event\Commands\DeleteEvent;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Place\Commands\DeletePlace;
+use PHPUnit\Framework\TestCase;
+
+class DeleteRequestHandlerTest extends TestCase
+{
+    private TraceableCommandBus $commandBus;
+    private DeleteRequestHandler $deleteRequestHandler;
+
+    protected function setUp(): void
+    {
+        $this->commandBus = new TraceableCommandBus();
+        $this->deleteRequestHandler = new DeleteRequestHandler($this->commandBus);
+        $this->commandBus->record();
+    }
+
+    /**
+     * @test
+     */
+    public function it_dispatches_a_delete_command_for_events(): void
+    {
+        $eventId = '9a79a8f4-3602-4852-91b9-9132167837f7';
+
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('offerType', 'events')
+            ->withRouteParameter('offerId', $eventId)
+            ->build('DELETE');
+
+        $expectedCommand = new DeleteEvent($eventId);
+
+        $this->deleteRequestHandler->handle($request);
+
+        $this->assertEquals([$expectedCommand], $this->commandBus->getRecordedCommands());
+    }
+
+    /**
+     * @test
+     */
+    public function it_dispatches_a_delete_command_for_places(): void
+    {
+        $placeId = '9a79a8f4-3602-4852-91b9-9132167837f7';
+
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('offerType', 'places')
+            ->withRouteParameter('offerId', $placeId)
+            ->build('DELETE');
+
+        $expectedCommand = new DeletePlace($placeId);
+
+        $this->deleteRequestHandler->handle($request);
+
+        $this->assertEquals([$expectedCommand], $this->commandBus->getRecordedCommands());
+    }
+}
