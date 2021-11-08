8c104110e48375b5d845b8424be8d151e36cb07a
Luc Wollants <lucwollants@gmail.com>
2021-11-08 15:20:14 +0100

    Add new UpdateAudienceHandler

---
+++ src/Event/CommandHandlers/UpdateAudienceHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Event\Commands\UpdateAudience;
+use CultuurNet\UDB3\Event\Event;
+use CultuurNet\UDB3\Event\EventRepository;
+
+final class UpdateAudienceHandler implements CommandHandler
+{
+    private EventRepository $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
+    public function handle($command): void
+    {
+        if (!$command instanceof UpdateAudience) {
+            return;
+        }
+
+        /** @var Event $event */
+        $event = $this->eventRepository->load($command->getItemId());
+
+        $event->updateAudience($command->getAudience());
+
+        $this->eventRepository->save($event);
+    }
+}
