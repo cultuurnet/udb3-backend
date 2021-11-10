1081a92b7960f7dcd26b7b2e040fc14436196fc0
Luc Wollants <lucwollants@gmail.com>
2021-11-10 09:57:48 +0100

    Linting

--- bin/udb3.php
+++ bin/udb3.php
@@ -121,10 +121,10 @@ $consoleApp->add(
 );
 $consoleApp->add(
     new ReindexEventsWithRecommendations(
-    $app['dbal_connection'],
-    $app['amqp.publisher'],
-    $app[EventJSONLDServiceProvider::JSONLD_PROJECTED_EVENT_FACTORY]
-)
+        $app['dbal_connection'],
+        $app['amqp.publisher'],
+        $app[EventJSONLDServiceProvider::JSONLD_PROJECTED_EVENT_FACTORY]
+    )
 );
 $consoleApp->add(new UpdateOfferStatusCommand(OfferType::EVENT(), $app['event_command_bus'], $app[Sapi3SearchServiceProvider::SEARCH_SERVICE_EVENTS]));
 $consoleApp->add(new UpdateOfferStatusCommand(OfferType::PLACE(), $app['event_command_bus'], $app[Sapi3SearchServiceProvider::SEARCH_SERVICE_PLACES]));
