0c89a8b08297aac1ee5711222d25c1d207df4491
Luc Wollants <lucwollants@gmail.com>
2021-11-15 15:48:03 +0100

    Add new `AvailableFromUpdated` event

---
+++ src/Offer/Events/AbstractAvailableFromUpdated.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\Events;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+abstract class AbstractAvailableFromUpdated extends AbstractEvent
+{
+    private DateTimeInterface $availableFrom;
+
+    final public function __construct(string $itemId, DateTimeInterface $availableFrom)
+    {
+        parent::__construct($itemId);
+
+        $this->availableFrom = $availableFrom;
+    }
+
+    public function getAvailableFrom(): DateTimeInterface
+    {
+        return $this->availableFrom;
+    }
+
+    public function serialize(): array
+    {
+        return parent::serialize() + [
+                'available_from' => $this->availableFrom->format(DateTimeInterface::ATOM),
+            ];
+    }
+
+    public static function deserialize(array $data): AbstractAvailableFromUpdated
+    {
+        return new static(
+            $data['item_id'],
+            DateTimeImmutable::createFromFormat(DateTimeInterface::ATOM, $data['available_from'])
+        );
+    }
+}
