ac24f323fadcdd6a2876d2d3154f398f1880e6a5
Luc Wollants <lucwollants@gmail.com>
2021-11-18 12:54:54 +0100

    Bootstrap RemoveThemeHandler

--- app/Event/EventCommandHandlerProvider.php
+++ app/Event/EventCommandHandlerProvider.php
@@ -12,6 +12,10 @@ final class EventCommandHandlerProvider implements ServiceProviderInterface
 {
     public function register(Application $app): void
     {
+        $app[RemoveThemeHandler::class] = $app->share(
+            fn (Application $application) => new RemoveThemeHandler($app['event_repository'])
+        );
+
         $app[UpdateAudienceHandler::class] = $app->share(
             fn (Application $application) => new UpdateAudienceHandler($app['event_repository'])
         );
