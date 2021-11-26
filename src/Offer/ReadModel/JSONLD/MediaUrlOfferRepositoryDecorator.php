f5695d47e5ba605f026976885344e53091199745
Luc Wollants <lucwollants@gmail.com>
2021-11-26 12:08:40 +0100

    Refactor mapping code to be reusable

--- src/Offer/ReadModel/JSONLD/MediaUrlOfferRepositoryDecorator.php
+++ src/Offer/ReadModel/JSONLD/MediaUrlOfferRepositoryDecorator.php
@@ -25,32 +25,61 @@ final class MediaUrlOfferRepositoryDecorator extends DocumentRepositoryDecorator
 
         return $document->applyAssoc(
             function (array $json) {
-                if (!isset($json['mediaObject']) || !is_array($json['mediaObject'])) {
-                    return $json;
-                }
-
-                $json['mediaObject'] = array_map(
-                    function ($mediaObject) {
-                        if (!is_array($mediaObject) ||
-                            !isset($mediaObject['contentUrl'], $mediaObject['thumbnailUrl'])) {
-                            return $mediaObject;
-                        }
-
-                        $mediaObject['contentUrl'] = $this->mediaUrlMapping->getUpdatedUrl($mediaObject['contentUrl']);
-                        $mediaObject['thumbnailUrl'] = $this->mediaUrlMapping->getUpdatedUrl($mediaObject['thumbnailUrl']);
-
-                        return $mediaObject;
-                    },
-                    $json['mediaObject']
-                );
-
-                if (!isset($json['image']) || !is_string($json['image'])) {
-                    return $json;
-                }
-                $json['image'] = $this->mediaUrlMapping->getUpdatedUrl($json['image']);
-
-                return $json;
+                $json = $this->mapMediaUrls($json);
+                return $this->mapMediaUrlsEmbeddedPlace($json);
             }
         );
+    }
+
+    private function mapMediaUrls(array $json): array
+    {
+        $json = $this->mapMediaObject($json);
+        return $this->mapImage($json);
+    }
+
+    private function mapMediaUrlsEmbeddedPlace(array $json): array
+    {
+        if (!isset($json['location'])) {
+            return $json;
+        }
+
+        $json['location'] = $this->mapMediaObject($json['location']);
+        $json['location'] = $this->mapImage($json['location']);
+        return $json;
+    }
+
+    private function mapMediaObject(array $json): array
+    {
+        if (!isset($json['mediaObject']) || !is_array($json['mediaObject'])) {
+            return $json;
+        }
+
+        $json['mediaObject'] = array_map(
+            function ($mediaObject) {
+                if (!is_array($mediaObject) ||
+                    !isset($mediaObject['contentUrl'], $mediaObject['thumbnailUrl'])) {
+                    return $mediaObject;
+                }
+
+                $mediaObject['contentUrl'] = $this->mediaUrlMapping->getUpdatedUrl($mediaObject['contentUrl']);
+                $mediaObject['thumbnailUrl'] = $this->mediaUrlMapping->getUpdatedUrl($mediaObject['thumbnailUrl']);
+
+                return $mediaObject;
+            },
+            $json['mediaObject']
+        );
+
+        return $json;
+    }
+
+    private function mapImage(array $json): array
+    {
+        if (!isset($json['image']) || !is_string($json['image'])) {
+            return $json;
+        }
+
+        $json['image'] = $this->mediaUrlMapping->getUpdatedUrl($json['image']);
+
+        return $json;
     }
 }
