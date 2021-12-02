7f146f7dd4159407eb9ec5a58bf6ee491d227694
Luc Wollants <lucwollants@gmail.com>
2021-12-02 10:37:12 +0100

    Add new exception for when news article was not found with the given id

---
+++ src/Curators/NewsArticleNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Curators;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use Exception;
+
+final class NewsArticleNotFoundException extends Exception
+{
+    public function __construct(UUID $id)
+    {
+        parent::__construct('News article with id "' . $id->toString() . '" was not found.');
+    }
+}
