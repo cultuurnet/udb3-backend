7c23fc27335f860cc3f05be7a84e3cc5c847a465
Luc Wollants <lucwollants@gmail.com>
2021-12-02 10:33:10 +0100

    Create a value object for NewsArticle

---
+++ src/Curators/NewsArticle.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Curators;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Model\ValueObject\Web\Url;
+
+final class NewsArticle
+{
+    private UUID $id;
+
+    private string $headline;
+
+    private Language $language;
+
+    private string $text;
+
+    private string $about;
+
+    private string $publisher;
+
+    private Url $url;
+
+    private Url $publisherLogo;
+
+    public function __construct(
+        UUID $id,
+        string $headline,
+        Language $language,
+        string $text,
+        string $about,
+        string $publisher,
+        Url $url,
+        Url $publisherLogo
+    ) {
+        $this->id = $id;
+        $this->headline = $headline;
+        $this->language = $language;
+        $this->text = $text;
+        $this->about = $about;
+        $this->publisher = $publisher;
+        $this->url = $url;
+        $this->publisherLogo = $publisherLogo;
+    }
+
+    public function getId(): UUID
+    {
+        return $this->id;
+    }
+
+    public function getHeadline(): string
+    {
+        return $this->headline;
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getAbout(): string
+    {
+        return $this->about;
+    }
+
+    public function getPublisher(): string
+    {
+        return $this->publisher;
+    }
+
+    public function getUrl(): Url
+    {
+        return $this->url;
+    }
+
+    public function getPublisherLogo(): Url
+    {
+        return $this->publisherLogo;
+    }
+}
