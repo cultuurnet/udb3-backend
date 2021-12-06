8797a89f34bafa0f94ec82ce8acd8f23c648b77e
Luc Wollants <lucwollants@gmail.com>
2021-12-06 16:56:57 +0100

    Add IsStringArray trait

--- src/Model/ValueObject/Contact/TelephoneNumbers.php
+++ src/Model/ValueObject/Contact/TelephoneNumbers.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\ValueObject\Contact;
 
+use CultuurNet\UDB3\Model\ValueObject\Collection\Behaviour\IsStringArray;
 use CultuurNet\UDB3\Model\ValueObject\Collection\Collection;
 
 class TelephoneNumbers extends Collection
 {
+    use IsStringArray;
+
     /**
      * @param TelephoneNumber[] ...$values
      */
--- src/Model/ValueObject/Web/EmailAddresses.php
+++ src/Model/ValueObject/Web/EmailAddresses.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\ValueObject\Web;
 
+use CultuurNet\UDB3\Model\ValueObject\Collection\Behaviour\IsStringArray;
 use CultuurNet\UDB3\Model\ValueObject\Collection\Collection;
 
 class EmailAddresses extends Collection
 {
+    use IsStringArray;
+
     /**
      * @param EmailAddress[] ...$values
      */
