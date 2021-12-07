2b4856ee938811806e7ef0cbd355b6878fc9e65c
Luc Wollants <lucwollants@gmail.com>
2021-12-07 15:09:08 +0100

    Add new UpdateNewsArticleRequestHandler

---
+++ src/Http/Curators/UpdateNewsArticleRequestHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Curators;
+
+use CultuurNet\UDB3\Curators\NewsArticle;
+use CultuurNet\UDB3\Curators\NewsArticleRepository;
+use CultuurNet\UDB3\Curators\Serializer\NewsArticleDenormalizer;
+use CultuurNet\UDB3\Curators\Serializer\NewsArticleNormalizer;
+use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class UpdateNewsArticleRequestHandler implements RequestHandlerInterface
+{
+    private NewsArticleRepository $newsArticleRepository;
+
+    public function __construct(NewsArticleRepository $newsArticleRepository)
+    {
+        $this->newsArticleRepository = $newsArticleRepository;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $articleId = $routeParameters->get('articleId');
+
+        $requestBodyParser = RequestBodyParserFactory::createBaseParser(
+            new JsonSchemaValidatingRequestBodyParser(JsonSchemaLocator::NEWS_ARTICLE_POST),
+            new DenormalizingRequestBodyParser(
+                new NewsArticleDenormalizer(new UUID($articleId)),
+                NewsArticle::class
+            )
+        );
+
+        /** @var NewsArticle $newsArticle */
+        $newsArticle = $requestBodyParser->parse($request)->getParsedBody();
+
+        $this->newsArticleRepository->update($newsArticle);
+
+        return new JsonResponse((new NewsArticleNormalizer())->normalize($newsArticle));
+    }
+}
---
+++ tests/Http/Curators/UpdateNewsArticleRequestHandlerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Curators;
+
+use CultuurNet\UDB3\Curators\NewsArticle;
+use CultuurNet\UDB3\Curators\NewsArticleRepository;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Model\ValueObject\Web\Url;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class UpdateNewsArticleRequestHandlerTest extends TestCase
+{
+    /** @var NewsArticleRepository|MockObject */
+    private $newsArticleRepository;
+
+    private UpdateNewsArticleRequestHandler $updateNewsArticleRequestHandler;
+
+    private Psr7RequestBuilder $psr7RequestBuilder;
+
+    protected function setUp(): void
+    {
+        $this->newsArticleRepository = $this->createMock(NewsArticleRepository::class);
+
+        $this->updateNewsArticleRequestHandler = new UpdateNewsArticleRequestHandler(
+            $this->newsArticleRepository,
+        );
+
+        $this->psr7RequestBuilder = new Psr7RequestBuilder();
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_a_news_article(): void
+    {
+        $createOrganizerRequest = $this->psr7RequestBuilder
+            ->withRouteParameter('articleId', '6c583739-a848-41ab-b8a3-8f7dab6f8ee1')
+            ->withBodyFromArray([
+                'headline' => 'publiq wint API award',
+                'inLanguage' => 'nl',
+                'text' => 'Op 10 januari 2020 wint publiq de API award',
+                'about' => '17284745-7bcf-461a-aad0-d3ad54880e75',
+                'publisher' => 'BILL',
+                'url' => 'https://www.publiq.be/blog/api-reward',
+                'publisherLogo' => 'https://www.bill.be/img/favicon.png',
+            ])
+            ->build('POST');
+
+        $this->newsArticleRepository->expects($this->once())
+            ->method('update')
+            ->with(new NewsArticle(
+                new UUID('6c583739-a848-41ab-b8a3-8f7dab6f8ee1'),
+                'publiq wint API award',
+                new Language('nl'),
+                'Op 10 januari 2020 wint publiq de API award',
+                '17284745-7bcf-461a-aad0-d3ad54880e75',
+                'BILL',
+                new Url('https://www.publiq.be/blog/api-reward'),
+                new Url('https://www.bill.be/img/favicon.png')
+            ));
+
+        $response = $this->updateNewsArticleRequestHandler->handle($createOrganizerRequest);
+
+        $this->assertEquals(
+            Json::encode([
+                'id' => '6c583739-a848-41ab-b8a3-8f7dab6f8ee1',
+                'headline' => 'publiq wint API award',
+                'inLanguage' => 'nl',
+                'text' => 'Op 10 januari 2020 wint publiq de API award',
+                'about' => '17284745-7bcf-461a-aad0-d3ad54880e75',
+                'publisher' => 'BILL',
+                'url' => 'https://www.publiq.be/blog/api-reward',
+                'publisherLogo' => 'https://www.bill.be/img/favicon.png',
+            ]),
+            $response->getBody()->getContents()
+        );
+    }
+}
