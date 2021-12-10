1b3878b67a1dc76762f68c0a72f4f908589e1409
Jonas <jonas@cultuurnet.be>
2021-12-10 11:02:53 +0100

    Refactor Organizer after LabelAwareAggregateRoot.

--- src/Offer/CommandHandlers/RemoveLabelHandler.php
+++ src/Offer/CommandHandlers/RemoveLabelHandler.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Offer\CommandHandlers;
 
 use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\LabelName;
 use CultuurNet\UDB3\Offer\Commands\RemoveLabel;
 use CultuurNet\UDB3\Offer\OfferRepository;
 
@@ -27,7 +29,8 @@ final class RemoveLabelHandler implements CommandHandler
         }
 
         $offer = $this->offerRepository->load($command->getItemId());
-        $offer->removeLabel($command->getLabel());
+        $label = new Label(new LabelName($command->getLabel()->getName()->toNative()), $command->getLabel()->isVisible());
+        $offer->removeLabel($label);
         $this->offerRepository->save($offer);
     }
 }
