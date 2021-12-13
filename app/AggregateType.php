e4e073b05519cb090521268a884cc7c7ee30e2c2
Luc Wollants <lucwollants@gmail.com>
2021-12-13 15:15:29 +0100

    Let AggregateType derive from CultuurNet\UDB3\Model\ValueObject\String\Enum

--- app/AggregateType.php
+++ app/AggregateType.php
@@ -4,24 +4,29 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex;
 
-use ValueObjects\Enum\Enum;
+use CultuurNet\UDB3\Model\ValueObject\String\Enum;
 
 /**
- * @method static AggregateType EVENT()
- * @method static AggregateType PLACE()
- * @method static AggregateType VARIATION()
- * @method static AggregateType ORGANIZER()
- * @method static AggregateType MEDIA_OBJECT()
- * @method static AggregateType ROLE()
- * @method static AggregateType LABEL()
+ * @method static AggregateType event()
+ * @method static AggregateType place()
+ * @method static AggregateType variation()
+ * @method static AggregateType organizer()
+ * @method static AggregateType media_object()
+ * @method static AggregateType role()
+ * @method static AggregateType label()
  */
 class AggregateType extends Enum
 {
-    public const EVENT = 'event';
-    public const PLACE = 'place';
-    public const VARIATION = 'variation';
-    public const ORGANIZER = 'organizer';
-    public const MEDIA_OBJECT = 'media_object';
-    public const ROLE = 'role';
-    public const LABEL = 'label';
+    public static function getAllowedValues(): array
+    {
+        return [
+            'event',
+            'place',
+            'variation',
+            'organizer',
+            'media_object',
+            'role',
+            'label',
+        ];
+    }
 }
