a72b0829e509727d9bfafd5f9a4a0bdfb14d627b
Luc Wollants <lucwollants@gmail.com>
2021-12-14 11:26:52 +0100

    Refactor CommandType to derive from Enum from Model namespace

--- src/Http/Label/CommandType.php
+++ src/Http/Label/CommandType.php
@@ -4,36 +4,37 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Label;
 
-use ValueObjects\Enum\Enum;
+use CultuurNet\UDB3\Model\ValueObject\String\Enum;
 
-/**
- * Class CommandType
- * @package CultuurNet\UDB3\Http\Label\Helper
- */
 class CommandType extends Enum
 {
-    public const MAKE_VISIBLE = 'MakeVisible';
-    public const MAKE_INVISIBLE = 'MakeInvisible';
-    public const MAKE_PUBLIC = 'MakePublic';
-    public const MAKE_PRIVATE = 'MakePrivate';
+    public static function getAllowedValues(): array
+    {
+        return [
+            'MakeVisible',
+            'MakeInvisible',
+            'MakePublic',
+            'MakePrivate',
+        ];
+    }
 
     public static function makeVisible(): self
     {
-        return self::fromNative(self::MAKE_VISIBLE);
+        return new CommandType('MakeVisible');
     }
 
     public static function makeInvisible(): self
     {
-        return self::fromNative(self::MAKE_INVISIBLE);
+        return new CommandType('MakeInvisible');
     }
 
     public static function makePublic(): self
     {
-        return self::fromNative(self::MAKE_PUBLIC);
+        return new CommandType('MakePublic');
     }
 
     public static function makePrivate(): self
     {
-        return self::fromNative(self::MAKE_PRIVATE);
+        return new CommandType('MakePrivate');
     }
 }
