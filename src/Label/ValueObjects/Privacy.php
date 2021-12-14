eb1b6875d8a5f19f69e15ecdf0fa1fcdb2e2f457
Jonas <jonas@cultuurnet.be>
2021-12-14 12:39:17 +0100

    Change Enum import on Privacy

--- src/Label/ValueObjects/Privacy.php
+++ src/Label/ValueObjects/Privacy.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Label\ValueObjects;
 
-use ValueObjects\Enum\Enum;
+//use ValueObjects\Enum\Enum;
+use CultuurNet\UDB3\Model\ValueObject\String\Enum;
 
 /**
  * Class Privacy
