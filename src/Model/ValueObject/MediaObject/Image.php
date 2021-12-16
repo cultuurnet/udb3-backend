a5d1a1ef8b98e3c5b86622672d1ad6d93c79d52d
Luc Wollants <lucwollants@gmail.com>
2021-12-16 15:13:18 +0100

    Add new VO class for Image

---
+++ src/Model/ValueObject/MediaObject/Image.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Model\ValueObject\MediaObject;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Text\Description;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+
+final class Image
+{
+    private UUID $id;
+
+    private Language $language;
+
+    private Description $description;
+
+    private CopyrightHolder $copyrightHolder;
+
+    public function __construct(
+        UUID $id,
+        Language $language,
+        Description $description,
+        CopyrightHolder $copyrightHolder
+    ) {
+        $this->id = $id;
+        $this->language = $language;
+        $this->description = $description;
+        $this->copyrightHolder = $copyrightHolder;
+    }
+
+    public function getId(): UUID
+    {
+        return $this->id;
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+
+    public function getDescription(): Description
+    {
+        return $this->description;
+    }
+
+    public function getCopyrightHolder(): CopyrightHolder
+    {
+        return $this->copyrightHolder;
+    }
+}
