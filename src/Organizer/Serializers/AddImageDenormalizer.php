4e8ee0de20264bc11a940c80ad583747a9cc7ee7
Luc Wollants <lucwollants@gmail.com>
2021-12-17 10:22:06 +0100

    Add denormalizer for AddImage command

---
+++ src/Organizer/Serializers/AddImageDenormalizer.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Organizer\Serializers;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\Image;
+use CultuurNet\UDB3\Model\ValueObject\Text\Description;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Organizer\Commands\AddImage;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+final class AddImageDenormalizer implements DenormalizerInterface
+{
+    private string $organizerId;
+
+    public function __construct(string $organizerId)
+    {
+        $this->organizerId = $organizerId;
+    }
+
+    public function denormalize($data, $type, $format = null, array $context = []): AddImage
+    {
+        return new AddImage(
+            $this->organizerId,
+            new Image(
+                new UUID($data['id']),
+                new Language($data['language']),
+                new Description($data['description']),
+                new CopyrightHolder($data['copyrightHolder'])
+            )
+        );
+    }
+
+    public function supportsDenormalization($data, $type, $format = null): bool
+    {
+        return $type === AddImage::class;
+    }
+}
