47c335eafb7bfc9988ae393010259b254240db56
Luc Wollants <lucwollants@gmail.com>
2021-12-21 09:50:25 +0100

    Create AbstractImageEvent for reuse with add and upate

---
+++ src/Organizer/Events/AbstractImageEvent.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Organizer\Events;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\Image;
+use CultuurNet\UDB3\Model\ValueObject\Text\Description;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+
+abstract class AbstractImageEvent
+{
+    private string $organizerId;
+
+    private string $imageId;
+
+    private string $language;
+
+    private string $description;
+
+    private string $copyrightHolder;
+
+    final public function __construct(
+        string $organizerId,
+        string $imageId,
+        string $language,
+        string $description,
+        string $copyrightHolder
+    ) {
+        $this->organizerId = $organizerId;
+        $this->imageId = $imageId;
+        $this->language = $language;
+        $this->description = $description;
+        $this->copyrightHolder = $copyrightHolder;
+    }
+
+    public function getOrganizerId(): string
+    {
+        return $this->organizerId;
+    }
+
+    public function getImageId(): string
+    {
+        return $this->imageId;
+    }
+
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getCopyrightHolder(): string
+    {
+        return $this->copyrightHolder;
+    }
+
+    public function getImage(): Image
+    {
+        return new Image(
+            new UUID($this->imageId),
+            new Language($this->language),
+            new Description($this->description),
+            new CopyrightHolder($this->copyrightHolder)
+        );
+    }
+
+    public function serialize(): array
+    {
+        return [
+            'organizerId' => $this->organizerId,
+            'imageId' => $this->imageId,
+            'language' => $this->language,
+            'description' => $this->description,
+            'copyrightHolder' => $this->copyrightHolder,
+        ];
+    }
+
+    public static function deserialize(array $data): AbstractImageEvent
+    {
+        return new static(
+            $data['organizerId'],
+            $data['imageId'],
+            $data['language'],
+            $data['description'],
+            $data['copyrightHolder']
+        );
+    }
+}
