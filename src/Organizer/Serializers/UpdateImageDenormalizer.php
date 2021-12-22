9022319bbae5f6962245273907234138cd18a037
Luc Wollants <lucwollants@gmail.com>
2021-12-22 11:01:35 +0100

    Add denormalizer for `UpdateImage`

---
+++ src/Organizer/Serializers/UpdateImageDenormalizer.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Organizer\Serializers;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
+use CultuurNet\UDB3\Model\ValueObject\Text\Description;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Organizer\Commands\UpdateImage;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+final class UpdateImageDenormalizer implements DenormalizerInterface
+{
+    private string $organizerId;
+
+    public function __construct(string $organizerId)
+    {
+        $this->organizerId = $organizerId;
+    }
+
+    public function denormalize($data, $type, $format = null, array $context = []): UpdateImage
+    {
+        $updateImage = new UpdateImage(
+            $this->organizerId,
+                new UUID($data['id'])
+        );
+
+        if (isset($data['language'])) {
+            $updateImage = $updateImage->withLanguage(new Language($data['language']));
+        }
+
+        if (isset($data['description'])) {
+            $updateImage = $updateImage->withDescription(new Description($data['description']));
+        }
+
+        if (isset($data['copyrightHolder'])) {
+            $updateImage = $updateImage->withCopyrightHolder(new CopyrightHolder($data['copyrightHolder']));
+        }
+
+        return $updateImage;
+    }
+
+    public function supportsDenormalization($data, $type, $format = null): bool
+    {
+        return $type === UpdateImage::class;
+    }
+}
