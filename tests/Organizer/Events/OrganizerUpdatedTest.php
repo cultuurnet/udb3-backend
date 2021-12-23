a22fb4ef097d657df3c57553de66a08473921d15
Luc Wollants <lucwollants@gmail.com>
2021-12-23 13:36:04 +0100

    Added `OrganizerUpdated` event
    For now this event only supports setting the main image id

--- tests/Organizer/Events/OrganizerUpdatedTest.php
+++ tests/Organizer/Events/OrganizerUpdatedTest.php
@@ -9,5 +9,65 @@ use PHPUnit\Framework\TestCase;
 
 final class OrganizerUpdatedTest extends TestCase
 {
+    private OrganizerUpdated $organizerUpdated;
 
+    protected function setUp(): void
+    {
+        $this->organizerUpdated = new OrganizerUpdated('36a5ab5e-042a-48df-8609-93fce2195be8');
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_an_organizer_id(): void
+    {
+        $this->assertEquals('36a5ab5e-042a-48df-8609-93fce2195be8', $this->organizerUpdated->getOrganizerId());
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_an_optional_main_image_id(): void
+    {
+        $organizerUpdated = $this->organizerUpdated->withMainImageId('10652dd4-e38d-4ade-a397-9e45b27f40fb');
+
+        $this->assertEquals('10652dd4-e38d-4ade-a397-9e45b27f40fb', $organizerUpdated->getMainImageId());
+    }
+
+    /**
+     * @test
+     * @dataProvider serializeDataProvider
+     */
+    public function it_can_serialize(array $organizerUpdatedAsArray, OrganizerUpdated $organizerUpdated): void
+    {
+        $this->assertEquals(
+            $organizerUpdatedAsArray,
+            $organizerUpdated->serialize()
+        );
+
+        $this->assertEquals(
+            $organizerUpdated,
+            OrganizerUpdated::deserialize($organizerUpdatedAsArray)
+        );
+    }
+
+    public function serializeDataProvider(): array
+    {
+        return [
+            'Only organizer id' => [
+                [
+                    'organizerId' => '36a5ab5e-042a-48df-8609-93fce2195be8',
+                ],
+                new OrganizerUpdated('36a5ab5e-042a-48df-8609-93fce2195be8'),
+            ],
+            'With main image id' => [
+                [
+                    'organizerId' => '36a5ab5e-042a-48df-8609-93fce2195be8',
+                    'mainImageId' => '10652dd4-e38d-4ade-a397-9e45b27f40fb',
+                ],
+                (new OrganizerUpdated('36a5ab5e-042a-48df-8609-93fce2195be8'))
+                    ->withMainImageId('10652dd4-e38d-4ade-a397-9e45b27f40fb'),
+            ],
+        ];
+    }
 }
