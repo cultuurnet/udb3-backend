6ff503a684249b24c118b33a73227fbebcfcda4b
Luc Wollants <lucwollants@gmail.com>
2021-12-23 13:37:27 +0100

    Fix namespace

--- tests/Organizer/Events/OrganizerUpdatedTest.php
+++ tests/Organizer/Events/OrganizerUpdatedTest.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace CultuurNet\UDB3\Silex\Organizer\Events;
+namespace CultuurNet\UDB3\Organizer\Events;
 
-use CultuurNet\UDB3\Organizer\Events\OrganizerUpdated;
 use PHPUnit\Framework\TestCase;
 
 final class OrganizerUpdatedTest extends TestCase
