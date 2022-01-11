591f61373bcca336e456f31cd959d918d4327143
Luc Wollants <lucwollants@gmail.com>
2022-01-11 17:45:11 +0100

    Add UpdateMainImageRequestHandler

---
+++ src/Http/Organizer/UpdateMainImageRequestHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Organizer;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
+use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Organizer\Commands\UpdateMainImage;
+use CultuurNet\UDB3\Organizer\Serializers\UpdateMainImageDenormalizer;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class UpdateMainImageRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $organizerId = $routeParameters->getOrganizerId();
+
+        $requestBodyParser = RequestBodyParserFactory::createBaseParser(
+            new JsonSchemaValidatingRequestBodyParser(JsonSchemaLocator::ORGANIZER_MAIN_IMAGE_PUT),
+            new DenormalizingRequestBodyParser(new UpdateMainImageDenormalizer($organizerId), UpdateMainImage::class)
+        );
+
+        /** @var UpdateMainImage $updateMainImage */
+        $updateMainImage = $requestBodyParser->parse($request)->getParsedBody();
+
+        $this->commandBus->dispatch($updateMainImage);
+
+        return new NoContentResponse();
+    }
+}
---
+++ tests/Http/Organizer/UpdateMainImageRequestHandlerTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Organizer;
+
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\AssertApiProblemTrait;
+use CultuurNet\UDB3\Http\ApiProblem\SchemaError;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Organizer\Commands\UpdateMainImage;
+use PHPUnit\Framework\TestCase;
+
+final class UpdateMainImageRequestHandlerTest extends TestCase
+{
+    use AssertApiProblemTrait;
+
+    private TraceableCommandBus $commandBus;
+
+    private UpdateMainImageRequestHandler $updateMainImageRequestHandler;
+
+    private Psr7RequestBuilder $psr7RequestBuilder;
+
+    protected function setUp(): void
+    {
+        $this->commandBus = new TraceableCommandBus();
+
+        $this->updateMainImageRequestHandler = new UpdateMainImageRequestHandler($this->commandBus);
+
+        $this->psr7RequestBuilder = new Psr7RequestBuilder();
+
+        $this->commandBus->record();
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_updating_the_main_image(): void
+    {
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('organizerId', 'c269632a-a887-4f21-8455-1631c31e4df5')
+            ->withBodyFromArray([
+                'imageId' => '03789a2f-5063-4062-b7cb-95a0a2280d92',
+            ])
+            ->build('PUT');
+
+        $expectedCommand = new UpdateMainImage(
+            'c269632a-a887-4f21-8455-1631c31e4df5',
+            new UUID('03789a2f-5063-4062-b7cb-95a0a2280d92')
+        );
+
+        $response = $this->updateMainImageRequestHandler->handle($request);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals([$expectedCommand], $this->commandBus->getRecordedCommands());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidBodyDataProvider
+     */
+    public function it_throws_an_api_problem_for_an_invalid_body(string $body, ApiProblem $expectedApiProblem): void
+    {
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('organizerId', 'c269632a-a887-4f21-8455-1631c31e4df5')
+            ->withBodyFromString($body)
+            ->build('PUT');
+
+        $this->assertCallableThrowsApiProblem(
+            $expectedApiProblem,
+            fn () => $this->updateMainImageRequestHandler->handle($request)
+        );
+    }
+
+    public function invalidBodyDataProvider(): array
+    {
+        return [
+            [
+                '',
+                ApiProblem::bodyMissing(),
+            ],
+            [
+                '{{}',
+                ApiProblem::bodyInvalidSyntax('JSON'),
+            ],
+            [
+                '{}',
+                ApiProblem::bodyInvalidData(
+                    new SchemaError('/', 'The required properties (imageId) are missing')
+                ),
+            ],
+            [
+                '{
+                    "imageId":"not a uuid"
+                }',
+                ApiProblem::bodyInvalidData(
+                    new SchemaError('/imageId', 'The data must match the \'uuid\' format')
+                ),
+            ],
+        ];
+    }
+}
