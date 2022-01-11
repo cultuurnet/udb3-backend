3f8e27bf09f46412ca10544fe8bafe73f225bf9d
Luc Wollants <lucwollants@gmail.com>
2022-01-11 17:42:05 +0100

    Add UpdateMainImage command

---
+++ src/Organizer/Commands/UpdateMainImage.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Organizer\Commands;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\AuthorizableCommand;
+
+final class UpdateMainImage implements AuthorizableCommand
+{
+    private string $organizerId;
+
+    private UUID $imageId;
+
+    public function __construct(string $organizerId, UUID $imageId)
+    {
+        $this->organizerId = $organizerId;
+        $this->imageId = $imageId;
+    }
+
+    public function getImageId(): UUID
+    {
+        return $this->imageId;
+    }
+
+    public function getItemId(): string
+    {
+        return $this->organizerId;
+    }
+
+    public function getPermission(): Permission
+    {
+        return Permission::ORGANISATIES_BEWERKEN();
+    }
+}
