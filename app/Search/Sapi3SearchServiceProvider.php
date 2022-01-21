b172a28fae4c92f9c99bf6f2b5d1afe488f03977
Luc Wollants <lucwollants@gmail.com>
2022-01-21 11:48:41 +0100

    Fix bootstrapping

--- app/Search/Sapi3SearchServiceProvider.php
+++ app/Search/Sapi3SearchServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Search;
 
+use CultuurNet\UDB3\Model\ValueObject\Identity\ItemIdentifierFactory;
 use CultuurNet\UDB3\Search\Sapi3SearchService;
 use GuzzleHttp\Psr7\Uri;
 use Http\Adapter\Guzzle6\Client;
@@ -15,6 +16,7 @@ class Sapi3SearchServiceProvider implements ServiceProviderInterface
     public const SEARCH_SERVICE_OFFERS = 'sapi3_search_service_offers';
     public const SEARCH_SERVICE_EVENTS = 'sapi3_search_service_events';
     public const SEARCH_SERVICE_PLACES = 'sapi3_search_service_places';
+    public const SEARCH_SERVICE_ORGANIZERS = 'sapi3_search_service_organizers';
 
     public function register(Application $app)
     {
@@ -23,7 +25,7 @@ class Sapi3SearchServiceProvider implements ServiceProviderInterface
                 return new Sapi3SearchService(
                     new Uri($app['config']['search']['v3']['base_url'] . '/offers/'),
                     new Client(new \GuzzleHttp\Client()),
-                    $app['iri_offer_identifier_factory'],
+                    new ItemIdentifierFactory($app['config']['item_url_regex']),
                     $app['config']['export']['search']['api_key'] ?? null
                 );
             }
@@ -34,7 +36,7 @@ class Sapi3SearchServiceProvider implements ServiceProviderInterface
                 return new Sapi3SearchService(
                     new Uri($app['config']['search']['v3']['base_url'] . '/events/'),
                     new Client(new \GuzzleHttp\Client()),
-                    $app['iri_offer_identifier_factory'],
+                    new ItemIdentifierFactory($app['config']['item_url_regex']),
                     $app['config']['export']['search']['api_key'] ?? null
                 );
             }
@@ -45,7 +47,7 @@ class Sapi3SearchServiceProvider implements ServiceProviderInterface
                 return new Sapi3SearchService(
                     new Uri($app['config']['search']['v3']['base_url'] . '/places/'),
                     new Client(new \GuzzleHttp\Client()),
-                    $app['iri_offer_identifier_factory'],
+                    new ItemIdentifierFactory($app['config']['item_url_regex']),
                     $app['config']['export']['search']['api_key'] ?? null
                 );
             }
