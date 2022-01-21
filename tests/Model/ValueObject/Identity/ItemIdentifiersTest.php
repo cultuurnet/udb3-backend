6765fd57d1921f0b74b7af27ae2ec5e363c0798a
Luc Wollants <lucwollants@gmail.com>
2022-01-21 11:43:06 +0100

    Add collection ItemIdentifiers

---
+++ tests/Model/ValueObject/Identity/ItemIdentifiersTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Silex\Model\ValueObject\Identity;
+
+use CultuurNet\UDB3\Model\ValueObject\Identity\ItemIdentifiers;
+use PHPUnit\Framework\TestCase;
+
+final class ItemIdentifiersTest extends TestCase
+{
+
+}
