28a79b07a21a42f9c0544bad7f33bcb4be351de6
Bert Ramakers <bertramakers@me.com>
2022-01-28 16:56:50 +0100

    Remove unnecessary HasMainLanguageRules from OrganizerValidator

--- src/Model/Validation/Organizer/OrganizerValidator.php
+++ src/Model/Validation/Organizer/OrganizerValidator.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\Validation\Organizer;
 
-use CultuurNet\UDB3\Model\Validation\ValueObject\Translation\HasMainLanguageRule;
-use Respect\Validation\Rules\Url;
 use Respect\Validation\Validator;
 
 class OrganizerValidator extends Validator
@@ -15,16 +13,6 @@ class OrganizerValidator extends Validator
      */
     public function __construct(array $extraRules = [])
     {
-        // Note that url is NOT required when validating Organizer JSON returned
-        // by UiTdatabank, because older organizers were created without url.
-        // However, url is required to create new organizers.
-        $rules = [
-            new HasMainLanguageRule('name'),
-            new HasMainLanguageRule('address'),
-        ];
-
-        $rules = array_merge($rules, $extraRules);
-
-        parent::__construct($rules);
+        parent::__construct($extraRules);
     }
 }
