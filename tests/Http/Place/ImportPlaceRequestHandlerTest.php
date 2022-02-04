92d6153cf50bf3e0333570d97d2ff6eed7501a3b
Luc Wollants <lucwollants@gmail.com>
2022-02-04 13:03:23 +0100

    Add test it_should_throw_an_exception_if_mediaObject_has_an_invalid_type

--- tests/Http/Place/ImportPlaceRequestHandlerTest.php
+++ tests/Http/Place/ImportPlaceRequestHandlerTest.php
@@ -2304,6 +2304,55 @@ final class ImportPlaceRequestHandlerTest extends TestCase
 
         $this->assertValidationErrors($place, $expectedErrors);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_mediaObject_has_an_invalid_type(): void
+    {
+        $place = [
+            '@id' => 'http://io.uitdatabank.be/place/b19d4090-db47-4520-ac1a-880684357ec9',
+            'mainLanguage' => 'nl',
+            'name' => [
+                'nl' => 'Test place',
+            ],
+            'calendarType' => 'permanent',
+            'terms' => [
+                [
+                    'id' => 'Yf4aZBfsUEu2NsQqsprngw',
+                    'domain' => 'eventtype',
+                    'label' => 'Cultuur- of ontmoetingscentrum',
+                ],
+            ],
+            'address' => [
+                'nl' => [
+                    'streetAddress' => 'Henegouwenkaai 41-43',
+                    'postalCode' => '1080',
+                    'addressLocality' => 'Brussel',
+                    'addressCountry' => 'BE',
+                ],
+            ],
+            'mediaObject' => [
+                [
+                    '@id' => 'http://io.uitdatabank.be/media/5cdacc0b-a96b-4613-81e0-1748c179432f',
+                    '@type' => 'schema:foo',
+                    'description' => 'Example description',
+                    'copyrightHolder' => 'Example copyright holder',
+                    'inLanguage' => 'nl',
+                ],
+            ],
+        ];
+
+        $expectedErrors = [
+            new SchemaError(
+                '/mediaObject/0/%40type',
+                'The data should match one item from enum'
+            ),
+        ];
+
+        $this->assertValidationErrors($place, $expectedErrors);
+    }
+
     private function assertValidationErrors(array $place, array $expectedErrors): void
     {
         $placeId = 'c4f1515a-7a73-4e18-a53a-9bf201d6fc9b';
