4e69bb081cc1c8fae8993c2baf41626862453f1f
Luc Wollants <lucwollants@gmail.com>
2022-02-04 13:03:14 +0100

    Add test it_should_throw_an_exception_if_mediaObject_has_an_invalid_contentUrl_or_thumbnailUrl

--- tests/Http/Place/ImportPlaceRequestHandlerTest.php
+++ tests/Http/Place/ImportPlaceRequestHandlerTest.php
@@ -2251,6 +2251,59 @@ final class ImportPlaceRequestHandlerTest extends TestCase
 
         $this->assertValidationErrors($place, $expectedErrors);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_mediaObject_has_an_invalid_contentUrl_or_thumbnailUrl(): void
+    {
+        $place = [
+            '@id' => 'http://io.uitdatabank.be/place/b19d4090-db47-4520-ac1a-880684357ec9',
+            'mainLanguage' => 'nl',
+            'name' => [
+                'nl' => 'Test place',
+            ],
+            'calendarType' => 'permanent',
+            'terms' => [
+                [
+                    'id' => 'Yf4aZBfsUEu2NsQqsprngw',
+                    'domain' => 'eventtype',
+                    'label' => 'Cultuur- of ontmoetingscentrum',
+                ],
+            ],
+            'address' => [
+                'nl' => [
+                    'streetAddress' => 'Henegouwenkaai 41-43',
+                    'postalCode' => '1080',
+                    'addressLocality' => 'Brussel',
+                    'addressCountry' => 'BE',
+                ],
+            ],
+            'mediaObject' => [
+                [
+                    '@id' => 'http://io.uitdatabank.be/media/5cdacc0b-a96b-4613-81e0-1748c179432f',
+                    'description' => 'Example description',
+                    'copyrightHolder' => 'Example copyright holder',
+                    'inLanguage' => 'nl',
+                    'contentUrl' => 'info@publiq.be',
+                    'thumbnailUrl' => 'info@publiq.be',
+                ],
+            ],
+        ];
+
+        $expectedErrors = [
+            new SchemaError(
+                '/mediaObject/0/contentUrl',
+                'The data must match the \'uri\' format'
+            ),
+            new SchemaError(
+                '/mediaObject/0/thumbnailUrl',
+                'The data must match the \'uri\' format'
+            ),
+        ];
+
+        $this->assertValidationErrors($place, $expectedErrors);
+    }
     private function assertValidationErrors(array $place, array $expectedErrors): void
     {
         $placeId = 'c4f1515a-7a73-4e18-a53a-9bf201d6fc9b';
