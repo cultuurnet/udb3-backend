24635437bfb1eb30b11d54e96fd4f641e5a83195
Luc Wollants <lucwollants@gmail.com>
2022-02-04 12:12:54 +0100

    Add test it_should_throw_an_exception_if_bookingInfo_has_an_invalid_availabilityStarts_or_availabilityEnds

--- tests/Http/Place/ImportPlaceRequestHandlerTest.php
+++ tests/Http/Place/ImportPlaceRequestHandlerTest.php
@@ -2161,6 +2161,52 @@ final class ImportPlaceRequestHandlerTest extends TestCase
         $this->assertValidationErrors($place, $expectedErrors);
     }
 
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_bookingInfo_has_an_invalid_availabilityStarts_or_availabilityEnds(): void
+    {
+        $place = [
+            '@id' => 'http://io.uitdatabank.be/place/b19d4090-db47-4520-ac1a-880684357ec9',
+            'mainLanguage' => 'nl',
+            'name' => [
+                'nl' => 'Test place',
+            ],
+            'calendarType' => 'permanent',
+            'terms' => [
+                [
+                    'id' => 'Yf4aZBfsUEu2NsQqsprngw',
+                    'domain' => 'eventtype',
+                    'label' => 'Cultuur- of ontmoetingscentrum',
+                ],
+            ],
+            'address' => [
+                'nl' => [
+                    'streetAddress' => 'Henegouwenkaai 41-43',
+                    'postalCode' => '1080',
+                    'addressLocality' => 'Brussel',
+                    'addressCountry' => 'BE',
+                ],
+            ],
+            'bookingInfo' => [
+                'availabilityStarts' => '01/01/2018',
+                'availabilityEnds' => '2018-01-02',
+            ],
+        ];
+
+        $expectedErrors = [
+            new SchemaError(
+                '/bookingInfo/availabilityStarts',
+                'The data must match the \'date-time\' format'
+            ),
+            new SchemaError(
+                '/bookingInfo/availabilityEnds',
+                'The data must match the \'date-time\' format'
+            ),
+        ];
+
+        $this->assertValidationErrors($place, $expectedErrors);
+    }
     private function assertValidationErrors(array $place, array $expectedErrors): void
     {
         $placeId = 'c4f1515a-7a73-4e18-a53a-9bf201d6fc9b';
