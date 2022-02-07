d7f60779bccfcac5f482630b6bcb528fdb1e99e6
Luc Wollants <lucwollants@gmail.com>
2022-02-07 15:40:21 +0100

    Add test it_should_throw_an_exception_if_videos_has_an_invalid_format

--- tests/Http/Place/ImportPlaceRequestHandlerTest.php
+++ tests/Http/Place/ImportPlaceRequestHandlerTest.php
@@ -2555,6 +2555,45 @@ final class ImportPlaceRequestHandlerTest extends TestCase
         $this->assertValidationErrors($place, $expectedErrors);
     }
 
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_videos_has_an_invalid_format(): void
+    {
+        $place = [
+            '@id' => 'http://io.uitdatabank.be/place/b19d4090-db47-4520-ac1a-880684357ec9',
+            'mainLanguage' => 'nl',
+            'name' => [
+                'nl' => 'Test place',
+            ],
+            'calendarType' => 'permanent',
+            'terms' => [
+                [
+                    'id' => 'Yf4aZBfsUEu2NsQqsprngw',
+                    'domain' => 'eventtype',
+                    'label' => 'Cultuur- of ontmoetingscentrum',
+                ],
+            ],
+            'address' => [
+                'nl' => [
+                    'streetAddress' => 'Henegouwenkaai 41-43',
+                    'postalCode' => '1080',
+                    'addressLocality' => 'Brussel',
+                    'addressCountry' => 'BE',
+                ],
+            ],
+            'videos' => 'wrong type',
+        ];
+
+        $expectedErrors = [
+            new SchemaError(
+                '/videos',
+                'The data (string) must match the type: array'
+            ),
+        ];
+
+        $this->assertValidationErrors($place, $expectedErrors);
+    }
     private function assertValidationErrors(array $place, array $expectedErrors): void
     {
         $placeId = 'c4f1515a-7a73-4e18-a53a-9bf201d6fc9b';
