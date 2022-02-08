22a02b29d26b35dd7c049f26ebd1c9e8f823f9f7
Luc Wollants <lucwollants@gmail.com>
2022-02-08 09:21:16 +0100

    Fix test by doing an encoding but skipping / and :

--- src/Model/ValueObject/Web/Url.php
+++ src/Model/ValueObject/Web/Url.php
@@ -20,10 +20,22 @@ class Url
         $this->guardString($value);
         $this->guardRegexPattern('/\\Ahttp[s]?:\\/\\//', $value);
 
+        $value = $this->encode($value);
+
         if (filter_var($value, FILTER_VALIDATE_URL) === false) {
             throw new \InvalidArgumentException('Given string is not a valid url.');
         }
 
         $this->setValue($value);
+    }
+
+    private function encode(string $value): string
+    {
+        // Take into account already encoded urls by first decoding them.
+        $value = urldecode($value);
+
+        // Encode the url but revert forward slash / and colon :
+        $value = urlencode($value);
+        return str_replace(['%2F', '%3A'], ['/', ':'], $value);
     }
 }
