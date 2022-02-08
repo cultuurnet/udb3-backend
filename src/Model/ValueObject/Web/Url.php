cb9a42d6be3ea7afc776544e4f09ffe654f41a0d
Luc Wollants <lucwollants@gmail.com>
2022-02-08 10:35:51 +0100

    Add source

--- src/Model/ValueObject/Web/Url.php
+++ src/Model/ValueObject/Web/Url.php
@@ -38,7 +38,7 @@ class Url
         $value = urlencode($value);
 
         // Revert meaningful characters.
-        // Taken from https://developers.google.com/maps/url-encoding
+        // Taken from https://developers.google.com/maps/url-encoding#special-characters
         $entities = ['%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D'];
         $replacements = ['!', '*', '\'', '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']'];
 
