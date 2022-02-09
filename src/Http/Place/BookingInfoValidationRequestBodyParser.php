6bfc5b7deaceddaabd4be081eb99aa353f0106d9
Luc Wollants <lucwollants@gmail.com>
2022-02-09 11:50:14 +0100

    Add a BookingInfoValidationRequestBodyParser

---
+++ src/Http/Place/BookingInfoValidationRequestBodyParser.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Place;
+
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\Offer\DateRangeValidator;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class BookingInfoValidationRequestBodyParser implements RequestBodyParser
+{
+    public function parse(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $errors = [];
+
+        $data = $request->getParsedBody();
+
+        if (!is_object($data)) {
+            // If the body data is not an object, there's nothing left to validate. Just re-throw the errors from the
+            // JSON schema validation.
+            throw ApiProblem::bodyInvalidData(...$errors);
+        }
+
+        if (isset($data->bookingInfo)) {
+            $errors = (new DateRangeValidator())->validate(
+                $data->bookingInfo,
+                'bookingInfo',
+                'availabilityStarts',
+                'availabilityEnds'
+            );
+        }
+
+        if (count($errors) > 0) {
+            throw ApiProblem::bodyInvalidData(...$errors);
+        }
+
+        return $request;
+    }
+}
