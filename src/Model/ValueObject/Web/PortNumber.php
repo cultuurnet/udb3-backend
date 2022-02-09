9cbaa6b5872fd8ab9015415f126c87472479e812
Jonas <jonas@cultuurnet.be>
2022-02-09 14:46:19 +0100

    linting

--- src/Model/ValueObject/Web/PortNumber.php
+++ src/Model/ValueObject/Web/PortNumber.php
@@ -6,7 +6,6 @@ namespace CultuurNet\UDB3\Model\ValueObject\Web;
 
 use CultuurNet\UDB3\Model\ValueObject\Integer\Behaviour\IsInteger;
 use CultuurNet\UDB3\Model\ValueObject\Integer\Behaviour\IsNatural;
-use ValueObjects\Exception\InvalidNativeArgumentException;
 
 class PortNumber
 {
@@ -16,16 +15,14 @@ class PortNumber
     public function __construct(int $value)
     {
         $this->guardNatural($value);
-        $options = array(
-            'options' => array(
+        $options = [
+            'options' => [
                 'min_range' => 0,
-                'max_range' => 65535
-            )
-        );
+                'max_range' => 65535,
+            ],
+        ];
 
-        $value = filter_var($value, FILTER_VALIDATE_INT, $options);
-
-        if (false === $value) {
+        if (!filter_var($value, FILTER_VALIDATE_INT, $options)) {
             throw new \InvalidArgumentException('Port Number should be an integer between 0 and 65535.');
         }
 
