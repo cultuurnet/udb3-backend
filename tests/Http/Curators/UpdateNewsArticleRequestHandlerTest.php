70f5328f8871c25c7bc5efa5b1501da8ada57619
Luc Wollants <lucwollants@gmail.com>
2022-02-10 12:53:28 +0100

    Use the UrlEncodingRequestBodyParser inside UpdateNewsArticleRequestHandler

--- tests/Http/Curators/UpdateNewsArticleRequestHandlerTest.php
+++ tests/Http/Curators/UpdateNewsArticleRequestHandlerTest.php
@@ -215,6 +215,64 @@ class UpdateNewsArticleRequestHandlerTest extends TestCase
     /**
      * @test
      */
+    public function it_updates_a_news_article_with_an_url_that_should_have_been_encoded(): void
+    {
+        $updateNewsArticleRequest = $this->psr7RequestBuilder
+            ->withRouteParameter('articleId', '6c583739-a848-41ab-b8a3-8f7dab6f8ee1')
+            ->withJsonBodyFromArray([
+                'headline' => 'publiq wint API award',
+                'inLanguage' => 'nl',
+                'text' => 'Op 10 januari 2020 wint publiq de API award',
+                'about' => '17284745-7bcf-461a-aad0-d3ad54880e75',
+                'publisher' => 'BILL',
+                'url' => 'https://www.publiq.be/blog/api-reward',
+                'publisherLogo' => 'https://www.bill.be/img/favicon.png',
+            ])
+            ->build('PUT');
+
+        $newsArticle = new NewsArticle(
+            new UUID('6c583739-a848-41ab-b8a3-8f7dab6f8ee1'),
+            'publiq wint API award',
+            new Language('nl'),
+            'Op 10 januari 2020 wint publiq de API award',
+            '17284745-7bcf-461a-aad0-d3ad54880e75',
+            'BILL',
+            new Url('https://www.publiq.be/blog/api-reward'),
+            new Url('https://www.bill.be/img/favicon.png')
+        );
+
+        $this->newsArticleRepository->expects($this->once())
+            ->method('getById')
+            ->with(new UUID('6c583739-a848-41ab-b8a3-8f7dab6f8ee1'))
+            ->willReturn($newsArticle);
+
+        $this->newsArticleRepository->expects($this->once())
+            ->method('update')
+            ->with($newsArticle);
+
+        $response = $this->updateNewsArticleRequestHandler->handle($updateNewsArticleRequest);
+
+        $this->assertEquals(
+            Json::encode([
+                '@context' => '/contexts/NewsArticle',
+                '@id' => '/news-articles/6c583739-a848-41ab-b8a3-8f7dab6f8ee1',
+                '@type' => 'https://schema.org/NewsArticle',
+                'id' => '6c583739-a848-41ab-b8a3-8f7dab6f8ee1',
+                'headline' => 'publiq wint API award',
+                'inLanguage' => 'nl',
+                'text' => 'Op 10 januari 2020 wint publiq de API award',
+                'about' => '17284745-7bcf-461a-aad0-d3ad54880e75',
+                'publisher' => 'BILL',
+                'url' => 'https://www.publiq.be/blog/api-reward',
+                'publisherLogo' => 'https://www.bill.be/img/favicon.png',
+            ]),
+            $response->getBody()->getContents()
+        );
+    }
+
+    /**
+     * @test
+     */
     public function it_throws_when_news_article_not_found(): void
     {
         $updateNewsArticleRequest = $this->psr7RequestBuilder
