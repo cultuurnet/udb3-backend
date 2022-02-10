28e7c29b0c95a535cffa6b138cb0e8bf4df6eb70
Bert Ramakers <bertramakers@me.com>
2022-02-10 17:32:09 +0100

    Never try to denormalize an embedded organizer completely
    Only denormalize the @id, which is all we need for the imports

--- src/Model/Serializer/Organizer/OrganizerReferenceDenormalizer.php
+++ src/Model/Serializer/Organizer/OrganizerReferenceDenormalizer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\Serializer\Organizer;
 
-use CultuurNet\UDB3\Model\Organizer\Organizer;
 use CultuurNet\UDB3\Model\Organizer\OrganizerIDParser;
 use CultuurNet\UDB3\Model\Organizer\OrganizerReference;
 use CultuurNet\UDB3\Model\ValueObject\Web\Url;
@@ -15,14 +14,9 @@ class OrganizerReferenceDenormalizer implements DenormalizerInterface
 {
     private OrganizerIDParser $organizerIDParser;
 
-    private OrganizerDenormalizer $organizerDenormalizer;
-
-    public function __construct(
-        OrganizerIDParser $organizerIDParser,
-        OrganizerDenormalizer $organizerDenormalizer
-    ) {
+    public function __construct(OrganizerIDParser $organizerIDParser)
+    {
         $this->organizerIDParser = $organizerIDParser;
-        $this->organizerDenormalizer = $organizerDenormalizer;
     }
 
     public function denormalize($data, $class, $format = null, array $context = [])
@@ -37,18 +31,6 @@ class OrganizerReferenceDenormalizer implements DenormalizerInterface
 
         $organizerIdUrl = new Url($data['@id']);
         $organizerId = $this->organizerIDParser->fromUrl($organizerIdUrl);
-        $organizer = null;
-        if (count($data) > 1) {
-            try {
-                $organizer = $this->organizerDenormalizer->denormalize($data, Organizer::class);
-            } catch (\Exception $e) {
-                $organizer = null;
-            }
-        }
-
-        if ($organizer) {
-            return OrganizerReference::createWithEmbeddedOrganizer($organizer);
-        }
 
         return OrganizerReference::createWithOrganizerId($organizerId);
     }
