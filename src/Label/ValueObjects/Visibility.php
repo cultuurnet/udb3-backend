693340e94aca4188e610b24e56d3cb70f69f46e5
Luc Wollants <lucwollants@gmail.com>
2022-02-16 15:46:37 +0100

    Refactor visibility to extend internal Enum

--- src/Label/ValueObjects/Visibility.php
+++ src/Label/ValueObjects/Visibility.php
@@ -4,16 +4,31 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Label\ValueObjects;
 
-use ValueObjects\Enum\Enum;
+use CultuurNet\UDB3\Model\ValueObject\String\Enum;
 
 /**
- * Class Visibility
- * @package CultuurNet\UDB3\Label\ValueObjects
- * @method static Visibility VISIBLE()
- * @method static Visibility INVISIBLE()
+ * @deprecated Use new CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label
  */
 class Visibility extends Enum
 {
     public const VISIBLE = 'visible';
     public const INVISIBLE = 'invisible';
+
+    public static function getAllowedValues(): array
+    {
+        return [
+            self::VISIBLE,
+            self::INVISIBLE,
+        ];
+    }
+
+    public static function VISIBLE(): Visibility
+    {
+        return new Visibility(self::VISIBLE);
+    }
+
+    public static function INVISIBLE(): Visibility
+    {
+        return new Visibility(self::INVISIBLE);
+    }
 }
