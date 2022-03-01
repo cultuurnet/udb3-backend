676952a42f9be8e662a3926e530a0ac1716c7e2f
Luc Wollants <lucwollants@gmail.com>
2022-03-01 15:22:58 +0100

    Avoid throwing when user is not allowed to use label

--- src/Offer/Commands/ImportLabels.php
+++ src/Offer/Commands/ImportLabels.php
@@ -6,10 +6,9 @@ namespace CultuurNet\UDB3\Offer\Commands;
 
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Labels;
-use CultuurNet\UDB3\Security\AuthorizableLabelCommand;
 use CultuurNet\UDB3\StringLiteral;
 
-final class ImportLabels extends AbstractCommand implements AuthorizableLabelCommand
+final class ImportLabels extends AbstractCommand
 {
     /**
      * @var Labels
