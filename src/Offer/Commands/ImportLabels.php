3eaf893c54579735071cbd46a10e4ee6b83bb0fe
Luc Wollants <lucwollants@gmail.com>
2022-03-02 16:32:47 +0100

    Remove unused properties from ImportLabels

--- src/Offer/Commands/ImportLabels.php
+++ src/Offer/Commands/ImportLabels.php
@@ -10,72 +10,17 @@ use CultuurNet\UDB3\StringLiteral;
 
 final class ImportLabels extends AbstractCommand
 {
-    /**
-     * @var Labels
-     */
-    private $labels;
-
-    /**
-     * @var Labels
-     */
-    private $labelsToKeepIfAlreadyOnOffer;
-
-    /**
-     * @var Labels
-     */
-    private $labelsToRemoveWhenOnOffer;
+    private Labels $labels;
 
     public function __construct(string $itemId, Labels $labels)
     {
         parent::__construct($itemId);
         $this->labels = $labels;
-        $this->labelsToKeepIfAlreadyOnOffer = new Labels();
-        $this->labelsToRemoveWhenOnOffer = new Labels();
     }
 
     public function getLabels(): Labels
     {
         return $this->labels;
-    }
-
-    public function withLabelsToKeepIfAlreadyOnOffer(Labels $labels): self
-    {
-        $c = clone $this;
-        $c->labelsToKeepIfAlreadyOnOffer = $labels;
-        return $c;
-    }
-
-    public function getLabelsToKeepIfAlreadyOnOffer(): Labels
-    {
-        return $this->labelsToKeepIfAlreadyOnOffer;
-    }
-
-    public function withLabelsToRemoveWhenOnOffer(Labels $labels): self
-    {
-        $c = clone $this;
-        $c->labelsToRemoveWhenOnOffer = $labels;
-        return $c;
-    }
-
-    public function getLabelsToRemoveWhenOnOffer(): Labels
-    {
-        return $this->labelsToRemoveWhenOnOffer;
-    }
-
-    public function getLabelsToImport(): Labels
-    {
-        $labelNamesToKeep = array_map(
-            function (Label $label) {
-                return $label->getName();
-            },
-            $this->labelsToKeepIfAlreadyOnOffer->toArray()
-        );
-
-        return $this->labels->filter(
-            function (Label $label) use ($labelNamesToKeep) {
-                return !in_array($label->getName(), $labelNamesToKeep);
-            }
-        );
     }
 
     public function getLabelNames(): array
@@ -84,7 +29,7 @@ final class ImportLabels extends AbstractCommand
             function (Label $label) {
                 return new StringLiteral($label->getName()->toString());
             },
-            $this->getLabelsToImport()->toArray()
+            $this->getLabels()->toArray()
         );
     }
 }
