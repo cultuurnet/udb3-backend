f0bc4a528138747f120292ae16e612a49e25b9c8
Luc Wollants <lucwollants@gmail.com>
2022-03-02 16:03:52 +0100

    Refactor event import to simple ImportLabels construct

--- src/Model/Import/Validation/Event/EventImportValidator.php
+++ src/Model/Import/Validation/Event/EventImportValidator.php
@@ -11,7 +11,6 @@ use CultuurNet\UDB3\Model\Import\Validation\Place\PlaceReferenceExistsValidator;
 use CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Category\CategoriesExistValidator;
 use CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Category\EventTypeCountValidator;
 use CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Category\ThemeCountValidator;
-use CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Label\DocumentLabelPermissionRule;
 use CultuurNet\UDB3\Model\Place\PlaceIDParser;
 use CultuurNet\UDB3\Model\Validation\Event\EventValidator;
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUIDParser;
@@ -44,12 +43,6 @@ class EventImportValidator extends EventValidator
                     new ThemeCountValidator()
                 ),
                 false
-            ),
-            new DocumentLabelPermissionRule(
-                $uuidParser,
-                $userId,
-                $labelsRepository,
-                $labelRelationsRepository
             ),
         ];
 
