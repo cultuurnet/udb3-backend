8eac33720f90d20ef2ceaabcd41cb229c881895d
Luc Wollants <lucwollants@gmail.com>
2022-03-10 15:46:44 +0100

    Refactor organizer legacy parser to use composition

--- src/Http/Organizer/LegacyOrganizerRequestBodyParser.php
+++ src/Http/Organizer/LegacyOrganizerRequestBodyParser.php
@@ -4,54 +4,36 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Organizer;
 
+use CultuurNet\UDB3\Http\Request\Body\CombinedRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\LegacyAddressRequestBodyParser;
+use CultuurNet\UDB3\Http\Request\Body\LegacyNameRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
 use Psr\Http\Message\ServerRequestInterface;
 use stdClass;
 
 final class LegacyOrganizerRequestBodyParser implements RequestBodyParser
 {
+    private CombinedRequestBodyParser $parser;
+
+    public function __construct()
+    {
+        $this->parser = new CombinedRequestBodyParser(
+            new LegacyAddressRequestBodyParser(),
+            new LegacyNameRequestBodyParser(),
+            new LegacyWebsiteRequestBodyParser()
+        );
+    }
+
     public function parse(ServerRequestInterface $request): ServerRequestInterface
     {
-        $data = $request->getParsedBody();
+        $parsedRequest = $this->parser->parse($request);
+
+        $data = $parsedRequest->getParsedBody();
 
         if (!($data instanceof stdClass)) {
             return $request;
         }
         $data = clone $data;
-
-        $mainLanguage = $data->mainLanguage ?? null;
-
-        if (isset($data->website) && !isset($data->url)) {
-            $data->url = $data->website;
-            unset($data->website);
-        }
-
-        if ($mainLanguage && isset($data->name) && is_string($data->name)) {
-            $data->name = (object) [
-                $data->mainLanguage => $data->name,
-            ];
-        }
-
-        if ($mainLanguage && isset($data->address->streetAddress)) {
-            $data->address->{$mainLanguage} = $data->address->{$mainLanguage} ?? (object) [];
-            $data->address->{$mainLanguage}->streetAddress = $data->address->streetAddress;
-            unset($data->address->streetAddress);
-        }
-        if ($mainLanguage && isset($data->address->postalCode)) {
-            $data->address->{$mainLanguage} = $data->address->{$mainLanguage} ?? (object) [];
-            $data->address->{$mainLanguage}->postalCode = $data->address->postalCode;
-            unset($data->address->postalCode);
-        }
-        if ($mainLanguage && isset($data->address->addressLocality)) {
-            $data->address->{$mainLanguage} = $data->address->{$mainLanguage} ?? (object) [];
-            $data->address->{$mainLanguage}->addressLocality = $data->address->addressLocality;
-            unset($data->address->addressLocality);
-        }
-        if ($mainLanguage && isset($data->address->addressCountry)) {
-            $data->address->{$mainLanguage} = $data->address->{$mainLanguage} ?? (object) [];
-            $data->address->{$mainLanguage}->addressCountry = $data->address->addressCountry;
-            unset($data->address->addressCountry);
-        }
 
         if (isset($data->contact) && is_array($data->contact)) {
             foreach ($data->contact as $contactEntry) {
