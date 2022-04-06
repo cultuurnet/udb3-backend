c7ad581e9c607bb5ad457ae97ae9b9f99b0d3981
Luc Wollants <lucwollants@gmail.com>
2022-04-06 11:11:51 +0200

    Linting

--- app/Migrations/Version20220406090353.php
+++ app/Migrations/Version20220406090353.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Migrations;
 
@@ -8,13 +10,13 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20220406090353 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $schema->getTable('duplicate_places')
             ->dropColumn('is_canonical');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $schema->getTable('duplicate_places')
             ->addColumn('is_canonical', Type::BOOLEAN)
