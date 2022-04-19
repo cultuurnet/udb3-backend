78f1e001af4c86e91a62a1cf3ab74a1c1891485e
Luc Wollants <lucwollants@gmail.com>
2022-04-19 16:48:48 +0200

    Extend Event Model with AttendanceMode

--- src/Model/Event/Event.php
+++ src/Model/Event/Event.php
@@ -7,9 +7,12 @@ namespace CultuurNet\UDB3\Model\Event;
 use CultuurNet\UDB3\Model\Offer\Offer;
 use CultuurNet\UDB3\Model\Place\PlaceReference;
 use CultuurNet\UDB3\Model\ValueObject\Audience\AudienceType;
+use CultuurNet\UDB3\Model\ValueObject\Virtual\AttendanceMode;
 
 interface Event extends Offer
 {
+    public function getAttendanceMode(): AttendanceMode;
+
     public function getAudienceType(): AudienceType;
 
     public function getPlaceReference(): PlaceReference;
