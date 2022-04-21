0fc7fea1c5197c565d1caa07e46a307ef2f8ff35
Luc Wollants <lucwollants@gmail.com>
2022-04-21 14:36:14 +0200

    Add unit test for isVirtualLocation

--- tests/Event/ValueObjects/LocationIdTest.php
+++ tests/Event/ValueObjects/LocationIdTest.php
@@ -11,11 +11,50 @@ class LocationIdTest extends TestCase
     /**
      * @test
      */
-    public function it_requires_a_non_empty_string_value()
+    public function it_requires_a_non_empty_string_value(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('LocationId can\'t have an empty value.');
 
         new LocationId('');
+    }
+
+    /**
+     * @dataProvider locationDataProvider
+     * @test
+     */
+    public function it_recognizes_virtual_locations(LocationId $locationId, bool $isVirtualLocation): void
+    {
+        $this->assertEquals($isVirtualLocation, $locationId->isVirtualLocation());
+    }
+
+    public function locationDataProvider(): array
+    {
+        return [
+            [
+                new LocationId('https://io.uitdatabank.dev/places/00000000-0000-0000-0000-000000000000'),
+                true,
+            ],
+            [
+                new LocationId('https://io.uitdatabank.dev/places/00000000-0000-0000-0000-00000000000'),
+                false,
+            ],
+            [
+                new LocationId('https://io.uitdatabank.dev/places/00000000-0000-0000-0000-0000000000001'),
+                false,
+            ],
+            [
+                new LocationId('https://io.uitdatabank.dev/places/df91e7b3-bdf1-4ca9-8cb2-9239fe14a7e1'),
+                false,
+            ],
+            [
+                new LocationId('00000000-0000-0000-0000-000000000000'),
+                true,
+            ],
+            [
+                new LocationId('82cf263f-f9ae-4d25-bdd4-75ce29c2a6af'),
+                false,
+            ],
+        ];
     }
 }
