32771de55ce262de8c77d20ee8b8dc44f6ef91e6
Luc Wollants <lucwollants@gmail.com>
2022-04-21 13:52:38 +0200

    Refactor LocationId to simple class without StringLiteral dependency

--- src/Event/ValueObjects/LocationId.php
+++ src/Event/ValueObjects/LocationId.php
@@ -4,25 +4,23 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Event\ValueObjects;
 
-use CultuurNet\UDB3\StringLiteral;
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsString;
 
-/**
- * @deprecated
- *   Use CultuurNet\UDB3\Model\ValueObject\Identity\UUID instead where possible
- */
-final class LocationId extends StringLiteral
+final class LocationId
 {
+    use IsString;
+
     private const VIRTUAL_LOCATION = '00000000-0000-0000-0000-000000000000';
 
     private static array $dummyPlaceForEducationIds = [];
 
-    public function __construct($value)
+    public function __construct(string $value)
     {
-        parent::__construct($value);
-
         if (empty($value)) {
             throw new \InvalidArgumentException('LocationId can\'t have an empty value.');
         }
+
+        $this->setValue($value);
     }
 
     public function isVirtualLocation(): bool
