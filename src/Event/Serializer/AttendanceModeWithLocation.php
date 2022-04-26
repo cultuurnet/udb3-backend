7de6ae91deec30fdea8e6766beba1d1716871048
Luc Wollants <lucwollants@gmail.com>
2022-04-26 16:29:32 +0200

    Create VO AttendanceModeWithLocation

---
+++ src/Event/Serializer/AttendanceModeWithLocation.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Serializer;
+
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Model\ValueObject\Virtual\AttendanceMode;
+
+final class AttendanceModeWithLocation
+{
+    private AttendanceMode $attendanceMode;
+
+    private ?LocationId $locationId;
+
+    public function __construct(AttendanceMode $attendanceMode, ?LocationId $locationId)
+    {
+        $this->attendanceMode = $attendanceMode;
+        $this->locationId = $locationId;
+    }
+
+    public function getAttendanceMode(): AttendanceMode
+    {
+        return $this->attendanceMode;
+    }
+
+    public function getLocationId(): ?LocationId
+    {
+        return $this->locationId;
+    }
+}
