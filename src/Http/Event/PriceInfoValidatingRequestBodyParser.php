64b97b5e4992fd7b71742681f372d65cce054220
Jonas <jonas@cultuurnet.be>
2022-05-02 13:22:36 +0200

    Create a RequestBodyParser for PriceInfo.

---
+++ src/Http/Event/PriceInfoValidatingRequestBodyParser.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Event;
+
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\SchemaError;
+use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
+use CultuurNet\UDB3\Model\Serializer\ValueObject\Price\PriceInfoDenormalizer;
+use CultuurNet\UDB3\Model\ValueObject\Price\PriceInfo;
+use Psr\Http\Message\ServerRequestInterface;
+
+class PriceInfoValidatingRequestBodyParser implements RequestBodyParser
+{
+    public function parse(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $data = $request->getParsedBody();
+        if (!isset($data->priceInfo) || !is_array($data->priceInfo)) {
+            return $request;
+        }
+
+        $priceInfoDenormalizer = new PriceInfoDenormalizer();
+        $priceInfo = $priceInfoDenormalizer->denormalize($data->priceInfo, PriceInfo::class);
+
+        if ($priceInfo->getTariffs()->hasDuplicates()) {
+            throw ApiProblem::bodyInvalidData(
+                new SchemaError(
+                    '/priceInfo',
+                    'Tariff names should be unique.'
+                )
+            );
+        }
+
+        return $request;
+    }
+}
