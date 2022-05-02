75682cbb170ede7b6c9abcaad5215e7462052ac6
Bert Ramakers <bertramakers@me.com>
2022-05-02 11:39:13 +0200

    Mark label ReadService & Interface as deprecated

--- src/Label/Services/ReadServiceInterface.php
+++ src/Label/Services/ReadServiceInterface.php
@@ -9,6 +9,10 @@ use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\Query;
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
 use CultuurNet\UDB3\StringLiteral;
 
+/**
+ * @deprecated
+ *   Use ReadRepositoryInterface directly.
+ */
 interface ReadServiceInterface
 {
     public function getByUuid(UUID $uuid): ?Entity;
