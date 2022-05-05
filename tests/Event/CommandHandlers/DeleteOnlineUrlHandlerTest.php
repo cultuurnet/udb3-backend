dadf5e9424d1fd4963526a0574df15d6ba7e04eb
Luc Wollants <lucwollants@gmail.com>
2022-05-05 12:06:55 +0200

    Add `DeleteOnlineUrlHandler` to handle `DeleteOnlineUrl` command

---
+++ tests/Event/CommandHandlers/DeleteOnlineUrlHandlerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventStore\EventStore;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType;
+use CultuurNet\UDB3\Event\Commands\DeleteOnlineUrl;
+use CultuurNet\UDB3\Event\EventRepository;
+use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\Events\OnlineUrlDeleted;
+use CultuurNet\UDB3\Event\Events\OnlineUrlUpdated;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\Theme;
+use CultuurNet\UDB3\Title;
+
+final class DeleteOnlineUrlHandlerTest extends CommandHandlerScenarioTestCase
+{
+    protected function createCommandHandler(EventStore $eventStore, EventBus $eventBus): CommandHandler
+    {
+        return new DeleteOnlineUrlHandler(new EventRepository($eventStore, $eventBus));
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_deleting_an_onlineUrl(): void
+    {
+        $eventId = '40021958-0ad8-46bd-8528-3ac3686818a1';
+
+        $this->scenario
+            ->withAggregateId($eventId)
+            ->given([
+                $this->getEventCreated($eventId),
+                new OnlineUrlUpdated($eventId, 'https://www.publiq.be/livestream'),
+            ])
+            ->when(new DeleteOnlineUrl($eventId))
+            ->then([new OnlineUrlDeleted($eventId)]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_delete_an_empty_onlineUrl(): void
+    {
+        $eventId = '40021958-0ad8-46bd-8528-3ac3686818a1';
+
+        $this->scenario
+            ->withAggregateId($eventId)
+            ->given([
+                $this->getEventCreated($eventId),
+            ])
+            ->when(new DeleteOnlineUrl($eventId))
+            ->then([]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_delete_an_already_deleted_onlineUrl(): void
+    {
+        $eventId = '40021958-0ad8-46bd-8528-3ac3686818a1';
+
+        $this->scenario
+            ->withAggregateId($eventId)
+            ->given([
+                $this->getEventCreated($eventId),
+                new OnlineUrlUpdated($eventId, 'https://www.publiq.be/livestream'),
+                new OnlineUrlDeleted($eventId),
+            ])
+            ->when(new DeleteOnlineUrl($eventId))
+            ->then([]);
+    }
+
+    private function getEventCreated(string $id): EventCreated
+    {
+        return new EventCreated(
+            $id,
+            new Language('nl'),
+            new Title('some representative title'),
+            new EventType('0.50.4.0.0', 'Concert'),
+            new LocationId('bfc60a14-6208-4372-942e-86e63744769a'),
+            new Calendar(CalendarType::PERMANENT()),
+            new Theme('1.8.1.0.0', 'Rock')
+        );
+    }
+}
