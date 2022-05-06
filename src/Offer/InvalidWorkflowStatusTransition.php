658fde95d2f85dbbf6c2f9b648dd2fbf07e83f69
Bert Ramakers <bertramakers@me.com>
2022-05-06 15:54:47 +0200

    Implement ConvertsToApiProblem on InvalidWorkflowStatusTransition

--- src/Offer/InvalidWorkflowStatusTransition.php
+++ src/Offer/InvalidWorkflowStatusTransition.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer;
 
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
 use CultuurNet\UDB3\Model\ValueObject\Moderation\WorkflowStatus;
 use Exception;
 
-final class InvalidWorkflowStatusTransition extends Exception
+final class InvalidWorkflowStatusTransition extends Exception implements ConvertsToApiProblem
 {
+    private WorkflowStatus $from;
+    private WorkflowStatus $to;
+
     public function __construct(WorkflowStatus $from, WorkflowStatus $to)
     {
         parent::__construct(
             'Cannot transition from workflowStatus "' . $from->toString() . '" to "' . $to->toString() . '".'
         );
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    public function toApiProblem(): ApiProblem
+    {
+        return ApiProblem::invalidWorkflowStatusTransition($this->from->toString(), $this->to->toString());
     }
 }
