5b6d17ba4b5faeb56e634b649c227230ab1a675f
Bert Ramakers <bertramakers@me.com>
2022-05-06 15:00:34 +0200

    Add failing test for empty location object in event json

--- tests/Http/Event/ImportEventRequestHandlerTest.php
+++ tests/Http/Event/ImportEventRequestHandlerTest.php
@@ -903,6 +903,34 @@ final class ImportEventRequestHandlerTest extends TestCase
     /**
      * @test
      */
+    public function it_does_not_crash_on_empty_location_object_but_returns_an_invalid_data_api_problem(): void
+    {
+        $event = [
+            'mainLanguage' => 'nl',
+            'name' => 'Pannenkoeken voor het goede doel',
+            'type' => [
+                'id' => '0.5.0.0.0',
+            ],
+            'theme' => [
+                'id' => '0.52.0.0.0',
+                'label' => 'Circus',
+            ],
+            'location' => (object) [],
+        ];
+
+        $expectedErrors = [
+            new SchemaError(
+                '/location',
+                'The required properties (@id) are missing'
+            ),
+        ];
+
+        $this->assertValidationErrors($event, $expectedErrors);
+    }
+
+    /**
+     * @test
+     */
     public function it_creates_a_new_event_from_legacy_format_and_ignores_address(): void
     {
         $eventId = 'f2850154-553a-4553-8d37-b32dd14546e4';
