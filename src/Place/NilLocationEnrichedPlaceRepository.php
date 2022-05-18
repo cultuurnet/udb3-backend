76f954cd6be3e88e7d2cf125fcc29f7068893a3a
Luc Wollants <lucwollants@gmail.com>
2022-05-18 14:10:22 +0200

    Add nil location enricher

---
+++ src/Place/NilLocationEnrichedPlaceRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place;
+
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Model\Place\ImmutablePlace;
+use CultuurNet\UDB3\Model\Serializer\Place\NilLocationNormalizer;
+use CultuurNet\UDB3\ReadModel\DocumentRepository;
+use CultuurNet\UDB3\ReadModel\DocumentRepositoryDecorator;
+use CultuurNet\UDB3\ReadModel\JsonDocument;
+
+final class NilLocationEnrichedPlaceRepository extends DocumentRepositoryDecorator
+{
+    private NilLocationNormalizer $nilLocationNormalizer;
+
+    public function __construct(NilLocationNormalizer $nilLocationNormalizer, DocumentRepository $documentRepository)
+    {
+        parent::__construct($documentRepository);
+        $this->nilLocationNormalizer = $nilLocationNormalizer;
+    }
+
+    public function fetch(string $id, bool $includeMetadata = false): JsonDocument
+    {
+        if ($id === LocationId::NIL_LOCATION) {
+            return (new JsonDocument($id))->withAssocBody(
+                $this->nilLocationNormalizer->normalize(ImmutablePlace::createNilLocation())
+            );
+        }
+
+        return parent::fetch($id, $includeMetadata);
+    }
+}
