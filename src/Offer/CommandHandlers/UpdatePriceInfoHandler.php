2706d7fbedf5ef837563beaffbeac02b8246582f
Jonas <jonas@cultuurnet.be>
2022-05-18 16:12:28 +0200

    Add Handler for UpdatePriceInfo for Offer

---
+++ src/Offer/CommandHandlers/UpdatePriceInfoHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Offer\Commands\UpdatePriceInfo;
+use CultuurNet\UDB3\Offer\OfferRepository;
+
+class UpdatePriceInfoHandler implements CommandHandler
+{
+    private OfferRepository $offerRepository;
+
+    public function __construct(OfferRepository $offerRepository)
+    {
+        $this->offerRepository = $offerRepository;
+    }
+
+    public function handle($command): void
+    {
+        if (!($command instanceof UpdatePriceInfo)) {
+            return;
+        }
+
+        $offer = $this->offerRepository->load($command->getItemId());
+        $offer->updatePriceInfo(
+            $command->getPriceInfo()
+        );
+        $this->offerRepository->save($offer);
+    }
+}
