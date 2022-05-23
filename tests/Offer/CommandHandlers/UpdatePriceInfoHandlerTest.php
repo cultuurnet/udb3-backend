897c701aff4ee73a22b72bc3fc3ad79b84295aaa
Luc Wollants <lucwollants@gmail.com>
2022-05-23 16:08:10 +0200

    Add test for UpdatePriceInfoHandler

---
+++ tests/Offer/CommandHandlers/UpdatePriceInfoHandlerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
+use Broadway\EventHandling\EventBus;
+use Broadway\EventStore\EventStore;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType;
+use CultuurNet\UDB3\Event\EventRepository;
+use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\Events\PriceInfoUpdated;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Language as LegacyLanguage;
+use CultuurNet\UDB3\Model\ValueObject\Price\PriceInfo;
+use CultuurNet\UDB3\Model\ValueObject\Price\Tariff;
+use CultuurNet\UDB3\Model\ValueObject\Price\TariffName;
+use CultuurNet\UDB3\Model\ValueObject\Price\Tariffs;
+use CultuurNet\UDB3\Model\ValueObject\Price\TranslatedTariffName;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Offer\Commands\UpdatePriceInfo;
+use CultuurNet\UDB3\Offer\OfferRepository;
+use CultuurNet\UDB3\Place\PlaceRepository;
+use CultuurNet\UDB3\Title;
+use Money\Currency;
+use Money\Money;
+
+final class UpdatePriceInfoHandlerTest extends CommandHandlerScenarioTestCase
+{
+    protected function createCommandHandler(EventStore $eventStore, EventBus $eventBus): CommandHandler
+    {
+        $repository = new OfferRepository(
+            new EventRepository($eventStore, $eventBus),
+            new PlaceRepository($eventStore, $eventBus)
+        );
+
+        return new UpdatePriceInfoHandler($repository);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_update_price_info(): void
+    {
+        $id = '1';
+
+        $command = new UpdatePriceInfo(
+            $id,
+            new PriceInfo(
+                new Tariff(
+                    new TranslatedTariffName(
+                        new Language('nl'),
+                        new TariffName('Basisprijs')
+                    ),
+                    new Money(1499, new Currency('EUR'))
+                ),
+                new Tariffs()
+            )
+        );
+
+        $expectedEvent = new PriceInfoUpdated(
+            $id,
+            \CultuurNet\UDB3\PriceInfo\PriceInfo::fromUdb3ModelPriceInfo($command->getPriceInfo())
+        );
+
+        $this->scenario
+            ->withAggregateId($id)
+            ->given([$this->getEventCreated($id)])
+            ->when($command)
+            ->then([$expectedEvent]);
+    }
+
+    private function getEventCreated(string $id): EventCreated
+    {
+        return new EventCreated(
+            $id,
+            new LegacyLanguage('nl'),
+            new Title('some representative title'),
+            new EventType('0.50.4.0.0', 'concert'),
+            new LocationId('d0cd4e9d-3cf1-4324-9835-2bfba63ac015'),
+            new Calendar(CalendarType::PERMANENT())
+        );
+    }
+}
