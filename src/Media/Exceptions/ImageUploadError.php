e229a20a8af18ecb6525136e00e6f8792f97f8ff
Luc Wollants <lucwollants@gmail.com>
2022-05-30 18:16:27 +0200

    Convert ImageUploadError to fileInvalidType

--- src/Media/Exceptions/ImageUploadError.php
+++ src/Media/Exceptions/ImageUploadError.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Media\Exceptions;
 
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
 use Exception;
 
-final class ImageUploadError extends Exception
+final class ImageUploadError extends Exception implements ConvertsToApiProblem
 {
+    public function toApiProblem(): ApiProblem
+    {
+        throw ApiProblem::fileInvalidType($this->getMessage());
+    }
 }
