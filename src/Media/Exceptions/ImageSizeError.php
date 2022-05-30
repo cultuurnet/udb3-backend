d9ac09114604f042fdaf77a7473883ac4975eea4
Luc Wollants <lucwollants@gmail.com>
2022-05-30 18:16:14 +0200

    Convert ImageSizeError to fileInvalidSize

--- src/Media/Exceptions/ImageSizeError.php
+++ src/Media/Exceptions/ImageSizeError.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Media\Exceptions;
 
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
 use Exception;
 
-final class ImageSizeError extends Exception
+final class ImageSizeError extends Exception implements ConvertsToApiProblem
 {
+    public function toApiProblem(): ApiProblem
+    {
+        throw ApiProblem::fileInvalidSize($this->getMessage());
+    }
 }
