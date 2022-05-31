b3a1ab316d129db37f3e61a06884b43ec00e9205
Bert Ramakers <bertramakers@me.com>
2022-05-31 18:01:46 +0200

    Fetch the label relations inline to simplify two foreaches into one

--- src/Label/ReadModels/JSON/ItemVisibilityProjector.php
+++ src/Label/ReadModels/JSON/ItemVisibilityProjector.php
@@ -10,6 +10,7 @@ use CultuurNet\UDB3\Label\Events\MadeInvisible;
 use CultuurNet\UDB3\Label\Events\MadeVisible;
 use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface;
 use CultuurNet\UDB3\Label\ValueObjects\LabelName as LegacyLabelName;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\LabelName;
 use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
 use CultuurNet\UDB3\ReadModel\DocumentRepository;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
@@ -57,12 +58,23 @@ class ItemVisibilityProjector implements EventListener, LoggerAwareInterface
 
     private function updateLabels(LegacyLabelName $labelName, bool $madeVisible): void
     {
-        $items = $this->getRelatedItems($labelName);
+        $labelRelations = $this->relationRepository->getLabelRelations($labelName);
 
         $removeFrom = $madeVisible ? 'hiddenLabels' : 'labels';
         $addTo = $madeVisible ? 'labels' : 'hiddenLabels';
 
-        foreach ($items as $item) {
+        foreach ($labelRelations as $labelRelation) {
+            try {
+                $item = $this->itemRepository->fetch((string) $labelRelation->getRelationId());
+            } catch (DocumentDoesNotExist $exception) {
+                $this->logger->alert(
+                    'Can not update visibility of label: "' . $labelRelation->getLabelName() . '"'
+                    . ' for the relation with id: "' . $labelRelation->getRelationId() . '"'
+                    . ' because the document could not be retrieved.'
+                );
+                continue;
+            }
+
             $offerLd = $item->getBody();
 
             $addToArray = isset($offerLd->{$addTo}) ? (array) $offerLd->{$addTo} : [];
@@ -81,26 +93,6 @@ class ItemVisibilityProjector implements EventListener, LoggerAwareInterface
             }
 
             $this->itemRepository->save($item->withBody($offerLd));
-        }
-    }
-
-    /**
-     * @return JsonDocument[]|Generator
-     */
-    private function getRelatedItems(LegacyLabelName $labelName): Generator
-    {
-        $labelRelations = $this->relationRepository->getLabelRelations($labelName);
-
-        foreach ($labelRelations as $labelRelation) {
-            try {
-                yield $this->itemRepository->fetch((string)$labelRelation->getRelationId());
-            } catch (DocumentDoesNotExist $exception) {
-                $this->logger->alert(
-                    'Can not update visibility of label: "' . $labelRelation->getLabelName() . '"'
-                    . ' for the relation with id: "' . $labelRelation->getRelationId() . '"'
-                    . ' because the document could not be retrieved.'
-                );
-            }
         }
     }
 }
