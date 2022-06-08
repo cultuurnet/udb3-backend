dd48f4012c523b3eceb2cf560351b5f1561cfe07
Luc Wollants <lucwollants@gmail.com>
2022-06-08 10:25:45 +0200

    Create migration to add canonical column to duplicate_places

---
+++ app/Migrations/Version20220608094122.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Silex\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220608094122 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $schema->getTable('duplicate_places')
+            ->addColumn('canonical', Type::GUID)
+            ->setLength(36)
+            ->setNotnull(false)
+            ->setDefault(null);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->getTable('duplicate_places')
+            ->dropColumn('canonical');
+    }
+}
