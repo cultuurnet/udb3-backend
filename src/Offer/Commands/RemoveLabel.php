36bf7234a115828f9ea28be650c073c83568f70d
Jonas <jonas@cultuurnet.be>
2022-06-16 12:51:23 +0200

    Add new methods to RemoveLabel

--- src/Offer/Commands/RemoveLabel.php
+++ src/Offer/Commands/RemoveLabel.php
@@ -4,6 +4,41 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer\Commands;
 
-final class RemoveLabel extends AbstractLabelCommand
+use CultuurNet\UDB3\Label;
+use CultuurNet\UDB3\StringLiteral;
+
+final class RemoveLabel extends AbstractCommand
 {
+    protected string $labelName;
+
+    protected bool $isVisible;
+
+    public function __construct(string $itemId, string $labelName, bool $isVisible = true)
+    {
+        parent::__construct($itemId);
+        $this->labelName = $labelName;
+        $this->isVisible = $isVisible;
+    }
+
+    public function getLabel(): Label
+    {
+        return new Label($this->labelName); // REMOVE
+    }
+
+    public function getLabelName(): string
+    {
+        return $this->labelName;
+    }
+
+    public function isVisible(): bool
+    {
+        return $this->isVisible;
+    }
+
+    public function getLabelNames(): array
+    {
+        return [
+            new StringLiteral($this->labelName),
+        ];
+    }
 }
