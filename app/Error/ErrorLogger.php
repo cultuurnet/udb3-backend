f40dcafad7009ec266a09cb7609bfcff0644456b
Luc Wollants <lucwollants@gmail.com>
2022-06-17 16:02:00 +0200

    Added new `ChangeNotAllowedByTicketSales` exception

--- app/Error/ErrorLogger.php
+++ app/Error/ErrorLogger.php
@@ -20,6 +20,7 @@ use CultuurNet\UDB3\Offer\CalendarTypeNotSupported;
 use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
 use CultuurNet\UDB3\Security\CommandAuthorizationException;
 use CultuurNet\UDB3\UiTPAS\Event\CommandHandling\Validation\EventHasTicketSalesException;
+use CultuurNet\UDB3\UiTPAS\Validation\ChangeNotAllowedByTicketSales;
 use Psr\Log\LoggerInterface;
 use Respect\Validation\Exceptions\GroupedValidationException;
 use Symfony\Component\Console\Exception\RuntimeException as SymfonyConsoleRuntimeException;
@@ -49,6 +50,7 @@ final class ErrorLogger
         AggregateNotFoundException::class,
         MethodNotAllowedHttpException::class,
         EventHasTicketSalesException::class,
+        ChangeNotAllowedByTicketSales::class,
         MediaObjectNotFoundException::class,
         DocumentDoesNotExist::class,
         NotWellFormedException::class,
