5054640205d4d50bc1cbcd9b193a7390928241a4
Luc Wollants <lucwollants@gmail.com>
2022-06-20 16:58:49 +0200

    Remove `ReadRepositoryInterface` from `RemoveLabelHandler`

--- src/Organizer/CommandHandler/RemoveLabelHandler.php
+++ src/Organizer/CommandHandler/RemoveLabelHandler.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Organizer\CommandHandler;
 
 use Broadway\CommandHandling\CommandHandler;
-use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface;
-use CultuurNet\UDB3\Label\ValueObjects\Visibility;
 use CultuurNet\UDB3\Organizer\Commands\RemoveLabel;
 use CultuurNet\UDB3\Organizer\OrganizerRepository;
 
@@ -14,24 +12,15 @@ final class RemoveLabelHandler implements CommandHandler
 {
     private OrganizerRepository $organizerRepository;
 
-    private ReadRepositoryInterface $labelRepository;
-
-    public function __construct(OrganizerRepository $organizerRepository, ReadRepositoryInterface $labelRepository)
+    public function __construct(OrganizerRepository $organizerRepository)
     {
         $this->organizerRepository = $organizerRepository;
-        $this->labelRepository = $labelRepository;
     }
 
     public function handle($command): void
     {
         if (!($command instanceof RemoveLabel)) {
             return;
-        }
-
-        $visible = true;
-        $readModelLabelEntity = $this->labelRepository->getByName($command->getLabelName());
-        if ($readModelLabelEntity) {
-            $visible = $readModelLabelEntity->getVisibility()->sameAs(Visibility::VISIBLE());
         }
 
         $labelName = $command->getLabelName();
