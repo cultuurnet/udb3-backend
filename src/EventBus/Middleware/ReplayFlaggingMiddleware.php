8ac8f72723b4d974ff2b499a0438bd05827a0bef
Bert Ramakers <bertramakers@me.com>
2022-07-18 15:03:31 +0200

    Add a middleware to flag replayed messages

---
+++ src/EventBus/Middleware/ReplayFlaggingMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\EventBus\Middleware;
+
+use Broadway\Domain\DomainEventStream;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Broadway\Domain\DomainMessageIsReplayed;
+use CultuurNet\UDB3\EventBus\EventBusMiddleware;
+
+final class ReplayFlaggingMiddleware implements EventBusMiddleware
+{
+    private static bool $replaying = false;
+
+    public static function startReplayMode(): void
+    {
+        self::$replaying = true;
+    }
+
+    public static function stopReplayMode(): void
+    {
+        self::$replaying = false;
+    }
+
+    public function beforePublish(DomainEventStream $domainEventStream): DomainEventStream
+    {
+        $replayMetadata = new Metadata(
+            [
+                DomainMessageIsReplayed::METADATA_REPLAY_KEY => self::$replaying,
+            ]
+        );
+
+        $messages = [];
+        foreach ($domainEventStream as $domainMessage) {
+            if ($domainMessage instanceof DomainMessage) {
+                $domainMessage = $domainMessage->andMetadata($replayMetadata);
+            }
+            $messages[] = $domainMessage;
+        }
+        return new DomainEventStream($messages);
+    }
+}
---
+++ tests/EventBus/Middleware/ReplayFlaggingMiddlewareTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\EventBus\Middleware;
+
+use Broadway\Domain\DateTime;
+use Broadway\Domain\DomainEventStream;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+final class ReplayFlaggingMiddlewareTest extends TestCase
+{
+    private ReplayFlaggingMiddleware $replayFlaggingMiddleware;
+
+    protected function setUp(): void
+    {
+        $this->replayFlaggingMiddleware = new ReplayFlaggingMiddleware();
+    }
+
+    /**
+     * @test
+     */
+    public function it_adds_a_boolean_replay_key_to_the_metadata_of_every_message(): void
+    {
+        $createDomainMessage = static function (int $id) {
+            return new DomainMessage(
+                Uuid::uuid4(),
+                0,
+                new Metadata(),
+                (object) ['id' => $id],
+                DateTime::now()
+            );
+        };
+
+        $domainMessage1 = $createDomainMessage(1);
+        $domainMessage2 = $createDomainMessage(2);
+        $stream1 = new DomainEventStream([$domainMessage1, $domainMessage2]);
+
+        $domainMessage3 = $createDomainMessage(3);
+        $domainMessage4 = $createDomainMessage(4);
+        $stream2 = new DomainEventStream([$domainMessage3, $domainMessage4]);
+
+        $domainMessage5 = $createDomainMessage(5);
+        $domainMessage6 = $createDomainMessage(6);
+        $stream3 = new DomainEventStream([$domainMessage5, $domainMessage6]);
+
+        $expectedReplayMetadata = [
+            false,
+            false,
+            true,
+            true,
+            false,
+            false,
+        ];
+
+        $processedStream1 = $this->replayFlaggingMiddleware->beforePublish($stream1);
+        ReplayFlaggingMiddleware::startReplayMode();
+        $processedStream2 = $this->replayFlaggingMiddleware->beforePublish($stream2);
+        ReplayFlaggingMiddleware::stopReplayMode();
+        $processedStream3 = $this->replayFlaggingMiddleware->beforePublish($stream3);
+
+        $actualReplayMetadata = [];
+        foreach ([$processedStream1, $processedStream2, $processedStream3] as $stream) {
+            foreach ($stream as $domainMessage) {
+                /** @var DomainMessage $domainMessage */
+                $metadata = $domainMessage->getMetadata();
+                $actualReplayMetadata[] = $metadata->get('replayed');
+            }
+        }
+
+        $this->assertEquals($expectedReplayMetadata, $actualReplayMetadata);
+    }
+}
