7e5b884f645d451ecd42a7ec4a78210b67a520a7
Bert Ramakers <bertramakers@me.com>
2022-08-03 16:00:07 +0200

    Boyscouting of types in event relation RepositoryInterface and DBAL implementation

--- src/Event/ReadModel/Relations/RepositoryInterface.php
+++ src/Event/ReadModel/Relations/RepositoryInterface.php
@@ -6,62 +6,13 @@ namespace CultuurNet\UDB3\Event\ReadModel\Relations;
 
 interface RepositoryInterface
 {
-    /**
-     * @param string $eventId
-     * @param string $placeId
-     * @param string $organizerId
-     */
-    public function storeRelations($eventId, $placeId, $organizerId);
-
-    /**
-     * @param string $eventId
-     * @param string $organizerId
-     */
-    public function storeOrganizer($eventId, $organizerId);
-
-    /**
-     * @param string $eventId
-     * @param string $placeId
-     */
-    public function storePlace($eventId, $placeId);
-
-    /**
-     * @param string $eventId
-     */
-    public function removeOrganizer($eventId);
-
-    /**
-     * @param string $placeId
-     *
-     * @return string[]
-     *  A list of event Ids.
-     */
-    public function getEventsLocatedAtPlace($placeId);
-
-    /**
-     * @param string $organizerId
-     *
-     * @return string[]
-     *  A list of event Ids.
-     */
-    public function getEventsOrganizedByOrganizer($organizerId);
-
-    /**
-     * @param string $eventId
-     * @return string|null
-     */
-    public function getPlaceOfEvent($eventId);
-
-    /**
-     * @param string $eventId
-     * @return string|null
-     */
-    public function getOrganizerOfEvent($eventId);
-
-    /**
-     * Remove all the relations that are linked to this event.
-     *
-     * @param string $eventId
-     */
-    public function removeRelations($eventId);
+    public function storeRelations(string $eventId, string $placeId, string $organizerId): void;
+    public function storeOrganizer(string $eventId, string $organizerId): void;
+    public function storePlace(string $eventId, string $placeId): void;
+    public function removeOrganizer(string $eventId): void;
+    public function getEventsLocatedAtPlace(string $placeId): array;
+    public function getEventsOrganizedByOrganizer(string $organizerId): array;
+    public function getPlaceOfEvent(string $eventId): ?string;
+    public function getOrganizerOfEvent(string $eventId): ?string;
+    public function removeRelations(string $eventId): void;
 }
