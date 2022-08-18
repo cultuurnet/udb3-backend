1ca90349a076cd449fd48e8d26d19a3a29800f8e
Luc Wollants <lucwollants@gmail.com>
2022-08-18 17:16:21 +0200

    Test new SkipEventsRequestHandler

---
+++ tests/Http/Productions/SkipEventsRequestHandlerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Productions;
+
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use CultuurNet\UDB3\Event\Productions\RejectSuggestedEventPair;
+use CultuurNet\UDB3\Event\Productions\SimilarEventPair;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+final class SkipEventsRequestHandlerTest extends TestCase
+{
+    private TraceableCommandBus $commandBus;
+
+    private SkipEventsRequestHandler $skipEventsRequestHandler;
+
+    protected function setUp(): void
+    {
+        $this->commandBus = new TraceableCommandBus();
+
+        $this->skipEventsRequestHandler = new SkipEventsRequestHandler(
+            $this->commandBus,
+            new SkipEventsValidator()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_skip_events(): void
+    {
+        $eventId1 = Uuid::uuid4()->toString();
+        $eventId2 = Uuid::uuid4()->toString();
+
+        $request = (new Psr7RequestBuilder())
+            ->withJsonBodyFromArray(
+                [
+                    'eventIds' => [
+                        $eventId1,
+                        $eventId2,
+                    ],
+                ]
+            )
+            ->build('POST');
+
+        $this->commandBus->record();
+
+        $response = $this->skipEventsRequestHandler->handle($request);
+
+        $this->assertEquals(
+            [new RejectSuggestedEventPair(new SimilarEventPair($eventId1, $eventId2))],
+            $this->commandBus->getRecordedCommands()
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+}
