7a89d29fe5a83b06217872259b52e53ff9769c53
Luc Wollants <lucwollants@gmail.com>
2022-08-18 14:28:29 +0200

    Add tests to verify the create production validation

--- tests/Http/Productions/CreateProductionRequestHandlerTest.php
+++ tests/Http/Productions/CreateProductionRequestHandlerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Http\Productions;
 
 use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use CultuurNet\UDB3\Deserializer\DataValidationException;
 use CultuurNet\UDB3\Event\Productions\GroupEventsAsProduction;
 use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
 use CultuurNet\UDB3\Http\Response\AssertJsonResponseTrait;
@@ -99,5 +100,77 @@ final class CreateProductionRequestHandlerTest extends TestCase
             new JsonLdResponse(['productionId' => $recordedCommand->getItemId()], 201),
             $response
         );
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidDataProvider
+     */
+    public function it_validates_incoming_data_to_create_a_production(array $invalidData, array $messages): void
+    {
+        $request = (new Psr7RequestBuilder())
+            ->withJsonBodyFromArray($invalidData)
+            ->build('POST');
+
+        $dataValidationException = new DataValidationException();
+        $dataValidationException->setValidationMessages($messages);
+
+        $this->assertDataValidationException(
+            $dataValidationException,
+            fn() => $this->createProductionRequestHandler->handle($request),
+        );
+    }
+
+    public function invalidDataProvider(): array
+    {
+        return [
+            'missing_name' => [
+                [
+                    'eventIds' => ['275a6c2b-4374-4ba5-a90b-3e268de24f5a'],
+                ],
+                [
+                    'name' => 'Required but could not be found',
+                ],
+            ],
+            'empty_name' => [
+                [
+                    'name' => '',
+                    'eventIds' => ['275a6c2b-4374-4ba5-a90b-3e268de24f5a'],
+                ],
+                [
+                    'name' => 'Cannot be empty',
+                ],
+            ],
+            'missing_event_ids' => [
+                [
+                    'name' => 'name',
+                ],
+                [
+                    'eventIds' => 'Required but could not be found',
+                ],
+            ],
+            'empty_event_ids' => [
+                [
+                    'name' => 'name',
+                    'eventIds' => [],
+                ],
+                [
+                    'eventIds' => 'At least one event should be provided',
+                ],
+            ],
+        ];
+    }
+
+    private function assertDataValidationException(
+        DataValidationException $dataValidationException,
+        callable $callback
+    ): void {
+        try {
+            $callback();
+        } catch (DataValidationException $exception) {
+            $this->assertEquals($dataValidationException->getMessage(), $exception->getMessage());
+            $this->assertEquals($dataValidationException->getValidationMessages(), $exception->getValidationMessages());
+            return;
+        }
     }
 }
