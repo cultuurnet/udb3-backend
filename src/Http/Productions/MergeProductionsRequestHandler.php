0c465f9f58fb4a362fd0884bbaf9b0b78be83fcf
Luc Wollants <lucwollants@gmail.com>
2022-08-18 16:46:11 +0200

    Add new request handler MergeProductionsRequestHandler

---
+++ src/Http/Productions/MergeProductionsRequestHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Productions;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Event\Productions\MergeProductions;
+use CultuurNet\UDB3\Event\Productions\ProductionId;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class MergeProductionsRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $fromProductionId = $routeParameters->get('fromProductionId');
+        $productionId = $routeParameters->get('productionId');
+
+        $command = new MergeProductions(
+            ProductionId::fromNative($fromProductionId),
+            ProductionId::fromNative($productionId)
+        );
+
+        $this->commandBus->dispatch($command);
+
+        return new NoContentResponse();
+    }
+}
