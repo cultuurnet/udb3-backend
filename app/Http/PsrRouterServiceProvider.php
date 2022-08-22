02a0febc2966fba2174a40ec22cfad40be533334
Bert Ramakers <bertramakers@me.com>
2022-08-22 19:09:10 +0200

    Use a custom strategy to handle OPTIONS requests

--- app/Http/PsrRouterServiceProvider.php
+++ app/Http/PsrRouterServiceProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Http;
 
+use CultuurNet\UDB3\Http\CustomLeagueRouterStrategy;
 use CultuurNet\UDB3\Http\Offer\GetDetailRequestHandler;
-use CultuurNet\UDB3\Http\Response\NoContentResponse;
 use League\Route\Router;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
@@ -17,10 +17,7 @@ final class PsrRouterServiceProvider implements ServiceProviderInterface
         $app[Router::class] = $app::share(
             function (Application $app) {
                 $router = new Router();
-
-                // Return 204 No Content for every OPTIONS request to support CORS.
-                // The necessary CORS headers will be added by a Middleware that adds them for every response.
-                $router->options('/{path:.*}', fn () => new NoContentResponse());
+                $router->setStrategy(new CustomLeagueRouterStrategy());
 
                 $router->get('/{offerType}/{offerId}/', [$app[GetDetailRequestHandler::class], 'handle']);
 
