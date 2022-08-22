b5f02e88dd6450c7ca4d838be6129ee9d8b29e56
Bert Ramakers <bertramakers@me.com>
2022-08-22 19:11:43 +0200

    Add catch-all route in Silex router again for OPTIONS requests
    Otherwise routes that are not registered in the new router yet will not work with CORS anymore

--- web/index.php
+++ web/index.php
@@ -252,6 +252,14 @@ $app->mount('/news-articles', new CuratorsControllerProvider());
 
 $app->mount(ImportControllerProvider::PATH, new ImportControllerProvider());
 
+// Match with any OPTIONS request with any URL and return a 204 No Content. Actual CORS headers will be added by an
+// ->after() middleware, which adds CORS headers to every request (so non-preflighted requests like simple GETs also get
+// the needed CORS headers).
+// Note that the new PSR router in PsrRouterServiceProvider already supports OPTIONS requests for all routes registered
+// in the new router, so this can be removed completely once all route definitions and handlers are moved to the new
+// router.
+$app->options('/{path}', fn () => new NoContentResponse())->assert('path', '^.+$');
+
 // Add CORS headers to every request. We explicitly allow everything, because we don't use cookies and our API is not on
 // an internal network, so CORS requests are never a security issue in our case. This greatly reduces the risk of CORS
 // bugs in our frontend and other integrations.
