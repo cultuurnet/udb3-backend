b6bafd7f892e2adcb41c8f07a1faf3b4b7a20a70
Bert Ramakers <bertramakers@me.com>
2022-08-23 15:06:46 +0200

    Add RequestAuthenticator that returns a 401 response for every route that is not public

---
+++ src/Http/Auth/RequestAuthenticator.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Auth;
+
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\Response\ApiProblemJsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class RequestAuthenticator
+{
+    private array $publicRoutes = [];
+
+    public function addPublicRoute(string $pathPattern, array $methods = []): void
+    {
+        $this->publicRoutes[$pathPattern] = $methods;
+    }
+
+    public function authenticate(ServerRequestInterface $request): ?ResponseInterface
+    {
+        if ($this->isPublicRoute($request)) {
+            return null;
+        }
+
+        return new ApiProblemJsonResponse(
+            ApiProblem::unauthorized('Route is not public')
+        );
+    }
+
+    private function isPublicRoute(ServerRequestInterface $request): bool
+    {
+        $method = $request->getMethod();
+        $path = $request->getUri()->getPath();
+
+        foreach ($this->publicRoutes as $pathPattern => $methods) {
+            if (in_array($method, $methods, true) && preg_match($pathPattern, $path) === 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
