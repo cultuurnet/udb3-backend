779c6945acba0e06f8f81e895dbf1b780059fcfb
Bert Ramakers <bertramakers@me.com>
2022-08-24 11:32:24 +0200

    Move auth services from bootstrap.php to AuthServiceProvider

--- app/Authentication/AuthServiceProvider.php
+++ app/Authentication/AuthServiceProvider.php
@@ -16,6 +16,81 @@ final class AuthServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app): void
     {
+        $app['current_user_id'] = $app::share(
+            function (Application $app) {
+                /* @var Impersonator $impersonator */
+                $impersonator = $app['impersonator'];
+                if ($impersonator->getUserId()) {
+                    return $impersonator->getUserId();
+                }
+
+                $token = $app['jwt'];
+                if (!($token instanceof JsonWebToken)) {
+                    // The token in the current request is missing (for example because it's a public route)
+                    return null;
+                }
+                return $token->getUserId();
+            }
+        );
+
+        $app['current_user_is_god_user'] = $app::share(
+            function (Application $app) {
+                return in_array(
+                    $app['current_user_id'],
+                    $app['config']['user_permissions']['allow_all'],
+                    true
+                );
+            }
+        );
+
+        $app['jwt'] = $app::share(
+            function (Application $app) {
+                // Check first if we're impersonating someone.
+                /* @var Impersonator $impersonator */
+                $impersonator = $app['impersonator'];
+                if ($impersonator->getJwt()) {
+                    return $impersonator->getJwt();
+                }
+
+                try {
+                    /* @var RequestAuthenticator $requestAuthenticator */
+                    $requestAuthenticator = $app[RequestAuthenticator::class];
+                } catch (\InvalidArgumentException $e) {
+                    // Running from CLI or unauthorized (will be further handled by the auth middleware)
+                    return null;
+                }
+
+                return $requestAuthenticator->getToken();
+            }
+        );
+
+        $app['api_key'] = $app->share(
+            function (Application $app) {
+                // Check first if we're impersonating someone.
+                // This is done when handling commands.
+                /* @var Impersonator $impersonator */
+                $impersonator = $app['impersonator'];
+                if ($impersonator->getApiKey()) {
+                    return $impersonator->getApiKey();
+                }
+
+                // If not impersonating then use the api key from the request.
+                // It is possible to work without api key then null is returned
+                // and will be handled with a pass through authorizer.
+                return isset($app['auth.api_key']) ? $app['auth.api_key'] : null;
+            }
+        );
+
+        $app['api_client_id'] = $app::share(
+            function (Application $app) {
+                $token = $app['jwt'];
+                if ($token instanceof JsonWebToken) {
+                    return $token->getClientId();
+                }
+                return null;
+            }
+        );
+
         $app[ConsumerReadRepository::class] = $app->share(
             function (Application $app): ConsumerReadRepository {
                 return new InMemoryConsumerRepository(
