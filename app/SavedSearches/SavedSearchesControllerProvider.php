a9daac89c0327a18b068d24b257e93a86a13c4e9
Jonas <jonas@cultuurnet.be>
2022-08-25 13:15:09 +0200

    create binding for SavedSearchesControllerProvider

--- app/SavedSearches/SavedSearchesControllerProvider.php
+++ app/SavedSearches/SavedSearchesControllerProvider.php
@@ -6,6 +6,7 @@ namespace CultuurNet\UDB3\Silex\SavedSearches;
 
 use CultuurNet\UDB3\Http\SavedSearches\EditSavedSearchesRestController;
 use CultuurNet\UDB3\Http\SavedSearches\ReadSavedSearchesController;
+use CultuurNet\UDB3\Http\SavedSearches\SaveSavedSearchesRequestHandler;
 use CultuurNet\UDB3\SavedSearches\ReadModel\SavedSearchRepositoryInterface;
 use Silex\Application;
 use Silex\ControllerCollection;
@@ -35,12 +36,21 @@ class SavedSearchesControllerProvider implements ControllerProviderInterface
             }
         );
 
+        $app[SaveSavedSearchesRequestHandler::class] = $app->share(
+            function (Application $app) {
+                return new EditSavedSearchesRestController(
+                    $app['current_user_id'],
+                    $app['event_command_bus']
+                );
+            }
+        );
+
         /* @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/v3/', 'saved_searches_read_controller:ownedByCurrentUser');
 
-        $controllers->post('/v3/', 'saved_searches_edit_controller:save');
+        $controllers->post('/v3/', SaveSavedSearchesRequestHandler::class);
         $controllers->delete('/v3/{id}/', 'saved_searches_edit_controller:delete');
 
         return $controllers;
