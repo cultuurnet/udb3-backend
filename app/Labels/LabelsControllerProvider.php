26b72a28b3d91097126adb35589a91ef7330a367
Luc Wollants <lucwollants@gmail.com>
2022-08-25 11:18:59 +0200

    Bind the new `PatchLabelRequestHandler`

--- app/Labels/LabelsControllerProvider.php
+++ app/Labels/LabelsControllerProvider.php
@@ -6,7 +6,7 @@ namespace CultuurNet\UDB3\Silex\Labels;
 
 use Broadway\UuidGenerator\Rfc4122\Version4Generator;
 use CultuurNet\UDB3\Http\Label\CreateLabelRequestHandler;
-use CultuurNet\UDB3\Http\Label\EditRestController;
+use CultuurNet\UDB3\Http\Label\PatchLabelRequestHandler;
 use CultuurNet\UDB3\Http\Label\ReadRestController;
 use Silex\Application;
 use Silex\ControllerCollection;
@@ -15,7 +15,6 @@ use Silex\ControllerProviderInterface;
 class LabelsControllerProvider implements ControllerProviderInterface
 {
     public const READ_REST_CONTROLLER = 'labels.read_rest_controller';
-    public const EDIT_REST_CONTROLLER = 'labels.edit_rest_controller';
 
     public function connect(Application $app): ControllerCollection
     {
@@ -26,11 +25,15 @@ class LabelsControllerProvider implements ControllerProviderInterface
             )
         );
 
+        $app[PatchLabelRequestHandler::class] = $app->share(
+            fn (Application $app) => new PatchLabelRequestHandler($app['event_command_bus'])
+        );
+
         $controllers = $app['controllers_factory'];
         $controllers->post('/', CreateLabelRequestHandler::class);
+        $controllers->patch('/{id}/', PatchLabelRequestHandler::class);
 
         $this->setUpReadRestController($app);
-        $this->setUpEditRestController($app);
 
         return $this->setControllerPaths($controllers);
     }
@@ -48,22 +51,9 @@ class LabelsControllerProvider implements ControllerProviderInterface
         );
     }
 
-
-    private function setUpEditRestController(Application $app): void
-    {
-        $app[self::EDIT_REST_CONTROLLER] = $app->share(
-            function (Application $app) {
-                return new EditRestController(
-                    $app[LabelServiceProvider::WRITE_SERVICE]
-                );
-            }
-        );
-    }
-
     private function setControllerPaths(ControllerCollection $controllers): ControllerCollection
     {
         $controllers->get('/{id}/', self::READ_REST_CONTROLLER . ':get');
-        $controllers->patch('/{id}/', self::EDIT_REST_CONTROLLER . ':patch');
         $controllers->get('/', self::READ_REST_CONTROLLER . ':search');
 
         return $controllers;
