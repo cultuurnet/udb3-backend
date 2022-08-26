b0d4a85a13fdc507ca5310e2dd3ee9e9b235fee0
Jonas <jonas@cultuurnet.be>
2022-08-26 14:13:49 +0200

    refactor GetPermissionsForCurrentUserRequestHandler

--- src/Http/Offer/GetPermissionsForCurrentUserRequestHandler.php
+++ src/Http/Offer/GetPermissionsForCurrentUserRequestHandler.php
@@ -8,22 +8,13 @@ use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\JsonResponse;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
 use CultuurNet\UDB3\Security\Permission\PermissionVoter;
-use CultuurNet\UDB3\StringLiteral;
 use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Slim\Psr7\Headers;
 
-final class GetPermissionsForCurrentUserRequestHandler implements RequestHandlerInterface
+final class GetPermissionsForCurrentUserRequestHandler extends GetPermissionsRequestHandler implements RequestHandlerInterface
 {
-    /**
-     * @var Permission[]
-     */
-    private array $permissions;
-
-    private PermissionVoter $permissionVoter;
-
     private ?string $currentUserId;
 
     /**
@@ -34,20 +25,17 @@ final class GetPermissionsForCurrentUserRequestHandler implements RequestHandler
         PermissionVoter $permissionVoter,
         ?string $currentUserId = null
     ) {
-        $this->permissions = $permissions;
-        $this->permissionVoter = $permissionVoter;
+        parent::__construct($permissions, $permissionVoter);
         $this->currentUserId = $currentUserId;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $headers = new Headers();
-        $headers->setHeader('Cache-Control', 'private');
         if (is_null($this->currentUserId)) {
             return new JsonResponse(
                 ['permissions' => []],
                 StatusCodeInterface::STATUS_OK,
-                $headers
+                $this->getPrivateHeaders()
             );
         }
 
@@ -58,24 +46,6 @@ final class GetPermissionsForCurrentUserRequestHandler implements RequestHandler
             $offerId,
             $this->currentUserId
         );
-        return new JsonResponse($permissions,StatusCodeInterface::STATUS_OK, $headers);
-    }
-
-    private function getPermissions(string $offerId, string $userId): array
-    {
-        $permissionsToReturn = [];
-        foreach ($this->permissions as $permission) {
-            $hasPermission = $this->permissionVoter->isAllowed(
-                $permission,
-                new StringLiteral($offerId),
-                new StringLiteral($userId)
-            );
-
-            if ($hasPermission) {
-                $permissionsToReturn[] = $permission->toString();
-            }
-        }
-
-        return ['permissions' => $permissionsToReturn];
+        return new JsonResponse($permissions, StatusCodeInterface::STATUS_OK, $this->getPrivateHeaders());
     }
 }
