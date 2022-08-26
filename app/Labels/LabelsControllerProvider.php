2a3022a5c39af74814fe9dc64a25f589ef4629e4
Luc Wollants <lucwollants@gmail.com>
2022-08-26 11:51:02 +0200

    Bind new `SearchLabelsRequestHandler`

--- app/Labels/LabelsControllerProvider.php
+++ app/Labels/LabelsControllerProvider.php
@@ -8,15 +8,13 @@ use Broadway\UuidGenerator\Rfc4122\Version4Generator;
 use CultuurNet\UDB3\Http\Label\CreateLabelRequestHandler;
 use CultuurNet\UDB3\Http\Label\GetLabelRequestHandler;
 use CultuurNet\UDB3\Http\Label\PatchLabelRequestHandler;
-use CultuurNet\UDB3\Http\Label\ReadRestController;
+use CultuurNet\UDB3\Http\Label\SearchLabelsRequestHandler;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
 
 class LabelsControllerProvider implements ControllerProviderInterface
 {
-    public const READ_REST_CONTROLLER = 'labels.read_rest_controller';
-
     public function connect(Application $app): ControllerCollection
     {
         $app[CreateLabelRequestHandler::class] = $app->share(
@@ -34,33 +32,19 @@ class LabelsControllerProvider implements ControllerProviderInterface
             fn (Application $app) => new GetLabelRequestHandler($app[LabelServiceProvider::JSON_READ_REPOSITORY])
         );
 
+        $app[SearchLabelsRequestHandler::class] = $app->share(
+            fn (Application $app) => new SearchLabelsRequestHandler(
+                $app[LabelServiceProvider::JSON_READ_REPOSITORY],
+                $app[LabelServiceProvider::QUERY_FACTORY]
+            )
+        );
+
         $controllers = $app['controllers_factory'];
         $controllers->post('/', CreateLabelRequestHandler::class);
         $controllers->patch('/{labelId}/', PatchLabelRequestHandler::class);
 
         $controllers->get('/{labelId}/', GetLabelRequestHandler::class);
-
-        $this->setUpReadRestController($app);
-
-        return $this->setControllerPaths($controllers);
-    }
-
-
-    private function setUpReadRestController(Application $app): void
-    {
-        $app[self::READ_REST_CONTROLLER] = $app->share(
-            function (Application $app) {
-                return new ReadRestController(
-                    $app[LabelServiceProvider::JSON_READ_REPOSITORY],
-                    $app[LabelServiceProvider::QUERY_FACTORY]
-                );
-            }
-        );
-    }
-
-    private function setControllerPaths(ControllerCollection $controllers): ControllerCollection
-    {
-        $controllers->get('/', self::READ_REST_CONTROLLER . ':search');
+        $controllers->get('/', SearchLabelsRequestHandler::class);
 
         return $controllers;
     }
