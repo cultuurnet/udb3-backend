9f886a811a7d2110f2d272c8fd573b9c9d81a1d5
Jonas <jonas@cultuurnet.be>
2022-08-29 10:10:38 +0200

    implement GivenUserHasPermissionRequestHandler

---
+++ src/Http/Offer/GivenUserHasPermissionRequestHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class GivenUserHasPermissionRequestHandler extends HasPermissionRequestHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $offerId = $routeParameters->getOfferId();
+        $userId = $routeParameters->get('userId');
+
+        $hasPermission = $this->hasPermission($offerId, $userId);
+
+        return new JsonResponse($hasPermission, StatusCodeInterface::STATUS_OK, $this->getPrivateHeaders());
+    }
+}
