96f88e8eb2edd1e2e2ee1ddf0adec88f73805e72
Jonas <jonas@cultuurnet.be>
2022-08-29 10:10:19 +0200

    create Abstract base class HasPermissionRequestHandler

---
+++ src/Http/Offer/HasPermissionRequestHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\Permission\PermissionVoter;
+use CultuurNet\UDB3\StringLiteral;
+use Slim\Psr7\Headers;
+
+abstract class HasPermissionRequestHandler
+{
+    protected Permission $permission;
+
+    protected PermissionVoter $permissionVoter;
+
+    public function __construct(
+        Permission $permission,
+        PermissionVoter $permissionVoter
+    ) {
+        $this->permission = $permission;
+        $this->permissionVoter = $permissionVoter;
+    }
+
+    protected function hasPermission(string $offerId, ?string $userId = null): array
+    {
+        if ($userId) {
+            $hasPermission = $this->permissionVoter->isAllowed(
+                $this->permission,
+                new StringLiteral($offerId),
+                new StringLiteral($userId)
+            );
+        } else {
+            $hasPermission = false;
+        }
+
+        return ['hasPermission' => $hasPermission];
+    }
+
+    protected function getPrivateHeaders(): Headers
+    {
+        $headers = new Headers();
+        $headers->setHeader('Cache-Control', 'private');
+        return $headers;
+    }
+}
