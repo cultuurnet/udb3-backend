26fd26b12abc133ccacbf38a706509b082d74807
Jonas <jonas@cultuurnet.be>
2022-08-29 10:10:47 +0200

    implement CurrentUserHasPermissionRequestHandler

---
+++ src/Http/Offer/CurrentUserHasPermissionRequestHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\Permission\PermissionVoter;
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CurrentUserHasPermissionRequestHandler extends HasPermissionRequestHandler implements RequestHandlerInterface
+{
+    private ?string $currentUserId;
+
+    public function __construct(
+        Permission $permission,
+        PermissionVoter $permissionVoter,
+        ?string $currentUserId = null
+    ) {
+        parent::__construct($permission, $permissionVoter);
+        $this->currentUserId = $currentUserId;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $offerId = $routeParameters->getOfferId();
+
+        $hasPermission = $this->hasPermission($offerId, $this->currentUserId);
+
+        return new JsonResponse($hasPermission, StatusCodeInterface::STATUS_OK, $this->getPrivateHeaders());
+    }
+}
