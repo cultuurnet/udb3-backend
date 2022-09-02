63b4c9c548402887cf1881aed36e48fc3257e317
Bert Ramakers <bertramakers@me.com>
2022-09-02 12:01:14 +0200

    Use the InvokableRequestHandlerContainer in the router so we can use the router's existing lazy-loading

--- app/Http/PsrRouterServiceProvider.php
+++ app/Http/PsrRouterServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Silex\Http;
 
 use CultuurNet\UDB3\Http\CustomLeagueRouterStrategy;
-use CultuurNet\UDB3\Http\LazyLoadingRequestHandler;
+use CultuurNet\UDB3\Http\InvokableRequestHandlerContainer;
 use CultuurNet\UDB3\Http\Offer\GetDetailRequestHandler;
 use CultuurNet\UDB3\Silex\PimplePSRContainerBridge;
 use League\Route\Router;
@@ -19,11 +19,23 @@ final class PsrRouterServiceProvider implements ServiceProviderInterface
         $app[Router::class] = $app::share(
             function (Application $app) {
                 $router = new Router();
-                $router->setStrategy(new CustomLeagueRouterStrategy());
 
+                // Create a PSR container based on the Silex (Pimple) container, to allow the router to resolve
+                // request handler class names to actual instances.
                 $container = new PimplePSRContainerBridge($app);
 
-                $router->get('/{offerType:events|places}/{offerId}/', new LazyLoadingRequestHandler($container, GetDetailRequestHandler::class));
+                // Decorate the PSR container with InvokableRequestHandlerContainer so that every
+                // RequestHandlerInterface that gets requested by the router is decorated with InvokableRequestHandler,
+                // because the League router needs the router to be a callable at the time of writing.
+                $container = new InvokableRequestHandlerContainer($container);
+
+                // Use a custom strategy so we can implement getOptionsCallable() on the strategy, to support CORS
+                // pre-flight requests. We also have to set the container on the strategy.
+                $routerStrategy = new CustomLeagueRouterStrategy();
+                $routerStrategy->setContainer($container);
+                $router->setStrategy($routerStrategy);
+
+                $router->get('/{offerType:events|places}/{offerId}/', GetDetailRequestHandler::class);
 
                 return $router;
             }
