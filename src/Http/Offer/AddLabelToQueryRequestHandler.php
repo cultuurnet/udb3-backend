21cc4f68b63a58d801d4c84af7c4a99b512e0939
Jonas <jonas@cultuurnet.be>
2022-09-07 13:35:26 +0200

    Create AddLabelToQueryRequestHandler

---
+++ src/Http/Offer/AddLabelToQueryRequestHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Offer;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\CommandHandling\AsyncCommand;
+use CultuurNet\UDB3\Deserializer\DeserializerInterface;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\StringLiteral;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class AddLabelToQueryRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    private DeserializerInterface $deserializer;
+
+    public function __construct(
+        DeserializerInterface $commandDeserializer,
+        CommandBus $commandBus
+    ) {
+        $this->deserializer = $commandDeserializer;
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $command = $this->deserializer->deserialize(
+            new StringLiteral($request->getBody()->getContents())
+        );
+        $this->commandBus->dispatch($command);
+
+        $commandId = '00000000-0000-0000-0000-000000000000';
+        if ($command instanceof AsyncCommand) {
+            $commandId = $command->getAsyncCommandId() ?? $commandId;
+        }
+
+        return new JsonResponse(['commandId' => $commandId]);
+    }
+}
