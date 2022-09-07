6b7555d96b9af53c0fa4a92879406f00c3704872
Jonas <jonas@cultuurnet.be>
2022-09-07 14:03:03 +0200

    Edit Bindings for AddLabelToMultiple & AddLabelToQueryRequestHandler

--- app/Offer/BulkLabelOfferControllerProvider.php
+++ app/Offer/BulkLabelOfferControllerProvider.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Offer;
 
+use CultuurNet\UDB3\Http\Offer\AddLabelToMultipleRequestHandler;
+use CultuurNet\UDB3\Http\Offer\AddLabelToQueryRequestHandler;
 use CultuurNet\UDB3\Offer\Commands\AddLabelToMultipleJSONDeserializer;
 use CultuurNet\UDB3\Offer\Commands\AddLabelToQueryJSONDeserializer;
 use CultuurNet\UDB3\Offer\IriOfferIdentifierJSONDeserializer;
-use CultuurNet\UDB3\Http\CommandDeserializerController;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
@@ -16,18 +17,18 @@ class BulkLabelOfferControllerProvider implements ControllerProviderInterface
 {
     public function connect(Application $app)
     {
-        $app['bulk_label_query_controller'] = $app->share(
+        $app[AddLabelToQueryRequestHandler::class] = $app->share(
             function (Application $app) {
-                return new CommandDeserializerController(
+                return new AddLabelToQueryRequestHandler(
                     new AddLabelToQueryJSONDeserializer(),
                     $app['bulk_label_offer_command_bus']
                 );
             }
         );
 
-        $app['bulk_label_selection_controller'] = $app->share(
+        $app[AddLabelToMultipleRequestHandler::class] = $app->share(
             function (Application $app) {
-                return new CommandDeserializerController(
+                return new AddLabelToMultipleRequestHandler(
                     new AddLabelToMultipleJSONDeserializer(
                         new IriOfferIdentifierJSONDeserializer(
                             $app['iri_offer_identifier_factory']
@@ -41,8 +42,8 @@ class BulkLabelOfferControllerProvider implements ControllerProviderInterface
         /* @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
 
-        $controllers->post('/query/labels/', 'bulk_label_query_controller:handle');
-        $controllers->post('/offers/labels/', 'bulk_label_selection_controller:handle');
+        $controllers->post('/query/labels/', AddLabelToQueryRequestHandler::class);
+        $controllers->post('/offers/labels/', AddLabelToMultipleRequestHandler::class);
 
         return $controllers;
     }
