767c7e03226e996e1ff8f3df63af0c6be4647d91
Luc Wollants <lucwollants@gmail.com>
2022-09-08 13:36:13 +0200

    Use `UploadedFileInterface` instead of `UploadedFile` inside `ImageUploaderInterface`

--- src/Media/ImageUploaderInterface.php
+++ src/Media/ImageUploaderInterface.php
@@ -7,13 +7,13 @@ namespace CultuurNet\UDB3\Media;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
 use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use CultuurNet\UDB3\StringLiteral;
+use Psr\Http\Message\UploadedFileInterface;
 
 interface ImageUploaderInterface
 {
     public function upload(
-        UploadedFile $file,
+        UploadedFileInterface $file,
         StringLiteral $description,
         CopyrightHolder $copyrightHolder,
         Language $language
