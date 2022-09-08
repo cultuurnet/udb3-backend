f66a02d77848d3af468791a13515b1cd18777696
steftrenson <steftrenson@gmail.com>
2022-09-08 08:38:05 +0200

    Make GetUsersWithRoleRequestHandler final

--- src/Http/Role/GetUsersWithRoleRequestHandler.php
+++ src/Http/Role/GetUsersWithRoleRequestHandler.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class GetUsersWithRoleRequestHandler implements RequestHandlerInterface
+final class GetUsersWithRoleRequestHandler implements RequestHandlerInterface
 {
     private DocumentRepository $userRolesRepository;
 
