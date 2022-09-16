d31cf246f257092a5516f0ce7e36e6b740b4a6fe
steftrenson <steftrenson@gmail.com>
2022-09-16 13:02:37 +0200

    Add UpdateRoleRequestHandler

--- src/Http/Role/UpdateRoleRequestHandler.php
+++ src/Http/Role/UpdateRoleRequestHandler.php
@@ -4,17 +4,40 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Role;
 
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\RenameRole;
 use CultuurNet\UDB3\Role\MissingContentTypeException;
 use CultuurNet\UDB3\Role\UnknownContentTypeException;
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Response;
 
 class UpdateRoleRequestHandler implements RequestHandlerInterface
 {
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $this->ensureContentTypeIsProvided($request);
+
+        $routeParameters = new RouteParameters($request);
+        $roleId = new UUID($routeParameters->get('id'));
+
+        $body = Json::decodeAssociatively($request->getBody()->getContents());
+
+        $this->commandBus->dispatch(new RenameRole($roleId, (string) $body['name']));
+
+        return new Response(StatusCodeInterface::STATUS_NO_CONTENT);
     }
 
     private function ensureContentTypeIsProvided(ServerRequestInterface $request): void
@@ -23,8 +46,8 @@ class UpdateRoleRequestHandler implements RequestHandlerInterface
             throw new MissingContentTypeException();
         }
 
-        $contentType = $request->getHeader('Content-Type');
-        if ($contentType !== 'application/ld+json;domain-model=RenameRole') {
+        $contentType = $request->getHeader('Content-Type')[0];
+        if ($contentType != 'application/ld+json;domain-model=RenameRole') {
             throw new UnknownContentTypeException();
         }
     }
--- tests/Http/Role/UpdateRoleRequestHandlerTest.php
+++ tests/Http/Role/UpdateRoleRequestHandlerTest.php
@@ -4,18 +4,30 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Role;
 
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
 use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Http\Response\AssertJsonResponseTrait;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\RenameRole;
 use CultuurNet\UDB3\Role\MissingContentTypeException;
 use CultuurNet\UDB3\Role\UnknownContentTypeException;
+use Fig\Http\Message\StatusCodeInterface;
 use PHPUnit\Framework\TestCase;
+use Slim\Psr7\Response;
 
 class UpdateRoleRequestHandlerTest extends TestCase
 {
+    use AssertJsonResponseTrait;
+
     private UpdateRoleRequestHandler $handler;
+
+    private TraceableCommandBus $commandBus;
 
     protected function setUp(): void
     {
-        $this->handler = new UpdateRoleRequestHandler();
+        $this->commandBus = new TraceableCommandBus();
+
+        $this->handler = new UpdateRoleRequestHandler($this->commandBus);
     }
 
     /**
@@ -23,8 +35,10 @@ class UpdateRoleRequestHandlerTest extends TestCase
      */
     public function it_throws_when_content_type_header_is_not_given(): void
     {
+        $roleId = '1ed1588b-a771-44ce-bac0-8f19f09a7d0f';
         $request = (new Psr7RequestBuilder())
-            ->build('POST');
+            ->withRouteParameter('id', $roleId)
+            ->build('PATCH');
 
         $this->expectException(MissingContentTypeException::class);
         $this->handler->handle($request);
@@ -35,11 +49,35 @@ class UpdateRoleRequestHandlerTest extends TestCase
      */
     public function it_throws_when_unknown_content_type_header_is_given(): void
     {
+        $roleId = '1ed1588b-a771-44ce-bac0-8f19f09a7d0f';
         $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('id', $roleId)
             ->withHeader('Content-Type', 'unknown')
-            ->build('POST');
+            ->build('PATCH');
 
         $this->expectException(UnknownContentTypeException::class);
         $this->handler->handle($request);
+    }
+
+    /**
+     * @test
+     */
+    public function it_updates_the_role_name(): void
+    {
+        $roleId = '1ed1588b-a771-44ce-bac0-8f19f09a7d0f';
+        $name = 'my-little-role';
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('id', $roleId)
+            ->withHeader('Content-Type', 'application/ld+json;domain-model=RenameRole')
+            ->withJsonBodyFromArray(['name' => $name])
+            ->build('PATCH');
+
+        $this->commandBus->record();
+
+        $response = $this->handler->handle($request);
+        $this->assertJsonResponse(new Response(StatusCodeInterface::STATUS_NO_CONTENT), $response);
+
+        $expectedCommand = [new RenameRole(new UUID($roleId), $name)];
+        $this->assertEquals($expectedCommand, $this->commandBus->getRecordedCommands());
     }
 }
