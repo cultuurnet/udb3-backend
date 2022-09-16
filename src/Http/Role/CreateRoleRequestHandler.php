8feba6298c2e149676c5febe5eced19703ebbc7b
steftrenson <steftrenson@gmail.com>
2022-09-16 09:47:02 +0200

    Add CreateRoleRequestHandler

---
+++ src/Http/Role/CreateRoleRequestHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Role;
+
+use Broadway\CommandHandling\CommandBus;
+use Broadway\UuidGenerator\UuidGeneratorInterface;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\CreateRole;
+use Fig\Http\Message\StatusCodeInterface;
+use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class CreateRoleRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    private UuidGeneratorInterface $uuidGenerator;
+
+    public function __construct(CommandBus $commandBus, UuidGeneratorInterface $uuidGenerator)
+    {
+        $this->commandBus = $commandBus;
+        $this->uuidGenerator = $uuidGenerator;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $body = Json::decodeAssociatively($request->getBody()->getContents());
+
+        if (empty($body['name'])) {
+            throw new InvalidArgumentException('Required fields are missing');
+        }
+
+        $roleId = new UUID($this->uuidGenerator->generate());
+        $this->commandBus->dispatch(new CreateRole($roleId, (string) $body['name']));
+
+        return new JsonResponse(['roleId' => $roleId->toString()], StatusCodeInterface::STATUS_CREATED);
+    }
+}
---
+++ tests/Http/Role/CreateRoleRequestHandlerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Role;
+
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use Broadway\UuidGenerator\Testing\MockUuidGenerator;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Http\Response\AssertJsonResponseTrait;
+use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\CreateRole;
+use Fig\Http\Message\StatusCodeInterface;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class CreateRoleRequestHandlerTest extends TestCase
+{
+    use AssertJsonResponseTrait;
+
+    private CreateRoleRequestHandler $handler;
+
+    private TraceableCommandBus $commandBus;
+
+    private string $uuid = '9714108c-dddc-4105-a736-2e32632999f4';
+
+    protected function setUp()
+    {
+        $this->commandBus = new TraceableCommandBus();
+
+        $this->handler = new CreateRoleRequestHandler(
+            $this->commandBus,
+            new MockUuidGenerator($this->uuid)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_invalid_argument_exception_when_an_empty_name_is_given(): void
+    {
+        $request = (new Psr7RequestBuilder())
+            ->withJsonBodyFromArray(['name' => ''])
+            ->build('POST');
+
+        $this->commandBus->record();
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->handler->handle($request);
+
+        $this->assertEmpty($this->commandBus->getRecordedCommands());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_create_a_role(): void
+    {
+        $name = 'test-role';
+        $request = (new Psr7RequestBuilder())
+            ->withJsonBodyFromArray(['name' => $name])
+            ->build('POST');
+
+        $this->commandBus->record();
+        $response = $this->handler->handle($request);
+
+        $this->assertJsonResponse(
+            new JsonResponse(['roleId' => $this->uuid], StatusCodeInterface::STATUS_CREATED),
+            $response
+        );
+
+        $this->assertEquals([new CreateRole(new UUID($this->uuid), $name)], $this->commandBus->getRecordedCommands());
+    }
+}
