0281f00036c41be7655b22acf975aad55336b57a
Luc Wollants <lucwollants@gmail.com>
2022-09-16 09:08:02 +0200

    Extend `PlaceProjectedToJSONLD` with `disableUpdatingEventsLocatedAtPlace`

--- src/Place/Events/PlaceProjectedToJSONLD.php
+++ src/Place/Events/PlaceProjectedToJSONLD.php
@@ -8,4 +8,17 @@ use CultuurNet\UDB3\Offer\Events\AbstractEventWithIri;
 
 final class PlaceProjectedToJSONLD extends AbstractEventWithIri
 {
+    private bool $disableUpdatingEventsLocatedAtPlace = false;
+
+    public function disableUpdatingEventsLocatedAtPlace(): PlaceProjectedToJSONLD
+    {
+        $c = clone $this;
+        $c->disableUpdatingEventsLocatedAtPlace = true;
+        return $c;
+    }
+
+    public function isUpdatingEventsLocatedAtPlaceDisabled(): bool
+    {
+        return $this->disableUpdatingEventsLocatedAtPlace;
+    }
 }
