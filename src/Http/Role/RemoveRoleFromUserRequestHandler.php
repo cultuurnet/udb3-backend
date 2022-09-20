7e21d69fefebc285d44be20f7128208b9b809442
steftrenson <steftrenson@gmail.com>
2022-09-20 10:12:58 +0200

    Add RemoveRoleFromUserRequestHandler

---
+++ src/Http/Role/RemoveRoleFromUserRequestHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Role;
+
+use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\RemoveUser;
+use Fig\Http\Message\StatusCodeInterface;
+use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Response;
+
+class RemoveRoleFromUserRequestHandler implements RequestHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $routeParameters = new RouteParameters($request);
+        $roleId = $routeParameters->getRoleId();
+
+        try {
+            $roleId = new UUID($roleId);
+        } catch (InvalidArgumentException $e) {
+            throw ApiProblem::invalidUUID('roleId');
+        }
+
+        $this->commandBus->dispatch(new RemoveUser($roleId, $routeParameters->getUserId()));
+
+        return new Response(StatusCodeInterface::STATUS_NO_CONTENT);
+    }
+}
