4f493272929be3860fd8b305a99f5228c82fbc2a
steftrenson <steftrenson@gmail.com>
2022-09-20 11:25:16 +0200

    Add DeleteRoleRequestHandler

---
+++ tests/Http/Role/DeleteRoleRequestHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Http\Role;
+
+use Broadway\CommandHandling\Testing\TraceableCommandBus;
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\AssertApiProblemTrait;
+use CultuurNet\UDB3\Http\Request\Psr7RequestBuilder;
+use CultuurNet\UDB3\Http\Response\AssertJsonResponseTrait;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\Commands\DeleteRole;
+use Fig\Http\Message\StatusCodeInterface;
+use PHPUnit\Framework\TestCase;
+use Slim\Psr7\Response;
+
+class DeleteRoleRequestHandlerTest extends TestCase
+{
+    use AssertApiProblemTrait;
+    use AssertJsonResponseTrait;
+
+    private DeleteRoleRequestHandler $handler;
+
+    private TraceableCommandBus $commandBus;
+
+    protected function setUp(): void
+    {
+        $this->commandBus = new TraceableCommandBus();
+        $this->commandBus->record();
+        $this->handler = new DeleteRoleRequestHandler($this->commandBus);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_when_role_id_is_invalid(): void
+    {
+        $roleId = 'not-a-uuid';
+
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('roleId', $roleId)
+            ->build('DELETE');
+
+        $this->assertCallableThrowsApiProblem(
+            ApiProblem::invalidUUID('roleId'),
+            fn () => $this->handler->handle($request)
+        );
+
+        $this->assertEmpty($this->commandBus->getRecordedCommands());
+    }
+
+    /**
+     * @test
+     */
+    public function it_deletes_a_role(): void
+    {
+        $roleId = 'd0212d4d-5760-42a6-9b70-838e95ee90df';
+
+        $request = (new Psr7RequestBuilder())
+            ->withRouteParameter('roleId', $roleId)
+            ->build('DELETE');
+
+        $actualResponse = $this->handler->handle($request);
+
+        $expectedResponse = new Response(StatusCodeInterface::STATUS_NO_CONTENT);
+        $expectedCommand = new DeleteRole(new UUID($roleId));
+
+        $this->assertJsonResponse($expectedResponse, $actualResponse);
+        $this->assertEquals([$expectedCommand], $this->commandBus->getRecordedCommands());
+    }
+}
