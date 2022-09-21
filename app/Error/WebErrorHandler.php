ff973abd515ff002094c93fbdffd2d5e4fb954ca
Bert Ramakers <bertramakers@me.com>
2022-09-21 10:52:36 +0200

    Make WebErrorHandler implement the PSR-15 middleware interface

--- app/Error/WebErrorHandler.php
+++ app/Error/WebErrorHandler.php
@@ -17,11 +17,14 @@ use CultuurNet\UDB3\Security\CommandAuthorizationException;
 use Error;
 use League\Route\Http\Exception\MethodNotAllowedException;
 use League\Route\Http\Exception\NotFoundException;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Respect\Validation\Exceptions\GroupedValidationException;
 use Throwable;
 
-final class WebErrorHandler
+final class WebErrorHandler implements MiddlewareInterface
 {
     private ErrorLogger $errorLogger;
     private bool $debugMode;
@@ -30,6 +33,15 @@ final class WebErrorHandler
     {
         $this->errorLogger = $errorLogger;
         $this->debugMode = $debugMode;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $handler->handle($request);
+        } catch (Throwable $e) {
+            return $this->handle($request, $e);
+        }
     }
 
     public function handle(ServerRequestInterface $request, Throwable $e): ApiProblemJsonResponse
