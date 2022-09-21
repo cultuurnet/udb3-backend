de19529084b3f6d9617be79efa6689f73de039b8
Luc Wollants <lucwollants@gmail.com>
2022-09-21 18:04:44 +0200

    Remove already registered roles

--- app/Role/RoleControllerProvider.php
+++ app/Role/RoleControllerProvider.php
@@ -4,19 +4,10 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Role;
 
-use CultuurNet\UDB3\Http\Role\GetPermissionsRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetRoleLabelsRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetRoleRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetRolesFromCurrentUserRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetRolesFromUserRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetUserPermissionsRequestHandler;
-use CultuurNet\UDB3\Http\Role\GetUsersWithRoleRequestHandler;
-use CultuurNet\UDB3\Http\Role\RolesSearchRequestHandler;
 use CultuurNet\UDB3\Role\Commands\UpdateRoleRequestDeserializer;
 use CultuurNet\UDB3\Silex\Labels\LabelServiceProvider;
 use CultuurNet\UDB3\Http\Deserializer\Role\QueryJSONDeserializer;
 use CultuurNet\UDB3\Http\Role\EditRoleRestController;
-use CultuurNet\UDB3\User\CurrentUser;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
@@ -37,53 +28,13 @@ class RoleControllerProvider implements ControllerProviderInterface
             }
         );
 
-        $app[GetRoleRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetRoleRequestHandler($app['role_read_repository'])
-        );
-
-        $app[GetUsersWithRoleRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetUsersWithRoleRequestHandler($app['role_users_read_repository'])
-        );
-
-        $app[GetRolesFromUserRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetRolesFromUserRequestHandler($app['user_roles_repository'])
-        );
-
-        $app[GetRolesFromCurrentUserRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetRolesFromCurrentUserRequestHandler($app['user_roles_repository'], $app[CurrentUser::class]->getId())
-        );
-
-        $app[GetPermissionsRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetPermissionsRequestHandler()
-        );
-
-        $app[GetUserPermissionsRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetUserPermissionsRequestHandler(
-                $app[UserPermissionsServiceProvider::USER_PERMISSIONS_READ_REPOSITORY],
-                $app[CurrentUser::class]->getId(),
-                $app[CurrentUser::class]->isGodUser(),
-            )
-        );
-
-        $app[GetRoleLabelsRequestHandler::class] = $app->share(
-            fn (Application $app) => new GetRoleLabelsRequestHandler($app['role_labels_read_repository'])
-        );
-
-        $app[RolesSearchRequestHandler::class] = $app->share(
-            fn (Application $app) => new RolesSearchRequestHandler($app['role_search_v3_repository'])
-        );
-
         /** @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
-
-        $controllers->get('/roles/', RolesSearchRequestHandler::class);
 
         $controllers->post(
             '/roles/',
             'role_edit_controller:create'
         );
-
-        $controllers->get('/roles/{roleId}/', GetRoleRequestHandler::class);
 
         $controllers->patch(
             '/roles/{id}/',
@@ -107,12 +58,6 @@ class RoleControllerProvider implements ControllerProviderInterface
 
         $controllers->delete('/roles/{id}/', 'role_edit_controller:delete');
 
-        $controllers->get('/permissions/', GetPermissionsRequestHandler::class);
-
-        $controllers->get('/user/permissions/', GetUserPermissionsRequestHandler::class);
-
-        $controllers->get('/roles/{roleId}/users/', GetUsersWithRoleRequestHandler::class);
-
         $controllers->put(
             '/roles/{roleId}/permissions/{permissionKey}/',
             'role_edit_controller:addPermission'
@@ -122,8 +67,6 @@ class RoleControllerProvider implements ControllerProviderInterface
             '/roles/{roleId}/permissions/{permissionKey}/',
             'role_edit_controller:removePermission'
         );
-
-        $controllers->get('/roles/{roleId}/labels/', GetRoleLabelsRequestHandler::class);
 
         $controllers->put(
             '/roles/{roleId}/labels/{labelIdentifier}/',
@@ -144,10 +87,6 @@ class RoleControllerProvider implements ControllerProviderInterface
             '/roles/{roleId}/users/{userId}/',
             'role_edit_controller:removeUser'
         );
-
-        $controllers->get('/users/{userId}/roles/', GetRolesFromUserRequestHandler::class);
-
-        $controllers->get('/user/roles/', GetRolesFromCurrentUserRequestHandler::class);
 
         return $controllers;
     }
