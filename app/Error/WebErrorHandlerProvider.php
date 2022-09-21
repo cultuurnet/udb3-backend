600e620501caaa183a29a2a52740884e77d47248
Bert Ramakers <bertramakers@me.com>
2022-09-21 10:43:22 +0200

    Remove unused imports

--- app/Error/WebErrorHandlerProvider.php
+++ app/Error/WebErrorHandlerProvider.php
@@ -4,26 +4,10 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Error;
 
-use Broadway\Repository\AggregateNotFoundException;
-use CultureFeed_Exception;
-use CultureFeed_HttpException;
-use CultuurNet\UDB3\Deserializer\DataValidationException;
-use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
-use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
-use CultuurNet\UDB3\Http\Request\RouteParameters;
-use CultuurNet\UDB3\Http\Response\ApiProblemJsonResponse;
-use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
-use CultuurNet\UDB3\Security\CommandAuthorizationException;
-use Error;
 use Exception;
-use League\Route\Http\Exception\MethodNotAllowedException;
-use League\Route\Http\Exception\NotFoundException;
-use Psr\Http\Message\ServerRequestInterface;
-use Respect\Validation\Exceptions\GroupedValidationException;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
-use Throwable;
 
 class WebErrorHandlerProvider implements ServiceProviderInterface
 {
