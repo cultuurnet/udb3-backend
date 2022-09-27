51adcf937718c8778cdc90c733cb998984e41848
Bert Ramakers <bertramakers@me.com>
2022-09-27 12:53:05 +0200

    Remove Silex middlewares

--- app/EventBus/EventBusServiceProvider.php
+++ app/EventBus/EventBusServiceProvider.php
@@ -103,42 +103,5 @@ final class EventBusServiceProvider implements ServiceProviderInterface
 
     public function boot(Application $app): void
     {
-        // Limit the amount of "ProjectedToJSONLD" messages on the AMQP queues by intercepting them on the event bus
-        // during the request handling, and only (re)publishing the unique ones after the request handler is done.
-        // Note that before() and after() callbacks are only called in the context of HTTP requests, so not in CLI
-        // commands where we don't want this behavior.
-        // Some examples where this is useful:
-        // - New event, place, organizer imports that contain one or more non-required fields which results in extra ProjectedToJSONLD messages
-        // - Event, place, organizer updates via imports that edit multiple fields which results in multiple ProjectedToJSONLD messages
-        // - Place creation that also does a geocoding which results in 2 PlaceProjectedToJSONLD messages
-        // - Organizer address update that also does a geocoding which results in 2 OrganizerProjectedToJSONLD messages
-        $app->before(
-            function () {
-                InterceptingMiddleware::startIntercepting(
-                    function (DomainMessage $message): bool {
-                        $payload = $message->getPayload();
-                        return $payload instanceof EventProjectedToJSONLD ||
-                            $payload instanceof PlaceProjectedToJSONLD ||
-                            $payload instanceof OrganizerProjectedToJSONLD;
-                    }
-                );
-            }
-        );
-        $app->after(
-            function () use ($app) {
-                InterceptingMiddleware::stopIntercepting();
-                $interceptedWithUniquePayload = InterceptingMiddleware::getInterceptedMessagesWithUniquePayload();
-
-                // Important! Only publish the intercepted messages if there are actually any. Otherwise the EventBus
-                // service will be instantiated for requests that do not require it, which in turn will trigger the
-                // command bus to be instantiated. And the command bus requires the current user id to work, which is
-                // not available on all requests (for example OPTIONS requests, or public GET requests).
-                if ($interceptedWithUniquePayload->getIterator()->count() > 0) {
-                    /** @var EventBus $eventBus */
-                    $eventBus = $app[EventBus::class];
-                    $eventBus->publish($interceptedWithUniquePayload);
-                }
-            }
-        );
     }
 }
--- web/index.php
+++ web/index.php
@@ -53,21 +53,6 @@ $app->register(new PsrRouterServiceProvider());
  */
 $app->register(new ProxyRequestHandlerServiceProvider());
 
-/**
- * Middleware that authenticates incoming HTTP requests using the RequestAuthenticator service.
- * @todo III-4235 Move to Middleware in new PSR router when all routes are registered on the new router.
- */
-$app->before(
-    static function (Request $request, Application $app): void {
-        $psrRequest = (new DiactorosFactory())->createRequest($request);
-
-        /** @var RequestAuthenticatorMiddleware $authenticator */
-        $authenticator = $app[RequestAuthenticatorMiddleware::class];
-        $authenticator->authenticate($psrRequest);
-    },
-    Application::EARLY_EVENT
-);
-
 // Match with any OPTIONS request with any URL and return a 204 No Content. Actual CORS headers will be added by an
 // ->after() middleware, which adds CORS headers to every request (so non-preflighted requests like simple GETs also get
 // the needed CORS headers).
@@ -75,30 +60,6 @@ $app->before(
 // in the new router, so this can be removed completely once all route definitions and handlers are moved to the new
 // router.
 $app->options('/{path}', fn () => new NoContentResponse())->assert('path', '^.+$');
-
-// Add CORS headers to every request. We explicitly allow everything, because we don't use cookies and our API is not on
-// an internal network, so CORS requests are never a security issue in our case. This greatly reduces the risk of CORS
-// bugs in our frontend and other integrations.
-// @todo III-4235 Move to Middleware in new PSR router when all routes are registered on the new router.
-$app->after(
-    function (Request $request, Response $response) {
-        // Allow any known method regardless of the URL.
-        $methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
-        $response->headers->set('Allow', implode(',', $methods));
-        $response->headers->set('Access-Control-Allow-Methods', implode(',', $methods));
-
-        // Allow the Authorization header to be used.
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
-
-        // If a specific origin has been requested to be used, echo it back. Otherwise allow *.
-        $requestedOrigin = $request->headers->get('Origin', '*');
-        $response->headers->set('Access-Control-Allow-Origin', $requestedOrigin);
-
-        // If specific headers have been requested to be used, echo them back. Otherwise allow the default headers.
-        $requestedHeaders = $request->headers->get('Access-Control-Request-Headers', 'authorization,x-api-key');
-        $response->headers->set('Access-Control-Allow-Headers', $requestedHeaders);
-    }
-);
 
 $app->register(new CatchAllRouteServiceProvider());
 
