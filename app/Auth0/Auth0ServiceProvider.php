c3154624b89345d1659d254d20a28d17da002b40
Jonas <jonas@cultuurnet.be>
2022-09-30 15:53:57 +0200

    refactor Auth0ServiceProvider

--- app/Auth0/Auth0ServiceProvider.php
+++ app/Auth0/Auth0ServiceProvider.php
@@ -6,46 +6,53 @@ namespace CultuurNet\UDB3\Auth0;
 
 use Auth0\SDK\API\Management;
 use Auth0\SDK\Configuration\SdkConfiguration;
+use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\User\Auth0ManagementTokenGenerator;
 use CultuurNet\UDB3\User\Auth0ManagementTokenProvider;
 use CultuurNet\UDB3\User\Auth0UserIdentityResolver;
 use CultuurNet\UDB3\User\CacheRepository;
 use GuzzleHttp\Client;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
 
-final class Auth0ServiceProvider implements ServiceProviderInterface
+final class Auth0ServiceProvider extends AbstractServiceProvider
 {
-    public function register(Application $app): void
+    protected function getProvidedServiceNames(): array
     {
-        $app['auth0.management-token'] = $app::share(
-            function (Application $app) {
+        return [
+            'auth0.management-token',
+            Auth0UserIdentityResolver::class
+        ];
+    }
+
+    public function register(): void
+    {
+        $container = $this->getContainer();
+
+        $container->addShared(
+            'auth0.management-token',
+            function () use ($container):string {
                 $provider = new Auth0ManagementTokenProvider(
                     new Auth0ManagementTokenGenerator(
                         new Client(),
-                        $app['config']['auth0']['client_id'],
-                        $app['config']['auth0']['domain'],
-                        $app['config']['auth0']['client_secret']
+                        $container->get('config')['auth0']['client_id'],
+                        $container->get('config')['auth0']['domain'],
+                        $container->get('config')['auth0']['client_secret']
                     ),
                     new CacheRepository(
-                        $app['cache']('auth0-management-token')
+                        $container->get('config')('auth0-management-token')
                     )
                 );
                 return $provider->token();
             }
         );
 
-        $app[Auth0UserIdentityResolver::class] = $app::share(
-            function (Application $app) {
+        $container->addShared(
+            'auth0.management-token',
+            function () use ($container): Auth0UserIdentityResolver {
                 $config = new SdkConfiguration(null, SdkConfiguration::STRATEGY_NONE);
-                $config->setDomain($app['config']['auth0']['domain']);
-                $config->setManagementToken($app['auth0.management-token']);
+                $config->setDomain($container->get('config')['auth0']['domain']);
+                $config->setManagementToken($container->get('auth0.management-token');
                 return new Auth0UserIdentityResolver(new Management($config));
             }
         );
-    }
-
-    public function boot(Application $app): void
-    {
     }
 }
