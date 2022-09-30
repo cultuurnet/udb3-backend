830c70ab02a4ab2059059b00c87fc5f5efa9bac0
Bert Ramakers <bertramakers@me.com>
2022-09-30 10:26:48 +0200

    Remove unused imports

--- app/Silex/UDB2EventServicesProvider.php
+++ app/Silex/UDB2EventServicesProvider.php
@@ -4,45 +4,10 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex;
 
-use Broadway\EventHandling\EventBus;
-use CultuurNet\UDB3\ApiName;
-use CultuurNet\UDB3\Deserializer\SimpleDeserializerLocator;
-use CultuurNet\UDB3\Broadway\AMQP\EventBusForwardingConsumerFactory;
-use CultuurNet\UDB3\Silex\Container\HybridContainerApplication;
-use CultuurNet\UDB3\Error\LoggerFactory;
-use CultuurNet\UDB3\Error\LoggerName;
-use CultuurNet\UDB3\UDB2\DomainEvents\ActorCreatedJSONDeserializer;
-use CultuurNet\UDB3\UDB2\DomainEvents\ActorUpdatedJSONDeserializer;
-use CultuurNet\UDB3\UDB2\DomainEvents\EventCreatedJSONDeserializer;
-use CultuurNet\UDB3\UDB2\DomainEvents\EventUpdatedJSONDeserializer;
 use CultuurNet\UDB3\Cdb\CdbId\EventCdbIdExtractor;
-use CultuurNet\UDB3\Cdb\Event\Any;
 use CultuurNet\UDB3\Cdb\ExternalId\ArrayMappingService;
-use CultuurNet\UDB3\UDB2\Actor\ActorImporter;
-use CultuurNet\UDB3\UDB2\Actor\ActorEventCdbXmlEnricher;
-use CultuurNet\UDB3\UDB2\Actor\ActorToUDB3OrganizerFactory;
-use CultuurNet\UDB3\UDB2\Actor\ActorToUDB3PlaceFactory;
-use CultuurNet\UDB3\UDB2\Actor\Specification\QualifiesAsOrganizerSpecification;
-use CultuurNet\UDB3\UDB2\Actor\Specification\QualifiesAsPlaceSpecification;
-use CultuurNet\UDB3\UDB2\Event\EventImporter;
-use CultuurNet\UDB3\UDB2\Event\EventCdbXmlEnricher;
-use CultuurNet\UDB3\UDB2\Event\EventXMLValidatorService;
-use CultuurNet\UDB3\UDB2\Label\LabelImporter;
-use CultuurNet\UDB3\UDB2\Media\ImageCollectionFactory;
-use CultuurNet\UDB3\UDB2\Media\MediaImporter;
-use CultuurNet\UDB3\UDB2\XML\CompositeXmlValidationService;
-use CultuurNet\UDB3\UDB2\XSD\CachedInMemoryXSDReader;
-use CultuurNet\UDB3\UDB2\XSD\FileGetContentsXSDReader;
-use CultuurNet\UDB3\UDB2\XSD\XSDAwareXMLValidationService;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
-use Http\Adapter\Guzzle7\Client as ClientAdapter;
-use Monolog\Handler\StreamHandler;
-use Ramsey\Uuid\UuidFactory;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use CultuurNet\UDB3\StringLiteral;
 
 class UDB2EventServicesProvider implements ServiceProviderInterface
 {
