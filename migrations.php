6b33f16777f2280ada41eabf4f0bd0fa39982a6c
Luc Wollants <lucwollants@gmail.com>
2022-10-04 15:13:30 +0200

    Fix migrations namespace by removing `Silex`

--- migrations.php
+++ migrations.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'UDB3 Migrations',
-    'migrations_namespace' => 'CultuurNet\UDB3\Silex\Migrations',
+    'migrations_namespace' => 'CultuurNet\UDB3\Migrations',
     'table_name' => 'doctrine_migration_versions',
     'migrations_directory' => 'app/Migrations',
 ];
