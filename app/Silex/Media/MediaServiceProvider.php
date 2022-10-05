283223065b3d56ec7ad78e54c2d2a4134f39af4e
Jonas <jonas@cultuurnet.be>
2022-10-05 17:10:38 +0200

    Move MediaServiceProvider PSR-11 compliant

--- app/Silex/Media/MediaServiceProvider.php
+++ app/Silex/Media/MediaServiceProvider.php
@@ -6,6 +6,7 @@ namespace CultuurNet\UDB3\Silex\Media;
 
 use Broadway\EventHandling\EventBus;
 use Broadway\UuidGenerator\Rfc4122\Version4Generator;
+use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Http\Media\GetMediaRequestHandler;
 use CultuurNet\UDB3\Http\Media\UploadMediaRequestHandler;
 use CultuurNet\UDB3\Iri\CallableIriGenerator;
@@ -16,118 +17,139 @@ use CultuurNet\UDB3\Media\MediaObjectRepository;
 use CultuurNet\UDB3\Media\Serialization\MediaObjectSerializer;
 use CultuurNet\UDB3\Media\SimplePathGenerator;
 use CultuurNet\UDB3\AggregateType;
-use CultuurNet\UDB3\Silex\Container\HybridContainerApplication;
 use CultuurNet\UDB3\Error\LoggerFactory;
 use CultuurNet\UDB3\Error\LoggerName;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
 
-class MediaServiceProvider implements ServiceProviderInterface
+class MediaServiceProvider extends AbstractServiceProvider
 {
-    public function register(Application $app): void
+    protected function getProvidedServiceNames(): array
     {
-        $app['image_uploader'] = $app->share(
-            function (Application $app) {
+        return [
+            'image_uploader',
+            'media_object_store',
+            'media_object_repository',
+            'media_object_iri_generator',
+            'content_url_generator',
+            'media_object_serializer',
+            'media_manager',
+            'media_url_mapping',
+            GetMediaRequestHandler::class,
+            UploadMediaRequestHandler::class,
+        ];
+    }
+
+    public function register(): void
+    {
+        $container = $this->getContainer();
+
+        $container->addShared(
+            'image_uploader',
+            function () use ($container) {
                 return new ImageUploaderService(
                     new Version4Generator(),
-                    $app['event_command_bus'],
-                    $app['local_file_system'],
-                    $app['media.upload_directory'],
-                    $app['media.file_size_limit']
+                    $container->get('event_command_bus'),
+                    $container->get('local_file_system'),
+                    $container->get('config')['media']['upload_directory'],
+                    $container->get('config')['media']['file_size_limit'] ?? 1000000
                 );
             }
         );
 
-        $app['media_object_store'] = $app->share(
-            function ($app) {
-                return $app['event_store_factory'](
+        $container->addShared(
+            'media_object_store',
+            function () use ($container) {
+                return $container->get('event_store_factory')(
                     AggregateType::media_object()
                 );
             }
         );
 
-        $app['media_object_repository'] = $app->share(
-            function ($app) {
+        $container->addShared(
+            'media_object_repository',
+            function () use ($container) {
                 return new MediaObjectRepository(
-                    $app['media_object_store'],
-                    $app[EventBus::class],
+                    $container->get('media_object_store'),
+                    $container->get(EventBus::class),
                     [
-                        $app['event_stream_metadata_enricher'],
+                        $container->get('event_stream_metadata_enricher'),
                     ]
                 );
             }
         );
 
-        $app['media_object_iri_generator'] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            'media_object_iri_generator',
+            function () use ($container) {
                 return new CallableIriGenerator(
-                    function ($filePath) use ($app) {
-                        return $app['config']['url'] . '/images/' . $filePath;
+                    function ($filePath) use ($container) {
+                        return $container->get('config')['url'] . '/images/' . $filePath;
                     }
                 );
             }
         );
 
-        $app['content_url_generator'] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            'content_url_generator',
+            function () use ($container) {
                 return new CallableIriGenerator(
-                    function ($filePath) use ($app) {
-                        return $app['config']['media']['content_url'] . '/' . $filePath;
+                    function ($filePath) use ($container) {
+                        return $container->get('config')['media']['content_url'] . '/' . $filePath;
                     }
                 );
             }
         );
 
-        $app['media_object_serializer'] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            'media_object_serializer',
+            function () use ($container) {
                 return new MediaObjectSerializer(
-                    $app['media_object_iri_generator']
+                    $container->get('media_object_iri_generator')
                 );
             }
         );
 
-        $app['media_manager'] = $app->share(
-            function (HybridContainerApplication $app) {
+        $container->addShared(
+            'media_manager',
+            function () use ($container) {
                 $mediaManager = new MediaManager(
-                    $app['content_url_generator'],
+                    $container->get('content_url_generator'),
                     new SimplePathGenerator(),
-                    $app['media_object_repository'],
-                    $app['image_storage']
+                    $container->get('media_object_repository'),
+                    $container->get('image_storage')
                 );
 
-                $mediaManager->setLogger(LoggerFactory::create($app->getLeagueContainer(), LoggerName::forService('media', 'manager')));
+                $mediaManager->setLogger(LoggerFactory::create($container, LoggerName::forService('media', 'manager')));
 
                 return $mediaManager;
             }
         );
 
-        $app['media_url_mapping'] = $app->share(
-            function (Application $app) {
-                return new MediaUrlMapping($app['config']['media']['media_url_mapping']);
+        $container->addShared(
+            'media_url_mapping',
+            function () use ($container) {
+                return new MediaUrlMapping($container->get('config')['media']['media_url_mapping']);
             }
         );
 
-        $app[GetMediaRequestHandler::class] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            GetMediaRequestHandler::class,
+            function () use ($container) {
                 return new GetMediaRequestHandler(
-                    $app['media_manager'],
-                    $app['media_object_serializer'],
-                    $app['media_url_mapping']
+                    $container->get('media_manager'),
+                    $container->get('media_object_serializer'),
+                    $container->get('media_url_mapping')
                 );
             }
         );
 
-        $app[UploadMediaRequestHandler::class] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            UploadMediaRequestHandler::class,
+            function () use ($container) {
                 return new UploadMediaRequestHandler(
-                    $app['image_uploader'],
-                    $app['media_object_iri_generator']
+                    $container->get('image_uploader'),
+                    $container->get('media_object_iri_generator')
                 );
             }
         );
-    }
-
-    public function boot(Application $app): void
-    {
     }
 }
