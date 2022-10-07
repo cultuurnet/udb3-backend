6c1c866e2c23538b0fbb02e46012e166689a0607
steftrenson <steftrenson@gmail.com>
2022-10-07 15:19:56 +0200

    Re-add change to bootstrap after merge conflict

--- bootstrap.php
+++ bootstrap.php
@@ -354,6 +354,7 @@ $app['places_locator_event_stream_decorator'] = $app->share(
     }
 );
 
+// @todo: remove usages of 'place_repository' with Class based share
 $app['place_repository'] = $app->share(
     function (Application $app) {
         $repository = new \CultuurNet\UDB3\Place\PlaceRepository(
@@ -366,6 +367,12 @@ $app['place_repository'] = $app->share(
         );
 
         return $repository;
+    }
+);
+$container->addShared(
+    \CultuurNet\UDB3\Place\PlaceRepository::class,
+    function () use ($container): \CultuurNet\UDB3\Place\PlaceRepository {
+        return $container->get('place_repository');
     }
 );
 
