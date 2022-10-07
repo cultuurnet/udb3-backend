d296681208f6784cd1756481ace0b4dda6835949
Jonas <jonas@cultuurnet.be>
2022-10-07 13:36:54 +0200

    share values of OrganizerPermissionServiceProvider via psr11-containner

--- app/Silex/Organizer/OrganizerPermissionServiceProvider.php
+++ app/Silex/Organizer/OrganizerPermissionServiceProvider.php
@@ -4,44 +4,46 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Organizer;
 
+use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Organizer\ReadModel\Permission\Projector;
 use CultuurNet\UDB3\Security\ResourceOwner\Doctrine\DBALResourceOwnerRepository;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
 use CultuurNet\UDB3\StringLiteral;
 
-final class OrganizerPermissionServiceProvider implements ServiceProviderInterface
+final class OrganizerPermissionServiceProvider extends AbstractServiceProvider
 {
     public const PERMISSION_PROJECTOR = 'organizer_permission.projector';
-    /**
-     * @inheritdoc
-     */
-    public function register(Application $app)
+
+    protected function getProvidedServiceNames(): array
     {
-        $app['organizer_owner.repository'] = $app->share(
-            function (Application $app) {
+        return [
+            'organizer_owner.repository',
+            self::PERMISSION_PROJECTOR,
+        ];
+    }
+
+    public function register(): void
+    {
+        $container = $this->getContainer();
+
+        $container->addShared(
+            'organizer_owner.repository',
+            function () use ($container) {
                 return new DBALResourceOwnerRepository(
                     new StringLiteral('organizer_permission_readmodel'),
-                    $app['dbal_connection'],
+                    $container->get('dbal_connection'),
                     new StringLiteral('organizer_id')
                 );
             }
         );
 
-        $app[self::PERMISSION_PROJECTOR] = $app->share(
-            function (Application $app) {
+        $container->addShared(
+            self::PERMISSION_PROJECTOR,
+            function () use ($container) {
                 return new Projector(
-                    $app['organizer_owner.repository'],
-                    $app['cdbxml_created_by_resolver']
+                    $container->get('organizer_owner.repository'),
+                    $container->get('cdbxml_created_by_resolver')
                 );
             }
         );
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function boot(Application $app)
-    {
     }
 }
