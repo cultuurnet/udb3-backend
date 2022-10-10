78d7c17ed5094408a54a646eb17affebf494dee4
Jonas <jonas@cultuurnet.be>
2022-10-10 15:36:01 +0200

    register UiTPASIncomingEventServicesProvider in psr11 Container

--- bootstrap.php
+++ bootstrap.php
@@ -750,7 +750,7 @@ $app->register(
     ]
 );
 
-$app->register(new \CultuurNet\UDB3\Silex\UiTPAS\UiTPASIncomingEventServicesProvider());
+$container->addServiceProvider(new \CultuurNet\UDB3\Silex\UiTPAS\UiTPASIncomingEventServicesProvider());
 
 $app->register(
     new \CultuurNet\UDB3\Silex\GeocodingServiceProvider(),
