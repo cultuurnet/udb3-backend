cd4f9f8d01d6403cb10fcf28124d38997d636ef4
Jonas <jonas@cultuurnet.be>
2022-10-10 14:23:53 +0200

    Make Sapi3SearchServiceProvider extend psr11 container

--- app/Silex/Search/Sapi3SearchServiceProvider.php
+++ app/Silex/Search/Sapi3SearchServiceProvider.php
@@ -4,68 +4,81 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Silex\Search;
 
+use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Model\ValueObject\Identity\ItemIdentifierFactory;
 use CultuurNet\UDB3\Search\Sapi3SearchService;
 use GuzzleHttp\Psr7\Uri;
 use Http\Adapter\Guzzle7\Client;
-use Silex\Application;
-use Silex\ServiceProviderInterface;
 
-class Sapi3SearchServiceProvider implements ServiceProviderInterface
+final class Sapi3SearchServiceProvider extends AbstractServiceProvider
 {
     public const SEARCH_SERVICE_OFFERS = 'sapi3_search_service_offers';
     public const SEARCH_SERVICE_EVENTS = 'sapi3_search_service_events';
     public const SEARCH_SERVICE_PLACES = 'sapi3_search_service_places';
     public const SEARCH_SERVICE_ORGANIZERS = 'sapi3_search_service_organizers';
 
-    public function register(Application $app): void
+    protected function getProvidedServiceNames(): array
     {
-        $app[self::SEARCH_SERVICE_OFFERS] = $app->share(
-            function ($app) {
-                return new Sapi3SearchService(
-                    new Uri($app['config']['search']['v3']['base_url'] . '/offers/'),
-                    new Client(new \GuzzleHttp\Client()),
-                    new ItemIdentifierFactory($app['config']['item_url_regex']),
-                    $app['config']['export']['search']['api_key'] ?? null
-                );
-            }
-        );
-
-        $app[self::SEARCH_SERVICE_EVENTS] = $app->share(
-            function ($app) {
-                return new Sapi3SearchService(
-                    new Uri($app['config']['search']['v3']['base_url'] . '/events/'),
-                    new Client(new \GuzzleHttp\Client()),
-                    new ItemIdentifierFactory($app['config']['item_url_regex']),
-                    $app['config']['export']['search']['api_key'] ?? null
-                );
-            }
-        );
-
-        $app[self::SEARCH_SERVICE_PLACES] = $app->share(
-            function ($app) {
-                return new Sapi3SearchService(
-                    new Uri($app['config']['search']['v3']['base_url'] . '/places/'),
-                    new Client(new \GuzzleHttp\Client()),
-                    new ItemIdentifierFactory($app['config']['item_url_regex']),
-                    $app['config']['export']['search']['api_key'] ?? null
-                );
-            }
-        );
-
-        $app[self::SEARCH_SERVICE_ORGANIZERS] = $app->share(
-            function ($app) {
-                return new Sapi3SearchService(
-                    new Uri($app['config']['search']['v3']['base_url'] . '/organizers/'),
-                    new Client(new \GuzzleHttp\Client()),
-                    new ItemIdentifierFactory($app['config']['item_url_regex']),
-                    $app['config']['export']['search']['api_key'] ?? null
-                );
-            }
-        );
+        return [
+            self::SEARCH_SERVICE_OFFERS,
+            self::SEARCH_SERVICE_EVENTS,
+            self::SEARCH_SERVICE_PLACES,
+            self::SEARCH_SERVICE_ORGANIZERS,
+        ];
     }
 
-    public function boot(Application $app): void
+    public function register(): void
     {
+        $container = $this->getContainer();
+
+        $container->addShared(
+            self::SEARCH_SERVICE_OFFERS,
+            function () use ($container) {
+                return new Sapi3SearchService(
+                    new Uri($container->get('config')['search']['v3']['base_url'] . '/offers/'),
+                    new Client(new \GuzzleHttp\Client()),
+                    new ItemIdentifierFactory($container->get('config')['item_url_regex']),
+                    $container->get('config')['export']['search']['api_key'] ?? null
+                );
+            }
+        );
+
+
+        $container->addShared(
+            self::SEARCH_SERVICE_EVENTS,
+            function () use ($container) {
+                return new Sapi3SearchService(
+                    new Uri($container->get('config')['search']['v3']['base_url'] . '/events/'),
+                    new Client(new \GuzzleHttp\Client()),
+                    new ItemIdentifierFactory($container->get('config')['item_url_regex']),
+                    $container->get('config')['export']['search']['api_key'] ?? null
+                );
+            }
+        );
+
+
+        $container->addShared(
+            self::SEARCH_SERVICE_PLACES,
+            function () use ($container) {
+                return new Sapi3SearchService(
+                    new Uri($container->get('config')['search']['v3']['base_url'] . '/places/'),
+                    new Client(new \GuzzleHttp\Client()),
+                    new ItemIdentifierFactory($container->get('config')['item_url_regex']),
+                    $container->get('config')['export']['search']['api_key'] ?? null
+                );
+            }
+        );
+
+        $container->addShared(
+            self::SEARCH_SERVICE_ORGANIZERS,
+            function () use ($container) {
+                return new Sapi3SearchService(
+                    new Uri($container->get('config')['search']['v3']['base_url'] . '/organizers/'),
+                    new Client(new \GuzzleHttp\Client()),
+                    new ItemIdentifierFactory($container->get('config')['item_url_regex']),
+                    $container->get('config')['export']['search']['api_key'] ?? null
+                );
+            }
+        );
     }
 }
