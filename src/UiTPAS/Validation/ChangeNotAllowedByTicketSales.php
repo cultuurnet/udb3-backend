5d7454b96b32a2af2e112f48f5b4432e1878a720
Bert Ramakers <bertramakers@me.com>
2022-10-11 17:23:25 +0200

    Implement ConvertsToApiProblem on ChangeNotAllowedByTicketSales

--- src/UiTPAS/Validation/ChangeNotAllowedByTicketSales.php
+++ src/UiTPAS/Validation/ChangeNotAllowedByTicketSales.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\UiTPAS\Validation;
 
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
 use Exception;
 
-final class ChangeNotAllowedByTicketSales extends Exception
+final class ChangeNotAllowedByTicketSales extends Exception implements ConvertsToApiProblem
 {
+    private string $eventId;
+
     public function __construct(string $eventId)
     {
         parent::__construct('Organizer change not allowed because event ' . $eventId . ' has ticket sales in UiTPAS');
+        $this->eventId = $eventId;
+    }
+
+    public function toApiProblem(): ApiProblem
+    {
+        return ApiProblem::eventHasUitpasTicketSales($this->eventId);
     }
 }
