dc8e4da91d80b309670fbfd75e4151d52b890f1b
steftrenson <steftrenson@gmail.com>
2022-10-12 10:05:11 +0200

    cache dependency shouldn't instantiate immediately but return a function

--- app/Cache/CacheServiceProvider.php
+++ app/Cache/CacheServiceProvider.php
@@ -6,6 +6,7 @@ namespace CultuurNet\UDB3\Cache;
 
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use Doctrine\Common\Cache\PredisCache;
+use League\Container\Argument\Literal\CallableArgument;
 use Predis\Client;
 
 final class CacheServiceProvider extends AbstractServiceProvider
@@ -23,10 +24,12 @@ final class CacheServiceProvider extends AbstractServiceProvider
 
         $container->addShared(
             'cache',
-            fn ($cacheType) => new PredisCache(
-                new Client(
-                    $container->get('config')['cache']['redis'],
-                    ['prefix' => $cacheType . '_']
+            new CallableArgument(
+                fn($cacheType) => new PredisCache(
+                    new Client(
+                        $container->get('config')['cache']['redis'],
+                        ['prefix' => $cacheType . '_'],
+                    )
                 )
             )
         );
