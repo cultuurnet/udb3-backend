bfabc9aad774a14f57bde4faf6bc57b04f747004
Bert Ramakers <bertramakers@me.com>
2022-10-12 17:20:38 +0200

    Simplify tests that aged like milk and actually test something useful
    Now that we have return types and property types, the it_extends_an_abstract_command() became a self-fulfilling prophecy. And PHPStan didn't like that we did an is_subclass_of() check on something that is always AbstractCommand anyway

--- tests/Label/Commands/MakeInvisibleTest.php
+++ tests/Label/Commands/MakeInvisibleTest.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Label\Commands;
 
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\AuthorizableLabelCommand;
+use PHPUnit\Framework\TestCase;
 
-class MakeInvisibleTest extends AbstractExtendsTest
+class MakeInvisibleTest extends TestCase
 {
     /**
-     * @inheritdoc
+     * @test
      */
-    public function createCommand(UUID $uuid)
+    public function it_requires_the_labels_beheren_permission(): void
     {
-        return new MakeInvisible($uuid);
+        $command = new MakeInvisible(new UUID('c08bfa48-9140-43c0-96f6-eced670ffc16'));
+        $this->assertInstanceOf(AuthorizableLabelCommand::class, $command);
+        $this->assertEquals(Permission::labelsBeheren(), $command->getPermission());
     }
 }
--- tests/Label/Commands/MakePublicTest.php
+++ tests/Label/Commands/MakePublicTest.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Label\Commands;
 
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\AuthorizableLabelCommand;
+use PHPUnit\Framework\TestCase;
 
-class MakePublicTest extends AbstractExtendsTest
+class MakePublicTest extends TestCase
 {
     /**
-     * @inheritdoc
+     * @test
      */
-    public function createCommand(UUID $uuid)
+    public function it_requires_the_labels_beheren_permission(): void
     {
-        return new MakePublic($uuid);
+        $command = new MakePublic(new UUID('c08bfa48-9140-43c0-96f6-eced670ffc16'));
+        $this->assertInstanceOf(AuthorizableLabelCommand::class, $command);
+        $this->assertEquals(Permission::labelsBeheren(), $command->getPermission());
     }
 }
