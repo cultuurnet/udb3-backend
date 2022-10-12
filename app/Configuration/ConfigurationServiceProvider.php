e019251e9f269450a271c5f223c7b6bae0ca415f
steftrenson <steftrenson@gmail.com>
2022-10-12 11:32:19 +0200

    Add a ConfigurationServiceProvider
    had to add this as an exception to the .gitignore as other config files should be excluded

---
+++ app/Configuration/ConfigurationServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Configuration;
+
+use CultuurNet\UDB3\Container\AbstractServiceProvider;
+
+final class ConfigurationServiceProvider extends AbstractServiceProvider
+{
+    protected function getProvidedServiceNames(): array
+    {
+        return [
+
+        ];
+    }
+
+    public function register(): void
+    {
+        // TODO: Implement register() method.
+    }
+}
