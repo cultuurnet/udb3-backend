a37edca5b82f62a782f68fe384e17c6a7786fde0
steftrenson <steftrenson@gmail.com>
2022-10-12 14:52:16 +0200

    the `event_store_factory` should be a callable service

--- app/EventStore/EventStoreServiceProvider.php
+++ app/EventStore/EventStoreServiceProvider.php
@@ -8,6 +8,7 @@ use CultuurNet\UDB3\AggregateType;
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\EventSourcing\DBAL\AggregateAwareDBALEventStore;
 use CultuurNet\UDB3\Labels\LabelServiceProvider;
+use League\Container\Argument\Literal\CallableArgument;
 
 final class EventStoreServiceProvider extends AbstractServiceProvider
 {
@@ -25,12 +26,14 @@ final class EventStoreServiceProvider extends AbstractServiceProvider
 
         $container->addShared(
             'event_store_factory',
-            fn (AggregateType $aggregateType) => new AggregateAwareDBALEventStore(
-                $container->get('dbal_connection'),
-                $container->get('eventstore_payload_serializer'),
-                new \Broadway\Serializer\SimpleInterfaceSerializer(),
-                'event_store',
-                $aggregateType
+            new CallableArgument(
+                fn(AggregateType $aggregateType) => new AggregateAwareDBALEventStore(
+                    $container->get('dbal_connection'),
+                    $container->get('eventstore_payload_serializer'),
+                    new \Broadway\Serializer\SimpleInterfaceSerializer(),
+                    'event_store',
+                    $aggregateType
+                )
             )
         );
 
