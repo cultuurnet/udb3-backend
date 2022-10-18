74e858a9bb5bf5ab30d3050ea9f6f9fdfdaaffb1
Bert Ramakers <bertramakers@me.com>
2022-10-18 12:13:55 +0200

    Use BookingInfoDenormalizer to parse booking info JSON

--- src/Http/Offer/UpdateBookingInfoRequestHandler.php
+++ src/Http/Offer/UpdateBookingInfoRequestHandler.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Http\Offer;
 
 use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\BookingInfo as LegacyBookingInfo;
 use CultuurNet\UDB3\Deserializer\JSONDeserializer;
 use CultuurNet\UDB3\Event\Commands\UpdateBookingInfo as EventUpdateBookingInfo;
 use CultuurNet\UDB3\Http\Deserializer\BookingInfo\BookingInfoJSONDeserializer;
+use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Model\Serializer\ValueObject\Contact\BookingInfoDenormalizer;
+use CultuurNet\UDB3\Model\ValueObject\Contact\BookingInfo;
 use CultuurNet\UDB3\Offer\OfferType;
 use CultuurNet\UDB3\Place\Commands\UpdateBookingInfo as PlaceUpdateBookingInfo;
-use CultuurNet\UDB3\StringLiteral;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -44,11 +47,14 @@ final class UpdateBookingInfoRequestHandler implements RequestHandlerInterface
                     JsonSchemaLocator::EVENT_BOOKING_INFO,
                     JsonSchemaLocator::PLACE_BOOKING_INFO
                 )
-            )
+            ),
+            new DenormalizingRequestBodyParser(new BookingInfoDenormalizer(), BookingInfo::class)
         );
         $request = $parser->parse($request);
 
-        $bookingInfo = $this->bookingInfoDeserializer->deserialize(new StringLiteral($request->getBody()->getContents()));
+        /** @var BookingInfo $bookingInfo */
+        $bookingInfo = $request->getParsedBody();
+        $bookingInfo = LegacyBookingInfo::fromUdb3ModelBookingInfo($bookingInfo);
 
         if ($routeParameters->getOfferType()->sameAs(OfferType::event())) {
             $updateBookingInfo = new EventUpdateBookingInfo(
