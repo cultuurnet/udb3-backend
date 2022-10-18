c834e77a21aed81af1c1c34460ec7e51fa8ea7c1
steftrenson <steftrenson@gmail.com>
2022-10-18 14:10:52 +0200

    Make the propertyName in BookingInfoValidatingRequestBodyParser configurable

--- src/Http/Offer/BookingInfoValidatingRequestBodyParser.php
+++ src/Http/Offer/BookingInfoValidatingRequestBodyParser.php
@@ -7,23 +7,32 @@ namespace CultuurNet\UDB3\Http\Offer;
 use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParser;
 use Psr\Http\Message\ServerRequestInterface;
+use stdClass;
 
 final class BookingInfoValidatingRequestBodyParser implements RequestBodyParser
 {
+    private string $propertyName;
+
+    public function __construct(string $propertyName = '')
+    {
+        $this->propertyName = $propertyName;
+    }
+
     public function parse(ServerRequestInterface $request): ServerRequestInterface
     {
-        $errors = [];
-
         $data = $request->getParsedBody();
 
-        if (isset($data->bookingInfo)) {
-            $errors = (new DateRangeValidator())->validate(
-                $data->bookingInfo,
-                'bookingInfo',
-                'availabilityStarts',
-                'availabilityEnds'
-            );
+        $objectToValidate = $data;
+        if (!empty($this->propertyName)) {
+            $objectToValidate = $data->{$this->propertyName} ?? new stdClass();
         }
+
+        $errors = (new DateRangeValidator())->validate(
+            $objectToValidate,
+            $this->propertyName,
+            'availabilityStarts',
+            'availabilityEnds'
+        );
 
         if (count($errors) > 0) {
             throw ApiProblem::bodyInvalidData(...$errors);
