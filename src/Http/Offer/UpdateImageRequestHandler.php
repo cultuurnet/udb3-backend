6a312011cb6d55d8db1993d9a6506a9e9a12467f
steftrenson <steftrenson@gmail.com>
2022-11-02 10:17:31 +0100

    Construct the UpdateImage command in a denormalizer

--- src/Http/Offer/UpdateImageRequestHandler.php
+++ src/Http/Offer/UpdateImageRequestHandler.php
@@ -5,20 +5,16 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Http\Offer;
 
 use Broadway\CommandHandling\CommandBus;
-use CultuurNet\UDB3\Event\Commands\UpdateImage as EventUpdateImage;
 use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaValidatingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
-use CultuurNet\UDB3\Json;
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
-use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
-use CultuurNet\UDB3\Offer\OfferType;
-use CultuurNet\UDB3\Place\Commands\UpdateImage as PlaceUpdateImage;
-use CultuurNet\UDB3\StringLiteral;
-use Psr\Http\Message\ResponseInterface;
+use CultuurNet\UDB3\Offer\Commands\Image\AbstractUpdateImage;
+use CultuurNet\UDB3\Offer\Serializers\UpdateImageDenormalizer;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
@@ -32,13 +28,11 @@ final class UpdateImageRequestHandler implements RequestHandlerInterface
         $this->commandBus = $commandBus;
     }
 
-    public function handle(ServerRequestInterface $request): ResponseInterface
+    public function handle(ServerRequestInterface $request): NoContentResponse
     {
         $routeParameters = new RouteParameters($request);
         $offerType = $routeParameters->getOfferType();
         $offerId = $routeParameters->getOfferId();
-
-        $bodyContent = Json::decode($request->getBody()->getContents());
 
         try {
             $mediaId = new UUID($routeParameters->getMediaId());
@@ -53,29 +47,17 @@ final class UpdateImageRequestHandler implements RequestHandlerInterface
                     JsonSchemaLocator::EVENT_IMAGE_PUT,
                     JsonSchemaLocator::PLACE_IMAGE_PUT,
                 )
-            )
+            ),
+            new DenormalizingRequestBodyParser(
+                new UpdateImageDenormalizer($offerType, $offerId, $mediaId),
+                AbstractUpdateImage::class,
+            ),
         );
 
-        $requestBodyParser->parse($request);
+        $request = $requestBodyParser->parse($request);
 
-        $description = new StringLiteral($bodyContent->description);
-        $copyrightHolder = new CopyrightHolder($bodyContent->copyrightHolder);
-
-        if ($offerType->sameAs(OfferType::event())) {
-            $updateImage = new EventUpdateImage(
-                $offerId,
-                $mediaId,
-                $description,
-                $copyrightHolder
-            );
-        } else {
-            $updateImage = new PlaceUpdateImage(
-                $offerId,
-                $mediaId,
-                $description,
-                $copyrightHolder
-            );
-        }
+        /** @var AbstractUpdateImage $updateImage */
+        $updateImage = $request->getParsedBody();
 
         $this->commandBus->dispatch($updateImage);
 
---
+++ src/Offer/Serializers/UpdateImageDenormalizer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\Serializers;
+
+use CultuurNet\UDB3\Event\Commands\UpdateImage as EventUpdateImage;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\MediaObject\CopyrightHolder;
+use CultuurNet\UDB3\Offer\Commands\Image\AbstractUpdateImage;
+use CultuurNet\UDB3\Offer\OfferType;
+use CultuurNet\UDB3\Place\Commands\UpdateImage as PlaceUpdateImage;
+use CultuurNet\UDB3\StringLiteral;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+final class UpdateImageDenormalizer implements DenormalizerInterface
+{
+    private OfferType $offerType;
+
+    private string $offerId;
+
+    private UUID $mediaObjectId;
+
+    public function __construct(OfferType $offerType, string $offerId, UUID $mediaObjectId)
+    {
+        $this->offerType = $offerType;
+        $this->offerId = $offerId;
+        $this->mediaObjectId = $mediaObjectId;
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = []): AbstractUpdateImage
+    {
+        if ($this->offerType->sameAs(OfferType::event())) {
+            return new EventUpdateImage(
+                $this->offerId,
+                $this->mediaObjectId,
+                new StringLiteral($data['description']),
+                new CopyrightHolder($data['copyrightHolder'])
+            );
+        } else {
+            return new PlaceUpdateImage(
+                $this->offerId,
+                $this->mediaObjectId,
+                new StringLiteral($data['description']),
+                new CopyrightHolder($data['copyrightHolder'])
+            );
+        }
+    }
+
+    public function supportsDenormalization($data, $type, $format = null): bool
+    {
+        return $type === AbstractUpdateImage::class;
+    }
+}
