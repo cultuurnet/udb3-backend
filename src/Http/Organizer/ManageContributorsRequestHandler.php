de0e4ad899707595c65d8a1eba3c692e726cbdcc
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-06 15:27:10 +0100

    Implement Temp logic in ManageContributors for Organizer

--- src/Http/Organizer/ManageContributorsRequestHandler.php
+++ src/Http/Organizer/ManageContributorsRequestHandler.php
@@ -4,15 +4,48 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Http\Organizer;
 
+use CultuurNet\UDB3\Contributor\ContributorRepositoryInterface;
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\SchemaError;
+use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Web\EmailAddress;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 final class ManageContributorsRequestHandler implements RequestHandlerInterface
 {
+    private ContributorRepositoryInterface $contributorRepository;
+
+    public function __construct(ContributorRepositoryInterface $contributorRepository)
+    {
+        $this->contributorRepository = $contributorRepository;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new NoContentResponse();
+        $routeParameters = new RouteParameters($request);
+        $offerId = $routeParameters->getOfferId();
+        // TODO: Use Json Scheme
+        $emails = JSON::decode($request->getBody()->getContents());
+
+        $this->contributorRepository->deleteContributors(new UUID($offerId));
+
+        try {
+            foreach ($emails as $email) {
+                $this->contributorRepository->addContributor(
+                    new UUID($offerId),
+                    new EmailAddress($email)
+                );
+            }
+            return new NoContentResponse();
+        } catch (\InvalidArgumentException $exception) {
+            throw ApiProblem::bodyInvalidData(
+                new SchemaError('/contributors', $exception->getMessage())
+            );
+        }
     }
 }
