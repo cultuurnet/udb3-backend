aa27ebdbab7a975aa9dbd834ca3468a4fec7801e
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-08 15:41:43 +0100

    Add CommandHandler for UpdateContributors

---
+++ src/Offer/CommandHandlers/UpdateContributorsHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Contributor\ContributorRepository;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Offer\Commands\UpdateContributors;
+use CultuurNet\UDB3\Offer\OfferRepository;
+
+final class UpdateContributorsHandler implements CommandHandler
+{
+    private OfferRepository $offerRepository;
+
+    private ContributorRepository $contributorRepository;
+
+    public function __construct(OfferRepository $offerRepository, ContributorRepository $contributorRepository)
+    {
+        $this->offerRepository = $offerRepository;
+        $this->contributorRepository = $contributorRepository;
+    }
+    public function handle($command): void
+    {
+        if (!($command instanceof UpdateContributors)) {
+            return;
+        }
+
+        $this->offerRepository->load($command->getItemId());
+
+        $this->contributorRepository->overwriteContributors(
+            new UUID($command->getItemId()),
+            $command->getEmailAddresses()
+        );
+    }
+}
