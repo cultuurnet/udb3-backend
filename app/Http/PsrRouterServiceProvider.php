e0d5cec3bfbf34ef03d94ab62932cd283193df04
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-08 12:25:00 +0100

    Rename MangeContributorsRequestHandler to UpdateContributorsRequestHandler for Organizer

--- app/Http/PsrRouterServiceProvider.php
+++ app/Http/PsrRouterServiceProvider.php
@@ -112,7 +112,7 @@ use CultuurNet\UDB3\Http\Organizer\GetOrganizerRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\GetPermissionsForCurrentUserRequestHandler as GetOrganizerPermissionsForCurrentUserRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\GetPermissionsForGivenUserRequestHandler as GetOrganizerPermissionsForGivenUserRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\ImportOrganizerRequestHandler;
-use CultuurNet\UDB3\Http\Organizer\UpdateContributorsRequestHandler as ManageContributorsOrganizerRequestHandler;
+use CultuurNet\UDB3\Http\Organizer\UpdateContributorsRequestHandler as UpdateContributorsOrganizerRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\UpdateAddressRequestHandler as UpdateOrganizerAddressRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\UpdateContactPointRequestHandler as UpdateOrganizerContactPointRequestHandler;
 use CultuurNet\UDB3\Http\Organizer\UpdateDescriptionRequestHandler as UpdateOrganizerDescriptionRequestHandler;
@@ -345,7 +345,7 @@ final class PsrRouterServiceProvider extends AbstractServiceProvider
             $routeGroup->get('{organizerId}/permissions/', GetOrganizerPermissionsForCurrentUserRequestHandler::class);
             $routeGroup->get('{organizerId}/permissions/{userId}/', GetOrganizerPermissionsForGivenUserRequestHandler::class);
 
-            $routeGroup->put('{organizerId}/contributors/', ManageContributorsOrganizerRequestHandler::class);
+            $routeGroup->put('{organizerId}/contributors/', UpdateContributorsOrganizerRequestHandler::class);
         });
     }
 
