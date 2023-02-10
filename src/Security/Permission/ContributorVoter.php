0e345992c221c11097de4588207d5039c5b6fed5
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-10 10:14:14 +0100

    make ContributorVoter

---
+++ src/Security/Permission/ContributorVoter.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Security\Permission;
+
+use CultuurNet\UDB3\Contributor\ContributorRepository;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Web\EmailAddresses;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Security\UserEmailAddressRepository;
+use CultuurNet\UDB3\StringLiteral;
+
+final class ContributorVoter implements PermissionVoter
+{
+    private UserEmailAddressRepository $repository;
+
+    private ContributorRepository $contributorRepository;
+
+    public function __construct(
+        UserEmailAddressRepository $repository,
+        ContributorRepository $contributorRepository
+    ) {
+        $this->repository = $repository;
+        $this->contributorRepository = $contributorRepository;
+    }
+
+    public function isAllowed(Permission $permission, StringLiteral $itemId, StringLiteral $userId): bool
+    {
+        return $this->repository->getEmailForUserId($userId->toNative()) &&
+            in_array(
+                $this->repository->getEmailForUserId($userId->toNative()),
+                $this->getEmailAddressesForItem($itemId->toNative())->toArray()
+            );
+    }
+
+    private function getEmailAddressesForItem(string $itemId): EmailAddresses
+    {
+        return $this->contributorRepository->getContributors(new UUID($itemId));
+    }
+}
