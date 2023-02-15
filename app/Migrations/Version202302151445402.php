f9e6c001b8c37b02fd99c7fc9e665c4ce545eb49
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-15 15:12:13 +0100

    Migration add type to contributors

---
+++ app/Migrations/Version202302151445402.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version202302151445402 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('contributor_relations');
+        $table->addColumn('type', Type::STRING)->setLength(255)->setNotnull(true);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('contributor_relations');
+        $table->dropColumn('offerType');
+    }
+}
