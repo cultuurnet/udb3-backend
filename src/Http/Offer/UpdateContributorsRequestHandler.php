047373ccbd6db4254be57b110902c19bbca5d40b
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-15 15:14:41 +0100

    Update RequestHandler to dispatch correct command

--- src/Http/Offer/UpdateContributorsRequestHandler.php
+++ src/Http/Offer/UpdateContributorsRequestHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Http\Offer;
 
 use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Event\Events\UpdateEventContributors;
 use CultuurNet\UDB3\Http\Deserializer\ContributorDenormalizer;
 use CultuurNet\UDB3\Http\Request\Body\DenormalizingRequestBodyParser;
 use CultuurNet\UDB3\Http\Request\Body\JsonSchemaLocator;
@@ -13,7 +14,8 @@ use CultuurNet\UDB3\Http\Request\Body\RequestBodyParserFactory;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\NoContentResponse;
 use CultuurNet\UDB3\Model\ValueObject\Web\EmailAddresses;
-use CultuurNet\UDB3\Offer\Commands\UpdateContributors;
+use CultuurNet\UDB3\Offer\OfferType;
+use CultuurNet\UDB3\Place\Events\UpdatePlaceContributors;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -49,8 +51,14 @@ final class UpdateContributorsRequestHandler implements RequestHandlerInterface
 
         /** @var EmailAddresses $emails */
         $emails = $parser->parse($request)->getParsedBody();
+
+        if ($offerType === OfferType::event()) {
+            $updateContributors = new UpdateEventContributors($offerId, $emails);
+        } else {
+            $updateContributors = new UpdatePlaceContributors($offerId, $emails);
+        }
         $this->commandBus->dispatch(
-            new UpdateContributors($offerId, $emails)
+            $updateContributors
         );
 
         return new NoContentResponse();
