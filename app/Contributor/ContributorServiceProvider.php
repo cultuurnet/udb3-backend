c82d4253f55dd3d28a30b407c376f628ac09f212
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-16 14:04:12 +0100

    use BroadcastingContributorRepository in ServiceProvider

--- app/Contributor/ContributorServiceProvider.php
+++ app/Contributor/ContributorServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Contributor;
 
+use Broadway\EventHandling\EventBus;
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
+use CultuurNet\UDB3\Iri\CallableIriGenerator;
 
 final class ContributorServiceProvider extends AbstractServiceProvider
 {
@@ -21,7 +23,23 @@ final class ContributorServiceProvider extends AbstractServiceProvider
 
         $container->addShared(
             ContributorRepository::class,
-            fn () => new DbalContributorRepository($container->get('dbal_connection'))
+            fn () => new BroadcastingContributorRepository(
+                new DbalContributorRepository($container->get('dbal_connection')),
+                $container->get(EventBus::class),
+                new ContributorsUpdatedFactory(
+                    new CallableIriGenerator(
+                        fn ($cdbid) => $container->get('config')['url'] . '/events/' . $cdbid . '/contributors'
+                    ),
+                    new CallableIriGenerator(
+                        fn ($cdbid) => $container->get('config')['url'] . '/places/' . $cdbid . '/contributors'
+                    ),
+                    new CallableIriGenerator(
+                        fn () => new CallableIriGenerator(
+                            fn ($cdbid) => $container->get('config')['url'] . '/organizers/' . $cdbid . '/contributors'
+                        )
+                    )
+                )
+            )
         );
     }
 }
