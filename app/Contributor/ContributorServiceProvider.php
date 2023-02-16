78389f85a8c88b2597aa2a563b321056997aa943
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-16 08:44:04 +0100

    register iri generators in ServiceProvider

--- app/Contributor/ContributorServiceProvider.php
+++ app/Contributor/ContributorServiceProvider.php
@@ -6,12 +6,16 @@ namespace CultuurNet\UDB3\Contributor;
 
 use Broadway\EventHandling\EventBus;
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
+use CultuurNet\UDB3\Iri\CallableIriGenerator;
 
 final class ContributorServiceProvider extends AbstractServiceProvider
 {
     protected function getProvidedServiceNames(): array
     {
         return [
+            'event_contributors_iri_generator',
+            'place_contributors_iri_generator',
+            'organizer_contributors_iri_generator',
             ContributorRepository::class,
         ];
     }
@@ -19,6 +23,27 @@ final class ContributorServiceProvider extends AbstractServiceProvider
     public function register(): void
     {
         $container = $this->getContainer();
+
+        $container->addShared(
+            'event_contributors_iri_generator',
+            fn () => new CallableIriGenerator(
+                fn ($cdbid) => $container->get('config')['url'] . '/events/' . $cdbid . '/contributors'
+            )
+        );
+
+        $container->addShared(
+            'place_contributors_iri_generator',
+            fn () => new CallableIriGenerator(
+                fn ($cdbid) => $container->get('config')['url'] . '/places/' . $cdbid . '/contributors'
+            )
+        );
+
+        $container->addShared(
+            'organizer_contributors_iri_generator',
+            fn () => new CallableIriGenerator(
+                fn ($cdbid) => $container->get('config')['url'] . '/organizers/' . $cdbid . '/contributors'
+            )
+        );
 
         $container->addShared(
             ContributorRepository::class,
