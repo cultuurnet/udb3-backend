53aebf5a8af6b22f8c179ca5cdcc68b126ca76ca
Bert Ramakers <bertramakers@me.com>
2023-02-21 14:54:45 +0100

    Implement ConvertsToGranularEvents on MajorInfoUpdated

--- src/Place/Events/MajorInfoUpdated.php
+++ src/Place/Events/MajorInfoUpdated.php
@@ -7,10 +7,11 @@ namespace CultuurNet\UDB3\Place\Events;
 use CultuurNet\UDB3\Address\Address;
 use CultuurNet\UDB3\Calendar;
 use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\EventSourcing\ConvertsToGranularEvents;
 use CultuurNet\UDB3\Place\PlaceEvent;
 use CultuurNet\UDB3\Title;
 
-final class MajorInfoUpdated extends PlaceEvent
+final class MajorInfoUpdated extends PlaceEvent implements ConvertsToGranularEvents
 {
     /**
      * @var Title
@@ -65,6 +66,20 @@ final class MajorInfoUpdated extends PlaceEvent
     public function getAddress(): Address
     {
         return $this->address;
+    }
+
+    public function toGranularEvents(): array
+    {
+        return array_values(
+            array_filter(
+                [
+                    new TitleUpdated($this->placeId, $this->title),
+                    new TypeUpdated($this->placeId, $this->eventType),
+                    new AddressUpdated($this->placeId, $this->address),
+                    new CalendarUpdated($this->placeId, $this->calendar),
+                ]
+            )
+        );
     }
 
     public function serialize(): array
