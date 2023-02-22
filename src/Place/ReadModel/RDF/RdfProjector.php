f098d9d4e57fee80844a429e031967b6dea8745a
Bert Ramakers <bertramakers@me.com>
2023-02-22 18:21:44 +0100

    Store main language of Place

--- src/Place/ReadModel/RDF/RdfProjector.php
+++ src/Place/ReadModel/RDF/RdfProjector.php
@@ -6,10 +6,44 @@ namespace CultuurNet\UDB3\Place\ReadModel\RDF;
 
 use Broadway\Domain\DomainMessage;
 use Broadway\EventHandling\EventListener;
+use CultuurNet\UDB3\EventSourcing\ConvertsToGranularEvents;
+use CultuurNet\UDB3\EventSourcing\MainLanguageDefined;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\RDF\MainLanguageRepository;
 
 final class RdfProjector implements EventListener
 {
+    private MainLanguageRepository $mainLanguageRepository;
+
+    public function __construct(MainLanguageRepository $mainLanguageRepository)
+    {
+        $this->mainLanguageRepository = $mainLanguageRepository;
+    }
+
     public function handle(DomainMessage $domainMessage): void
     {
+        $payload = $domainMessage->getPayload();
+        $granularEvents = $payload instanceof ConvertsToGranularEvents ? $payload->toGranularEvents() : [];
+        $events = [$payload, ...$granularEvents];
+
+        $mapping = [
+            MainLanguageDefined::class => fn ($e) => $this->handleMainLanguageDefined($e, $domainMessage),
+        ];
+
+        foreach ($events as $event) {
+            foreach ($mapping as $class => $handler) {
+                if ($event instanceof $class) {
+                    $handler($event);
+                }
+            }
+        }
+    }
+
+    private function handleMainLanguageDefined(MainLanguageDefined $event, DomainMessage $domainMessage): void
+    {
+        $this->mainLanguageRepository->save(
+            $domainMessage->getId(),
+            new Language($event->getMainLanguage()->getCode())
+        );
     }
 }
---
+++ tests/Place/ReadModel/RDF/RdfProjectorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place\ReadModel\RDF;
+
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Address\Address as LegacyAddress;
+use CultuurNet\UDB3\Address\Locality as LegacyLocality;
+use CultuurNet\UDB3\Address\PostalCode as LegacyPostalCode;
+use CultuurNet\UDB3\Address\Street as LegacyStreet;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType as LegacyCalendarType;
+use CultuurNet\UDB3\Event\EventType as LegacyEventType;
+use CultuurNet\UDB3\Language as LegacyLanguage;
+use CultuurNet\UDB3\Model\ValueObject\Geography\CountryCode;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Place\Events\PlaceCreated;
+use CultuurNet\UDB3\RDF\InMemoryMainLanguageRepository;
+use CultuurNet\UDB3\RDF\MainLanguageRepository;
+use CultuurNet\UDB3\Title as LegacyTitle;
+use PHPUnit\Framework\TestCase;
+
+class RdfProjectorTest extends TestCase
+{
+    private MainLanguageRepository $mainLanguageRepository;
+    private RdfProjector $rdfProjector;
+
+    protected function setUp(): void
+    {
+        $this->mainLanguageRepository = new InMemoryMainLanguageRepository();
+        $this->rdfProjector = new RdfProjector($this->mainLanguageRepository);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_place_created(): void
+    {
+        $placeId = 'd4b46fba-6433-4f86-bcb5-edeef6689fea';
+        $placeCreated = new PlaceCreated(
+            $placeId,
+            new LegacyLanguage('nl'),
+            new LegacyTitle('Voorbeeld titel'),
+            new LegacyEventType('0.14.0.0.0', 'Monument'),
+            new LegacyAddress(
+                new LegacyStreet('Martelarenlaan 1'),
+                new LegacyPostalCode('3000'),
+                new LegacyLocality('Leuven'),
+                new CountryCode('BE')
+            ),
+            new Calendar(LegacyCalendarType::PERMANENT())
+        );
+        $domainMessage = DomainMessage::recordNow($placeId, 0, new Metadata(), $placeCreated);
+
+        $this->rdfProjector->handle($domainMessage);
+
+        $expected = new Language('nl');
+        $actual = $this->mainLanguageRepository->get($placeId);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
