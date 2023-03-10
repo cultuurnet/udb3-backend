51f33d0fadcfa7b7703f34d1b8120bc1cdb984c4
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-10 09:44:16 +0100

    Add migration to change newsArticle table

---
+++ app/Migrations/Version20230309140014.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230309140014 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('news_article');
+        $table->addColumn('image_url', Type::TEXT)->setDefault(null)->setNotnull(false);
+        $table->addColumn('copyrightHolder', Type::TEXT)
+            ->setDefault(null)
+            ->setNotnull(false)
+            ->setLength(250);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('news_article');
+        $table->dropColumn('image_url');
+        $table->dropColumn('copyrightHolder');
+    }
+}
