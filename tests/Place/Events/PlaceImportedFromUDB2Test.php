c8e332dc9183e5510f5051005c4f52864b8fe97d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-16 13:19:15 +0100

    Add test for TitleTranslated

--- tests/Place/Events/PlaceImportedFromUDB2Test.php
+++ tests/Place/Events/PlaceImportedFromUDB2Test.php
@@ -44,4 +44,26 @@ final class PlaceImportedFromUDB2Test extends TestCase
             $placeImportedFromUDB2->toGranularEvents()
         );
     }
+
+    /**
+     * @test
+     */
+    public function it_can_convert_translated_places_to_modern_granular_events(): void
+    {
+        $placeId = '0452b4ae-7c18-4b33-a6c6-eba2288c9ac3';
+        $placeImportedFromUDB2 = new PlaceImportedFromUDB2(
+            $placeId,
+            file_get_contents(__DIR__ . '/../actor_with_translations.xml'),
+            'http://www.cultuurdatabank.com/XMLSchema/CdbXSD/3.2/FINAL'
+        );
+
+        $this->assertEquals(
+            [
+                new TitleUpdated($placeId, new Title('CC Palethe')),
+                new TitleTranslated($placeId, new Language('fr'), new Title('Centre culturel Palethe')),
+                new TitleTranslated($placeId, new Language('de'), new Title('Kulturzentrum Palethe')),
+            ],
+            $placeImportedFromUDB2->toGranularEvents()
+        );
+    }
 }
