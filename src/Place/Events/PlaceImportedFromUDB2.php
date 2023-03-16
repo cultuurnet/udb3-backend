c1a025a9e01f65f3e529cdfd4daa59d859dc85ba
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-16 13:35:50 +0100

    Add AddressUpdated to GranualarEvents in PlaceImportedFromUDB2

--- src/Place/Events/PlaceImportedFromUDB2.php
+++ src/Place/Events/PlaceImportedFromUDB2.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Place\Events;
 
 use CultureFeed_Cdb_Data_ActorDetail;
+use CultureFeed_Cdb_Data_Address;
 use CultureFeed_Cdb_Item_Actor;
 use CultuurNet\UDB3\Actor\ActorImportedFromUDB2;
+use CultuurNet\UDB3\Address\Address;
+use CultuurNet\UDB3\Address\Locality;
+use CultuurNet\UDB3\Address\PostalCode;
+use CultuurNet\UDB3\Address\Street;
 use CultuurNet\UDB3\EventSourcing\ConvertsToGranularEvents;
 use CultuurNet\UDB3\EventSourcing\MainLanguageDefined;
 use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\Model\ValueObject\Geography\CountryCode;
 use CultuurNet\UDB3\Title;
 
 final class PlaceImportedFromUDB2 extends ActorImportedFromUDB2 implements MainLanguageDefined, ConvertsToGranularEvents
@@ -40,6 +46,19 @@ final class PlaceImportedFromUDB2 extends ActorImportedFromUDB2 implements MainL
             );
             $details->next();
         }
+        /** @var CultureFeed_Cdb_Data_Address $address */
+        $address = $cultureFeedActor->getContactInfo()->getAddresses()[0];
+        $physicalAddress = $address->getPhysicalAddress();
+
+        $granularEvents[] = new AddressUpdated(
+            $this->actorId,
+            new Address(
+                new Street($physicalAddress->getStreet() . ' ' . $physicalAddress->getHouseNumber()),
+                new PostalCode($physicalAddress->getZip()),
+                new Locality($physicalAddress->getCity()),
+                new CountryCode($physicalAddress->getCountry())
+            )
+        );
 
         return $granularEvents;
     }
