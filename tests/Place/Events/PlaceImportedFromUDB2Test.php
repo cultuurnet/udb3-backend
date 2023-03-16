50e94a51880fecef0bae447da87ffc03d503a668
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-16 13:04:58 +0100

    Add Test To Check updateToGranularEvents

--- tests/Place/Events/PlaceImportedFromUDB2Test.php
+++ tests/Place/Events/PlaceImportedFromUDB2Test.php
@@ -6,6 +6,7 @@ namespace CultuurNet\UDB3\Place\Events;
 
 use CultuurNet\UDB3\EventSourcing\MainLanguageDefined;
 use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\Title;
 use PHPUnit\Framework\TestCase;
 
 final class PlaceImportedFromUDB2Test extends TestCase
@@ -23,5 +24,24 @@ final class PlaceImportedFromUDB2Test extends TestCase
 
         $this->assertInstanceOf(MainLanguageDefined::class, $event);
         $this->assertEquals(new Language('nl'), $event->getMainLanguage());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_converted_to_modern_granular_events(): void
+    {
+        $placeImportedFromUDB2 = new PlaceImportedFromUDB2(
+            '0452b4ae-7c18-4b33-a6c6-eba2288c9ac3',
+            file_get_contents(__DIR__ . '/../actor2.xml'),
+            'http://www.cultuurdatabank.com/XMLSchema/CdbXSD/3.2/FINAL'
+        );
+
+        $this->assertEquals(
+            [
+                new TitleUpdated('0452b4ae-7c18-4b33-a6c6-eba2288c9ac3', new Title('CC Palethe')),
+            ],
+            $placeImportedFromUDB2->toGranularEvents()
+        );
     }
 }
