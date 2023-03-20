1b103287ce34ac8d1626ec593ed3968c49182bb0
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-20 11:36:08 +0100

    don't use Culturefeed for parsing event xml

--- src/Event/Events/EventFromUDB2.php
+++ src/Event/Events/EventFromUDB2.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Event\Events;
 
-use CultureFeed_Cdb_Data_EventDetail;
-use CultureFeed_Cdb_Item_Event;
 use CultuurNet\UDB3\Event\EventType;
 use CultuurNet\UDB3\Language;
+use CultuurNet\UDB3\SerializableXML;
 use CultuurNet\UDB3\Title;
 
 trait EventFromUDB2
@@ -15,30 +14,28 @@ trait EventFromUDB2
     public function toGranularEvents(): array
     {
         $granularEvents = [];
-        $cultureFeedEvent = $this->getCultureFeedEvent();
-        $details = $cultureFeedEvent->getDetails();
-        $firstDetail = $details->getFirst();
+        $eventAsArray = $this->getEventAsArray();
+        $details = $eventAsArray['event']['eventdetails'][0]['eventdetail'];
 
-        $granularEvents[] = new TitleUpdated($this->eventId, new Title($firstDetail->getTitle()));
-
-        $details->next();
-        while ($details->valid()) {
-            /** @var CultureFeed_Cdb_Data_EventDetail $detail */
-            $detail = $details->current();
-            $granularEvents[] = new TitleTranslated(
-                $this->eventId,
-                new Language($detail->getLanguage()),
-                new Title($detail->getTitle())
-            );
-            $details->next();
+        foreach ($details as $key => $detail) {
+            if ($key == 0) {
+                $granularEvents[] = new TitleUpdated($this->eventId, new Title($detail['title'][0]['_text']));
+            } else {
+                $granularEvents[] = new TitleTranslated(
+                    $this->eventId,
+                    new Language($detail['@attributes']['lang']),
+                    new Title($detail['title'][0]['_text'])
+                );
+            }
         }
 
-        /* @var \Culturefeed_Cdb_Data_Category $category */
-        foreach ($cultureFeedEvent->getCategories() as $category) {
-            if ($category->getType() === 'eventtype') {
+        $categories = $eventAsArray['event']['categories'][0]['category'];
+
+        foreach ($categories as $category) {
+            if ($category['@attributes']['type'] === 'eventtype') {
                 $granularEvents[] = new TypeUpdated(
                     $this->eventId,
-                    new EventType($category->getId(), $category->getName())
+                    new EventType($category['@attributes']['catid'], $category['_text'])
                 );
             }
         }
@@ -46,16 +43,15 @@ trait EventFromUDB2
         return $granularEvents;
     }
 
-
-    private function getCultureFeedEvent(): CultureFeed_Cdb_Item_Event
+    public function getEventAsArray(): array
     {
-        $cdbXml = new \SimpleXMLElement(
+        $cdbXml = new SerializableXML(
             $this->cdbXml,
             0,
             false,
             $this->cdbXmlNamespaceUri
         );
 
-        return CultureFeed_Cdb_Item_Event::parseFromCdbXml($cdbXml);
+        return $cdbXml->serialize();
     }
 }
