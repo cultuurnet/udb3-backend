07d691395145101b7b12532e6d73f4ad14339eb8
Bert Ramakers <bertramakers@me.com>
2023-03-22 16:04:39 +0100

    Put line separator in a constant
    It could also become a configurable property instead if ever needed

--- src/Address/FullAddressFormatter.php
+++ src/Address/FullAddressFormatter.php
@@ -6,9 +6,11 @@ namespace CultuurNet\UDB3\Address;
 
 final class FullAddressFormatter implements AddressFormatter
 {
+    private const LINE_SEPARATOR = ', ';
+
     public function format(Address $address): string
     {
-        return implode(', ', [
+        return implode(self::LINE_SEPARATOR, [
             $address->getStreetAddress()->toNative(),
             $address->getPostalCode()->toNative() . ' ' . $address->getLocality()->toNative(),
             $address->getCountryCode()->toString(),
