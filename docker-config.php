4f32e6a234a01928ab7dd54e061ab6614ff5352a
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-29 14:27:14 +0200

    add docker-config.php file

---
+++ docker-config.php
@@ -0,0 +1,26 @@
+<?php
+
+return array_replace_recursive(
+    file_exists(__DIR__ . '/config.vagrant.php') ? require __DIR__ . '/config.vagrant.php' : [],
+    [
+        'url' => 'http://host.docker.internal:8000',
+        'search' => [
+            'v3' => [
+                'base_url' => 'http://host.docker.internal:9000',
+                'scheme' => 'http',
+                'port' => 9000,
+            ],
+        ],
+        'database' => [
+            'host' => 'mysql',
+        ],
+        'cache' => [
+            'redis' => [
+                'host' => 'redis',
+            ],
+        ],
+        'amqp' => [
+            'host' => 'rabbitmq',
+        ]
+    ]
+);
