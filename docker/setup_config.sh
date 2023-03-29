16940160925e87e84e76789a46478d354cdf69d3
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-29 14:26:20 +0200

    save config.php as config.vagrant.php

--- docker/setup_config.sh
+++ docker/setup_config.sh
@@ -3,11 +3,15 @@
 echo Please provide a classic Github access token from https://github.com/settings/tokens
 read token
 
+curl -H 'Authorization: token '"$token" \
+  -H 'Accept: application/vnd.github.v3.raw' \
+  -o config.vagrant.php \
+  -L https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.php
+
 # retrieve necessary files from udb3-vagrant
 array=(
   https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/keys/public.pem
   https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/keys/public-auth0.pem
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.php
   https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.allow_all.php
   https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.excluded_labels.php
   https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.external_id_mapping_organizer.php
