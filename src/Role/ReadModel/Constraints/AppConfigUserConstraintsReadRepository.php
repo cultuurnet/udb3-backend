4545fbd6e7523c78c3f08bef76443312fa27876c
simon-debruijn <simon.debruijn@hotmail.com>
2023-04-07 16:58:27 +0200

    Add AppConfigUserConstraintsReadRepository

---
+++ src/Role/ReadModel/Constraints/AppConfigUserConstraintsReadRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Role\ReadModel\Constraints;
+
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\StringLiteral;
+
+final class AppConfigUserConstraintsReadRepository implements UserConstraintsReadRepositoryInterface
+{
+
+    /**
+     * @var array<string, array>
+     */
+    private array $clientIdToPermissionsConfig;
+
+    private UserConstraintsReadRepositoryInterface $userConstraintsReadRepository;
+
+
+    public function __construct(UserConstraintsReadRepositoryInterface $userConstraintsReadRepository, array $clientIdToPermissionsConfig)
+    {
+        $this->userConstraintsReadRepository = $userConstraintsReadRepository;
+        $this->clientIdToPermissionsConfig = $clientIdToPermissionsConfig;
+    }
+
+    public function getByUserAndPermission(StringLiteral $userId, Permission $permission): array
+    {
+        $config = $this->clientIdToPermissionsConfig[$userId->toNative()] ?? [];
+        $permissions = $config['permissions'] ?? [];
+        $constraint = $config['sapi3_constraint'] ?? null;
+
+        if (!in_array($permission, $permissions) || $constraint === null) {
+            return $this->userConstraintsReadRepository->getByUserAndPermission($userId, $permission);
+        }
+
+        return [new StringLiteral($constraint)];
+    }
+}
