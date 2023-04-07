eac2fedc314d3b4d24e2baf51776a046e397e007
simon-debruijn <simon.debruijn@hotmail.com>
2023-04-07 16:58:47 +0200

    Decorate UserConstraintsReadRepository with AppConfigUserConstraintsReadRepository

--- app/Security/GeneralSecurityServiceProvider.php
+++ app/Security/GeneralSecurityServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Security;
 
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
+use CultuurNet\UDB3\Role\ReadModel\Constraints\AppConfigUserConstraintsReadRepository;
 use CultuurNet\UDB3\Role\ReadModel\Constraints\Doctrine\UserConstraintsReadRepository;
 use CultuurNet\UDB3\Role\RoleServiceProvider;
 use CultuurNet\UDB3\Role\UserPermissionsServiceProvider;
@@ -34,11 +35,14 @@ final class GeneralSecurityServiceProvider extends AbstractServiceProvider
 
         $container->addShared(
             'user_constraints_read_repository',
-            fn () => new UserConstraintsReadRepository(
-                $container->get('dbal_connection'),
-                UserPermissionsServiceProvider::USER_ROLES_TABLE,
-                UserPermissionsServiceProvider::ROLE_PERMISSIONS_TABLE,
-                RoleServiceProvider::ROLE_SEARCH_V3_REPOSITORY_TABLE_NAME,
+            fn () => new AppConfigUserConstraintsReadRepository(
+                new UserConstraintsReadRepository(
+                    $container->get('dbal_connection'),
+                    UserPermissionsServiceProvider::USER_ROLES_TABLE,
+                    UserPermissionsServiceProvider::ROLE_PERMISSIONS_TABLE,
+                    RoleServiceProvider::ROLE_SEARCH_V3_REPOSITORY_TABLE_NAME,
+                ),
+                $container->get('config')['client_permissions']
             )
         );
     }
