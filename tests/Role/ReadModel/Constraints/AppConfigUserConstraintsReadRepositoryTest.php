f998a5e9253a10f311f3ddb24c6d8e716f78f607
simon-debruijn <simon.debruijn@hotmail.com>
2023-04-07 17:58:54 +0200

    Add AppConfigUserConstraintsReadRepositoryTest with first test

---
+++ tests/Role/ReadModel/Constraints/AppConfigUserConstraintsReadRepositoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Role\ReadModel\Constraints;
+
+use CultuurNet\UDB3\Role\ReadModel\Constraints\Doctrine\UserConstraintsReadRepository;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\StringLiteral;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class AppConfigUserConstraintsReadRepositoryTest extends TestCase
+{
+    /**
+     * @var UserConstraintsReadRepository|MockObject
+     */
+    private $databaseRepository;
+
+    protected function setUp(): void
+    {
+        $this->databaseRepository = $this->createMock(UserConstraintsReadRepository::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_calls_database_repository_when_it_has_no_sapi3_constraint() {
+        $config = [
+            'jkfhsjkfsdhjk@clients' => [
+                'permissions' => [Permission::aanbodBewerken(), Permission::productiesAanmaken()],
+                'labels' => ['UiTinLeuven'],
+            ]
+        ];
+        $repository = new AppConfigUserConstraintsReadRepository($this->databaseRepository, $config);
+
+        $this->databaseRepository->expects($this->once())->method('getByUserAndPermission');
+
+        $repository->getByUserAndPermission(new StringLiteral('jkfhsjkfsdhjk@clients'), Permission::aanbodBewerken());
+    }
+
+}
