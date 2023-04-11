a9dfa1329e4f923b1af5b0913520c808e8c8c3c5
simon-debruijn <simon.debruijn@hotmail.com>
2023-04-11 13:31:06 +0200

    Add empty test for AppConfigReadRepositoryDecoratorTest

---
+++ tests/Label/ReadModels/JSON/Repository/AppConfigReadRepositoryDecoratorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Label\ReadModels\JSON\Repository;
+
+use PHPUnit\Framework\TestCase;
+
+final class AppConfigReadRepositoryDecoratorTest extends TestCase
+{
+
+    protected function setUp(): void
+    {
+    }
+
+    public function it_tests() {
+
+    }
+
+}
