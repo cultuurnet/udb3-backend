b1266e2d5eddef065bc5095adecbb59768181a22
Luc Wollants <lucwollants@gmail.com>
2023-04-13 10:46:59 +0200

    Create `WorkflowEditor` to update the RDF workflow status on event and place

---
+++ app/RDF/WorkflowEditor.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\RDF;
+
+use EasyRdf\Graph;
+use EasyRdf\Literal;
+use EasyRdf\Resource;
+
+final class WorkflowEditor
+{
+    private Graph $graph;
+
+    private const PROPERTY_WORKFLOW_STATUS = 'udb:workflowStatus';
+    private const PROPERTY_WORKFLOW_STATUS_DRAFT = 'https://data.publiq.be/concepts/workflowStatus/draft';
+    private const PROPERTY_WORKFLOW_STATUS_READY_FOR_VALIDATION = 'https://data.publiq.be/concepts/workflowStatus/ready-for-validation';
+    private const PROPERTY_AVAILABLE_FROM = 'udb:availableFrom';
+
+    private function __construct(Graph $graph)
+    {
+        $this->graph = $graph;
+    }
+
+    public static function for(Graph $graph): self
+    {
+        return new self($graph);
+    }
+
+    public function draft(string $resourceUri): void
+    {
+        $resource = $this->graph->resource($resourceUri);
+
+        if (!$resource->hasProperty(self::PROPERTY_WORKFLOW_STATUS)) {
+            $resource->set(
+                self::PROPERTY_WORKFLOW_STATUS,
+                new Resource(self::PROPERTY_WORKFLOW_STATUS_DRAFT)
+            );
+        }
+    }
+
+    public function publish(string $resourceUri, string $publicationDate): void
+    {
+        $resource = $this->graph->resource($resourceUri);
+
+        $resource->set(
+            self::PROPERTY_WORKFLOW_STATUS,
+            new Resource(self::PROPERTY_WORKFLOW_STATUS_READY_FOR_VALIDATION)
+        );
+
+        $resource->set(
+            self::PROPERTY_AVAILABLE_FROM,
+            new Literal($publicationDate, null, 'xsd:dateTime')
+        );
+    }
+}
