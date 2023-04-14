02263c74bb4e04a794154d4f2e8f9ccc9c77ac74
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-04-14 12:05:43 +0200

    add copy of env & docker-config to shellscript

--- docker/setup_config.sh
+++ docker/setup_config.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# copy config files
+cp .env.dist .env
+cp docker-config.php config.php
+
+# retrieve necessary files
 if [ ! -d udb3-docker-config ]; then
   git clone git@github.com:cultuurnet/udb3-docker-config.git;
   cd udb3-docker-config;
