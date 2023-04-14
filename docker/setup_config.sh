11f2cc75f88f56cd4032ff47537d0e72a947f9c1
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-04-14 12:00:33 +0200

    simplify getting config files

--- docker/setup_config.sh
+++ docker/setup_config.sh
@@ -1,35 +1,14 @@
 #!/bin/sh
 
-echo Please provide a classic Github access token from https://github.com/settings/tokens
-read token
+if [ ! -d udb3-docker-config ]; then
+  git clone git@github.com:cultuurnet/udb3-docker-config.git;
+  cd udb3-docker-config;
+else
+  cd udb3-docker-config;
+  git stash push --include-untracked;
+  git checkout main;
+  git pull;
+fi
 
-# copy config files
-cp .env.dist .env
-cp docker-config.php config.php
-
-# retrieve config.php as config.vagrant.php from udb3-vagrant
-curl -H 'Authorization: token '"$token" \
-  -H 'Accept: application/vnd.github.v3.raw' \
-  -o config.vagrant.php \
-  -L https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.php
-
-# retrieve necessary files from udb3-vagrant
-array=(
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/keys/public.pem
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/keys/public-auth0.pem
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.allow_all.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.excluded_labels.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.external_id_mapping_organizer.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.external_id_mapping_place.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.term_mapping_facilities.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.term_mapping_themes.php
-  https://raw.githubusercontent.com/cultuurnet/udb3-vagrant/main/config/udb3-backend/config.term_mapping_types.php
-)
-
-for file in "${array[@]}"
-do
-  curl -H 'Authorization: token '"$token" \
-    -H 'Accept: application/vnd.github.v3.raw' \
-    -O \
-    -L "$file"
-done
+cp *.pem ../
+cp *.php ../
