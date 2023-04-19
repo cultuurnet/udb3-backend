2b8d709ac8daad0d8677a171df53c1a30251810c
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-04-19 11:36:07 +0200

    update shellscript to cp hidden files.

--- docker/setup_config.sh
+++ docker/setup_config.sh
@@ -4,6 +4,8 @@
 DIR="../appconfig/files/udb3/docker/udb3-backend/"
 if [ -d "$DIR" ]; then
   cp "$DIR"/* .
+  # needed because it is hidden
+  cp "$DIR"/.env .
 else
   echo "Error: missing appconfig see docker.md prerequisites to fix this."
   exit 1
