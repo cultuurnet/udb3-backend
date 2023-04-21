52a2010c3cc3b4efb7d01e8679c5b15f0135bb59
Luc Wollants <lucwollants@gmail.com>
2023-04-21 14:26:28 +0200

    Make OfferLDProjector use LoggerAwareTrait

--- src/Offer/ReadModel/JSONLD/OfferLDProjector.php
+++ src/Offer/ReadModel/JSONLD/OfferLDProjector.php
@@ -59,9 +59,13 @@ use CultuurNet\UDB3\ReadModel\MultilingualJsonLDProjectorTrait;
 use CultuurNet\UDB3\RecordedOn;
 use CultuurNet\UDB3\SluggerInterface;
 use DateTimeInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 
 abstract class OfferLDProjector implements OrganizerServiceInterface
 {
+    use LoggerAwareTrait;
+
     use MultilingualJsonLDProjectorTrait;
     use DelegateEventHandlingToSpecificMethodTrait {
         DelegateEventHandlingToSpecificMethodTrait::handle as handleUnknownEvents;
@@ -110,6 +114,8 @@ abstract class OfferLDProjector implements OrganizerServiceInterface
         $this->videoNormalizer = $videoNormalizer;
 
         $this->slugger = new CulturefeedSlugger();
+
+        $this->logger = new NullLogger();
     }
 
     public function handle(DomainMessage $domainMessage): void
