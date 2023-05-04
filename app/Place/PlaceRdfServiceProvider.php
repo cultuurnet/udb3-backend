ae93799dfae6aca8e1035ebc92e6d9357b3cd2b2
Luc Wollants <lucwollants@gmail.com>
2023-05-04 15:54:10 +0200

    Replace Graph Store with Cache Store when enabled
    This config can be set to true on the replay server

--- app/Place/PlaceRdfServiceProvider.php
+++ app/Place/PlaceRdfServiceProvider.php
@@ -11,6 +11,7 @@ use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Error\LoggerFactory;
 use CultuurNet\UDB3\Error\LoggerName;
 use CultuurNet\UDB3\Place\ReadModel\RDF\RdfProjector;
+use CultuurNet\UDB3\RDF\CacheGraphRepository;
 use CultuurNet\UDB3\RDF\MainLanguageRepository;
 use CultuurNet\UDB3\RDF\RdfServiceProvider;
 
@@ -26,11 +27,19 @@ final class PlaceRdfServiceProvider extends AbstractServiceProvider
 
     public function register(): void
     {
+        $graphStoreRepository = RdfServiceProvider::createGraphStoreRepository(
+            $this->container->get('config')['rdf']['placesGraphStoreUrl']
+        );
+
+        if ($this->container->get('config')['rdf']['placesCacheGraphEnabled']) {
+            $graphStoreRepository = new CacheGraphRepository($this->container->get('cache')('rdf_place'));
+        }
+
         $this->container->addShared(
             RdfProjector::class,
             fn (): RdfProjector => new RdfProjector(
                 $this->container->get(MainLanguageRepository::class),
-                RdfServiceProvider::createGraphStoreRepository($this->container->get('config')['rdf']['placesGraphStoreUrl']),
+                $graphStoreRepository,
                 RdfServiceProvider::createIriGenerator($this->container->get('config')['rdf']['placesRdfBaseUri']),
                 $this->container->get(AddressParser::class)
             )
