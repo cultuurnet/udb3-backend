65ab20aa485ac6bf1a5feea07c2adbc8c0dc0f81
Luc Wollants <lucwollants@gmail.com>
2023-05-04 15:53:10 +0200

    Create command to copy from Cache to Fuseki

---
+++ app/Console/Command/CopyToFuseki.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Console\Command;
+
+use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use CultuurNet\UDB3\RDF\CacheGraphRepository;
+use CultuurNet\UDB3\RDF\GraphStoreRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class CopyToFuseki extends Command
+{
+    private CacheGraphRepository $cacheGraphRepository;
+    private GraphStoreRepository $graphStoreRepository;
+    private IriGeneratorInterface $iriGenerator;
+
+    public function __construct(
+        string $name,
+        CacheGraphRepository $cacheGraphRepository,
+        GraphStoreRepository $graphStoreRepository,
+        IriGeneratorInterface $iriGenerator
+    ) {
+        $this->cacheGraphRepository = $cacheGraphRepository;
+        $this->graphStoreRepository = $graphStoreRepository;
+        $this->iriGenerator = $iriGenerator;
+
+        parent::__construct($name);
+    }
+
+    public function configure(): void
+    {
+        $this->setDescription('Copy given id from cache to graph store.');
+        $this->addArgument('id', InputArgument::REQUIRED, 'id to copy.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $id = $input->getArgument('id');
+
+        $uri = $this->iriGenerator->iri($id);
+
+        $graph = $this->cacheGraphRepository->get($uri);
+        $this->graphStoreRepository->save($uri, $graph);
+
+        return 0;
+    }
+}
