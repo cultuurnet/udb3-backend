b13f5f6bab65f2553d7eedacb4b5c3c30f8634c9
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-05-26 10:03:13 +0200

    Add migration to add excluded column to labels

---
+++ app/Migrations/Version20230526075219.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230526075219 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('labels_json');
+        $table->addColumn('excluded', Type::BOOLEAN)
+            ->setDefault(false)
+            ->setNotnull(true);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('labels_json');
+        $table->dropColumn('excluded');
+    }
+}
