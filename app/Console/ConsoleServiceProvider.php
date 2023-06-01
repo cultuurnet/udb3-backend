3d2427179cac30201b6037040bf25fc6568b05f7
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-06-01 14:27:08 +0200

    register cli commands

--- app/Console/ConsoleServiceProvider.php
+++ app/Console/ConsoleServiceProvider.php
@@ -15,6 +15,7 @@ use CultuurNet\UDB3\Console\Command\ChangePlaceTypeOnPlacesWithEventEventType;
 use CultuurNet\UDB3\Console\Command\ConsumeCommand;
 use CultuurNet\UDB3\Console\Command\CopyToFuseki;
 use CultuurNet\UDB3\Console\Command\EventAncestorsCommand;
+use CultuurNet\UDB3\Console\Command\ExcludeLabel;
 use CultuurNet\UDB3\Console\Command\FindOutOfSyncProjections;
 use CultuurNet\UDB3\Console\Command\FireProjectedToJSONLDCommand;
 use CultuurNet\UDB3\Console\Command\FireProjectedToJSONLDForRelationsCommand;
@@ -22,6 +23,7 @@ use CultuurNet\UDB3\Console\Command\GeocodeEventCommand;
 use CultuurNet\UDB3\Console\Command\GeocodeOrganizerCommand;
 use CultuurNet\UDB3\Console\Command\GeocodePlaceCommand;
 use CultuurNet\UDB3\Console\Command\ImportOfferAutoClassificationLabels;
+use CultuurNet\UDB3\Console\Command\IncludeLabel;
 use CultuurNet\UDB3\Console\Command\ProcessDuplicatePlaces;
 use CultuurNet\UDB3\Console\Command\PurgeModelCommand;
 use CultuurNet\UDB3\Console\Command\ReindexEventsWithRecommendations;
@@ -71,6 +73,8 @@ final class ConsoleServiceProvider extends AbstractServiceProvider
         'console.offer:change-owner-bulk',
         'console.organizer:change-owner',
         'console.organizer:change-owner-bulk',
+        'console.label:exclude',
+        'console.label:include',
         'console.label:update-unique',
         'console.organizer:update-unique',
         'console.place:facilities:remove',
@@ -311,6 +315,16 @@ final class ConsoleServiceProvider extends AbstractServiceProvider
                 $container->get('event_command_bus'),
                 $container->get('organizer_owner.repository')
             )
+        );
+
+        $container->addShared(
+            'console.label:exclude',
+            fn () => new ExcludeLabel($container->get('event_command_bus'),)
+        );
+
+        $container->addShared(
+            'console.label:include',
+            fn () => new IncludeLabel($container->get('event_command_bus'),)
         );
 
         $container->addShared(
