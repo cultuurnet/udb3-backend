20de2bf305f648cad056aa750aa7530012abc58d
Luc Wollants <lucwollants@gmail.com>
2023-06-01 12:33:23 +0200

    Handle periodic calendar

--- src/Event/ReadModel/RDF/RdfProjector.php
+++ src/Event/ReadModel/RDF/RdfProjector.php
@@ -11,6 +11,11 @@ use CultuurNet\UDB3\Iri\IriGeneratorInterface;
 use CultuurNet\UDB3\Model\Event\Event;
 use CultuurNet\UDB3\Model\Event\ImmutableEvent;
 use CultuurNet\UDB3\Model\Place\PlaceReference;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\Calendar;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\CalendarType;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\DateRange;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\PeriodicCalendar;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\SubEvent;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Categories;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Category;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryDomain;
@@ -35,11 +40,21 @@ final class RdfProjector implements EventListener
     private DenormalizerInterface $eventDenormalizer;
 
     private const TYPE_ACTIVITEIT = 'cidoc:E7_Activity';
+    private const TYPE_SPACE_TIME = 'cidoc:E92_Spacetime_Volume';
+    private const TYPE_PERIOD = 'm8g:PeriodOfTime';
+    private const TYPE_DATE_TIME = 'xsd:dateTime';
 
     private const PROPERTY_ACTIVITEIT_NAAM = 'dcterms:title';
     private const PROPERTY_ACTIVITEIT_TYPE = 'dcterms:type';
     private const PROPERTY_ACTVITEIT_LOCATIE = 'prov:atLocation';
     private const PROPERTY_ACTIVITEIT_DESCRIPTION = 'dcterms:description';
+
+    private const PROPERTY_RUIMTE_TIJD = 'cp:ruimtetijd';
+    private const PROPERTY_RUIMTE_TIJD_LOCATION = 'cidoc:P161_has_spatial_projection';
+    private const PROPERTY_RUIMTE_TIJD_CALENDAR_TYPE = 'cidoc:P160_has_temporal_projection';
+
+    private const PROPERTY_PERIOD_START = 'm8g:startTime';
+    private const PROPERTY_PERIOD_END = 'm8g:endTime';
 
     public function __construct(
         GraphRepository $graphRepository,
@@ -79,7 +94,7 @@ final class RdfProjector implements EventListener
 
         $this->setTerms($resource, $event->getTerms());
 
-        $this->setLocation($resource, $event->getPlaceReference());
+        $this->setCalendarWithLocation($resource, $event->getCalendar(), $event->getPlaceReference());
 
         if ($event->getDescription()) {
             $this->setDescription($resource, $event->getDescription());
@@ -120,10 +135,46 @@ final class RdfProjector implements EventListener
         }
     }
 
-    private function setLocation(Resource $resource, PlaceReference $placeReference): void
+    private function setCalendarWithLocation(Resource $resource, Calendar $calendar, PlaceReference $placeReference): void
     {
         $locationIri = $this->placesIriGenerator->iri($placeReference->getPlaceId()->toString());
-        $resource->set(self::PROPERTY_ACTVITEIT_LOCATIE, new Resource($locationIri));
+
+        if ($calendar->getType()->sameAs(CalendarType::permanent())) {
+            $resource->set(self::PROPERTY_ACTVITEIT_LOCATIE, new Resource($locationIri));
+            return;
+        }
+
+        $subEvents = [];
+
+        if ($calendar instanceof PeriodicCalendar) {
+            $subEvents[] = new SubEvent(
+                new DateRange(
+                    $calendar->getStartDate(),
+                    $calendar->getEndDate()
+                ),
+                $calendar->getStatus(),
+                $calendar->getBookingAvailability(),
+            );
+        }
+
+        foreach ($subEvents as $subEvent) {
+            $spaceTimeResource = $resource->getGraph()->newBNode([self::TYPE_SPACE_TIME]);
+            $resource->add(self::PROPERTY_RUIMTE_TIJD, $spaceTimeResource);
+
+            $spaceTimeResource->set(self::PROPERTY_RUIMTE_TIJD_LOCATION, new Resource($locationIri));
+
+            $calendarTypeResource = $spaceTimeResource->getGraph()->newBNode([self::TYPE_PERIOD]);
+            $spaceTimeResource->add(self::PROPERTY_RUIMTE_TIJD_CALENDAR_TYPE, $calendarTypeResource);
+
+            $calendarTypeResource->set(
+                self::PROPERTY_PERIOD_START,
+                new Literal($subEvent->getDateRange()->getFrom()->format(DateTime::ATOM), null, self::TYPE_DATE_TIME)
+            );
+            $calendarTypeResource->set(
+                self::PROPERTY_PERIOD_END,
+                new Literal($subEvent->getDateRange()->getTo()->format(DateTime::ATOM), null, self::TYPE_DATE_TIME)
+            );
+        }
     }
 
     private function setDescription(Resource $resource, TranslatedDescription $translatedDescription): void
