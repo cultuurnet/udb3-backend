b6a65bcaf8087d9d478da6055761d1ff1bbc282a
Luc Wollants <lucwollants@gmail.com>
2023-06-01 10:06:20 +0200

    Add test to verify translations

--- tests/Event/ReadModel/RDF/RdfProjectorTest.php
+++ tests/Event/ReadModel/RDF/RdfProjectorTest.php
@@ -119,6 +119,49 @@ class RdfProjectorTest extends TestCase
         $this->assertTurtleData($eventId, file_get_contents(__DIR__ . '/ttl/event-with-description.ttl'));
     }
 
+    /**
+     * @test
+     */
+    public function it_converts_an_event_with_translations(): void
+    {
+        $eventId = 'd4b46fba-6433-4f86-bcb5-edeef6689fea';
+
+        $event = [
+            '@id' => 'https://mock.io.uitdatabank.be/events/' . $eventId,
+            'mainLanguage' => 'nl',
+            'calendarType' => 'permanent',
+            'terms' => [
+                [
+                    'id' => '0.50.4.0.0',
+                    'domain' => 'eventtype',
+                ],
+            ],
+            'name' => [
+                'nl' => 'Faith no more',
+                'fr' => 'Foi non plus',
+            ],
+            'location' => [
+                '@id' => 'https://mock.io.uitdatabank.be/places/bfc60a14-6208-4372-942e-86e63744769a',
+            ],
+            'description' => [
+                'nl' => 'Dit is het laatste concert van Faith no more',
+                'fr' => 'Ceci est le dernier concert de Foi non plus',
+                'en' => 'This is the last concert of Faith no more',
+            ],
+        ];
+
+        $this->documentRepository->save(new JsonDocument($eventId, json_encode($event)));
+
+        $this->project(
+            $eventId,
+            [
+                new EventProjectedToJSONLD($eventId, 'https://mock.io.uitdatabank.be/events/' . $eventId),
+            ]
+        );
+
+        $this->assertTurtleData($eventId, file_get_contents(__DIR__ . '/ttl/event-with-translations.ttl'));
+    }
+
     private function project(string $eventId, array $events): void
     {
         $playhead = -1;
