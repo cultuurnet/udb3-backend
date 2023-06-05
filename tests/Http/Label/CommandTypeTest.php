3103fb38bc67cce51113ba5a016e4ee8387065cb
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-06-05 09:20:01 +0200

    Update tests/Http/Label/CommandTypeTest.php
    Co-authored-by: Luc Wollants <lucwollants@gmail.com>

--- tests/Http/Label/CommandTypeTest.php
+++ tests/Http/Label/CommandTypeTest.php
@@ -51,7 +51,7 @@ class CommandTypeTest extends TestCase
     /**
      * @test
      */
-    public function it_has_a_make_include_option()
+    public function it_has_an_include_option()
     {
         $commandType = CommandType::include();
 
