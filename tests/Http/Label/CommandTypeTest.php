c0191d025c22d9349d1c9bff36d9d210a64587ea
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-06-05 09:20:07 +0200

    Update tests/Http/Label/CommandTypeTest.php
    Co-authored-by: Luc Wollants <lucwollants@gmail.com>

--- tests/Http/Label/CommandTypeTest.php
+++ tests/Http/Label/CommandTypeTest.php
@@ -61,7 +61,7 @@ class CommandTypeTest extends TestCase
     /**
      * @test
      */
-    public function it_has_a_make_exclude_option()
+    public function it_has_a_exclude_option()
     {
         $commandType = CommandType::exclude();
 
