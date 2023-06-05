55a623afcaf76bf940e9e595f5e902571c2f1c13
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-06-05 14:52:03 +0200

    refactor ExcludeMalformedLabel Command

--- app/Console/Command/ExcludeMalformedLabel.php
+++ app/Console/Command/ExcludeMalformedLabel.php
@@ -8,19 +8,23 @@ use Broadway\CommandHandling\CommandBus;
 use CultuurNet\UDB3\Label\Commands\ExcludeLabel as ExcludeLabelCommand;
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\FetchMode;
+use PDO;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 final class ExcludeMalformedLabel extends AbstractCommand
 {
+    private const LABEL_REGEX = '/^[a-zA-Z\d_\-]{2,50}$/';
+
+    private const MAX_RESULTS = 1;
     private Connection $connection;
 
     public function configure(): void
     {
         $this->setName('label:exclude-malformed');
-        $this->setDescription('Excludes 1000 malformed labels');
+        $this->setDescription('Excludes malformed labels based on a regex');
     }
 
     public function __construct(CommandBus $commandBus, Connection $connection)
@@ -29,57 +33,60 @@ final class ExcludeMalformedLabel extends AbstractCommand
         $this->connection = $connection;
     }
 
-    public function execute(InputInterface $input, OutputInterface $output): int
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $queryBuilder = $this->connection->createQueryBuilder();
+        $labelsCount = $this->getAllLabelsCount();
 
-        $malformedLabelIdCount = $queryBuilder->select('*')
-            ->from('labels_json')
-            ->where('excluded = :excluded')
-            ->setParameter(':excluded', 0)
-            ->andWhere('name REGEXP \'^[a-zA-Z\d_\-]{2,50}$\'')
-            ->execute()
-            ->rowCount();
-
-        $labelsToExclude = min($malformedLabelIdCount, 1000);
-
-        if (!$this->askConfirmation($input, $output, $malformedLabelIdCount, $labelsToExclude)) {
+        $helper = $this->getHelper('question');
+        $updateQuestion = new ConfirmationQuestion('Check ' . $labelsCount . ' label(s)? [y/N] ', false);
+        if (!$helper->ask($input, $output, $updateQuestion)) {
             return 0;
         }
 
-        $malformedLabelIds = $queryBuilder->select('uuid_col')
-            ->from('labels_json')
-            ->where('excluded = :excluded')
-            ->setParameter(':excluded', 0)
-            ->andWhere('name REGEXP \'^[a-zA-Z\d_\-]{2,50}$\'')
-            ->setMaxResults($labelsToExclude)
-            ->execute()
-            ->fetchAll(FetchMode::COLUMN);
+        $progressBar = new ProgressBar($output, $labelsCount);
 
-        foreach ($malformedLabelIds as $malformedLabelId) {
-            $this->commandBus->dispatch(
-                new ExcludeLabelCommand(new UUID($malformedLabelId))
-            );
-        }
+        $offset = 0;
+        do {
+            $labels = $this->getAllLabels($offset);
+
+            foreach ($labels as $label) {
+                $labelId = new Uuid($label['uuid_col']);
+                $labelName = $label['name'];
+                $excluded = (bool) $label['excluded'];
+
+                if (!$excluded && !preg_match(self::LABEL_REGEX, $labelName)) {
+                    $this->commandBus->dispatch(
+                        new ExcludeLabelCommand($labelId)
+                    );
+                }
+                $progressBar->advance();
+            }
+
+            $offset += count($labels);
+        } while ($offset < $labelsCount);
+
+        $progressBar->finish();
+        $output->writeln('');
 
         return 0;
     }
 
-    private function askConfirmation(
-        InputInterface $input,
-        OutputInterface $output,
-        int $total,
-        int $labelsToExclude
-    ): bool {
-        return $this
-            ->getHelper('question')
-            ->ask(
-                $input,
-                $output,
-                new ConfirmationQuestion(
-                    "This are {$total} malformed labels to exclude, exclude next {$labelsToExclude}? [y/N] ",
-                    false
-                )
-            );
+    private function getAllLabelsCount(): int
+    {
+        return $this->connection->createQueryBuilder()->select('*')
+            ->from('labels_json')
+            ->execute()
+            ->rowCount();
+    }
+
+    private function getAllLabels(int $offset): array
+    {
+        return $this->connection->createQueryBuilder()
+            ->select('uuid_col, name, excluded')
+            ->from('labels_json')
+            ->setFirstResult($offset)
+            ->setMaxResults(self::MAX_RESULTS)
+            ->execute()
+            ->fetchAll(PDO::FETCH_ASSOC);
     }
 }
