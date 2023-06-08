f5697db0d1cc103446d93473ab1bbcaecdc57240
Luc Wollants <lucwollants@gmail.com>
2023-06-08 14:35:05 +0200

    Add basic place steps

---
+++ features/Steps/PlaceSteps.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Steps;
+
+trait PlaceSteps
+{
+    /**
+     * @Given I create a place from :fileName and save the :jsonPath as :variableName
+     */
+    public function iCreateAPlaceFromAndSaveTheAs(string $fileName, $jsonPath, $variableName): void
+    {
+        $place = $this->fixtures->loadJson($fileName, $this->variables);
+
+        $response = $this->getHttpClient()->postJSON(
+            $this->requestState->getBaseUrl() . '/places',
+            $place
+        );
+        $this->responseState->setResponse($response);
+
+        $this->theResponseStatusShouldBe(201);
+        $this->theResponseBodyShouldBeValidJson();
+        $this->iKeepTheValueOfTheJsonResponseAtAs($jsonPath, $variableName);
+    }
+
+    /**
+     * @Given I get the place at :url
+     */
+    public function iGetThePlaceAt(string $url): void
+    {
+        $this->responseState->setResponse(
+            $this->getHttpClient()->getJSON($this->variables->getVariable($url))
+        );
+
+        $this->theResponseStatusShouldBe(200);
+        $this->theResponseBodyShouldBeValidJson();
+    }
+}
