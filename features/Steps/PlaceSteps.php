447abec2340b98620e7bfb14c7c1e510a4fd6df1
Luc Wollants <lucwollants@gmail.com>
2023-06-09 09:23:36 +0200

    Add extra methods to `PlaceSteps`

--- features/Steps/PlaceSteps.php
+++ features/Steps/PlaceSteps.php
@@ -7,21 +7,53 @@ namespace CultuurNet\UDB3\Steps;
 trait PlaceSteps
 {
     /**
+     * @Given I create a minimal place and save the :jsonPath as :variableName
+     */
+    public function iCreateAMinimalPlaceAndSaveTheAs($jsonPath, $variableName): void
+    {
+        $this->createPlace(
+            '/places',
+            $this->fixtures->loadJson('places/place-with-required-fields.json', $this->variableState),
+            $jsonPath,
+            $variableName
+        );
+    }
+
+    /**
      * @Given I create a place from :fileName and save the :jsonPath as :variableName
      */
     public function iCreateAPlaceFromAndSaveTheAs(string $fileName, $jsonPath, $variableName): void
     {
-        $place = $this->fixtures->loadJson($fileName, $this->variableState);
-
-        $response = $this->getHttpClient()->postJSON(
-            $this->requestState->getBaseUrl() . '/places',
-            $place
+        $this->createPlace(
+            '/places',
+            $this->fixtures->loadJson($fileName, $this->variableState),
+            $jsonPath,
+            $variableName
         );
-        $this->responseState->setResponse($response);
+    }
 
-        $this->theResponseStatusShouldBe(201);
-        $this->theResponseBodyShouldBeValidJson();
-        $this->iKeepTheValueOfTheJsonResponseAtAs($jsonPath, $variableName);
+    /**
+     * @Given I import a new place from :fileName and save the :jsonPath as :variableName
+     */
+    public function iImportANewPlaceFromAndSaveTheAs(string $fileName, $jsonPath, $variableName): void
+    {
+        $this->createPlace(
+            '/imports/places',
+            $this->fixtures->loadJson($fileName, $this->variableState),
+            $jsonPath,
+            $variableName
+        );
+    }
+
+    /**
+     * @When I update the place at :url from :fileName
+     */
+    public function iUpdateThePlaceAtFrom(string $url, string $fileName): void
+    {
+        $this->getHttpClient()->putJSON(
+            $url,
+            $this->fixtures->loadJsonWithRandomName($fileName, $this->variableState)
+        );
     }
 
     /**
@@ -35,5 +67,18 @@ trait PlaceSteps
 
         $this->theResponseStatusShouldBe(200);
         $this->theResponseBodyShouldBeValidJson();
+    }
+
+    private function createPlace(string $endpoint, string $json, string $jsonPath, string $variableName): void
+    {
+        $response = $this->getHttpClient()->postJSON(
+            $this->requestState->getBaseUrl() . $endpoint,
+            $json
+        );
+        $this->responseState->setResponse($response);
+
+        $this->theResponseStatusShouldBe(str_contains($endpoint, 'imports') ? 200 : 201);
+        $this->theResponseBodyShouldBeValidJson();
+        $this->iKeepTheValueOfTheJsonResponseAtAs($jsonPath, $variableName);
     }
 }
