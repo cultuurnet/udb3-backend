1ad31a84adc5e41cff03bc92534e078abc8ba7be
Luc Wollants <lucwollants@gmail.com>
2023-06-14 10:11:39 +0200

    Add index on `email` inside the `contributor_relations` table

---
+++ app/Migrations/Version20230614094817.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230614094817 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('contributor_relations');
+        $table->addIndex(['email'], 'email_idx');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('contributor_relations');
+        $table->dropIndex('email_idx');
+    }
+}
