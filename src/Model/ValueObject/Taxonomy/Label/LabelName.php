b2844a5f41de4e7dad3acab56f9c9a9f1c9836b2
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-06-14 15:23:41 +0200

    use correct new regex

--- src/Model/ValueObject/Taxonomy/Label/LabelName.php
+++ src/Model/ValueObject/Taxonomy/Label/LabelName.php
@@ -16,7 +16,7 @@ class LabelName
 
     public const LEGACY_REGEX = '/^(?=.{2,255}$)(?=.*\S.*\S.*)[^;]*$/';
 
-    public const REGEX = '/^(?=[^\s\-_;&.,#\'"]{2,50}$)(?=.*\S.*\S.*)[^;.&,\'#"\'\s]*$/';
+    public const REGEX = '/^(?![-_ ])(?=.{2,50}$)(?=.*\S.*\S.*)[^;,\#\'!&]*$/';
 
     /**
      * @param string $value
