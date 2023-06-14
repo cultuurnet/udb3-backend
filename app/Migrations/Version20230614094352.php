c8845016ee95561383ce18cdb4f0ee9b29f115b6
Luc Wollants <lucwollants@gmail.com>
2023-06-14 10:11:05 +0200

    Add index on `user_id` inside the `place_permission_readmodel` table

---
+++ app/Migrations/Version20230614094352.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230614094352 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('place_permission_readmodel');
+        $table->addIndex(['user_id'], 'user_id_idx');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('place_permission_readmodel');
+        $table->dropIndex('user_id_idx');
+    }
+}
