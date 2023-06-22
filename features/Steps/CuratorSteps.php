30f73317a355009633fa12cb1be13f8d27fd81f3
Luc Wollants <lucwollants@gmail.com>
2023-06-22 18:09:19 +0200

    Linting

--- features/Steps/CuratorSteps.php
+++ features/Steps/CuratorSteps.php
@@ -22,7 +22,7 @@ trait CuratorSteps
             'about' => '17284745-7bcf-461a-aad0-d3ad54880e75',
             'publisher' => 'BILL',
             'publisherLogo' => 'https://www.bill.be/img/favicon.png',
-            'url' => 'https://www.publiq.be/blog/%{name}'
+            'url' => 'https://www.publiq.be/blog/%{name}',
         ];
 
         $response = $this->getHttpClient()->postJSON(
