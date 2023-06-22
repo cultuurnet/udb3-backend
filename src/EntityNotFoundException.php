60ca195ddea0371791b938bddd1256af9a6d4bf3
Luc Wollants <lucwollants@gmail.com>
2023-06-22 13:02:35 +0200

    Tweak exceptions

--- src/EntityNotFoundException.php
+++ src/EntityNotFoundException.php
@@ -6,7 +6,7 @@ namespace CultuurNet\UDB3;
 
 class EntityNotFoundException extends \Exception
 {
-    public function __construct($message = '', $code = 404, \Exception $previous = null)
+    public function __construct(string $message = '', int $code = 404, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
