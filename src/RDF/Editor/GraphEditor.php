4d84d3cd0d8ac096002e8462f102f69c1356dc9f
Luc Wollants <lucwollants@gmail.com>
2023-08-17 13:08:13 +0200

    Add structured identifier

--- src/RDF/Editor/GraphEditor.php
+++ src/RDF/Editor/GraphEditor.php
@@ -12,6 +12,7 @@ final class GraphEditor
     private Graph $graph;
 
     private const TYPE_IDENTIFICATOR = 'adms:Identifier';
+    private const TYPE_STRUCTURED_IDENTIFICATOR = 'generiek:gestructureerdeIdentificator';
 
     private const PROPERTY_IDENTIFICATOR = 'adms:identifier';
 
@@ -19,6 +20,9 @@ final class GraphEditor
     private const PROPERTY_LAATST_AANGEPAST = 'dcterms:modified';
 
     private const PROPERTY_IDENTIFICATOR_NOTATION = 'skos:notation';
+
+    private const PROPERTY_NAAMRUIMTE = 'generiek:naamruimte';
+    private const PROPERTY_LOKALE_IDENTIFICATOR = 'generiek:lokaleIdentificator';
 
     private function __construct(Graph $graph)
     {
@@ -40,23 +44,32 @@ final class GraphEditor
 
         $resource->setType($type);
 
-        $resource->set(
-            self::PROPERTY_AANGEMAAKT_OP,
-            new Literal($createdOn, null, 'xsd:dateTime')
-        );
+        $resource->set(self::PROPERTY_AANGEMAAKT_OP, new Literal($createdOn, null, 'xsd:dateTime'));
 
-        $resource->set(
-            self::PROPERTY_LAATST_AANGEPAST,
-            new Literal($modifiedOn, null, 'xsd:dateTime')
-        );
+        $resource->set(self::PROPERTY_LAATST_AANGEPAST, new Literal($modifiedOn, null, 'xsd:dateTime'));
 
-        if (!$resource->hasProperty(self::PROPERTY_IDENTIFICATOR)) {
-            $identificator = $this->graph->newBNode();
-            $identificator->setType(self::TYPE_IDENTIFICATOR);
-            $identificator->add(self::PROPERTY_IDENTIFICATOR_NOTATION, new Literal($resourceIri, null, 'xsd:anyUri'));
-            $resource->add(self::PROPERTY_IDENTIFICATOR, $identificator);
-        }
+        $identifier = $this->graph->newBNode();
+        $identifier->setType(self::TYPE_IDENTIFICATOR);
+        $identifier->add(self::PROPERTY_IDENTIFICATOR_NOTATION, new Literal($resourceIri, null, 'xsd:anyUri'));
+
+        list($namespace, $localIdentifier) = $this->splitIdentifier($resourceIri);
+        $structuredIdentifier = $this->graph->newBNode();
+        $structuredIdentifier->setType(self::TYPE_STRUCTURED_IDENTIFICATOR);
+        $structuredIdentifier->add(self::PROPERTY_NAAMRUIMTE, new Literal($namespace));
+        $structuredIdentifier->add(self::PROPERTY_LOKALE_IDENTIFICATOR, new Literal($localIdentifier));
+        $identifier->add(self::TYPE_STRUCTURED_IDENTIFICATOR, $structuredIdentifier);
+
+        $resource->add(self::PROPERTY_IDENTIFICATOR, $identifier);
 
         return $this;
+    }
+
+    private function splitIdentifier(string $iri): array
+    {
+        $parts = explode('/', $iri);
+        $localIdentifier = array_pop($parts);
+        $namespace = implode('/', $parts) . '/';
+
+        return [$namespace, $localIdentifier];
     }
 }
