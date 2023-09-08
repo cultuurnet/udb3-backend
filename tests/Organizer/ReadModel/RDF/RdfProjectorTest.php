cb1648e98e3beb2b42b92bab228318d9034282aa
Luc Wollants <lucwollants@gmail.com>
2023-09-08 09:48:50 +0200

    Add test for projecting a simple organizer to RDF

---
+++ tests/Organizer/ReadModel/RDF/RdfProjectorTest.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Organizer\ReadModel\RDF;
+
+use Broadway\Domain\DateTime as BroadwayDateTime;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Iri\CallableIriGenerator;
+use CultuurNet\UDB3\Model\Serializer\Organizer\OrganizerDenormalizer;
+use CultuurNet\UDB3\Organizer\OrganizerProjectedToJSONLD;
+use CultuurNet\UDB3\RDF\GraphRepository;
+use CultuurNet\UDB3\RDF\InMemoryGraphRepository;
+use CultuurNet\UDB3\ReadModel\DocumentRepository;
+use CultuurNet\UDB3\ReadModel\InMemoryDocumentRepository;
+use CultuurNet\UDB3\ReadModel\JsonDocument;
+use DateTime;
+use EasyRdf\Serialiser\Turtle;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class RdfProjectorTest extends TestCase
+{
+    private GraphRepository $graphRepository;
+
+    private DocumentRepository $documentRepository;
+
+    private RdfProjector $rdfProjector;
+
+    /**
+     * @var LoggerInterface|MockObject
+     */
+    private $logger;
+
+    protected function setUp(): void
+    {
+        $this->graphRepository = new InMemoryGraphRepository();
+        $this->documentRepository = new InMemoryDocumentRepository();
+        $this->logger = $this->createMock(LoggerInterface::class);
+
+        $this->rdfProjector = new RdfProjector(
+            $this->graphRepository,
+            new CallableIriGenerator(fn (string $item): string => 'https://mock.data.publiq.be/organizers/' . $item),
+            $this->documentRepository,
+            new OrganizerDenormalizer(),
+            $this->logger
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_a_simple_organizer(): void
+    {
+        $organizerId = '56f1efdb-fe25-44f6-b9d7-4a6a836799d7';
+
+        $organizer = [
+            '@id' => 'https://mock.io.uitdatabank.be/organizers/' . $organizerId,
+            'mainLanguage' => 'nl',
+            'url' => 'https://www.publiq.be',
+            'name' => [
+                'nl' => 'publiq',
+            ],
+            'created' => '2023-01-01T12:30:15+01:00',
+        ];
+
+        $this->documentRepository->save(new JsonDocument($organizerId, json_encode($organizer)));
+
+        $this->project(
+            $organizerId,
+            [
+                new OrganizerProjectedToJSONLD($organizerId, 'https://mock.io.uitdatabank.be/organizer/' . $organizerId),
+            ]
+        );
+
+        $this->assertTurtleData($organizerId, file_get_contents(__DIR__ . '/ttl/organizer.ttl'));
+    }
+
+    private function project(string $organizerId, array $events): void
+    {
+        $playhead = -1;
+        $recordedOn = new DateTime('2022-12-31T12:30:15+01:00');
+        foreach ($events as $event) {
+            $playhead++;
+            $recordedOn->modify('+1 day');
+            $domainMessage = new DomainMessage(
+                $organizerId,
+                $playhead,
+                new Metadata(),
+                $event,
+                BroadwayDateTime::fromString($recordedOn->format(DateTime::ATOM))
+            );
+            $this->rdfProjector->handle($domainMessage);
+        }
+    }
+
+    private function assertTurtleData(string $placeId, string $expectedTurtleData): void
+    {
+        $uri = 'https://mock.data.publiq.be/organizers/' . $placeId;
+        $actualTurtleData = (new Turtle())->serialise($this->graphRepository->get($uri), 'turtle');
+        $this->assertEquals(trim($expectedTurtleData), trim($actualTurtleData));
+    }
+}
