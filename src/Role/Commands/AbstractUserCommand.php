10c9ce273f454642e66a7cf9a3539bb921e69947
Luc Wollants <lucwollants@gmail.com>
2023-09-15 12:35:07 +0200

    Refactored `AbstractUserCommand` to accept `string` inside constructor

--- src/Role/Commands/AbstractUserCommand.php
+++ src/Role/Commands/AbstractUserCommand.php
@@ -5,24 +5,20 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Role\Commands;
 
 use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
-use CultuurNet\UDB3\StringLiteral;
 
 abstract class AbstractUserCommand extends AbstractCommand
 {
-    /**
-     * @var StringLiteral
-     */
-    private $userId;
+    private string $userId;
 
     public function __construct(
         UUID $uuid,
-        StringLiteral $userId
+        string $userId
     ) {
         parent::__construct($uuid);
         $this->userId = $userId;
     }
 
-    public function getUserId(): StringLiteral
+    public function getUserId(): string
     {
         return $this->userId;
     }
