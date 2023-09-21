e8485abc9599698be1447c2e1204349cea2046eb
Koen Eelen <koen.eelenpubliq.be>
2023-09-21 16:37:16 +0200

    Add Uses comments so the dynamicly called methods of applyX can be followed in an IDE
    There where a lot of methods that showed up as unused in the IDE because they where dynmaicly called from the handle() method.

--- src/EventHandling/DelegateEventHandlingToSpecificMethodTrait.php
+++ src/EventHandling/DelegateEventHandlingToSpecificMethodTrait.php
@@ -5,12 +5,31 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\EventHandling;
 
 use Broadway\Domain\DomainMessage;
+use CultuurNet\UDB3\Event\ReadModel\Relations\EventRelationsProjector;
 
 trait DelegateEventHandlingToSpecificMethodTrait
 {
+    /**
+     * @uses  Projector::applyEventImportedFromUDB2()
+     * @uses  Projector::applyEventCreated()
+     * @uses  Projector::applyEventCopied()
+     * @uses  Projector::applyOwnerChanged()
+     * @uses  EventRelationsProjector::applyEventImportedFromUDB2
+     * @uses  EventRelationsProjector::applyEventUpdatedFromUDB2
+     * @uses  EventRelationsProjector::applyEventCreated
+     * @uses  EventRelationsProjector::applyEventCopied
+     * @uses  EventRelationsProjector::applyMajorInfoUpdated
+     * @uses  EventRelationsProjector::applyLocationUpdated
+     * @uses  EventRelationsProjector::applyEventDeleted
+     * @uses  EventRelationsProjector::applyOrganizerUpdated
+     * @uses  EventRelationsProjector::applyOrganizerDeleted
+     * @uses  LabelRolesProjector::applyLabelAdded
+     * @uses  LabelRolesProjector::applyLabelRemoved
+     * @uses  LabelRolesProjector::applyRoleDeleted
+     */
     public function handle(DomainMessage $domainMessage): void
     {
-        $event  = $domainMessage->getPayload();
+        $event = $domainMessage->getPayload();
         $method = $this->getHandleMethodName($event);
 
         if ($method) {
