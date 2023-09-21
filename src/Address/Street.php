0af4c17eee5502159f0e61da0ee96f7ed1150015
Koen Eelen <koen.eelenpubliq.be>
2023-09-21 10:27:26 +0200

    remove stringliteral on legacy street

--- src/Address/Street.php
+++ src/Address/Street.php
@@ -4,12 +4,42 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Address;
 
-use CultuurNet\UDB3\StringLiteral;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Street as Udb3ModelStreet;
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\Trims;
 
 /**
  * @deprecated
  *   Use CultuurNet\UDB3\Model\ValueObject\Geography\Street instead.
  */
-class Street extends StringLiteral
+final class Street
 {
+    use Trims;
+
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $value = $this->trim($value);
+        $this->value = $value;
+    }
+
+    public function toNative(): string
+    {
+        return $this->value;
+    }
+
+    public function sameValueAs(self $title): bool
+    {
+        return $this->toNative() === $title->toNative();
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->value;
+    }
+
+    public static function fromUdb3ModelStreet(Udb3ModelStreet $title): self
+    {
+        return new self($title->toString());
+    }
 }
