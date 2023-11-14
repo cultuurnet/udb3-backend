9ebba56725c827c505b2e92e7c11f3d823b7d40a
Luc Wollants <lucwollants@gmail.com>
2023-11-14 17:51:29 +0100

    Add unit tests for turtle format on place and event

--- tests/Http/Offer/GetDetailRequestHandlerTest.php
+++ tests/Http/Offer/GetDetailRequestHandlerTest.php
@@ -74,6 +74,39 @@ class GetDetailRequestHandlerTest extends TestCase
     /**
      * @test
      */
+    public function it_returns_the_requested_event_turtle_if_found(): void
+    {
+        $eventId = 'c09b7a51-b17c-4121-b278-eef71ef04e47';
+        $uri = 'https://io.uitdatabank.dev/events/' . $eventId;
+
+        $request = (new Psr7RequestBuilder())
+            ->withUriFromString('/events/' . $eventId)
+            ->withRouteParameter('offerType', 'events')
+            ->withRouteParameter('offerId', $eventId)
+            ->withHeader('Accept', 'text/turtle')
+            ->build('GET');
+
+        $graph = new Graph($uri);
+        $resource = $graph->resource($uri);
+        $resource->setType('cidoc:E7_Activity');
+        $resource->addLiteral('dcterms:title', ['OSLO release party']);
+
+        $this->graphStoreRepository->expects($this->once())
+            ->method('get')
+            ->with($uri)
+            ->willReturn($graph);
+
+        $response = $this->getDetailRequestHandler->handle($request);
+
+        $this->assertEquals(
+            file_get_contents(__DIR__ . '/samples/event.ttl'),
+            $response->getBody()->getContents()
+        );
+    }
+
+    /**
+     * @test
+     */
     public function it_returns_the_requested_place_json_ld_if_found(): void
     {
         $this->mockPlaceDocument('fced66fb-72e9-47c3-bde0-7494d299962b');
@@ -91,6 +124,39 @@ class GetDetailRequestHandlerTest extends TestCase
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertArrayHasKey('@id', $decodedResponseBody);
         $this->assertArrayNotHasKey('metadata', $decodedResponseBody);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_the_requested_place_turtle_if_found(): void
+    {
+        $placeId = 'fced66fb-72e9-47c3-bde0-7494d299962b';
+        $uri = 'https://io.uitdatabank.dev/places/' . $placeId;
+
+        $request = (new Psr7RequestBuilder())
+            ->withUriFromString('/places/' . $placeId)
+            ->withRouteParameter('offerType', 'places')
+            ->withRouteParameter('offerId', $placeId)
+            ->withHeader('Accept', 'text/turtle')
+            ->build('GET');
+
+        $graph = new Graph($uri);
+        $resource = $graph->resource($uri);
+        $resource->setType('dcterms:Location');
+        $resource->addLiteral('locn:locatorName', ['Het Depot']);
+
+        $this->graphStoreRepository->expects($this->once())
+            ->method('get')
+            ->with($uri)
+            ->willReturn($graph);
+
+        $response = $this->getDetailRequestHandler->handle($request);
+
+        $this->assertEquals(
+            file_get_contents(__DIR__ . '/samples/place.ttl'),
+            $response->getBody()->getContents()
+        );
     }
 
     /**
