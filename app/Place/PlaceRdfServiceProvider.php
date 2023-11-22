09c6e624fb08dfe39a381cb0f2c896c28d6b2d9e
Luc Wollants <lucwollants@gmail.com>
2023-11-22 17:08:35 +0100

    Remove `RdfProjector` for place

--- app/Place/PlaceRdfServiceProvider.php
+++ app/Place/PlaceRdfServiceProvider.php
@@ -10,8 +10,6 @@ use CultuurNet\UDB3\Error\LoggerFactory;
 use CultuurNet\UDB3\Error\LoggerName;
 use CultuurNet\UDB3\Model\Serializer\Place\PlaceDenormalizer;
 use CultuurNet\UDB3\Place\ReadModel\RDF\PlaceJsonToTurtleConverter;
-use CultuurNet\UDB3\Place\ReadModel\RDF\RdfProjector;
-use CultuurNet\UDB3\RDF\CacheGraphRepository;
 use CultuurNet\UDB3\RDF\RdfServiceProvider;
 
 final class PlaceRdfServiceProvider extends AbstractServiceProvider
@@ -19,35 +17,12 @@ final class PlaceRdfServiceProvider extends AbstractServiceProvider
     protected function getProvidedServiceNames(): array
     {
         return [
-            RdfProjector::class,
             PlaceJsonToTurtleConverter::class,
         ];
     }
 
     public function register(): void
     {
-        $graphStoreRepository = RdfServiceProvider::createGraphStoreRepository(
-            $this->container->get('config')['rdf']['placesGraphStoreUrl'],
-            $this->container->get('config')['rdf']['useDeleteAndInsert'] ?? false
-        );
-
-        if ($this->container->get('config')['rdf']['useCache']) {
-            $graphStoreRepository = new CacheGraphRepository($this->container->get('cache')('rdf_place'));
-        }
-
-        $this->container->addShared(
-            RdfProjector::class,
-            fn (): RdfProjector => new RdfProjector(
-                $graphStoreRepository,
-                RdfServiceProvider::createIriGenerator($this->container->get('config')['rdf']['placesRdfBaseUri']),
-                RdfServiceProvider::createIriGenerator($this->container->get('config')['taxonomy']['terms']),
-                $this->container->get('place_jsonld_repository'),
-                new PlaceDenormalizer(),
-                $this->container->get(AddressParser::class),
-                LoggerFactory::create($this->getContainer(), LoggerName::forService('rdf'))
-            )
-        );
-
         $this->container->addShared(
             PlaceJsonToTurtleConverter::class,
             fn (): PlaceJsonToTurtleConverter => new PlaceJsonToTurtleConverter(
