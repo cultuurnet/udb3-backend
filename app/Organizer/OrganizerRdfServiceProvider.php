164e7f42be088f0d36f2f1af65fa9d861731f305
Luc Wollants <lucwollants@gmail.com>
2023-11-22 17:13:11 +0100

    Remove `RdfProjector` for organizer

--- app/Organizer/OrganizerRdfServiceProvider.php
+++ app/Organizer/OrganizerRdfServiceProvider.php
@@ -10,8 +10,6 @@ use CultuurNet\UDB3\Error\LoggerFactory;
 use CultuurNet\UDB3\Error\LoggerName;
 use CultuurNet\UDB3\Model\Serializer\Organizer\OrganizerDenormalizer;
 use CultuurNet\UDB3\Organizer\ReadModel\RDF\OrganizerJsonToTurtleConverter;
-use CultuurNet\UDB3\Organizer\ReadModel\RDF\RdfProjector;
-use CultuurNet\UDB3\RDF\CacheGraphRepository;
 use CultuurNet\UDB3\RDF\RdfServiceProvider;
 
 final class OrganizerRdfServiceProvider extends AbstractServiceProvider
@@ -19,34 +17,12 @@ final class OrganizerRdfServiceProvider extends AbstractServiceProvider
     protected function getProvidedServiceNames(): array
     {
         return [
-            RdfProjector::class,
             OrganizerJsonToTurtleConverter::class,
         ];
     }
 
     public function register(): void
     {
-        $graphStoreRepository = RdfServiceProvider::createGraphStoreRepository(
-            $this->container->get('config')['rdf']['organizersGraphStoreUrl'],
-            $this->container->get('config')['rdf']['useDeleteAndInsert'] ?? false
-        );
-
-        if ($this->container->get('config')['rdf']['useCache']) {
-            $graphStoreRepository = new CacheGraphRepository($this->container->get('cache')('rdf_organizer'));
-        }
-
-        $this->container->addShared(
-            RdfProjector::class,
-            fn (): RdfProjector => new RdfProjector(
-                $graphStoreRepository,
-                RdfServiceProvider::createIriGenerator($this->container->get('config')['rdf']['organizersRdfBaseUri']),
-                $this->container->get('organizer_jsonld_repository'),
-                new OrganizerDenormalizer(),
-                $this->container->get(AddressParser::class),
-                LoggerFactory::create($this->getContainer(), LoggerName::forService('rdf'))
-            )
-        );
-
         $this->container->addShared(
             OrganizerJsonToTurtleConverter::class,
             fn (): OrganizerJsonToTurtleConverter => new OrganizerJsonToTurtleConverter(
