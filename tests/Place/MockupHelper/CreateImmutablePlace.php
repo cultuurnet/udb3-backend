b8e5e74cb40b70ac6f062726062039bd2a4b4e1c
Koen Eelen <koen.eelenpubliq.be>
2023-12-15 20:26:44 +0100

    standalone helper classes for test to quickly build a place

---
+++ tests/Place/MockupHelper/CreateImmutablePlace.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place\MockupHelper;
+
+use CultuurNet\UDB3\Address\Address;
+use CultuurNet\UDB3\Address\Locality;
+use CultuurNet\UDB3\Address\PostalCode;
+use CultuurNet\UDB3\Address\Street;
+use CultuurNet\UDB3\Model\Place\ImmutablePlace;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\OpeningHours;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\PermanentCalendar;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Address as Udb3Address;
+use CultuurNet\UDB3\Model\ValueObject\Geography\CountryCode;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Locality as Udb3Locality;
+use CultuurNet\UDB3\Model\ValueObject\Geography\PostalCode as Udb3PostalCode;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Street as Udb3Street;
+use CultuurNet\UDB3\Model\ValueObject\Geography\TranslatedAddress;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Categories;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Category;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryDomain;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryID;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryLabel;
+use CultuurNet\UDB3\Model\ValueObject\Text\Title;
+use CultuurNet\UDB3\Model\ValueObject\Text\TranslatedTitle;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+
+class CreateImmutablePlace
+{
+    public function create(): ImmutablePlace
+    {
+        return new ImmutablePlace(
+            new UUID('aadcee95-6180-4924-a8eb-ed829d4957a2'),
+            new Language('nl'),
+            new TranslatedTitle(
+                new Language('nl'),
+                new Title('Online')
+            ),
+            new PermanentCalendar(new OpeningHours()),
+            new TranslatedAddress(new Language('nl'), new Udb3Address(
+                new Udb3Street('Tesstraat 1'),
+                new Udb3PostalCode('2000'),
+                new Udb3Locality('Antwerpen'),
+                new CountryCode('BE')
+            )),
+            new Categories(
+                new Category(
+                    new CategoryID('0.6.0.0.0'),
+                    new CategoryLabel('Beurs'),
+                    new CategoryDomain('eventtype')
+                )
+            )
+        );
+    }
+}
