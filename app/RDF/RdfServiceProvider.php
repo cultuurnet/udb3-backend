489a1114f73deab093efb0b4648419dc1c1b575b
Luc Wollants <lucwollants@gmail.com>
2024-01-02 16:38:08 +0100

    Bootstrap creating AddressParser

--- app/RDF/RdfServiceProvider.php
+++ app/RDF/RdfServiceProvider.php
@@ -13,6 +13,9 @@ use CultuurNet\UDB3\Error\LoggerFactory;
 use CultuurNet\UDB3\Error\LoggerName;
 use CultuurNet\UDB3\Iri\CallableIriGenerator;
 use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use Geocoder\Provider\GoogleMaps\GoogleMaps;
+use Geocoder\StatefulGeocoder;
+use Http\Adapter\Guzzle7\Client;
 
 final class RdfServiceProvider extends AbstractServiceProvider
 {
@@ -27,18 +30,53 @@ final class RdfServiceProvider extends AbstractServiceProvider
     {
         $this->container->addShared(
             AddressParser::class,
-            function (): AddressParser {
-                $logger = LoggerFactory::create($this->getContainer(), LoggerName::forService('geopunt'));
-
-                $parser = new GeopuntAddressParser($this->container->get('config')['geopuntAddressParser']['url'] ?? '');
-                $parser->setLogger($logger);
-
-                $parser = new CachingAddressParser($parser, $this->container->get('cache')('geopunt_addresses'));
-                $parser->setLogger($logger);
-
-                return $parser;
-            }
+            fn (): AddressParser => $this->createAddressParser(
+                $this->container->get('config')['google_maps_api_key'] ?? false
+            )
         );
+    }
+
+    private function createAddressParser(bool $googleMapsAddressParser): AddressParser
+    {
+        if ($googleMapsAddressParser) {
+            return $this->createGoogleMapsAddressParser();
+        }
+
+        return $this->createGeoPuntAddressParser();
+    }
+
+    private function createGoogleMapsAddressParser(): AddressParser
+    {
+        $logger = LoggerFactory::create($this->getContainer(), LoggerName::forService('address_parser', 'google'));
+
+        $parser = new GoogleMapsAddressParser(
+            new StatefulGeocoder(
+                new GoogleMaps(
+                    new Client(),
+                    null,
+                    $this->container->get('config')['google_maps_api_key']
+                )
+            ),
+        );
+        $parser->setLogger($logger);
+
+        $parser = new CachingAddressParser($parser, $this->container->get('cache')('google_addresses'));
+        $parser->setLogger($logger);
+
+        return $parser;
+    }
+
+    private function createGeoPuntAddressParser(): AddressParser
+    {
+        $logger = LoggerFactory::create($this->getContainer(), LoggerName::forService('address_parser', 'geopunt'));
+
+        $parser = new GeopuntAddressParser($this->container->get('config')['geopuntAddressParser']['url'] ?? '');
+        $parser->setLogger($logger);
+
+        $parser = new CachingAddressParser($parser, $this->container->get('cache')('geopunt_addresses'));
+        $parser->setLogger($logger);
+
+        return $parser;
     }
 
     public static function createIriGenerator(string $baseUri): IriGeneratorInterface
