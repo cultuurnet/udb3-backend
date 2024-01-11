148cf972b240e1b17af1e86b203946b19232baeb
Luc Wollants <lucwollants@gmail.com>
2024-01-11 10:52:34 +0100

    Add custom exception when JSON data could not be converted to RDF

---
+++ src/RDF/JsonDataCouldNotBeConverted.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\RDF;
+
+use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
+use CultuurNet\UDB3\Http\ApiProblem\ConvertsToApiProblem;
+use Exception;
+
+final class JsonDataCouldNotBeConverted extends Exception implements ConvertsToApiProblem
+{
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
+
+    public function toApiProblem(): ApiProblem
+    {
+        return ApiProblem::invalidJsonData($this->message);
+    }
+}
