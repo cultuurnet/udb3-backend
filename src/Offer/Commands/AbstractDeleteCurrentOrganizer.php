7291f4991b0435e87abe6b7b8a71219065073172
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-01-18 12:57:15 +0100

    make AbstractDeleteCurrentOrganizer no longer abstract

--- src/Offer/Commands/AbstractDeleteCurrentOrganizer.php
+++ src/Offer/Commands/AbstractDeleteCurrentOrganizer.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer\Commands;
 
-abstract class AbstractDeleteCurrentOrganizer extends AbstractCommand
+class AbstractDeleteCurrentOrganizer extends AbstractCommand
 {
 }
