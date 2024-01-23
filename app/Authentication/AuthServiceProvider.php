b1946d43271301698df2a61d8fb594fa251f2ed1
Luc Wollants <lucwollants@gmail.com>
2024-01-23 13:55:12 +0100

    Linting

--- app/Authentication/AuthServiceProvider.php
+++ app/Authentication/AuthServiceProvider.php
@@ -16,7 +16,6 @@ use CultuurNet\UDB3\Http\Auth\Jwt\JsonWebToken;
 use CultuurNet\UDB3\Http\Auth\Jwt\UitIdV1JwtValidator;
 use CultuurNet\UDB3\Http\Auth\Jwt\UitIdV2JwtValidator;
 use CultuurNet\UDB3\Http\Auth\RequestAuthenticatorMiddleware;
-use CultuurNet\UDB3\Http\CheckTypeOfOfferMiddleware;
 use CultuurNet\UDB3\Impersonator;
 use CultuurNet\UDB3\Role\UserPermissionsServiceProvider;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
--- src/Http/CheckTypeOfOfferMiddleware.php
+++ src/Http/CheckTypeOfOfferMiddleware.php
@@ -11,7 +11,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use RuntimeException;
 
 class CheckTypeOfOfferMiddleware implements MiddlewareInterface
 {
