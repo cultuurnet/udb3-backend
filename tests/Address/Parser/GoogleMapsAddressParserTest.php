a0ec7cf93c3d9d21c006510ed48756801ee7668f
Luc Wollants <lucwollants@gmail.com>
2024-02-02 15:29:34 +0100

    Add test `it_geocodes_an_address_with_missing_number_and_returns_a_parsed_address`

--- tests/Address/Parser/GoogleMapsAddressParserTest.php
+++ tests/Address/Parser/GoogleMapsAddressParserTest.php
@@ -12,6 +12,7 @@ use Geocoder\Model\AdminLevelCollection;
 use Geocoder\Model\Country;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 class GoogleMapsAddressParserTest extends TestCase
 {
@@ -22,10 +23,18 @@ class GoogleMapsAddressParserTest extends TestCase
 
     private GoogleMapsAddressParser $parser;
 
+    /**
+     * @var LoggerInterface|MockObject
+     */
+    private $logger;
+
     protected function setUp(): void
     {
         $this->geocoder = $this->createMock(Geocoder::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+
         $this->parser = new GoogleMapsAddressParser($this->geocoder);
+        $this->parser->setLogger($this->logger);
 
         parent::setUp();
     }
@@ -68,6 +77,51 @@ class GoogleMapsAddressParserTest extends TestCase
         );
     }
 
+    /**
+     * @test
+     */
+    public function it_geocodes_an_address_with_missing_number_and_returns_a_parsed_address(): void
+    {
+        $this->geocoder->expects($this->once())
+            ->method('geocode')
+            ->with('Martelarenplein, 3000 Leuven, BE')
+            ->willReturn(
+                new AddressCollection([
+                    new Address(
+                        'Google',
+                        new AdminLevelCollection(),
+                        null,
+                        null,
+                        null,
+                        'Martelarenplein',
+                        '3000',
+                        'Leuven',
+                        null,
+                        new Country('BE')
+                    ),
+                ])
+            );
+
+        $this->logger->expects($this->once())
+            ->method('warning')
+            ->with('No street number found for address: "Martelarenplein, 3000 Leuven, BE"');
+
+        $parsedAddress = $this->parser->parse('Martelarenplein, 3000 Leuven, BE');
+
+        $this->assertEquals(
+            new ParsedAddress(
+                'Martelarenplein',
+                null,
+                '3000',
+                'Leuven'
+            ),
+            $parsedAddress
+        );
+    }
+
+    /**
+     * @test
+     */
     public function it_handles_a_geocoder_exception(): void
     {
         $this->geocoder->expects($this->once())
