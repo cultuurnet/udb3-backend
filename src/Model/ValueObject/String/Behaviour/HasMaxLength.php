3f5c15f7295264c09aa235c8753d4d8f2f920809
Koen Eelen <koen.eelenpubliq.be>
2024-02-14 10:26:03 +0100

    PR feedback

--- src/Model/ValueObject/String/Behaviour/HasMaxLength.php
+++ src/Model/ValueObject/String/Behaviour/HasMaxLength.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\ValueObject\String\Behaviour;
 
-use CultuurNet\UDB3\Http\ApiProblem\ApiProblem;
-use CultuurNet\UDB3\Http\ApiProblem\SchemaError;
+use InvalidArgumentException;
 
 trait HasMaxLength
 {
-    private function hasMaxLength(string $value, int $maxLength, string $path='/'): void
+    private function guardTooLong(string $value, int $maxLength, string $path='/'): void
     {
         if (mb_strlen($value) > $maxLength) {
-            throw ApiProblem::bodyInvalidData(
-                new SchemaError($path, sprintf('Given string should not be longer than %d characters.', $maxLength)),
-            );
+            throw new InvalidArgumentException(sprintf('Given string should not be longer than %d characters.', $maxLength));
         }
     }
 }
