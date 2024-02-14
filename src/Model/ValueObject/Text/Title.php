fb2c00de3930a649128a8f9ec4bca54ffd734773
Koen Eelen <koen.eelenpubliq.be>
2024-02-14 09:49:33 +0100

    add length check to title

--- src/Model/ValueObject/Text/Title.php
+++ src/Model/ValueObject/Text/Title.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\ValueObject\Text;
 
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\HasMaxLength;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsNotEmpty;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsString;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\Trims;
@@ -13,11 +14,15 @@ class Title
     use IsString;
     use IsNotEmpty;
     use Trims;
+    use HasMaxLength;
+
+    private const MAX_LENGTH = 90;
 
     public function __construct(string $value)
     {
         $value = $this->trim($value);
         $this->guardNotEmpty($value);
+        $this->hasMaxLength($value, self::MAX_LENGTH, '/title');
         $this->setValue($value);
     }
 }
