4af7d7f6c382dee6073c18122644bd303d7bad9a
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-02-23 11:23:36 +0100

    Add unit test

--- tests/Event/ReadModel/JSONLD/EventLDProjectorTest.php
+++ tests/Event/ReadModel/JSONLD/EventLDProjectorTest.php
@@ -1644,6 +1644,56 @@ class EventLDProjectorTest extends OfferLDProjectorTestBase
 
     /**
      * @test
+     * @dataProvider typesThatAreAvailableTillStart
+     */
+    public function it_sets_available_to_end_date_on_type_updated_from_selected_types(string $termId, string $termName): void
+    {
+        $eventId = '1a08516e-aba4-47f0-887e-df37b61a1e8d';
+
+        $eventThatShouldAvailableTillStart = new JsonDocument(
+            $eventId,
+            Json::encode([
+                '@id' => $eventId,
+                '@type' => 'event',
+                'calendar' => [
+                    'calendarType' => 'single',
+                    'timeSpans' => [
+                        [
+                            'start' => '2018-01-01T12:00:00+01:00',
+                            'end' => '2020-01-01T12:00:00+01:00',
+                        ],
+                    ],
+                ],
+                'startDate' => '2018-01-01T12:00:00+01:00',
+                'endDate' => '2020-01-01T12:00:00+01:00',
+                'availableTo' => '2018-01-01T12:00:00+01:00',
+                'terms' => [
+                    (object) [
+                        'id' => $termId,
+                        'label' => $termName,
+                        'domain' => 'theme',
+                    ],
+                    (object) [
+                        'id' => '0.7.0.0.0',
+                        'label' => 'Begeleide uitstap of rondleiding',
+                        'domain' => 'eventtype',
+                    ],
+                ],
+            ])
+        );
+        $this->documentRepository->save($eventThatShouldAvailableTillStart);
+
+        $endDate = DateTimeImmutable::createFromFormat(\DATE_ATOM, '2020-01-01T12:00:00+01:00');
+
+        $typeUpdated = new TypeUpdated($eventId, (new EventTypeResolver())->byId('0.50.4.0.0'));
+
+        $updatedItem = $this->project($typeUpdated, $eventId);
+
+        $this->assertEquals($endDate->format(DATE_ATOM), $updatedItem->availableTo);
+    }
+
+    /**
+     * @test
      */
     public function it_keeps_available_to_for_standard_types_on_type_updated(): void
     {
