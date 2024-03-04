816e0aaf58b472c0e04f2a5a4384a014c272ee0e
Luc Wollants <lucwollants@gmail.com>
2024-03-04 12:10:09 +0100

    Extend `RequestOwnership` command with requesterId

--- tests/Ownership/Commands/RequestOwnershipTest.php
+++ tests/Ownership/Commands/RequestOwnershipTest.php
@@ -21,7 +21,8 @@ class RequestOwnershipTest extends TestCase
             new UUID('e6e1f3a0-3e5e-4b3e-8e3e-3f3e3e3e3e3e'),
             new UUID('9e68dafc-01d8-4c1c-9612-599c918b981d'),
             ItemType::organizer(),
-            new UserId('auth0|63e22626e39a8ca1264bd29b')
+            new UserId('auth0|63e22626e39a8ca1264bd29b'),
+            new UserId('google-oauth2|102486314601596809843')
         );
     }
 
@@ -66,6 +67,17 @@ class RequestOwnershipTest extends TestCase
         $this->assertEquals(
             new UserId('auth0|63e22626e39a8ca1264bd29b'),
             $this->requestOwnership->getOwnerId()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_a_requester_id(): void
+    {
+        $this->assertEquals(
+            new UserId('google-oauth2|102486314601596809843'),
+            $this->requestOwnership->getRequesterId()
         );
     }
 }
