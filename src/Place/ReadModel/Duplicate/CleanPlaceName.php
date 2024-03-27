a0e72adb120401c7ba27bb1d76d6ec4e662cb181
Koen Eelen <koen.eelenpubliq.be>
2024-03-27 16:14:28 +0100

    Removing edge cases

--- src/Place/ReadModel/Duplicate/CleanPlaceName.php
+++ src/Place/ReadModel/Duplicate/CleanPlaceName.php
@@ -10,14 +10,9 @@ use CultuurNet\UDB3\Model\ValueObject\Geography\Address;
 class CleanPlaceName
 {
     private const MAX_LENGTH_TITLE = 150;
-    private const NONE = 'none';
 
     public static function transform(Address $address, string $title): string
     {
-        if (mb_strtolower($title) === self::NONE) {
-            return '';
-        }
-
         if (mb_strlen($title) > self::MAX_LENGTH_TITLE) {
             return '';
         }
@@ -29,13 +24,55 @@ class CleanPlaceName
         // the goal is to remove as much HTML as we can, if strip_tags misses a tag, we decode the html chars
         $title = htmlspecialchars_decode(strip_tags($title));
 
-        $title = str_replace([' BE', 'BE ', ' NL', 'NL '], [' Belgium', 'Belgium ', ' Netherlands', 'Netherlands '], $title);
-
-        // A specific hack to fix a location, because it gives errors in the google maps api
-        // Based on the Ai Python script
-        $title = str_replace('Tennisclub%20Kouterslag', 'Tennisclub_Kouterslag', $title);
+        $title = self::replaceCountryCodes($title);
 
         // Decode the unicode characters
         return Json::decode('"' . $title . '"');
+    }
+
+    private static function replaceCountryCodes(string $title): string
+    {
+        $countries = [
+            // All EU countries, probably overkill - thank you chat GPT
+            'AT' => 'Austria',
+            'BE' => 'Belgium',
+            'BG' => 'Bulgaria',
+            'CY' => 'Cyprus',
+            'CZ' => 'Czech Republic',
+            'DE' => 'Germany',
+            'DK' => 'Denmark',
+            'EE' => 'Estonia',
+            'ES' => 'Spain',
+            'FI' => 'Finland',
+            'FR' => 'France',
+            'GR' => 'Greece',
+            'HR' => 'Croatia',
+            'HU' => 'Hungary',
+            'IE' => 'Ireland',
+            'IT' => 'Italy',
+            'LT' => 'Lithuania',
+            'LU' => 'Luxembourg',
+            'LV' => 'Latvia',
+            'MT' => 'Malta',
+            'NL' => 'Netherlands',
+            'PL' => 'Poland',
+            'PT' => 'Portugal',
+            'RO' => 'Romania',
+            'SE' => 'Sweden',
+            'SI' => 'Slovenia',
+            'SK' => 'Slovakia',
+        ];
+
+        foreach ($countries as $countryCode => $countryName) {
+            if ($title === $countryCode || $title === $countryName) {
+                // Of the full title is just the country or language code, it is double information -> we remove it.
+                return '';
+            }
+
+            // Check space before or after to make sure we are not changing BE in the middle of a word
+            $title = str_replace(["$countryCode ", " $countryCode"], ["$countryName ", " $countryName"], $title);
+        }
+
+        return $title;
     }
 }
