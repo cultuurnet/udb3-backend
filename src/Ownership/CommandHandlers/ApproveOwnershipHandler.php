b762a0084f4a3a5b67bc776ac60964f8e0ef7415
Luc Wollants <lucwollants@gmail.com>
2024-04-02 11:26:04 +0200

    Add command handler for `ApproveOwnership`

---
+++ src/Ownership/CommandHandlers/ApproveOwnershipHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Ownership\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Ownership\Commands\ApproveOwnership;
+use CultuurNet\UDB3\Ownership\OwnershipRepository;
+
+final class ApproveOwnershipHandler implements CommandHandler
+{
+    private OwnershipRepository $ownershipRepository;
+
+    public function __construct(OwnershipRepository $ownershipRepository)
+    {
+        $this->ownershipRepository = $ownershipRepository;
+    }
+
+    public function handle($command): void
+    {
+        if (!$command instanceof ApproveOwnership) {
+            return;
+        }
+
+        $ownership = $this->ownershipRepository->load($command->getId()->toString());
+
+        $ownership->approve($command->getApproverId());
+
+        $this->ownershipRepository->save($ownership);
+    }
+}
