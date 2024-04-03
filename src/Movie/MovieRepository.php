60bd39f81d3eecf6bb78297b77fd66bc53e96bb5
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-04-03 10:14:58 +0200

    add repository and test

---
+++ src/Movie/MovieRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Movie;
+
+use Doctrine\DBAL\Connection;
+
+final class MovieRepository
+{
+    private Connection $connection;
+
+    private const TABLE = 'movie_mapping';
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getEventIdByMovieId(string $movieId): ?string
+    {
+        $result = $this->connection->createQueryBuilder()
+            ->select('event_id')
+            ->from(self::TABLE)
+            ->where('movie_id' . ' = :movie_id')
+            ->setParameter('movie_id', $movieId)
+            ->execute()
+            ->fetchFirstColumn();
+
+        return sizeof($result) === 0 ? null : $result[0];
+    }
+
+    public function addRelation(string $eventId, string $movieId): void
+    {
+        $this->connection->insert(
+            self::TABLE,
+            [
+                'event_id' => $eventId,
+                'movie_id' => $movieId,
+            ]
+        );
+    }
+}
---
+++ tests/Movie/MovieRepositoryTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Movie;
+
+use CultuurNet\UDB3\DBALTestConnectionTrait;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\Types;
+use PHPUnit\Framework\TestCase;
+
+final class MovieRepositoryTest extends TestCase
+{
+    use DBALTestConnectionTrait;
+    public const TABLE_NAME = 'movie_mapping';
+
+    private string $eventId;
+
+    private string $movieId;
+
+    private MovieRepository $movieRepository;
+
+    private static function getTableDefinition(Schema $schema): Table
+    {
+        $table = $schema->createTable(self::TABLE_NAME);
+
+        $table->addColumn('event_id', Types::GUID)->setLength(36)->setNotnull(true);
+        $table->addColumn('movie_id', Types::STRING)->setNotnull(true);
+
+        return $table;
+    }
+
+    public function setUp(): void
+    {
+        $this->eventId = '31c43ce5-4879-49a7-8627-dd1b60c5a9ed';
+
+        $this->movieId = 'Kinepolis:tDECAm47127';
+
+        $this->createTable(
+            self::getTableDefinition(
+                $this->createSchema()
+            )
+        );
+
+        $this->getConnection()->insert(
+            self::TABLE_NAME,
+            [
+                'event_id' => $this->eventId,
+                'movie_id' => $this->movieId,
+            ]
+        );
+
+        $this->movieRepository = new MovieRepository(
+            $this->getConnection()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_get_the_event_id_for_a_given_movie(): void
+    {
+        $this->assertEquals(
+            $this->eventId,
+            $this->movieRepository->getEventIdByMovieId($this->movieId)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_for_an_new_movie_id(): void
+    {
+        $this->assertNull(
+            $this->movieRepository->getEventIdByMovieId('Kinepolis:tKOOSTm21298')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_save_a_new_relation(): void
+    {
+        $newEventId = 'a25ec271-a19b-4881-9498-44b1ad4711a3';
+        $newMovieId = 'Kinepolis:tKOOSTm21298';
+
+        $this->movieRepository->addRelation($newEventId, $newMovieId);
+
+        $this->assertEquals(
+            $newEventId,
+            $this->movieRepository->getEventIdByMovieId($newMovieId)
+        );
+    }
+}
