0d9a24aefc1f1bb5135462fb1444be684cd34fa3
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-04-10 13:06:38 +0200

    Add force option

--- app/Console/Command/FetchMoviesFromKinepolisApi.php
+++ app/Console/Command/FetchMoviesFromKinepolisApi.php
@@ -7,7 +7,9 @@ namespace CultuurNet\UDB3\Console\Command;
 use CultuurNet\UDB3\Kinepolis\KinepolisService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 final class FetchMoviesFromKinepolisApi extends Command
 {
@@ -21,12 +23,35 @@ final class FetchMoviesFromKinepolisApi extends Command
     public function configure(): void
     {
         $this->setName('movies:fetch');
-        $this->setDescription('Fetches movies from an external API');
+        $this->setDescription('Fetches movies from the Kinepolis API');
+        $this->addOption(
+            'force',
+            null,
+            InputOption::VALUE_NONE,
+            'Skip confirmation.'
+        );
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->service->fetch();
+        if ($this->askConfirmation($input, $output)) {
+            $this->service->fetch();
+        }
         return 0;
+    }
+
+    private function askConfirmation(InputInterface $input, OutputInterface $output): bool
+    {
+        if ($input->getOption('force')) {
+            return true;
+        }
+
+        return $this
+            ->getHelper('question')
+            ->ask(
+                $input,
+                $output,
+                new ConfirmationQuestion('Fetch & import movies from the Kinepolis API? [y/N] ', false)
+            );
     }
 }
