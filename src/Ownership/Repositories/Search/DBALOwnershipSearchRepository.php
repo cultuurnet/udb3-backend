4b0333880b09b00dda18ebd74b06d9189a4cff0d
Luc Wollants <lucwollants@gmail.com>
2024-04-16 10:12:15 +0200

    Extend the search repo with state update and search method

--- src/Ownership/Repositories/Search/DBALOwnershipSearchRepository.php
+++ src/Ownership/Repositories/Search/DBALOwnershipSearchRepository.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Ownership\Repositories\Search;
 
+use CultuurNet\UDB3\Http\Ownership\Search\SearchQuery;
+use CultuurNet\UDB3\Ownership\OwnershipState;
 use CultuurNet\UDB3\Ownership\Repositories\OwnershipItem;
 use CultuurNet\UDB3\Ownership\Repositories\OwnershipItemCollection;
 use CultuurNet\UDB3\Ownership\Repositories\OwnershipItemNotFound;
@@ -12,6 +14,11 @@ use Doctrine\DBAL\Connection;
 final class DBALOwnershipSearchRepository implements OwnershipSearchRepository
 {
     private Connection $connection;
+
+    private const URL_PARAMETER_TO_COLUMN = [
+        'itemId' => 'item_id',
+        'state' => 'state',
+    ];
 
     public function __construct(Connection $connection)
     {
@@ -25,7 +32,17 @@ final class DBALOwnershipSearchRepository implements OwnershipSearchRepository
             'item_id' => $ownershipSearchItem->getItemId(),
             'item_type' => $ownershipSearchItem->getItemType(),
             'owner_id' => $ownershipSearchItem->getOwnerId(),
+            'state' => $ownershipSearchItem->getState(),
         ]);
+    }
+
+    public function updateState(string $id, OwnershipState $state): void
+    {
+        $this->connection->update(
+            'ownership_search',
+            ['state' => $state->toString()],
+            ['id' => $id]
+        );
     }
 
     public function getById(string $id): OwnershipItem
@@ -64,13 +81,21 @@ final class DBALOwnershipSearchRepository implements OwnershipSearchRepository
         return $this->createOwnershipSearchItem($ownershipSearchRow[0]);
     }
 
-    public function getByItemId(string $itemId): OwnershipItemCollection
+    public function search(SearchQuery $searchQuery): OwnershipItemCollection
     {
-        $ownershipSearchRows = $this->connection->createQueryBuilder()
+        $queryBuilder = $this->connection->createQueryBuilder()
             ->select('*')
-            ->from('ownership_search')
-            ->where('item_id = :item_id')
-            ->setParameter('item_id', $itemId)
+            ->from('ownership_search');
+
+        foreach ($searchQuery->getParameters() as $parameter) {
+            $column = self::URL_PARAMETER_TO_COLUMN[$parameter->getUrlParameter()];
+
+            $queryBuilder
+                ->andWhere($column . ' = :' . $column)
+                ->setParameter($column, $parameter->getValue());
+        }
+
+        $ownershipSearchRows = $queryBuilder
             ->execute()
             ->fetchAllAssociative();
 
@@ -88,7 +113,8 @@ final class DBALOwnershipSearchRepository implements OwnershipSearchRepository
             $ownershipSearchRow['id'],
             $ownershipSearchRow['item_id'],
             $ownershipSearchRow['item_type'],
-            $ownershipSearchRow['owner_id']
+            $ownershipSearchRow['owner_id'],
+            $ownershipSearchRow['state']
         );
     }
 }
