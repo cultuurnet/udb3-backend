754ef1e027bf028db76eaf83b8a75a4227c9fb26
Luc Wollants <lucwollants@gmail.com>
2024-06-11 10:05:48 +0200

    Move token repositories to sub folder

--- src/User/TokenRepository/CacheRepository.php
+++ src/User/TokenRepository/CacheRepository.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace CultuurNet\UDB3\User\Auth0;
+namespace CultuurNet\UDB3\User\TokenRepository;
 
 use CultuurNet\UDB3\User\ManagementToken;
 use DateTimeImmutable;
 use Doctrine\Common\Cache\Cache;
 
-class CacheRepository implements Auth0ManagementTokenRepository
+class CacheRepository implements TokenRepository
 {
     private const TOKEN_KEY = 'auth0_token';
 
