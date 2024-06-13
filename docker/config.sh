a6190af8439ab373be6415de452d2ed21a46c1a1
Luc Wollants <lucwollants@gmail.com>
2024-06-13 13:42:00 +0200

    Remove comments from script

--- docker/config.sh
+++ docker/config.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-# Add host.docker.internal to /etc/hosts
 if ! grep -q "host.docker.internal" /etc/hosts; then
   echo "host.docker.internal has to be in your hosts-file, to add you need sudo privileges"
   sudo sh -c 'echo "127.0.0.1 host.docker.internal" >> /etc/hosts'
 fi
 
-# setup config & key files
 DIR="../appconfig/files/udb3/docker/udb3-backend/"
 if [ -d "$DIR" ]; then
   cp -R "$DIR"/* .
-  # needed because it is hidden
   cp "$DIR"/.env .
 else
   echo "Error: missing appconfig see docker.md prerequisites to fix this."
