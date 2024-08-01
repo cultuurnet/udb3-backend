7aca14b46678a4dc53e46355be3d8e4ced5319c6
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-08-01 10:33:14 +0200

    make unit test

--- tests/Http/Offer/PriceInfoDuplicateNameValidatingRequestBodyParserTest.php
+++ tests/Http/Offer/PriceInfoDuplicateNameValidatingRequestBodyParserTest.php
@@ -117,4 +117,65 @@ final class PriceInfoDuplicateNameValidatingRequestBodyParserTest extends TestCa
             fn () => $this->priceInfoDuplicateNameValidatingRequestBodyParser->parse($request)
         );
     }
+
+    /**
+     * @test
+     */
+    public function it_throws_on_same_names_with_different_spacing(): void
+    {
+        $priceInfo = (object) [
+            'priceInfo' => (object) [
+                (object)[
+                    'category' => 'tariff',
+                    'name' => (object) [
+                        'nl' => 'Studenten',
+                    ],
+                    'price' => 10,
+                    'priceCurrency' => 'EUR',
+                ],
+                (object)[
+                    'category' => 'base',
+                    'name' => (object) [
+                        'nl' => 'Basistarief',
+                    ],
+                    'price' => 50,
+                    'priceCurrency' => 'EUR',
+                ],
+                (object)[
+                    'category' => 'tariff',
+                    'name' => (object) [
+                        'nl' => 'Studenten ',
+                    ],
+                    'price' => 15,
+                    'priceCurrency' => 'EUR',
+                ],
+                (object)[
+                    'category' => 'tariff',
+                    'name' => (object) [
+                        'nl' => 'Leraren',
+                    ],
+                    'price' => 20,
+                ],
+                (object)[
+                    'category' => 'tariff',
+                    'name' => (object) [
+                        'nl' => 'Studenten   ',
+                    ],
+                    'price' => 30,
+                ],
+            ],
+        ];
+
+        $request = $this->requestBuilder
+            ->build('POST')
+            ->withParsedBody($priceInfo);
+
+        $this->assertCallableThrowsApiProblem(
+            ApiProblem::bodyInvalidData(
+                new SchemaError('/priceInfo/2/name/nl', 'Tariff name "Studenten" must be unique.'),
+                new SchemaError('/priceInfo/4/name/nl', 'Tariff name "Studenten" must be unique.')
+            ),
+            fn () => $this->priceInfoDuplicateNameValidatingRequestBodyParser->parse($request)
+        );
+    }
 }
