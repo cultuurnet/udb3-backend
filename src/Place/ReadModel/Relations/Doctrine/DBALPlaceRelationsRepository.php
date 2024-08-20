0db78f7963ee98ca98c8182e0f753198584cc21d
Luc Wollants <lucwollants@gmail.com>
2024-08-20 09:48:06 +0200

    Remove schema configuration from relations table of Places

--- src/Place/ReadModel/Relations/Doctrine/DBALPlaceRelationsRepository.php
+++ src/Place/ReadModel/Relations/Doctrine/DBALPlaceRelationsRepository.php
@@ -7,8 +7,6 @@ namespace CultuurNet\UDB3\Place\ReadModel\Relations\Doctrine;
 use CultuurNet\UDB3\Place\ReadModel\Relations\PlaceRelationsRepository;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\Statement as DriverStatement;
-use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Schema\Table;
 
 final class DBALPlaceRelationsRepository implements PlaceRelationsRepository
 {
@@ -66,36 +64,5 @@ final class DBALPlaceRelationsRepository implements PlaceRelationsRepository
             ->setParameter(0, $placeId);
 
         $q->execute();
-    }
-
-    public function configureSchema(Schema $schema): ?Table
-    {
-        if ($schema->hasTable($this->tableName)) {
-            return null;
-        }
-
-        return $this->configureTable();
-    }
-
-    public function configureTable(): ?Table
-    {
-        $schema = new Schema();
-
-        $table = $schema->createTable($this->tableName);
-
-        $table->addColumn(
-            'place',
-            'string',
-            ['length' => 36, 'notnull' => false]
-        );
-        $table->addColumn(
-            'organizer',
-            'string',
-            ['length' => 36, 'notnull' => false]
-        );
-
-        $table->setPrimaryKey(['place']);
-
-        return $table;
     }
 }
