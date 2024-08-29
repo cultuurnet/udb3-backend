6b1f3ff62dfc6918f6ba938a51d73a21c2ed9b2b
Emma Fabre <ehtnam6@gmail.com>
2024-08-29 13:15:18 +0200

    Add return types

--- src/EventExport/Format/HTML/TransformingIteratorIterator.php
+++ src/EventExport/Format/HTML/TransformingIteratorIterator.php
@@ -23,7 +23,7 @@ class TransformingIteratorIterator extends IteratorIterator
         $this->function = $function;
     }
 
-    public function current()
+    public function current(): mixed
     {
         $fn = $this->function;
         $current = parent::current();
