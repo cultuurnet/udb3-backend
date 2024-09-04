ee716501c5772f75eb7e9f0ee06f4754a26bc4b3
Koen Eelen <koen.eelenpubliq.be>
2024-09-04 12:52:19 +0200

    forgot to push DTO

---
+++ src/Place/Canonical/ClusterRecordRow.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place\Canonical;
+
+class ClusterRecordRow
+{
+    private string $clusterId;
+    private string $placeUuid;
+    private ?string $canonical;
+
+    public function __construct(string $clusterId, string $placeUuid, string $canonical = null)
+    {
+        $this->clusterId = $clusterId;
+        $this->placeUuid = $placeUuid;
+        $this->canonical = $canonical;
+    }
+
+    public function getClusterId(): string
+    {
+        return $this->clusterId;
+    }
+
+    public function getPlaceUuid(): string
+    {
+        return $this->placeUuid;
+    }
+
+    public function getCanonical(): ?string
+    {
+        return $this->canonical;
+    }
+}
