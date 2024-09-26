cb48459480bbb7aa4312c3229ebd761f42a7e530
Luc Wollants <lucwollants@gmail.com>
2024-09-26 12:05:26 +0200

    Refactor `AbstractDescriptionTranslated` to use new `Description`

--- src/Offer/Events/AbstractDescriptionTranslated.php
+++ src/Offer/Events/AbstractDescriptionTranslated.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer\Events;
 
-use CultuurNet\UDB3\Description;
+use CultuurNet\UDB3\Model\ValueObject\Text\Description;
 use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
 
 class AbstractDescriptionTranslated extends AbstractPropertyTranslatedEvent
