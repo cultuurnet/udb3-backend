6fe236f992f4539cf0f9210019b40abdcd4f1827
Luc Wollants <lucwollants@gmail.com>
2024-09-27 09:40:21 +0200

    Refactor `AbstractUpdateBookingInfo`

--- src/Offer/Commands/AbstractUpdateBookingInfo.php
+++ src/Offer/Commands/AbstractUpdateBookingInfo.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer\Commands;
 
-use CultuurNet\UDB3\BookingInfo;
+use CultuurNet\UDB3\Model\ValueObject\Contact\BookingInfo;
 
 abstract class AbstractUpdateBookingInfo extends AbstractCommand
 {
