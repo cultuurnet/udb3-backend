89fdc8070bbad271f425f191b030c2e01666b6b7
Koen Eelen <koen.eelenpubliq.be>
2024-11-14 14:43:41 +0100

    remove useless code

--- src/Offer/Commands/DeleteOffer.php
+++ src/Offer/Commands/DeleteOffer.php
@@ -8,8 +8,4 @@ use CultuurNet\UDB3\Role\ValueObjects\Permission;
 
 final class DeleteOffer extends AbstractCommand
 {
-    public function getPermission(): Permission
-    {
-        return Permission::aanbodBewerken();
-    }
 }
