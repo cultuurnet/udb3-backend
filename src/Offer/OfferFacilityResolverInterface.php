892a125934b478b00a9f10104b9492b379d9bb04
Luc Wollants <lucwollants@gmail.com>
2024-12-04 15:57:26 +0100

    Refactor `OfferFacilityResolverInterface` to use `Category`

--- src/Offer/OfferFacilityResolverInterface.php
+++ src/Offer/OfferFacilityResolverInterface.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Offer;
 
-use CultuurNet\UDB3\Facility;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Category;
 
 interface OfferFacilityResolverInterface
 {
-    public function byId(string $facilityId): Facility;
+    public function byId(string $facilityId): Category;
 }
