b2a03bc93cbc0f46790bab1fd51ddc0b8670a2dc
Koen Eelen <koen.eelenpubliq.be>
2024-12-10 13:25:10 +0100

    Change UUID DTO, add new uuid4 method

---
+++ src/Model/ValueObject/Identity/Uuid.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Model\ValueObject\Identity;
+
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsString;
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\MatchesRegexPattern;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+class Uuid
+{
+    use IsString;
+    use MatchesRegexPattern;
+
+    /**
+     * Ensures backwards compatibility with older, malformed, uuids present in UDB.
+     */
+    public const BC_REGEX = '\\A[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{12}\\z';
+
+    public function __construct(string $value)
+    {
+        $this->guardRegexPattern(
+            '/' . self::BC_REGEX . '/',
+            $value,
+            $value . ' is not a valid uuid.'
+        );
+
+        $this->setValue($value);
+    }
+
+    public static function uuid4(): self
+    {
+        return new Uuid(RamseyUuid::uuid4()->toString());
+    }
+}
