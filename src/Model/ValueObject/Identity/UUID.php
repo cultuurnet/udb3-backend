b1c2bc3b990f88c05eae766590e005558eabcc40
Koen Eelen <koen.eelenpubliq.be>
2024-12-11 09:14:39 +0100

    Make BC_REGEX private / update unit test

--- src/Model/ValueObject/Identity/UUID.php
+++ src/Model/ValueObject/Identity/UUID.php
@@ -18,7 +18,7 @@ class UUID
     /**
      * Ensures backwards compatibility with older, malformed, uuids present in UDB.
      */
-    public const BC_REGEX = '/\\A[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{12}\\z/';
+    private const BC_REGEX = '/\\A[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{12}\\z/';
 
     public function __construct(string $value)
     {
