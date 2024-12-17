89090439f0ab5ba8585ba8fb0fb34fd6c3e0a22b
Luc Wollants <lucwollants@gmail.com>
2024-12-17 14:17:20 +0100

    Allow `Street` to be empty

--- src/Model/ValueObject/Geography/Street.php
+++ src/Model/ValueObject/Geography/Street.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Model\ValueObject\Geography;
 
-use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsNotEmpty;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsString;
 
 class Street
 {
     use IsString;
-    use IsNotEmpty;
 
     public function __construct(string $value)
     {
-        $this->guardNotEmpty($value);
         $this->setValue($value);
     }
 }
