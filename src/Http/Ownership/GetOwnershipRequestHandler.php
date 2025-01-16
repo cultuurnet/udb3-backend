130c916e1a818ee89b09181b23e1a66e52a63db9
Luc Wollants <lucwollants@gmail.com>
2025-01-16 11:20:38 +0100

    Use gaurd inside the `GetOwnershipRequestHandler`

--- src/Http/Ownership/GetOwnershipRequestHandler.php
+++ src/Http/Ownership/GetOwnershipRequestHandler.php
@@ -9,6 +9,7 @@ use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\JsonLdResponse;
 use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
 use CultuurNet\UDB3\ReadModel\DocumentRepository;
+use CultuurNet\UDB3\User\CurrentUser;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -16,16 +17,25 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class GetOwnershipRequestHandler implements RequestHandlerInterface
 {
     private DocumentRepository $ownershipRepository;
+    private CurrentUser $currentUser;
+    private OwnershipStatusGuard $ownershipStatusGuard;
 
-    public function __construct(DocumentRepository $ownershipRepository)
-    {
+    public function __construct(
+        DocumentRepository $ownershipRepository,
+        CurrentUser $currentUser,
+        OwnershipStatusGuard $ownershipStatusGuard
+    ) {
         $this->ownershipRepository = $ownershipRepository;
+        $this->currentUser = $currentUser;
+        $this->ownershipStatusGuard = $ownershipStatusGuard;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $routeParameters = new RouteParameters($request);
         $ownershipId = $routeParameters->getOwnershipId();
+
+        $this->ownershipStatusGuard->isAllowedToGet($ownershipId, $this->currentUser);
 
         try {
             return new JsonLdResponse(
