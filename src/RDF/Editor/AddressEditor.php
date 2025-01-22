87fae6a1c85241da41185e30729cbe7adb879231
Koen Eelen <koen.eelenpubliq.be>
2025-01-22 10:29:05 +0100

    revert file I commited by accident

--- src/RDF/Editor/AddressEditor.php
+++ src/RDF/Editor/AddressEditor.php
@@ -6,10 +6,7 @@ namespace CultuurNet\UDB3\RDF\Editor;
 
 use CultuurNet\UDB3\Address\Formatter\FullAddressFormatter;
 use CultuurNet\UDB3\Address\Parser\AddressParser;
-use CultuurNet\UDB3\Model\Serializer\ValueObject\Geography\AddressNormalizer;
 use CultuurNet\UDB3\Model\ValueObject\Geography\TranslatedAddress;
-use CultuurNet\UDB3\RDF\NodeUri\CRC32HashGenerator;
-use CultuurNet\UDB3\RDF\NodeUri\NodeUriGenerator;
 use EasyRdf\Literal;
 use EasyRdf\Resource;
 
@@ -17,36 +14,24 @@ final class AddressEditor
 {
     private AddressParser $addressParser;
 
+    private const TYPE_ADRES = 'locn:Address';
+
     private const PROPERTY_ADRES_STRAATNAAM = 'locn:thoroughfare';
     private const PROPERTY_ADRES_HUISNUMMER = 'locn:locatorDesignator';
     private const PROPERTY_ADRES_POSTCODE = 'locn:postCode';
     private const PROPERTY_ADRES_GEMEENTENAAM = 'locn:postName';
     private const PROPERTY_ADRES_LAND = 'locn:adminUnitL1';
     private const PROPERTY_ADRES_VOLLEDIG_ADRES = 'locn:fullAddress';
-    private AddressNormalizer $addressNormalizer;
 
-    public function __construct(AddressParser $addressParser, AddressNormalizer $addressNormalizer = null)
+    public function __construct(AddressParser $addressParser)
     {
         $this->addressParser = $addressParser;
-        $this->addressNormalizer = $addressNormalizer ?? new AddressNormalizer();
     }
 
     public function setAddress(Resource $resource, string $property, TranslatedAddress $translatedAddress): Resource
     {
-        //start
-        $nodeUriGenerator = new NodeUriGenerator(new CRC32HashGenerator());
-
-        $addressJson = [];
-        foreach ($translatedAddress->getLanguages() as $language) {
-            $addressJson[] = $this->addressNormalizer->normalize($translatedAddress->getTranslation($language));
-        }
-
-        $addressResource = $resource->getGraph()->resource($nodeUriGenerator->generate(
-            'address',
-            $addressJson
-        ));
+        $addressResource = $resource->getGraph()->newBNode([self::TYPE_ADRES]);
         $resource->add($property, $addressResource);
-        //end
 
         foreach ($translatedAddress->getLanguages() as $language) {
             $address = $translatedAddress->getTranslation($language);
