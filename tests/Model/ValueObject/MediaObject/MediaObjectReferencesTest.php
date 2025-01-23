ef7af7db47fe03b20576ffbf6e824645bcdd6cc8
Luc Wollants <lucwollants@gmail.com>
2025-01-23 10:54:44 +0100

    Remove methods only used in tests

--- tests/Model/ValueObject/MediaObject/MediaObjectReferencesTest.php
+++ tests/Model/ValueObject/MediaObject/MediaObjectReferencesTest.php
@@ -11,49 +11,4 @@ use PHPUnit\Framework\TestCase;
 
 class MediaObjectReferencesTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function it_should_return_all_references_either_with_or_without_an_embedded_media_object(): void
-    {
-        $referenceWithEmbeddedMediaObject = MediaObjectReference::createWithEmbeddedMediaObject(
-            new MediaObject(
-                new Uuid('0bda23b1-3332-4866-b69b-1f1c1d1dbcb4'),
-                MediaObjectType::imageObject()
-            ),
-            new Description('Some image description'),
-            new CopyrightHolder('Publiq vzw'),
-            new Language('en')
-        );
-
-        $referenceWithoutEmbeddedMediaObject = MediaObjectReference::createWithMediaObjectId(
-            new Uuid('0bda23b1-3332-4866-b69b-1f1c1d1dbcb4'),
-            new Description('Een afbeelding beschrijving'),
-            new CopyrightHolder('Publiq vzw'),
-            new Language('nl')
-        );
-
-        $references = new MediaObjectReferences(
-            $referenceWithEmbeddedMediaObject,
-            $referenceWithoutEmbeddedMediaObject
-        );
-
-        $expectedReferencesWithEmbeddedMediaObject = new MediaObjectReferences(
-            $referenceWithEmbeddedMediaObject
-        );
-
-        $expectedReferencesWithoutEmbeddedMediaObject = new MediaObjectReferences(
-            $referenceWithoutEmbeddedMediaObject
-        );
-
-        $this->assertEquals(
-            $expectedReferencesWithEmbeddedMediaObject,
-            $references->getReferencesWithEmbeddedMediaObject()
-        );
-
-        $this->assertEquals(
-            $expectedReferencesWithoutEmbeddedMediaObject,
-            $references->getReferencesWithoutEmbeddedMediaObject()
-        );
-    }
 }
