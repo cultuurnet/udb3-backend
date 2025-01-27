84ddd2778994437226f358fb60cbbdf853512e6b
Koen Eelen <koen.eelenpubliq.be>
2025-01-27 08:30:46 +0100

    fix tests

--- tests/Organizer/ReadModel/RDF/OrganizerJsonToTurtleConverterTest.php
+++ tests/Organizer/ReadModel/RDF/OrganizerJsonToTurtleConverterTest.php
@@ -9,6 +9,9 @@ use CultuurNet\UDB3\Address\Parser\ParsedAddress;
 use CultuurNet\UDB3\Iri\CallableIriGenerator;
 use CultuurNet\UDB3\Json;
 use CultuurNet\UDB3\Model\Serializer\Organizer\OrganizerDenormalizer;
+use CultuurNet\UDB3\RDF\NodeUri\CRC32HashGenerator;
+use CultuurNet\UDB3\RDF\NodeUri\NodeUriGenerator;
+use CultuurNet\UDB3\RDF\NodeUri\ResourceFactory\RdfResourceFactoryWithoutBlankNodes;
 use CultuurNet\UDB3\ReadModel\DocumentRepository;
 use CultuurNet\UDB3\ReadModel\InMemoryDocumentRepository;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
@@ -73,6 +76,7 @@ class OrganizerJsonToTurtleConverterTest extends TestCase
             new OrganizerDenormalizer(),
             $addressParser,
             $this->imageNormalizer,
+            new RdfResourceFactoryWithoutBlankNodes(new NodeUriGenerator(new CRC32HashGenerator())),
             $logger
         );
     }
