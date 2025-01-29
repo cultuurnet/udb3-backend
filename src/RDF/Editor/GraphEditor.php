cfced89ded35a71e47cb7e5842d2a7107d0ab493
Koen Eelen <koen.eelenpubliq.be>
2025-01-29 10:02:18 +0100

    Convert blanknode for identifier + StructuredIdentifactor

--- src/RDF/Editor/GraphEditor.php
+++ src/RDF/Editor/GraphEditor.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\RDF\Editor;
 
+use CultuurNet\UDB3\RDF\NodeUri\ResourceFactory\RdfResourceFactory;
 use EasyRdf\Graph;
 use EasyRdf\Literal;
 
 final class GraphEditor
 {
     private Graph $graph;
+    private RdfResourceFactory $resourceFactory;
+
 
     private const TYPE_IDENTIFICATOR = 'adms:Identifier';
     private const TYPE_STRUCTURED_IDENTIFICATOR = 'generiek:GestructureerdeIdentificator';
@@ -25,14 +28,15 @@ final class GraphEditor
     private const PROPERTY_NAAMRUIMTE = 'generiek:naamruimte';
     private const PROPERTY_LOKALE_IDENTIFICATOR = 'generiek:lokaleIdentificator';
 
-    private function __construct(Graph $graph)
+    private function __construct(Graph $graph, RdfResourceFactory $resourceFactory)
     {
         $this->graph = $graph;
+        $this->resourceFactory = $resourceFactory;
     }
 
-    public static function for(Graph $graph): self
+    public static function for(Graph $graph, RdfResourceFactory $resourceFactory): self
     {
-        return new self($graph);
+        return new self($graph, $resourceFactory);
     }
 
     public function setGeneralProperties(
@@ -46,17 +50,23 @@ final class GraphEditor
         $resource->setType($type);
 
         $resource->set(self::PROPERTY_AANGEMAAKT_OP, new Literal($createdOn, null, 'xsd:dateTime'));
-
         $resource->set(self::PROPERTY_LAATST_AANGEPAST, new Literal($modifiedOn, null, 'xsd:dateTime'));
 
-        $identifier = $this->graph->newBNode();
-        $identifier->setType(self::TYPE_IDENTIFICATOR);
+        [$namespace, $localIdentifier] = $this->splitIdentifier($resourceIri);
+
+        $identifier = $this->resourceFactory->create($resource,self::TYPE_IDENTIFICATOR, [
+            self::PROPERTY_AANGEMAAKT_OP => $createdOn,
+            self::PROPERTY_LAATST_AANGEPAST => $modifiedOn,
+            self::PROPERTY_NAAMRUIMTE => $namespace,
+            self::PROPERTY_LOKALE_IDENTIFICATOR => $localIdentifier,
+        ]);
         $identifier->add(self::PROPERTY_IDENTIFICATOR_NOTATION, new Literal($resourceIri, null, 'xsd:anyURI'));
         $resource->add(self::PROPERTY_IDENTIFICATOR, $identifier);
 
-        list($namespace, $localIdentifier) = $this->splitIdentifier($resourceIri);
-        $structuredIdentifier = $this->graph->newBNode();
-        $structuredIdentifier->setType(self::TYPE_STRUCTURED_IDENTIFICATOR);
+        $structuredIdentifier = $this->resourceFactory->create($resource,self::TYPE_STRUCTURED_IDENTIFICATOR, [
+            self::PROPERTY_NAAMRUIMTE => $namespace,
+            self::PROPERTY_LOKALE_IDENTIFICATOR => $localIdentifier,
+        ]);
         $structuredIdentifier->add(self::PROPERTY_NAAMRUIMTE, new Literal($namespace));
         $structuredIdentifier->add(self::PROPERTY_LOKALE_IDENTIFICATOR, new Literal($localIdentifier));
         $identifier->add(self::PROPERTY_STRUCTURED_IDENTIFICATOR, $structuredIdentifier);
