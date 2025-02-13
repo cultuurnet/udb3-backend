bae91e454b12fbd515ee6d1e8fbcc31333e8f929
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-13 10:05:05 +0100

    Add CacheFactory

---
+++ src/Cache/CacheFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CultuurNet\UDB3\Cache;
+
+use Predis\Client;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Contracts\Cache\CacheInterface;
+
+final class CacheFactory
+{
+    public static function create(
+        ContainerInterface $container,
+        string $namespace,
+        int $defaultLifeTime
+    ): CacheInterface {
+        return new RedisAdapter($container->get(Client::class), $namespace, $defaultLifeTime);
+    }
+}
