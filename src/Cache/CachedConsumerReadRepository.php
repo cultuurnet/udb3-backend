7392ca29bc7385a972d58d8cc5d2cf0c0d176116
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-19 16:18:00 +0100

    Add CachedConsumerReadRepository

---
+++ src/Cache/CachedConsumerReadRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Cache;
+
+use CultureFeed_Consumer;
+use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
+use CultuurNet\UDB3\ApiGuard\Consumer\Consumer;
+use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepository;
+use CultuurNet\UDB3\ApiGuard\CultureFeed\CultureFeedConsumerAdapter;
+use Symfony\Contracts\Cache\CacheInterface;
+
+final class CachedConsumerReadRepository implements ConsumerReadRepository
+{
+    private ConsumerReadRepository $baseConsumerReadRepository;
+
+    private CacheInterface $cache;
+
+    public function __construct(ConsumerReadRepository $baseConsumerReadRepository, CacheInterface $cache)
+    {
+        $this->baseConsumerReadRepository = $baseConsumerReadRepository;
+        $this->cache = $cache;
+    }
+
+    public function getConsumer(ApiKey $apiKey): ?Consumer
+    {
+        $consumerAsArray = $this->cache->get(
+            $apiKey->toString(),
+            function () use ($apiKey) {
+                $consumer = $this->baseConsumerReadRepository->getConsumer($apiKey);
+                return $consumer !== null ? $this->consumerAsArray($consumer) : null;
+            }
+        );
+        if ($consumerAsArray !== null) {
+            return $this->arrayToConsumer($consumerAsArray);
+        }
+        return null;
+    }
+
+    private function consumerAsArray(Consumer $consumer): array
+    {
+        return [
+            'api_key' => $consumer->getApiKey(),
+            'default_query' => $consumer->getDefaultQuery(),
+            'permission_group_ids' => $consumer->getPermissionGroupIds(),
+            'name' => $consumer->getName(),
+            'blocked' => $consumer->isBlocked(),
+            'removed' => $consumer->isRemoved(),
+        ];
+    }
+
+    private function arrayToConsumer(array $consumerAsArray): Consumer
+    {
+        $consumer = new CultureFeed_Consumer();
+        $consumer->apiKeySapi3 = $consumerAsArray['api_key'];
+        $consumer->searchPrefixSapi3 = $consumerAsArray['default_query'];
+        $consumer->group = $consumerAsArray['permission_group_ids'];
+        $consumer->name = $consumerAsArray['name'];
+        if ($consumerAsArray['blocked']) {
+            $consumer->status = 'BLOCKED';
+        }
+        if ($consumerAsArray['removed']) {
+            $consumer->status = 'REMOVED';
+        }
+        return new CultureFeedConsumerAdapter($consumer);
+    }
+}
