284deee7164bffb5071b5db5139989669fa9a4e4
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-21 10:59:01 +0100

    Make new SerializableConsumer

--- src/Cache/CachedConsumerReadRepository.php
+++ src/Cache/CachedConsumerReadRepository.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Cache;
 
-use CultureFeed_Consumer;
 use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
 use CultuurNet\UDB3\ApiGuard\Consumer\Consumer;
 use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepository;
-use CultuurNet\UDB3\ApiGuard\CultureFeed\CultureFeedConsumerAdapter;
 use Symfony\Contracts\Cache\CacheInterface;
 
 final class CachedConsumerReadRepository implements ConsumerReadRepository
@@ -25,39 +23,16 @@ final class CachedConsumerReadRepository implements ConsumerReadRepository
 
     public function getConsumer(ApiKey $apiKey): ?Consumer
     {
-        $consumerAsArray = $this->cache->get(
+        $serializableConsumer = $this->cache->get(
             $apiKey->toString(),
             function () use ($apiKey) {
                 $consumer = $this->baseConsumerReadRepository->getConsumer($apiKey);
-                return $consumer !== null ? $this->consumerAsArray($consumer) : null;
+                return $consumer !== null ? SerializableConsumer::serialize($consumer) : null;
             }
         );
-        if ($consumerAsArray !== null) {
-            return $this->arrayToConsumer($consumerAsArray);
+        if ($serializableConsumer !== null) {
+            return SerializableConsumer::deserialize($serializableConsumer);
         }
         return null;
-    }
-
-    private function consumerAsArray(Consumer $consumer): array
-    {
-        return [
-            'api_key' => $consumer->getApiKey()->toString(),
-            'default_query' => $consumer->getDefaultQuery(),
-            'permission_group_ids' => $consumer->getPermissionGroupIds(),
-            'name' => $consumer->getName(),
-            'status' => $consumer->isBlocked() ? 'BLOCKED' :
-                ($consumer->isRemoved() ? 'REMOVED' : 'ACTIVE'),
-        ];
-    }
-
-    private function arrayToConsumer(array $consumerAsArray): Consumer
-    {
-        $consumer = new CultureFeed_Consumer();
-        $consumer->apiKeySapi3 = $consumerAsArray['api_key'];
-        $consumer->searchPrefixSapi3 = $consumerAsArray['default_query'];
-        $consumer->group = $consumerAsArray['permission_group_ids'];
-        $consumer->name = $consumerAsArray['name'];
-        $consumer->status = $consumerAsArray['status'];
-        return new CultureFeedConsumerAdapter($consumer);
     }
 }
