c767310ba845738f67a7cce03a1b4dbc95fab845
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-21 10:59:08 +0100

    update tests

--- tests/Cache/CachedConsumerReadRepositoryTest.php
+++ tests/Cache/CachedConsumerReadRepositoryTest.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Cache;
 
-use CultureFeed_Consumer;
 use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
+use CultuurNet\UDB3\ApiGuard\Consumer\Consumer;
 use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepository;
-use CultuurNet\UDB3\ApiGuard\CultureFeed\CultureFeedConsumerAdapter;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
@@ -23,7 +22,7 @@ final class CachedConsumerReadRepositoryTest extends TestCase
 
     private CachedConsumerReadRepository $cachedConsumerReadRepository;
 
-    private CultureFeed_Consumer $cachedConsumer;
+    private Consumer $cachedConsumer;
 
     protected function setUp(): void
     {
@@ -31,25 +30,19 @@ final class CachedConsumerReadRepositoryTest extends TestCase
         $cache = new ArrayAdapter();
         $this->cachedApiKey = new ApiKey('b26e5a7b-5e01-46c1-8da8-f45edc51d01a');
 
-        $this->cachedConsumer = new CultureFeed_Consumer();
-        $this->cachedConsumer->apiKeySapi3 = $this->cachedApiKey->toString();
-        $this->cachedConsumer->searchPrefixSapi3 = 'regions:nis-44021';
-        $this->cachedConsumer->group = [1, 2, 3];
-        $this->cachedConsumer->name = 'Foobar';
-        $this->cachedConsumer->status = 'ACTIVE';
+        $this->cachedConsumer = SerializableConsumer::deserialize([
+            'api_key' => $this->cachedApiKey,
+            'default_query' => 'regions:nis-44021',
+            'permission_group_ids' => [1, 2, 3],
+            'name' => 'Foobar',
+            'is_blocked' => false,
+            'is_removed' => false,
+        ]);
 
         $cache->get(
             $this->cachedApiKey->toString(),
             function () {
-                return [
-                    'api_key' => $this->cachedConsumer->apiKeySapi3,
-                    'default_query' => $this->cachedConsumer->searchPrefixSapi3,
-                    'permission_group_ids' => $this->cachedConsumer->group,
-                    'name' => $this->cachedConsumer->name,
-                    'blocked' => false,
-                    'removed' => false,
-                    'status' => 'ACTIVE',
-                ];
+                return SerializableConsumer::serialize($this->cachedConsumer);
             }
         );
 
@@ -68,7 +61,7 @@ final class CachedConsumerReadRepositoryTest extends TestCase
             ->method('getConsumer');
 
         $this->assertEquals(
-            new CultureFeedConsumerAdapter($this->cachedConsumer),
+            $this->cachedConsumer,
             $this->cachedConsumerReadRepository->getConsumer($this->cachedApiKey)
         );
     }
@@ -79,19 +72,22 @@ final class CachedConsumerReadRepositoryTest extends TestCase
     public function it_can_get_an_uncached_consumer_from_the_decoretee(): void
     {
         $uncachedApiKey = new ApiKey('c90fbc92-a572-4c39-a002-53f02f58844c');
-        $uncachedConsumer = new CultureFeed_Consumer();
-        $uncachedConsumer->apiKeySapi3 = $uncachedApiKey->toString();
-        $uncachedConsumer->searchPrefixSapi3 = 'regions:nis-44021';
-        $uncachedConsumer->group = [4, 5, 6];
-        $uncachedConsumer->name = 'Bar Foo';
-        $uncachedConsumer->status = 'ACTIVE';
+
+        $uncachedConsumer = SerializableConsumer::deserialize([
+            'api_key' => $uncachedApiKey,
+            'default_query' => 'regions:nis-44021',
+            'permission_group_ids' => [4, 5, 6],
+            'name' => 'Bar Foo',
+            'is_blocked' => false,
+            'is_removed' => false,
+        ]);
 
         $this->fallbackConsumerReadRepository->expects($this->once())
             ->method('getConsumer')
-            ->willReturn(new CultureFeedConsumerAdapter($uncachedConsumer));
+            ->willReturn($uncachedConsumer);
 
         $this->assertEquals(
-            new CultureFeedConsumerAdapter($uncachedConsumer),
+            $uncachedConsumer,
             $this->cachedConsumerReadRepository->getConsumer($uncachedApiKey)
         );
     }
