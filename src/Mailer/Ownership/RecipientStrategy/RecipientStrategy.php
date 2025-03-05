6087ade7f40c803c20e6210e2c420e8b35cf1cae
Koen Eelen <koen.eelenpubliq.be>
2025-03-05 17:14:05 +0100

    remove throw

--- src/Mailer/Ownership/RecipientStrategy/RecipientStrategy.php
+++ src/Mailer/Ownership/RecipientStrategy/RecipientStrategy.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Mailer\Ownership\RecipientStrategy;
 
 use CultuurNet\UDB3\Ownership\Repositories\OwnershipItem;
-use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
 use CultuurNet\UDB3\User\UserIdentityDetails;
 
 interface RecipientStrategy
 {
     /**
-     * @throws DocumentDoesNotExist
      * @return UserIdentityDetails[]
      */
     public function getRecipients(OwnershipItem $item): array;
