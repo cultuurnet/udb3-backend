712354453b3ec4851e30efaadc7c1cddcd73f611
Koen Eelen <koen.eelenpubliq.be>
2025-03-12 12:24:30 +0100

    add role_id as valid search param

--- src/Http/Ownership/Search/SearchParameter.php
+++ src/Http/Ownership/Search/SearchParameter.php
@@ -6,7 +6,7 @@ namespace CultuurNet\UDB3\Http\Ownership\Search;
 
 final class SearchParameter
 {
-    public const SUPPORTED_URL_PARAMETERS = ['itemId', 'state', 'ownerId'];
+    public const SUPPORTED_URL_PARAMETERS = ['itemId', 'state', 'ownerId', 'roleId'];
 
     private string $urlParameter;
     private string $value;
