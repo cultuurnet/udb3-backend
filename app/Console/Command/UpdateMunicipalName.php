772e5b20b54cbf3f9c644e70994fb207b951defd
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-04-16 14:51:59 +0200

    make cli command

--- app/Console/Command/UpdateMunicipalName.php
+++ app/Console/Command/UpdateMunicipalName.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Console\Command;
 
 use Broadway\CommandHandling\CommandBus;
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Address;
+use CultuurNet\UDB3\Model\ValueObject\Geography\CountryCode;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Locality;
+use CultuurNet\UDB3\Model\ValueObject\Geography\PostalCode;
+use CultuurNet\UDB3\Model\ValueObject\Geography\Street;
+use CultuurNet\UDB3\Model\ValueObject\Translation\Language;
+use CultuurNet\UDB3\Place\Commands\UpdateAddress;
+use CultuurNet\UDB3\ReadModel\DocumentDoesNotExist;
+use CultuurNet\UDB3\ReadModel\DocumentRepository;
 use CultuurNet\UDB3\Search\ResultsGenerator;
 use CultuurNet\UDB3\Search\ResultsGeneratorInterface;
 use CultuurNet\UDB3\Search\SearchServiceInterface;
 use CultuurNet\UDB3\Search\Sorting;
+use Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 final class UpdateMunicipalName extends AbstractCommand
 {
@@ -23,9 +35,12 @@ final class UpdateMunicipalName extends AbstractCommand
 
     private ResultsGeneratorInterface $searchResultsGenerator;
 
+    private DocumentRepository $documentRepository;
+
     public function __construct(
         CommandBus $commandBus,
-        SearchServiceInterface $searchService
+        SearchServiceInterface $searchService,
+        DocumentRepository $documentRepository
     ) {
         parent::__construct($commandBus);
         $this->searchResultsGenerator = new ResultsGenerator(
@@ -33,6 +48,7 @@ final class UpdateMunicipalName extends AbstractCommand
             new Sorting('created', 'asc'),
             100
         );
+        $this->documentRepository = $documentRepository;
     }
 
     public function configure(): void
@@ -68,7 +84,72 @@ final class UpdateMunicipalName extends AbstractCommand
             return self::FAILURE;
         }
 
+        $query = str_replace('q=', '', $query);
+
+        $count = $this->searchResultsGenerator->count($query);
+
+        if ($count <= 0) {
+            $output->writeln('<error>No places found</error>');
+            return self::SUCCESS;
+        }
+
+
+        if (!$this->askConfirmation($input, $output, $count)) {
+            return self::SUCCESS;
+        }
+
+        foreach ($this->searchResultsGenerator->search($query) as $place) {
+            try {
+                $placeId = $place->getId();
+
+                $document = $this->documentRepository->fetch($placeId);
+                $jsonLd = Json::decodeAssociatively($document->getRawBody());
+
+                if (!isset(
+                    $jsonLd['address']['nl']['streetAddress'],
+                    $jsonLd['address']['nl']['postalCode'],
+                    $jsonLd['address']['nl']['addressCountry'])
+                ) {
+                    throw new Exception('Address is incomplete in Json');
+                }
+
+                $command = new UpdateAddress(
+                    $place->getId(),
+                    new Address(
+                        new Street($jsonLd['address']['nl']['streetAddress']),
+                        new PostalCode($jsonLd['address']['nl']['postalCode']),
+                        new Locality($newMunicipalName),
+                        new CountryCode($jsonLd['address']['nl']['addressCountry'])
+                    ),
+                    new Language('nl')
+                );
+                $output->writeln('Dispatching UpdateLocation for place with id ' . $command->getItemId());
+
+                if (!$input->getOption(self::DRY_RUN)) {
+                    $this->commandBus->dispatch($command);
+                }
+            } catch (DocumentDoesNotExist $e) {
+                $output->writeln("Skipping {$placeId}. (Could not find JSON-LD in local repository.)");
+                return null;
+            } catch (Exception $exception) {
+                $output->writeln(sprintf('<error>Place with id: %s caused an exception: %s</error>', $place->getId(), $exception->getMessage()));
+            }
+        }
 
         return self::SUCCESS;
+    }
+
+    private function askConfirmation(InputInterface $input, OutputInterface $output, int $count): bool
+    {
+        return $this
+            ->getHelper('question')
+            ->ask(
+                $input,
+                $output,
+                new ConfirmationQuestion(
+                    sprintf('This action will update the municipal name of %d places, continue? [y/N] ', $count),
+                    true
+                )
+            );
     }
 }
