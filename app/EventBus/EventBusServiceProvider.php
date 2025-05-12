671160a653ee4b6c3d90e3ef205bce99ab20d2c9
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-05-12 11:49:42 +0200

    linting

--- app/EventBus/EventBusServiceProvider.php
+++ app/EventBus/EventBusServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\EventBus;
 
 use Broadway\EventHandling\EventBus;
-use CultuurNet\UDB3\ApiName;
 use CultuurNet\UDB3\Broadway\AMQP\AMQPPublisher;
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Event\EventJSONLDServiceProvider;
