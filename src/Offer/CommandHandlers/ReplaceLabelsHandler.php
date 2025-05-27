05aa8e85723b6e970d9f51606f9a7c2b26abbc1a
Koen Eelen <koen.eelenpubliq.be>
2025-05-27 13:03:02 +0200

    add handler

---
+++ src/Offer/CommandHandlers/ReplaceLabelsHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Offer\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Label\LabelImportPreProcessor;
+use CultuurNet\UDB3\Offer\Commands\ImportLabels;
+use CultuurNet\UDB3\Offer\Commands\ReplaceLabels;
+use CultuurNet\UDB3\Offer\OfferRepository;
+
+// We hebben een nieuwe ReplaceLabelsHandler nodig
+final class ReplaceLabelsHandler implements CommandHandler
+{
+    private OfferRepository $offerRepository;
+    private LabelImportPreProcessor $labelImportPreProcessor;
+
+    public function __construct(
+        OfferRepository $offerRepository,
+        LabelImportPreProcessor $labelImportPreProcessor
+    ) {
+        $this->offerRepository = $offerRepository;
+        $this->labelImportPreProcessor = $labelImportPreProcessor;
+    }
+
+    public function handle($command): void
+    {
+        if (!($command instanceof ReplaceLabels)) {
+            return;
+        }
+
+        $offer = $this->offerRepository->load($command->getItemId());
+
+        $labels = $this->labelImportPreProcessor->preProcessImportLabels($command->getLabels(), $offer->getLabels());
+        $offer->replaceLabels($labels);
+
+        $this->offerRepository->save($offer);
+    }
+}
