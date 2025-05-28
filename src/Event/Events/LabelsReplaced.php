1ba45d844d29f2aebdce20ec39d5706f5a6fdc2d
Koen Eelen <koen.eelenpubliq.be>
2025-05-28 11:49:43 +0200

    the new labelsreplaced events

---
+++ src/Event/Events/LabelsReplaced.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Events;
+
+use CultuurNet\UDB3\Offer\Events\AbstractLabelsImported;
+
+final class LabelsReplaced extends AbstractLabelsImported
+{
+}
---
+++ src/Place/Events/LabelsReplaced.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Place\Events;
+
+use CultuurNet\UDB3\Offer\Events\AbstractLabelsImported;
+
+final class LabelsReplaced extends AbstractLabelsImported
+{
+}
