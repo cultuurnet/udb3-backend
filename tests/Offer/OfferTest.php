89991b07a3c9af72854f860cfbd956bb373a62ac
Luc Wollants <lucwollants@gmail.com>
2025-06-12 11:47:49 +0200

    Rework replace test to match existing import test with improved names

--- tests/Offer/OfferTest.php
+++ tests/Offer/OfferTest.php
@@ -517,50 +517,49 @@ class OfferTest extends AggregateRootScenarioTestCase
 
         $labels = new Labels(
             new Label(
-                new LabelName('new_label_1'),
+                new LabelName('new_label'),
                 true
             ),
             new Label(
-                new LabelName('existing_label_1_added_via_ui_and_also_added_in_new_replace_command'),
+                new LabelName('existing_label_added_via_ui_and_also_in_new_replace'),
                 true
             ),
+            new Label(
+                new LabelName('existing_label_added_via_replace_and_also_in_new_replace'),
+                true
+            )
         );
 
         $this->scenario
             ->given([
                 new ItemCreated($itemId),
-                new LabelAdded($itemId, 'existing_label_1_added_via_ui_and_also_added_in_new_replace_command'),
-                new LabelAdded($itemId, 'existing_label_2_added_via_ui'),
+                new LabelAdded($itemId, 'existing_label_added_via_ui_and_also_in_new_replace'),
+                new LabelAdded($itemId, 'existing_label_added_via_ui'),
                 new LabelsReplaced(
                     $itemId,
                     [
-                        'existing_label_3_added_via_replace_and_also_in_new_replace',
-                        'existing_label_4_added_via_replace',
+                        'existing_label_added_via_replace_and_also_in_new_replace',
+                        'existing_label_added_via_replace',
                     ],
                     []
                 ),
-                new LabelAdded($itemId, 'existing_label_3_added_via_replace'),
+                new LabelAdded($itemId, 'existing_label_added_via_replace_and_also_in_new_replace'),
+                new LabelAdded($itemId, 'existing_label_added_via_replace'),
             ])
             ->when(
                 function (Item $item) use ($labels): void {
-                    $item->replaceLabels($labels);
                     $item->replaceLabels($labels);
                 }
             )
             ->then([
                 new LabelsReplaced(
                     $itemId,
-                    ['new_label_1'],
+                    ['new_label'],
                     []
                 ),
-                new LabelRemoved($itemId, 'existing_label_2_added_via_ui'),
-                new LabelRemoved($itemId, 'existing_label_3_added_via_replace'),
-                new LabelAdded($itemId, 'new_label_1'),
-                new LabelsReplaced(
-                    $itemId,
-                    [],
-                    []
-                ),
+                new LabelRemoved($itemId, 'existing_label_added_via_ui'),
+                new LabelRemoved($itemId, 'existing_label_added_via_replace'),
+                new LabelAdded($itemId, 'new_label'),
             ]);
     }
 
