e3db15bee6d40645cd3ce2e7d4ba4a44b501d77f
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-08-28 13:22:26 +0200

    refactor unit tests to use dataprovider

--- tests/SavedSearches/OwnershipSavedSearchRepositoryTest.php
+++ tests/SavedSearches/OwnershipSavedSearchRepositoryTest.php
@@ -52,220 +52,179 @@ final class OwnershipSavedSearchRepositoryTest extends TestCase
             $this->ownershipSearchRepository
         );
     }
+
     /**
      * @test
+     * @dataProvider SavedSearchDataprovider
      */
-    public function it_returns_an_empty_ownership_collection_if_no_were_found(): void
+    public function it_returns_an_empty_ownership_collection_if_no_were_found(array $jsonDocuments, OwnershipItemCollection $ownershipItemCollection, array $savedSearches): void
     {
+        foreach ($jsonDocuments as $jsonDocument) {
+            $this->organizerDocumentRepository->save($jsonDocument);
+        }
+
         $this->ownershipSearchRepository->expects($this->once())
         ->method('search')
             ->with(new SearchQuery([
                 new SearchParameter('state', OwnershipState::approved()->toString()),
                 new SearchParameter('ownerId', self::USER_ID),
             ]))
-            ->willReturn(new OwnershipItemCollection());
+            ->willReturn($ownershipItemCollection);
 
         $this->assertEquals(
-            [],
+            $savedSearches,
             $this->ownershipSavedSearchRepository->ownedByCurrentUser()
         );
     }
 
-    /**
-     * @test
-     */
-    public function it_returns_ownership_collections_if_some_were_found(): void
+    public function SavedSearchDataprovider(): array
     {
         $itemId1 = 'c84d6167-5e71-4f2c-aedf-ad346e569e03';
-        $jsonLd = new JsonDocument(
-            $itemId1,
-            Json::encode(
-                [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'nl' => 'Foobar NL',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd);
-
         $itemId2 = '24bc3c50-5c9e-4e34-8bc7-1c9826352775';
-        $jsonLd2 = new JsonDocument(
-            $itemId2,
-            Json::encode(
-                [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'nl' => 'Bar Foo NL',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd2);
+        $ownershipItemId1 = 'a700c463-1401-4d91-a85c-fa289b6d8d8e';
+        $ownershipItem2 = '8ba5ee51-8e0c-4179-8516-3942ac4ab6d2';
 
-        $this->ownershipSearchRepository->expects($this->once())
-            ->method('search')
-            ->with(new SearchQuery([
-                new SearchParameter('state', OwnershipState::approved()->toString()),
-                new SearchParameter('ownerId', self::USER_ID),
-            ]))
-            ->willReturn(
+        return [
+            'no_approved_ownerships' => [
+                [],
+                new OwnershipItemCollection(),
+                [],
+            ],
+            'approved_ownership_were_found' => [
+                [
+                    new JsonDocument(
+                        $itemId1,
+                        Json::encode(
+                            [
+                                '@type' => 'organizer',
+                                'name' => [
+                                    'nl' => 'Foobar NL',
+                                ],
+                            ]
+                        )
+                    ),
+                    new JsonDocument(
+                        $itemId2,
+                        Json::encode(
+                            [
+                                '@type' => 'organizer',
+                                'name' => [
+                                    'nl' => 'Bar Foo NL',
+                                ],
+                            ]
+                        )
+                    ),
+                ],
                 new OwnershipItemCollection(
                     new OwnershipItem(
-                        'a700c463-1401-4d91-a85c-fa289b6d8d8e',
+                        $ownershipItemId1,
                         $itemId1,
                         'organizer',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     ),
                     new OwnershipItem(
-                        '8ba5ee51-8e0c-4179-8516-3942ac4ab6d2',
+                        $ownershipItem2,
                         $itemId2,
                         'organizer',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     )
-                )
-            );
-
-        $this->assertEquals(
-            [
-                new SavedSearch('Aanbod Foobar NL', new QueryString('organizer.id:' . $itemId1)),
-                new SavedSearch('Aanbod Bar Foo NL', new QueryString('organizer.id:' . $itemId2)),
+                ),
+                [
+                    new SavedSearch('Aanbod Foobar NL', new QueryString('organizer.id:' . $itemId1)),
+                    new SavedSearch('Aanbod Bar Foo NL', new QueryString('organizer.id:' . $itemId2)),
+                ],
             ],
-            $this->ownershipSavedSearchRepository->ownedByCurrentUser()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function it_can_fallback_to_other_languages_if_dutch_is_not_present(): void
-    {
-        $itemId1 = 'c84d6167-5e71-4f2c-aedf-ad346e569e03';
-        $jsonLd = new JsonDocument(
-            $itemId1,
-            Json::encode(
+            'fallback_if_dutch_is_not_present' => [
                 [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'en' => 'Foobar EN',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd);
-
-        $itemId2 = '24bc3c50-5c9e-4e34-8bc7-1c9826352775';
-        $jsonLd2 = new JsonDocument(
-            $itemId2,
-            Json::encode(
-                [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'fr' => 'Bar Foo FR',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd2);
-
-        $this->ownershipSearchRepository->expects($this->once())
-            ->method('search')
-            ->with(new SearchQuery([
-                new SearchParameter('state', OwnershipState::approved()->toString()),
-                new SearchParameter('ownerId', self::USER_ID),
-            ]))
-            ->willReturn(
+                    new JsonDocument(
+                        $itemId1,
+                        Json::encode(
+                            [
+                                '@type' => 'organizer',
+                                'name' => [
+                                    'en' => 'Foobar EN',
+                                ],
+                            ]
+                        )
+                    ),
+                    new JsonDocument(
+                        $itemId2,
+                        Json::encode(
+                            [
+                                '@type' => 'organizer',
+                                'name' => [
+                                    'fr' => 'Bar Foo FR',
+                                ],
+                            ]
+                        )
+                    ),
+                ],
                 new OwnershipItemCollection(
                     new OwnershipItem(
-                        'a700c463-1401-4d91-a85c-fa289b6d8d8e',
+                        $ownershipItemId1,
                         $itemId1,
                         'organizer',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     ),
                     new OwnershipItem(
-                        '8ba5ee51-8e0c-4179-8516-3942ac4ab6d2',
+                        $ownershipItem2,
                         $itemId2,
                         'organizer',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     )
-                )
-            );
-
-        $this->assertEquals(
-            [
-                new SavedSearch('Aanbod Foobar EN', new QueryString('organizer.id:' . $itemId1)),
-                new SavedSearch('Aanbod Bar Foo FR', new QueryString('organizer.id:' . $itemId2)),
+                ),
+                [
+                    new SavedSearch('Aanbod Foobar EN', new QueryString('organizer.id:' . $itemId1)),
+                    new SavedSearch('Aanbod Bar Foo FR', new QueryString('organizer.id:' . $itemId2)),
+                ],
             ],
-            $this->ownershipSavedSearchRepository->ownedByCurrentUser()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function it_only_support_organizers(): void
-    {
-        $itemId1 = 'c84d6167-5e71-4f2c-aedf-ad346e569e03';
-        $jsonLd = new JsonDocument(
-            $itemId1,
-            Json::encode(
+            'no_organizers' => [
                 [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'en' => 'Foobar EN',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd);
-
-        $itemId2 = '24bc3c50-5c9e-4e34-8bc7-1c9826352775';
-        $jsonLd2 = new JsonDocument(
-            $itemId2,
-            Json::encode(
-                [
-                    '@type' => 'organizer',
-                    'name' => [
-                        'fr' => 'Bar Foo FR',
-                    ],
-                ]
-            )
-        );
-        $this->organizerDocumentRepository->save($jsonLd2);
-
-        $this->ownershipSearchRepository->expects($this->once())
-            ->method('search')
-            ->with(new SearchQuery([
-                new SearchParameter('state', OwnershipState::approved()->toString()),
-                new SearchParameter('ownerId', self::USER_ID),
-            ]))
-            ->willReturn(
+                    new JsonDocument(
+                        $itemId1,
+                        Json::encode(
+                            [
+                                '@type' => 'event',
+                                'name' => [
+                                    'nl' => 'Foobar EN',
+                                ],
+                            ]
+                        )
+                    ),
+                    new JsonDocument(
+                        $itemId2,
+                        Json::encode(
+                            [
+                                '@type' => 'place',
+                                'name' => [
+                                    'nl' => 'Bar Foo FR',
+                                ],
+                            ]
+                        )
+                    ),
+                ],
                 new OwnershipItemCollection(
                     new OwnershipItem(
-                        'a700c463-1401-4d91-a85c-fa289b6d8d8e',
+                        $ownershipItemId1,
                         $itemId1,
                         'event',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     ),
                     new OwnershipItem(
-                        '8ba5ee51-8e0c-4179-8516-3942ac4ab6d2',
+                        $ownershipItem2,
                         $itemId2,
                         'place',
                         self::USER_ID,
                         OwnershipState::approved()->toString()
                     )
-                )
-            );
-
-        $this->assertEquals(
-            [],
-            $this->ownershipSavedSearchRepository->ownedByCurrentUser()
-        );
+                ),
+                [],
+            ],
+        ];
     }
 }
