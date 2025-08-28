2b34ca8b74db7833236a98f5575c300c1dbc5766
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-08-28 11:17:19 +0200

    Add OwnershipSavedSearchRepository

---
+++ src/SavedSearches/OwnershipSavedSearchRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\SavedSearches;
+
+use CultuurNet\UDB3\Http\Auth\Jwt\JsonWebToken;
+use CultuurNet\UDB3\Http\Ownership\Search\SearchParameter;
+use CultuurNet\UDB3\Http\Ownership\Search\SearchQuery;
+use CultuurNet\UDB3\Ownership\OwnershipState;
+use CultuurNet\UDB3\Ownership\Repositories\Search\OwnershipSearchRepository;
+use CultuurNet\UDB3\ReadModel\DocumentRepository;
+use CultuurNet\UDB3\SavedSearches\Properties\QueryString;
+use CultuurNet\UDB3\SavedSearches\ReadModel\SavedSearch;
+use CultuurNet\UDB3\SavedSearches\ReadModel\SavedSearchesOwnedByCurrentUser;
+
+class OwnershipSavedSearchRepository implements SavedSearchesOwnedByCurrentUser
+{
+    private JsonWebToken $token;
+
+    private DocumentRepository $organizerDocumentRepository;
+
+    private OwnershipSearchRepository $ownershipSearchRepository;
+
+    public function __construct(
+        JsonWebToken $token,
+        DocumentRepository $organizerDocumentRepository,
+        OwnershipSearchRepository $ownershipSearchRepository
+    ) {
+        $this->token = $token;
+        $this->organizerDocumentRepository = $organizerDocumentRepository;
+        $this->ownershipSearchRepository = $ownershipSearchRepository;
+    }
+
+    /**
+     * @return SavedSearch[]
+     */
+    public function ownedByCurrentUser(): array
+    {
+        $ownershipQueryStrings = $this->getOwnershipQueryStrings();
+
+        $savedSearches = [];
+        foreach ($ownershipQueryStrings as $ownershipName => $ownershipQueryString) {
+            $savedSearches[] = new SavedSearch('Aanbod ' . $ownershipName, $ownershipQueryString);
+        }
+        return $savedSearches;
+    }
+
+    /**
+     * @return QueryString[]
+     */
+    private function getOwnershipQueryStrings(): array
+    {
+        $token = $this->token->getUserId();
+
+        $ownershipItemCollection = $this->ownershipSearchRepository->search(
+            new SearchQuery([
+                new SearchParameter('state', OwnershipState::approved()->toString()),
+                new SearchParameter('ownerId', $token),
+            ])
+        );
+
+        $ownershipQueries = [];
+
+        foreach ($ownershipItemCollection as $ownershipItem) {
+            if ($ownershipItem->getItemType() === 'organizer') {
+                $organizerId = $ownershipItem->getItemId();
+                $organizerAsJson = $this->organizerDocumentRepository->fetch($organizerId);
+                $body = $organizerAsJson->getAssocBody();
+                $organizerName = $body['name']['nl'];
+                $ownershipQueries[$organizerName] = new QueryString('organizer.id:' . $organizerId);
+            }
+        }
+        return $ownershipQueries;
+    }
+}
