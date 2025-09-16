72fa4bb0b1a8ce53b5f3782b50b11c4686c18d0c
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-09-16 11:40:17 +0200

    register new dbal repo

--- app/Event/EventPermissionServiceProvider.php
+++ app/Event/EventPermissionServiceProvider.php
@@ -7,6 +7,7 @@ namespace CultuurNet\UDB3\Event;
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
 use CultuurNet\UDB3\Event\ReadModel\Permission\Projector;
 use CultuurNet\UDB3\Security\ResourceOwner\Doctrine\DBALResourceOwnerRepository;
+use CultuurNet\UDB3\Security\ResourceOwner\Doctrine\DBALResourceRelatedOwnerRepository;
 
 final class EventPermissionServiceProvider extends AbstractServiceProvider
 {
@@ -14,6 +15,7 @@ final class EventPermissionServiceProvider extends AbstractServiceProvider
     {
         return [
             'event_owner.repository',
+            'event_organizer_owner.repository',
             'event_permission.projector',
         ];
     }
@@ -29,6 +31,18 @@ final class EventPermissionServiceProvider extends AbstractServiceProvider
                     'event_permission_readmodel',
                     $container->get('dbal_connection'),
                     'event_id'
+                );
+            }
+        );
+
+        $container->addShared(
+            'event_organizer_owner.repository',
+            function () use ($container): DBALResourceRelatedOwnerRepository {
+                return new DBALResourceRelatedOwnerRepository(
+                    'organizer_permission_readmodel',
+                    'event_relations',
+                    $container->get('dbal_connection'),
+                    'event'
                 );
             }
         );
