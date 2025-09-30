7ce74e2316b4cdd9d6f4c0ac91a7980daffea406
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-09-30 11:00:49 +0200

    Add interface for StreetSuggestion

---
+++ src/Address/StreetSuggester/StreetSuggester.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Address\StreetSuggester;
+
+interface StreetSuggester
+{
+    /**
+     * @return string[]
+     */
+    public function suggest(string $postalCode, string $locality, string $streetQuery): array;
+}
