e613e00bcc8435254195db3016d4334fa38f1301
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-10-15 10:36:57 +0200

    Add logic for unique_address_identifier_v2

--- src/Place/ReadModel/Duplicate/UniqueAddressIdentifierFactory.php
+++ src/Place/ReadModel/Duplicate/UniqueAddressIdentifierFactory.php
@@ -5,9 +5,20 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Place\ReadModel\Duplicate;
 
 use CultuurNet\UDB3\Model\ValueObject\Geography\Address;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class UniqueAddressIdentifierFactory
 {
+    public function create(string $title, Address $address): string
+    {
+        $parts = array_map(
+            fn ($part) => str_replace(' ', '_', trim($part)),
+            $this->getParts($title, $address)
+        );
+
+        return mb_strtolower($this->escapeSpecialCharacters(implode('_', array_filter($parts))));
+    }
+
     public function legacyCreate(string $title, Address $address, string $currentUserId): string
     {
         $parts = array_map(
@@ -18,16 +29,21 @@ class UniqueAddressIdentifierFactory
         return mb_strtolower($this->escapeReservedElasticsearchCharacters(implode('_', array_filter($parts))));
     }
 
-    private function getParts(string $title, Address $address, string $currentUserId): array
+    private function getParts(string $title, Address $address, string $currentUserId = null): array
     {
-        return [
+        $parts = [
             $title,
             $address->getStreet()->toString(),
             $address->getPostalCode()->toString(),
             $address->getLocality()->toString(),
             $address->getCountryCode()->toString(),
-            $currentUserId,
         ];
+
+        if ($currentUserId !== null) {
+            $parts[] = $currentUserId;
+        }
+
+        return $parts;
     }
 
     private function escapeReservedElasticsearchCharacters(string $query): string
@@ -41,5 +57,10 @@ class UniqueAddressIdentifierFactory
         }
 
         return $query;
+    }
+
+    private function escapeSpecialCharacters(string $query): string
+    {
+        return (new AsciiSlugger())->slug($query, '_')->toString();
     }
 }
