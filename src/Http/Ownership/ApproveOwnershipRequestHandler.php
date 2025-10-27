774d9e3860250a79814f3effbeec60828ffd744f
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-10-27 16:52:00 +0100

    Add extra parameters needed to check for null on creator

--- src/Http/Ownership/ApproveOwnershipRequestHandler.php
+++ src/Http/Ownership/ApproveOwnershipRequestHandler.php
@@ -7,8 +7,12 @@ namespace CultuurNet\UDB3\Http\Ownership;
 use Broadway\CommandHandling\CommandBus;
 use CultuurNet\UDB3\Http\Request\RouteParameters;
 use CultuurNet\UDB3\Http\Response\JsonResponse;
+use CultuurNet\UDB3\Json;
 use CultuurNet\UDB3\Model\ValueObject\Identity\Uuid;
+use CultuurNet\UDB3\Organizer\Commands\ChangeOwner;
 use CultuurNet\UDB3\Ownership\Commands\ApproveOwnership;
+use CultuurNet\UDB3\Ownership\Repositories\Search\OwnershipSearchRepository;
+use CultuurNet\UDB3\ReadModel\DocumentRepository;
 use CultuurNet\UDB3\User\CurrentUser;
 use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -18,17 +22,27 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class ApproveOwnershipRequestHandler implements RequestHandlerInterface
 {
     private CommandBus $commandBus;
+
     private CurrentUser $currentUser;
+
     private OwnershipStatusGuard $ownershipStatusGuard;
+
+    private OwnershipSearchRepository $ownershipSearchRepository;
+
+    private DocumentRepository $organizerRepository;
 
     public function __construct(
         CommandBus $commandBus,
         CurrentUser $currentUser,
-        OwnershipStatusGuard $ownershipStatusGuard
+        OwnershipStatusGuard $ownershipStatusGuard,
+        OwnershipSearchRepository $ownershipSearchRepository,
+        DocumentRepository $organizerRepository
     ) {
         $this->commandBus = $commandBus;
         $this->currentUser = $currentUser;
         $this->ownershipStatusGuard = $ownershipStatusGuard;
+        $this->ownershipSearchRepository = $ownershipSearchRepository;
+        $this->organizerRepository = $organizerRepository;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -42,9 +56,24 @@ final class ApproveOwnershipRequestHandler implements RequestHandlerInterface
             new ApproveOwnership(new Uuid($ownershipId))
         );
 
+        $ownership = $this->ownershipSearchRepository->getById($ownershipId);
+
+        if (!$this->itemHasCreator($ownership->getItemId())) {
+            $this->commandBus->dispatch(
+                new ChangeOwner($ownership->getItemId(), $ownership->getOwnerId())
+            );
+        }
+
         return new JsonResponse(
             [],
             StatusCodeInterface::STATUS_NO_CONTENT
         );
+    }
+
+    private function itemHasCreator(string $organizerId): bool
+    {
+        $document = $this->organizerRepository->fetch($organizerId);
+        $jsonLd = Json::decodeAssociatively($document->getRawBody());
+        return $jsonLd->creator !== null;
     }
 }
