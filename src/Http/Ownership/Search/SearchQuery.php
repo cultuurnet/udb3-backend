b346e231de3cf4d3c05c20a75ba1c58eee271d9b
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-11-07 11:34:22 +0100

    Add sortOrder

--- src/Http/Ownership/Search/SearchQuery.php
+++ src/Http/Ownership/Search/SearchQuery.php
@@ -7,19 +7,24 @@ namespace CultuurNet\UDB3\Http\Ownership\Search;
 final class SearchQuery
 {
     private int $start;
+
     private int $limit;
+
+    private string $sortOrder;
+
     /** @var SearchParameter[] */
     private array $parameters;
 
     /**
      * @param SearchParameter[] $parameters
      */
-    public function __construct(array $parameters, ?int $start = null, ?int $limit = null)
+    public function __construct(array $parameters, ?int $start = null, ?int $limit = null, ?string $sortOrder = null)
     {
         $this->parameters = $parameters;
 
         $this->start = $start !== null ? $start : 0;
         $this->limit = $limit !== null ? $limit : 50;
+        $this->sortOrder = $sortOrder !== null ? $sortOrder : 'owner_id';
     }
 
     public function getStart(): int
@@ -30,6 +35,22 @@ final class SearchQuery
     public function getLimit(): int
     {
         return $this->limit;
+    }
+
+    public function getSortBy(): string
+    {
+        if (strpos($this->sortOrder, '-') === 0) {
+            return substr($this->sortOrder, 1);
+        }
+        return $this->sortOrder;
+    }
+
+    public function getOrderBy(): string
+    {
+        if (strpos($this->sortOrder, '-') === 0) {
+            return 'DESC';
+        }
+        return 'ASC';
     }
 
     /**
