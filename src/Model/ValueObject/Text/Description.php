e7f4cd9ee99394fa4a44b0ad9b800633234770ac
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-11-19 10:30:08 +0100

    Add Sanitize to Description

--- src/Model/ValueObject/Text/Description.php
+++ src/Model/ValueObject/Text/Description.php
@@ -6,6 +6,7 @@ namespace CultuurNet\UDB3\Model\ValueObject\Text;
 
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsNotEmpty;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\IsString;
+use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\Sanitizes;
 use CultuurNet\UDB3\Model\ValueObject\String\Behaviour\Trims;
 use InvalidArgumentException;
 
@@ -14,10 +15,12 @@ class Description
     use IsString;
     use IsNotEmpty;
     use Trims;
+    use Sanitizes;
 
     public function __construct(string $value)
     {
         $value = $this->trim($value);
+        $value = $this->sanitize($value);
         try {
             $this->guardNotEmpty($value);
         } catch (InvalidArgumentException $e) {
