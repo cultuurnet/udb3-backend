05c50af3982713567f5be9bdc70f13d361d7ab5b
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-11-19 11:57:24 +0100

    move allowed tags in function

--- src/Model/ValueObject/String/Behaviour/StripTags.php
+++ src/Model/ValueObject/String/Behaviour/StripTags.php
@@ -6,8 +6,8 @@ namespace CultuurNet\UDB3\Model\ValueObject\String\Behaviour;
 
 trait StripTags
 {
-    private function stripTags(string $value, string $allowed = '<a><em><li><ol><p><strong><ul>'): string
+    private function stripTags(string $value): string
     {
-        return strip_tags($value, $allowed);
+        return strip_tags($value, '<a><em><li><ol><p><strong><ul>');
     }
 }
