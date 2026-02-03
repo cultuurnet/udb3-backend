7323d6e35b65b9b4ac29e5a3e9d6be0ac43e60bc
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-02-03 14:56:55 +0100

    Add unit test

---
+++ tests/User/Keycloak/KeycloakClientIdResolverTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\User\Keycloak;
+
+use CultuurNet\UDB3\Json;
+use CultuurNet\UDB3\User\ClientIdResolver;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+
+final class KeycloakClientIdResolverTest extends TestCase
+{
+    private ClientInterface&MockObject $client;
+
+    protected ClientIdResolver $clientIdResolver;
+
+    public function setUp(): void
+    {
+        $this->client = $this->createMock(ClientInterface::class);
+        $this->clientIdResolver = new KeycloakClientIdResolver(
+            $this->client,
+            'http://keycloak',
+            'realm',
+            'token'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_check_if_a_client_has_entry_access(): void
+    {
+        $this->client->expects($this->any())
+            ->method('sendRequest')
+            ->with($this->callback(function (RequestInterface $request) {
+                return $request->getUri()->getPath() === '/admin/realms/realm/clients/'  &&
+                    $request->getMethod() === 'GET' &&
+                    $request->getHeaderLine('Authorization') === 'Bearer token';
+            }))
+            ->willReturn(
+                new Response(
+                    200,
+                    [],
+                    Json::encode([
+                        0 => [
+                            'defaultClientScopes' => ['publiq-api-entry-scope']
+                        ]
+                    ])
+                )
+            );
+
+        $this->assertTrue($this->clientIdResolver->hasEntryAccess('entry_api_key'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_check_if_a_client_does_not_have_entry_access(): void
+    {
+        $this->client->expects($this->any())
+            ->method('sendRequest')
+            ->with($this->callback(function (RequestInterface $request) {
+                return $request->getUri()->getPath() === '/admin/realms/realm/clients/'  &&
+                    $request->getMethod() === 'GET' &&
+                    $request->getHeaderLine('Authorization') === 'Bearer token';
+            }))
+            ->willReturn(
+                new Response(
+                    200,
+                    [],
+                    Json::encode([
+                        0 => [
+                            'defaultClientScopes' => ['publiq-api-search-scope']
+                        ]
+                    ])
+                )
+            );
+
+        $this->assertFalse($this->clientIdResolver->hasEntryAccess('entry_api_key'));
+    }
+}
