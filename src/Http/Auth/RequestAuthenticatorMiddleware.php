81f0a6f9bc4548314403980f8a6f22226980ba38
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-02-12 14:54:30 +0100

    no longer validate v1 tokens.

--- src/Http/Auth/RequestAuthenticatorMiddleware.php
+++ src/Http/Auth/RequestAuthenticatorMiddleware.php
@@ -103,9 +103,9 @@ final class RequestAuthenticatorMiddleware implements MiddlewareInterface
 
         $this->authenticateToken($request);
 
-        // Requests that use a token from the JWT provider (v1 or v2) require an API key from UiTID v1.
+        // Requests that use a token from the JWT provider (v2) require an API key from UiTID v1.
         // Requests that use a token that they got from a clientId do not require an API key.
-        if ($this->token->getType() === JsonWebToken::UIT_ID_V1_JWT_PROVIDER_TOKEN || $this->token->getType() === JsonWebToken::UIT_ID_V2_JWT_PROVIDER_TOKEN) {
+        if ($this->token->getType() === JsonWebToken::UIT_ID_V2_JWT_PROVIDER_TOKEN) {
             $this->authenticateApiKey($request);
         }
 
@@ -150,11 +150,8 @@ final class RequestAuthenticatorMiddleware implements MiddlewareInterface
             throw ApiProblem::unauthorized('Token "' . $tokenString . '" is not a valid JWT.');
         }
 
-        $isV1 = $this->token->getType() === JsonWebToken::UIT_ID_V1_JWT_PROVIDER_TOKEN;
-        $validator = $isV1 ? $this->uitIdV1JwtValidator : $this->uitIdV2JwtValidator;
-
-        $validator->verifySignature($this->token);
-        $validator->validateClaims($this->token);
+        $this->uitIdV2JwtValidator->verifySignature($this->token);
+        $this->uitIdV2JwtValidator->validateClaims($this->token);
     }
 
     private function authenticateApiKey(ServerRequestInterface $request): void
