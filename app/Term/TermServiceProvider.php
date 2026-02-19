a0db772486cf627eb87aeb9171df2e1700ca8ec7
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-02-19 10:45:26 +0100

    Get TermRepo from endpoint instead of Config files

--- app/Term/TermServiceProvider.php
+++ app/Term/TermServiceProvider.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Term;
 
 use CultuurNet\UDB3\Container\AbstractServiceProvider;
+use CultuurNet\UDB3\Taxonomy\JsonTaxonomyApiClient;
+use CultuurNet\UDB3\Taxonomy\TaxonomyApiClient;
+use GuzzleHttp\Client;
 
 final class TermServiceProvider extends AbstractServiceProvider
 {
     protected function getProvidedServiceNames(): array
     {
         return [
+            TaxonomyApiClient::class,
             TermRepository::class,
         ];
     }
@@ -20,31 +24,20 @@ final class TermServiceProvider extends AbstractServiceProvider
         $container = $this->getContainer();
 
         $container->addShared(
-            TermRepository::class,
-            fn () => $this->provideTermRepository()
+            TaxonomyApiClient::class,
+            fn () => new JsonTaxonomyApiClient(
+                new Client(),
+                $container->get('config')['taxonomy']['terms']
+            )
         );
-    }
 
-    private function provideTermRepository(): TermRepository
-    {
-        $mapping = [];
-
-        $files = [
-            __DIR__ . '/../../term_mapping_facilities.php',
-            __DIR__ . '/../../term_mapping_themes.php',
-            __DIR__ . '/../../term_mapping_types.php',
-        ];
-
-        foreach ($files as $file) {
-            if (file_exists($file)) {
-                $terms  = require $file;
-
-                if (is_array($terms)) {
-                    $mapping = array_merge($mapping, $terms);
-                }
+        $container->addShared(
+            TermRepository::class,
+            function () use ($container): TermRepository {
+                /** @var TaxonomyApiClient $taxonomyApiClient */
+                $taxonomyApiClient = $container->get(TaxonomyApiClient::class);
+                return new TermRepository($taxonomyApiClient->getMapping());
             }
-        }
-
-        return new TermRepository($mapping);
+        );
     }
 }
