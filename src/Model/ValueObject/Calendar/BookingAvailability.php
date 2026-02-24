c2237505c4c824263df7cb7a23cfb9e1ab84764f
Koen Eelen <koen.eelenpubliq.be>
2026-02-24 12:27:58 +0100

    change DTO

--- src/Model/ValueObject/Calendar/BookingAvailability.php
+++ src/Model/ValueObject/Calendar/BookingAvailability.php
@@ -8,6 +8,10 @@ final class BookingAvailability
 {
     private BookingAvailabilityType $type;
 
+    private ?int $capacity = null;
+
+    private ?int $availability = null;
+
     public function __construct(BookingAvailabilityType $type)
     {
         $this->type = $type;
@@ -16,6 +20,30 @@ final class BookingAvailability
     public function getType(): BookingAvailabilityType
     {
         return $this->type;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function getAvailability(): ?int
+    {
+        return $this->availability;
+    }
+
+    public function withCapacity(?int $capacity): self
+    {
+        $clone = clone $this;
+        $clone->capacity = $capacity;
+        return $clone;
+    }
+
+    public function withAvailability(?int $availability): self
+    {
+        $clone = clone $this;
+        $clone->availability = $availability;
+        return $clone;
     }
 
     public static function Available(): self
