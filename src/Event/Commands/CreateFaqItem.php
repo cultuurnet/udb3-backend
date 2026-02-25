da3ecc40bc6c09aeac71b521206e5c6e7a004402
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-02-25 15:06:27 +0100

    Add Commands

---
+++ src/Event/Commands/CreateFaqItem.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Commands;
+
+use CultuurNet\UDB3\Model\ValueObject\Faq\TranslatedFaqItem;
+use CultuurNet\UDB3\Offer\Commands\AbstractCommand;
+
+class CreateFaqItem extends AbstractCommand
+{
+    private TranslatedFaqItem $faqItem;
+
+    public function __construct(string $itemId, TranslatedFaqItem $faqItem)
+    {
+        parent::__construct($itemId);
+        $this->faqItem = $faqItem;
+    }
+
+    public function getFaqItem(): TranslatedFaqItem
+    {
+        return $this->faqItem;
+    }
+}
---
+++ src/Event/Commands/DeleteFaqItem.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\Commands;
+
+use CultuurNet\UDB3\Offer\Commands\AbstractCommand;
+
+class DeleteFaqItem extends AbstractCommand
+{
+    private string $faqItemId;
+
+    public function __construct(string $itemId, string $faqItemId)
+    {
+        parent::__construct($itemId);
+        $this->faqItemId = $faqItemId;
+    }
+
+    public function getFaqItemId(): string
+    {
+        return $this->faqItemId;
+    }
+}
