12daf5b2e35d0a115374ff9e4e1dd738339bd560
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-02-25 15:07:56 +0100

    Make events

---
+++ src/Event/CommandHandlers/UpdateFaqItemHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Event\CommandHandlers;
+
+use Broadway\CommandHandling\CommandHandler;
+use CultuurNet\UDB3\Event\Commands\UpdateFaqItem;
+use CultuurNet\UDB3\Event\Event;
+use CultuurNet\UDB3\Event\EventRepository;
+
+final class UpdateFaqItemHandler implements CommandHandler
+{
+    public function __construct(private readonly EventRepository $eventRepository)
+    {
+    }
+
+    public function handle($command): void
+    {
+        if (!$command instanceof UpdateFaqItem) {
+            return;
+        }
+
+        /** @var Event $event */
+        $event = $this->eventRepository->load($command->getItemId());
+
+        $event->updateFaqItem($command->getFaqItem());
+
+        $this->eventRepository->save($event);
+    }
+}
