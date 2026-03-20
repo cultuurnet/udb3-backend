741985c135ea81c09c1d94a9178bb175621e34f5
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-03-20 11:14:57 +0100

    Also add warning to Interface

--- src/Place/Canonical/CanonicalService.php
+++ src/Place/Canonical/CanonicalService.php
@@ -7,5 +7,12 @@ namespace CultuurNet\UDB3\Place\Canonical;
 interface CanonicalService
 {
     public function getCanonical(string $clusterId): string;
+
+    /**
+     * @param string[] $placeIds
+     * This function is only used by LookupDuplicatePlaceWithSapi3 to make sure
+     * we always return an id. Therefore, it does not throw exceptions and
+     * should probably not be used anywhere else.
+     */
     public function getCanonicalFromArrayWithoutThrowing(array $placeIds): string;
 }
