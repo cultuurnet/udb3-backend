fd92cb50936033fd01241e411bb27515a6019edc
Luc Wollants <lucwollants@gmail.com>
2026-06-16 18:15:08 +0200

    Cleanup test of `ManagementTokenProvider`

--- tests/User/ManagementToken/ManagementTokenProviderTest.php
+++ tests/User/ManagementToken/ManagementTokenProviderTest.php
@@ -4,111 +4,39 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\User\ManagementToken;
 
-use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 class ManagementTokenProviderTest extends TestCase
 {
     /**
      * @test
      */
-    public function it_generates_new_token_if_no_token_in_repository(): void
+    public function it_generates_a_token_on_a_cache_miss(): void
     {
-        $tokenRepository = $this->createMock(TokenRepository::class);
-        $tokenRepository->expects($this->atLeast(1))
-            ->method('token')
-            ->willReturn(null);
-
-        $token = new ManagementToken(
-            'my_token',
-            new DateTimeImmutable(),
-            3600
-        );
-
-        $tokenRepository->expects($this->once())
-            ->method('store')
-            ->with($token);
-
         $tokenGenerator = $this->createMock(ManagementTokenGenerator::class);
-        $tokenGenerator->expects($this->atLeast(1))
+        $tokenGenerator->expects($this->once())
             ->method('newToken')
-            ->willReturn($token);
+            ->willReturn(new ManagementToken('my_token', 3600));
 
-        $service = new ManagementTokenProvider(
-            $tokenGenerator,
-            $tokenRepository
-        );
+        $provider = new ManagementTokenProvider($tokenGenerator, new ArrayAdapter());
 
-        $result = $service->token();
-
-        $this->assertEquals('my_token', $result);
+        $this->assertEquals('my_token', $provider->token());
     }
 
     /**
      * @test
      */
-    public function it_returns_token_if_valid_token_is_in_repository(): void
+    public function it_reuses_the_cached_token_without_regenerating(): void
     {
-        $token = new ManagementToken(
-            'my_token',
-            new DateTimeImmutable(),
-            3600
-        );
-
-        $tokenRepository = $this->createMock(TokenRepository::class);
-        $tokenRepository->expects($this->atLeast(1))
-            ->method('token')
-            ->willReturn($token);
-
         $tokenGenerator = $this->createMock(ManagementTokenGenerator::class);
-        $tokenGenerator->expects($this->never())
-            ->method('newToken');
-
-        $service = new ManagementTokenProvider(
-            $tokenGenerator,
-            $tokenRepository
-        );
-
-        $result = $service->token();
-
-        $this->assertEquals('my_token', $result);
-    }
-
-    /**
-     * @test
-     */
-    public function it_generates_new_token_if_current_is_about_to_expire(): void
-    {
-        // A token is also considered as expired when it will expire within 5 minutes.
-        $expiredToken = new ManagementToken(
-            'expired_token',
-            new DateTimeImmutable(),
-            300
-        );
-
-        $newToken = new ManagementToken(
-            'new_token',
-            new DateTimeImmutable(),
-            3600
-        );
-
-        $tokenRepository = $this->createMock(TokenRepository::class);
-        $tokenRepository->expects($this->atLeast(1))
-            ->method('token')
-            ->willReturn($expiredToken);
-
-        $tokenGenerator = $this->createMock(ManagementTokenGenerator::class);
-        $tokenGenerator->expects($this->atLeast(1))
+        $tokenGenerator->expects($this->once())
             ->method('newToken')
-            ->willReturn($newToken);
+            ->willReturn(new ManagementToken('my_token', 3600));
 
-        $service = new ManagementTokenProvider(
-            $tokenGenerator,
-            $tokenRepository
-        );
+        $provider = new ManagementTokenProvider($tokenGenerator, new ArrayAdapter());
 
-        $result = $service->token();
-
-        $this->assertEquals('new_token', $result);
+        $this->assertEquals('my_token', $provider->token());
+        $this->assertEquals('my_token', $provider->token());
     }
 }
